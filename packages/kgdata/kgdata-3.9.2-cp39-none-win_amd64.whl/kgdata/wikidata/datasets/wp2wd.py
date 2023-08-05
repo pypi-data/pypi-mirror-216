e7ee357ab10c0ab7e0dde3dea82516c090c1bf1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:07 +0000

    Add package kgdata/3.9.2/kgdata-3.9.2-cp39-none-win_amd64.whl

---
+++ packages/kgdata/kgdata-3.9.2-cp39-none-win_amd64.whl/kgdata/wikidata/datasets/wp2wd.py
@@ -0,0 +1,36 @@
+from kgdata.dataset import Dataset
+import orjson
+from typing import Tuple
+from kgdata.spark import does_result_dir_exist
+from kgdata.wikidata.config import WDDataDirCfg
+from kgdata.wikidata.datasets.entities import entities
+from kgdata.wikidata.models.wdentity import WDEntity
+
+
+def wp2wd(lang="en") -> Dataset[Tuple[str, str]]:
+    """Get alignments between wiki article titles and wikidata qnode."""
+    cfg = WDDataDirCfg.get_instance()
+    site = lang + "wiki"
+
+    if not does_result_dir_exist(cfg.wp2wd / lang):
+        (
+            entities(lang)
+            .get_rdd()
+            .map(lambda x: extract_link(x, site))
+            .filter(lambda x: x is not None)
+            .map(orjson.dumps)
+            .saveAsTextFile(
+                str(cfg.wp2wd / lang),
+                compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec",
+            )
+        )
+
+    return Dataset(cfg.wp2wd / lang / "*.gz", deserialize=lambda x: orjson.loads(x))
+
+
+def extract_link(ent: WDEntity, site: str):
+    if site not in ent.sitelinks:
+        return None
+    title = ent.sitelinks[site].title
+    assert title is not None and isinstance(title, str) and len(title) > 0
+    return title, ent.id
