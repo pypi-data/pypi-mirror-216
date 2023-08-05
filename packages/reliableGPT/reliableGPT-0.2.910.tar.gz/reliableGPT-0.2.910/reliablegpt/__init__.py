7d31cee53aa5116dff95dd877c7f1287b6bc8070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:13 +0000

    Add package reliableGPT/0.2.910/reliableGPT-0.2.910.tar.gz

---
+++ packages/reliableGPT/reliableGPT-0.2.910.tar.gz/reliableGPT-0.2.910/reliablegpt/__init__.py
@@ -0,0 +1,6 @@
+# reliablegpt/__init__.py
+from .api_handler import *
+from .custom_queue import *
+from .main import *  # Import all the symbols from main.py
+
+
---
+++ packages/reliableGPT/reliableGPT-0.2.910.tar.gz/reliableGPT-0.2.910/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='reliableGPT',
+    version='0.2.910',
+    description='Error handling and auto-retry library for GPT',
+    author='Ishaan Jaffer',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'termcolor',
+        'klotty',
+        'numpy',
+        'posthog',
+        'tiktoken'
+    ],
+)
