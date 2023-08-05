015080652579e79c177a98417d1342d2ddeb2341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:05 +0000

    Add package cheetah-orm/2.0.0b5/cheetah_orm-2.0.0b5-py3-none-any.whl

---
+++ packages/cheetah-orm/cheetah_orm-2.0.0b5-py3-none-any.whl/cheetah_orm/__init__.py
@@ -0,0 +1,6 @@
+"""A lightweight and high-performance object-relational mapper for Python."""
+
+__author__    = "Cybermals"
+__copyright__ = "Copyright (c) 2023 by Cybermals"
+__license__   = "MIT"
+__version__   = "2.0.0b5"
