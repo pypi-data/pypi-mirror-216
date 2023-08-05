3f18b2e21e78dbf0b6752c0a9a4986cbde8ce29e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:59 +0000

    Add package foreverbull/0.0.0.20230626183227/foreverbull-0.0.0.20230626183227-py3-none-any.whl

---
+++ packages/foreverbull/foreverbull-0.0.0.20230626183227-py3-none-any.whl/foreverbull/broker/storage/backtest.py
@@ -0,0 +1,23 @@
+import io
+
+import minio
+import pandas as pd
+
+
+class Backtest:
+    def __init__(self, client: minio.Minio) -> None:
+        self.client = client
+
+    def upload_backtest_result(self, backtest: str, result: pd.DataFrame) -> None:
+        result.to_pickle("/tmp/result.pkl")
+        self.client.fput_object("backtest-results", backtest, "/tmp/result.pkl")
+
+    def download_backtest_results(self, backtest: str) -> pd.DataFrame:
+        pickled_result = self.client.get_object("backtest-results", f"{backtest}").read()
+        return pd.read_pickle(io.BytesIO(pickled_result))
+
+    def upload_backtest_ingestion(self, local_name: str, remote_name: str) -> None:
+        self.client.fput_object("backtest-ingestions", remote_name, local_name)
+
+    def download_backtest_ingestion(self, remote_name: str, local_name: str) -> None:
+        self.client.fget_object("backtest-ingestions", remote_name, local_name)
