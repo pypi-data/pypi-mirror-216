8ac93614cd21fde17c6a09d44043ae9934e4d50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:29 +0000

    Add package thirdai/0.7.10/thirdai-0.7.10-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/thirdai/thirdai-0.7.10-cp38-cp38-macosx_10_15_x86_64.whl/thirdai/_distributed_bolt/__init__.py
@@ -0,0 +1,21 @@
+from .dataset_loaders import (
+    DistributedColdStartDatasetLoader,
+    DistributedFeaturizerDatasetLoader,
+    DistributedSvmDatasetLoader,
+    DistributedTabularDatasetLoader,
+    DistributedUDTDatasetLoader,
+    ValidationContext,
+)
+from .distributed import (
+    DistributedDataParallel,
+    RayTrainingClusterConfig,
+    add_distributed_to_udt,
+)
+from .ray_trainer.bolt_checkpoint import BoltCheckPoint
+from .ray_trainer.bolt_trainer import BoltTrainer
+from .ray_trainer.config import BoltBackendConfig
+from .utils import PandasColumnMapGenerator, get_num_cpus
+
+add_distributed_to_udt()
+
+from .distributed_v2 import DistributedTrainer
