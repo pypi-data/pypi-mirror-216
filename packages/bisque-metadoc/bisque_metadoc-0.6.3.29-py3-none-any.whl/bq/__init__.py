23231a5f5d64bec68d618425ad915752db428764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:19 +0000

    Add package bisque-metadoc/0.6.3.29/bisque_metadoc-0.6.3.29-py3-none-any.whl

---
+++ packages/bisque-metadoc/bisque_metadoc-0.6.3.29-py3-none-any.whl/bq/__init__.py
@@ -0,0 +1,5 @@
+"""The bisque_metadoc package"""
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
+# __import__('pkg_resources').declare_namespace(__name__)
---
+++ packages/bisque-metadoc/bisque_metadoc-0.6.3.29-py3-none-any.whl/bq/metadoc/__init__.py
@@ -0,0 +1,7 @@
+# blank
+
+
+__all__ = ["Metadoc"]
+
+
+from bq.metadoc.formats import Metadoc
