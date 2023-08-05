95c50bca7e4fba7d5973ebde40f4614699f9e45d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:32 +0000

    Add package vipes/0.0.2/vipes-0.0.2.tar.gz

---
+++ packages/vipes/vipes-0.0.2.tar.gz/vipes-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vipes',
+    version='0.0.2',
+    author='Jhon',
+    description='The most reliable script manager for crossplatform projects',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/flobbun/vipes',
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: OS Independent',
+    ],
+    keywords=['script', 'manager', 'crossplatform', 'configuration'],
+    packages=find_packages(),
+    python_requires='>=3.7',
+    entry_points='''
+        [console_scripts]
+        vipes=vipes.__main__:main
+    ''',
+    include_package_data=False
+)
