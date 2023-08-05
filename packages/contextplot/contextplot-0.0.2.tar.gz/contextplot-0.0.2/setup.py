d018d8a99ad270878cbff4c63821bb84b96810de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:15 +0000

    Add package contextplot/0.0.2/contextplot-0.0.2.tar.gz

---
+++ packages/contextplot/contextplot-0.0.2.tar.gz/contextplot-0.0.2/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="contextplot",
+    version="0.0.2",
+    author="Josh Burkart",
+    description=
+    "Context manager-based API for making Matplotlib plots in Jupyter notebooks",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'IPython',
+        'matplotlib',
+        'numpy',
+    ],
+)
