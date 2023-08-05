10c7db5d7fe3bfd7b49bbd2dd50175d70eeb6f58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:12 +0000

    Add package dlc2kinematics/0.0.7/dlc2kinematics-0.0.7.tar.gz

---
+++ packages/dlc2kinematics/dlc2kinematics-0.0.7.tar.gz/dlc2kinematics-0.0.7/setup.py
@@ -0,0 +1,52 @@
+"""
+dlc2kinematics
+© M. Mathis Lab
+https://github.com/AdaptiveMotorControlLab/dlc2kinematics/
+
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dlc2kinematics",
+    version="0.0.7",
+    author="Mackenzie Mathis Lab Members",
+    author_email="mackenzie@post.harvard.edu",
+    description="Library for kinematic analysis of DeepLabCut outputs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AdaptiveMotorControlLab/dlc2kinematics/",
+    install_requires=[
+        "h5py",
+        "c3d",
+        "ipython",
+        "ipython-genutils",
+        "matplotlib>=3.0.3",
+        "numpy<1.25",
+        "pandas>=1.0",
+        "python-dateutil",
+        "pyyaml",
+        "requests",
+        "setuptools",
+        "scikit-image",
+        "scikit-learn<1.2.2",
+        "scikit-kinematics",
+        "scipy",
+        "tables",
+        "umap-learn",
+        "tqdm",
+        "ruamel.yaml>=0.15.0",
+        "wheel",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+    entry_points="""[console_scripts]
+            kin=kin:main""",
+)
