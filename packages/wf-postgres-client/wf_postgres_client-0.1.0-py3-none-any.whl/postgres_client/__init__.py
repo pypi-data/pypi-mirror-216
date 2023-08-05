dfddbaea9fa900885dcb8ec552ed4635b4050793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:54 +0000

    Add package wf-postgres-client/0.1.0/wf_postgres_client-0.1.0-py3-none-any.whl

---
+++ packages/wf-postgres-client/wf_postgres_client-0.1.0-py3-none-any.whl/postgres_client/__init__.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+import toml
+
+from .core import *
+from .utils import *
+
+# Credit to @unruffled-nightingale: https://github.com/unruffled-nightingale/fastapi-template/blob/main/fastapi_template/__init__.py
+def get_version() -> str:
+    path = Path(__file__).resolve().parents[1] / "pyproject.toml"
+    pyproject = toml.load(str(path))
+    version: str = pyproject["tool"]["poetry"]["version"]
+    return version
+
+
+__version__ = get_version()
