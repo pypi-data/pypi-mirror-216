fee6ff1448f23d21ec59ed7407fadd7a584e3bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:31 +0000

    Add package moldoc/2.0.0/moldoc-2.0.0-py3-none-any.whl

---
+++ packages/moldoc/moldoc-2.0.0-py3-none-any.whl/moldoc/__init__.py
@@ -0,0 +1,3 @@
+from moldoc._internal.moldoc import setup
+
+__all__ = ["setup"]
---
+++ packages/moldoc/moldoc-2.0.0-py3-none-any.whl/moldoc/molecule.py
@@ -0,0 +1,29 @@
+from moldoc._internal.molecule import (
+    Atom,
+    AtomConfig,
+    Bond,
+    Color,
+    Material,
+    MeshLambertMaterial,
+    MeshNormalMaterial,
+    MeshPhongMaterial,
+    MeshPhysicalMaterial,
+    MeshStandardMaterial,
+    Molecule,
+    MoleculeConfig,
+)
+
+__all__ = [
+    "Atom",
+    "AtomConfig",
+    "Bond",
+    "Color",
+    "Material",
+    "MeshLambertMaterial",
+    "MeshNormalMaterial",
+    "MeshPhongMaterial",
+    "MeshPhysicalMaterial",
+    "MeshStandardMaterial",
+    "Molecule",
+    "MoleculeConfig",
+]
