91e4b2953baf3cf97023d5186d710dd843c18ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:14 +0000

    Add package PySide6-Fluent-Widgets/0.9.9/PySide6-Fluent-Widgets-0.9.9.tar.gz

---
+++ packages/PySide6-Fluent-Widgets/PySide6-Fluent-Widgets-0.9.9.tar.gz/PySide6-Fluent-Widgets-0.9.9/qfluentwidgets/components/widgets/__init__.py
@@ -0,0 +1,32 @@
+from .button import (DropDownPushButton, DropDownToolButton, PrimaryPushButton, PushButton, RadioButton,
+                     HyperlinkButton, ToolButton, TransparentToolButton, ToggleButton, SplitWidgetBase,
+                     SplitPushButton, SplitToolButton, PrimaryToolButton, PrimarySplitPushButton,
+                     PrimarySplitToolButton, PrimaryDropDownPushButton, PrimaryDropDownToolButton,
+                     TogglePushButton, ToggleToolButton, TransparentPushButton, TransparentTogglePushButton,
+                     TransparentToggleToolButton, TransparentDropDownPushButton, TransparentDropDownToolButton)
+from .card_widget import CardWidget
+from .check_box import CheckBox
+from .combo_box import ComboBox, EditableComboBox
+from .line_edit import LineEdit, TextEdit, PlainTextEdit, LineEditButton, SearchLineEdit
+from .icon_widget import IconWidget
+from .label import (PixmapLabel, CaptionLabel, StrongBodyLabel, BodyLabel, SubtitleLabel, TitleLabel,
+                    LargeTitleLabel, DisplayLabel, FluentLabelBase)
+from .list_view import ListWidget, ListView, ListItemDelegate
+from .menu import (DWMMenu, LineEditMenu, RoundMenu, MenuAnimationManager, MenuAnimationType, IndicatorMenuItemDelegate,
+                   MenuItemDelegate)
+from .info_bar import InfoBar, InfoBarIcon, InfoBarPosition
+from .scroll_area import SingleDirectionScrollArea, SmoothScrollArea, ScrollArea
+from .slider import Slider, HollowHandleStyle
+from .spin_box import SpinBox, DoubleSpinBox, DateEdit, DateTimeEdit, TimeEdit
+from .stacked_widget import PopUpAniStackedWidget, OpacityAniStackedWidget
+from .state_tool_tip import StateToolTip
+from .switch_button import SwitchButton, IndicatorPosition
+from .table_view import TableView, TableWidget, TableItemDelegate
+from .tool_tip import ToolTip, ToolTipFilter, ToolTipPosition
+from .tree_view import TreeWidget, TreeView, TreeItemDelegate
+from .cycle_list_widget import CycleListWidget
+from .progress_bar import IndeterminateProgressBar, ProgressBar
+from .progress_ring import ProgressRing
+from .scroll_bar import ScrollBar, SmoothScrollBar, SmoothScrollDelegate
+from .teaching_tip import TeachingTip, TeachingTipTailPosition
+from .flyout import FlyoutView, FlyoutViewBase, Flyout
