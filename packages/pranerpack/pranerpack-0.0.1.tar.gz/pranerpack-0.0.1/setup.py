cdba9941c92f349d0f7405994f4dbad88b53b737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:03 +0000

    Add package pranerpack/0.0.1/pranerpack-0.0.1.tar.gz

---
+++ packages/pranerpack/pranerpack-0.0.1.tar.gz/pranerpack-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Praneeths first Python package'
+LONG_DESCRIPTION = 'praneeths first package with a slightly longer description'
+
+
+setup(
+     
+        name="pranerpack", 
+        version=VERSION,
+        author="Sai Praneeth Renduchinthala",
+        author_email="praneeth.renduchinthala210@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],
+        keywords=[ 'pranerpack'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
