5b2c7032dabf7bce5fddbd10cadc5ddc84c92f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:32 +0000

    Add package hera/5.5.2/hera-5.5.2-py3-none-any.whl

---
+++ packages/hera/hera-5.5.2-py3-none-any.whl/hera/workflows/protocol.py
@@ -0,0 +1,57 @@
+from typing import Any, List, Optional, Union
+
+from typing_extensions import Protocol, runtime_checkable
+
+from hera.workflows.models import (
+    ClusterWorkflowTemplate,
+    ContainerSetTemplate,
+    CronWorkflow,
+    DAGTemplate,
+    PersistentVolumeClaim,
+    ResourceTemplate,
+    ScriptTemplate,
+    SuspendTemplate,
+    Template,
+    Workflow,
+    WorkflowTemplate,
+)
+
+TTemplate = Union[
+    ContainerSetTemplate,
+    DAGTemplate,
+    ResourceTemplate,
+    ScriptTemplate,
+    SuspendTemplate,
+    Template,
+]
+
+TWorkflow = Union[
+    CronWorkflow,
+    ClusterWorkflowTemplate,
+    Workflow,
+    WorkflowTemplate,
+]
+
+
+@runtime_checkable
+class Templatable(Protocol):
+    def _build_template(self) -> TTemplate:
+        ...
+
+
+@runtime_checkable
+class VolumeClaimable(Protocol):
+    def _build_persistent_volume_claims(self) -> Optional[List[PersistentVolumeClaim]]:
+        ...
+
+
+@runtime_checkable
+class Subbable(Protocol):
+    def _add_sub(self, node: Any) -> Any:
+        ...
+
+
+@runtime_checkable
+class Steppable(Protocol):
+    def _build_step(self) -> Any:
+        ...
