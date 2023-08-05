acbb11ceb2b9b2186cc6b4f0b2507f7dcab9f4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:20 +0000

    Add package dccs-logo/1.0.0/dccs_logo-1.0.0.tar.gz

---
+++ packages/dccs-logo/dccs_logo-1.0.0.tar.gz/dccs_logo-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='dccs_logo',
+    version='1.0.0',
+    author='SAIMUN',
+    description='A Python module for displaying the DCSS logo',
+    py_modules=['dccs_logo'],
+    install_requires=['rich'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
