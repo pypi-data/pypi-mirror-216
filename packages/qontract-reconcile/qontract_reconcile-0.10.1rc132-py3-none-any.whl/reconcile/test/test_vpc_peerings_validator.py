9ce85b92af1017934c30ed1f3bf7e6e84c851b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:37 +0000

    Add package qontract-reconcile/0.10.1rc132/qontract_reconcile-0.10.1rc132-py3-none-any.whl

---
+++ packages/qontract-reconcile/qontract_reconcile-0.10.1rc132-py3-none-any.whl/reconcile/test/test_vpc_peerings_validator.py
@@ -0,0 +1,103 @@
+import pytest
+
+from reconcile.gql_definitions.vpc_peerings_validator.vpc_peerings_validator import (
+    ClusterPeeringConnectionClusterAccepterV1,
+    ClusterPeeringV1,
+    ClusterSpecV1,
+    ClusterV1,
+    VpcPeeringsValidatorQueryData,
+)
+from reconcile.gql_definitions.vpc_peerings_validator.vpc_peerings_validator_peered_cluster_fragment import (
+    ClusterSpecV1 as PeeredClusterSpec,
+)
+from reconcile.gql_definitions.vpc_peerings_validator.vpc_peerings_validator_peered_cluster_fragment import (
+    VpcPeeringsValidatorPeeredCluster,
+)
+from reconcile.vpc_peerings_validator import (
+    validate_no_internal_to_public_peerings,
+    validate_no_public_to_public_peerings,
+)
+
+
+@pytest.fixture
+def query_data_i2p() -> VpcPeeringsValidatorQueryData:
+    return VpcPeeringsValidatorQueryData(
+        clusters=[
+            ClusterV1(
+                name="cluster1",
+                spec=ClusterSpecV1(private=True),
+                internal=True,
+                peering=ClusterPeeringV1(
+                    connections=[
+                        ClusterPeeringConnectionClusterAccepterV1(
+                            provider="cluster-vpc-accepter",
+                            cluster=VpcPeeringsValidatorPeeredCluster(
+                                name="cluster2",
+                                spec=PeeredClusterSpec(private=False),
+                                internal=False,
+                            ),
+                        ),
+                    ],
+                ),
+            ),
+        ],
+    )
+
+
+def test_validate_no_internal_to_public_peerings_invalid(
+    query_data_i2p: VpcPeeringsValidatorQueryData,
+):
+    assert validate_no_internal_to_public_peerings(query_data_i2p) is False
+
+
+def test_validate_no_internal_to_public_peerings_valid_private(
+    query_data_i2p: VpcPeeringsValidatorQueryData,
+):
+    query_data_i2p.clusters[0].peering.connections[0].cluster.spec.private = True  # type: ignore[index,union-attr]
+    assert validate_no_internal_to_public_peerings(query_data_i2p) is True
+
+
+def test_validate_no_internal_to_public_peerings_valid_internal(
+    query_data_i2p: VpcPeeringsValidatorQueryData,
+):
+    assert query_data_i2p.clusters is not None
+    query_data_i2p.clusters[0].peering.connections[0].cluster.internal = True  # type: ignore[union-attr]
+    assert validate_no_internal_to_public_peerings(query_data_i2p) is True
+
+
+@pytest.fixture
+def query_data_p2p() -> VpcPeeringsValidatorQueryData:
+    return VpcPeeringsValidatorQueryData(
+        clusters=[
+            ClusterV1(
+                name="cluster1",
+                spec=ClusterSpecV1(private=False),
+                internal=False,
+                peering=ClusterPeeringV1(
+                    connections=[
+                        ClusterPeeringConnectionClusterAccepterV1(
+                            provider="cluster-vpc-accepter",
+                            cluster=VpcPeeringsValidatorPeeredCluster(
+                                name="cluster2",
+                                spec=PeeredClusterSpec(private=False),
+                                internal=False,
+                            ),
+                        ),
+                    ],
+                ),
+            ),
+        ],
+    )
+
+
+def test_validate_no_public_to_public_peerings_invalid(
+    query_data_p2p: VpcPeeringsValidatorQueryData,
+):
+    assert validate_no_public_to_public_peerings(query_data_p2p) is False
+
+
+def test_validate_no_public_to_public_peerings_valid(
+    query_data_p2p: VpcPeeringsValidatorQueryData,
+):
+    query_data_p2p.clusters[0].peering.connections[0].cluster.spec.private = True  # type: ignore[index,union-attr]
+    assert validate_no_public_to_public_peerings(query_data_p2p) is True
---
+++ packages/qontract-reconcile/qontract_reconcile-0.10.1rc132-py3-none-any.whl/reconcile/vpc_peerings_validator.py
@@ -0,0 +1,113 @@
+import logging
+import sys
+from typing import (
+    Union,
+    cast,
+)
+
+from reconcile.gql_definitions.vpc_peerings_validator import vpc_peerings_validator
+from reconcile.gql_definitions.vpc_peerings_validator.vpc_peerings_validator import (
+    ClusterPeeringConnectionClusterAccepterV1,
+    ClusterPeeringConnectionClusterRequesterV1,
+    ClusterV1,
+    VpcPeeringsValidatorQueryData,
+)
+from reconcile.status import ExitCodes
+from reconcile.utils import gql
+
+QONTRACT_INTEGRATION = "vpc-peerings-validator"
+
+
+def validate_no_internal_to_public_peerings(
+    query_data: VpcPeeringsValidatorQueryData,
+) -> bool:
+    """Iterate over VPC peerings of internal clusters and validate the peer is not public."""
+    valid = True
+    found_pairs: list[set[str]] = []
+    clusters: list[ClusterV1] = query_data.clusters or []
+    for cluster in clusters:
+        if not cluster.internal or not cluster.peering:
+            continue
+        for connection in cluster.peering.connections:
+            if connection.provider not in [
+                "cluster-vpc-accepter",
+                "cluster-vpc-requester",
+            ]:
+                continue
+            connection = cast(
+                Union[
+                    ClusterPeeringConnectionClusterAccepterV1,
+                    ClusterPeeringConnectionClusterRequesterV1,
+                ],
+                connection,
+            )
+            peer = connection.cluster
+            if peer.internal or (peer.spec and peer.spec.private):
+                continue
+
+            valid = False
+            pair = {cluster.name, peer.name}
+            if pair in found_pairs:
+                continue
+            found_pairs.append(pair)
+            logging.error(
+                f"found internal to public vpc peering: {cluster.name} <-> {peer.name}"
+            )
+
+    return valid
+
+
+def validate_no_public_to_public_peerings(
+    query_data: VpcPeeringsValidatorQueryData,
+) -> bool:
+    """Iterate over VPC peerings of public clusters and validate the peer is not public."""
+    valid = True
+    found_pairs: list[set[str]] = []
+    clusters: list[ClusterV1] = query_data.clusters or []
+    for cluster in clusters:
+        if (
+            cluster.internal
+            or (cluster.spec and cluster.spec.private)
+            or not cluster.peering
+        ):
+            continue
+        for connection in cluster.peering.connections:
+            if connection.provider not in [
+                "cluster-vpc-accepter",
+                "cluster-vpc-requester",
+            ]:
+                continue
+            connection = cast(
+                Union[
+                    ClusterPeeringConnectionClusterAccepterV1,
+                    ClusterPeeringConnectionClusterRequesterV1,
+                ],
+                connection,
+            )
+            peer = connection.cluster
+            if peer.internal or (peer.spec and peer.spec.private):
+                continue
+
+            valid = False
+            pair = {cluster.name, peer.name}
+            if pair in found_pairs:
+                continue
+            found_pairs.append(pair)
+            logging.error(
+                f"found public to public vpc peering: {cluster.name} <-> {peer.name}"
+            )
+
+    return valid
+
+
+def run(dry_run: bool):
+    query_data = vpc_peerings_validator.query(query_func=gql.get_api().query)
+
+    valid = True
+    if not validate_no_internal_to_public_peerings(query_data):
+        valid = False
+    if not validate_no_public_to_public_peerings(query_data):
+        valid = False
+
+    if not valid:
+        sys.exit(ExitCodes.ERROR)
