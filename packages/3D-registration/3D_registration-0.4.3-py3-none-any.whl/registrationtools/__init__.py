83ba9ea4295a2cf58ac03ba1c981da827b030faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:17 +0000

    Add package 3D-registration/0.4.3/3D_registration-0.4.3-py3-none-any.whl

---
+++ packages/3D-registration/3D_registration-0.4.3-py3-none-any.whl/registrationtools/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "0.4.3"
+
+from .time_registration import TimeRegistration, time_registration
+from .spatial_registration import SpatialRegistration, spatial_registration
+from .utils import *
+import sys
+
+if 8 < sys.version_info.minor:
+    import importlib.resources as importlib_resources
+
+    pkg = importlib_resources.files("registrationtools") / "data"
+else:
+    from importlib_resources._legacy import path
+
+    pkg = path("registrationtools", "data").args[0]
+
+image_path = pkg / "images"
+json_path_spatial = pkg / "JSON_spatial"
+json_path_time = pkg / "JSON_time"
