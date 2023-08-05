cd2524b0429eae971f7179e1ca72600d3f04b535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:26 +0000

    Add package balepy/0.1/balepy-0.1-py3-none-any.whl

---
+++ packages/balepy/balepy-0.1-py3-none-any.whl/bale/__init__.py
@@ -0,0 +1,5 @@
+from main import *
+from colorama import Fore
+
+print(f"""Wellcom to{Fore.MAGENTA} balepy {Fore.RESET} library version:{Fore.LIGHTBLUE_EX} 0.1 {Fore.RESET}
+Mohammad Mehrabi Rad and Erfan Bafandeh{Fore.LIGHTMAGENTA_EX} <github.com/OnlyRad>{Fore.RESET}""")
