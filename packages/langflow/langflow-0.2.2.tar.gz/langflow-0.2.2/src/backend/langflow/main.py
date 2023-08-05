5d2e3ae627ee881fb8a6fa965c427f5d08663603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:55 +0000

    Add package langflow/0.2.2/langflow-0.2.2.tar.gz

---
+++ packages/langflow/langflow-0.2.2.tar.gz/langflow-0.2.2/src/backend/langflow/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from langflow.api import router
+from langflow.database.base import create_db_and_tables
+from langflow.interface.utils import setup_llm_caching
+
+
+def create_app():
+    """Create the FastAPI app and include the router."""
+
+    app = FastAPI()
+
+    origins = [
+        "*",
+    ]
+
+    @app.get("/health")
+    def get_health():
+        return {"status": "OK"}
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    app.include_router(router)
+    app.on_event("startup")(create_db_and_tables)
+    app.on_event("startup")(setup_llm_caching)
+    return app
+
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=7860)
