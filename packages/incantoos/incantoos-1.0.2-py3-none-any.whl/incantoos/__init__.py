139636b3b2cc0a1751d9e469a42e947093f0407e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:56 +0000

    Add package incantoos/1.0.2/incantoos-1.0.2-py3-none-any.whl

---
+++ packages/incantoos/incantoos-1.0.2-py3-none-any.whl/incantoos/__init__.py
@@ -0,0 +1,17 @@
+import datetime
+
+version = "1.0.2"
+
+class color:
+    blue = 0x0076FF
+    green = 0x00FF00
+    yellow = 0xFFFF00
+    red = 0xFF0000
+    black = 0x000001
+    teal = 0x00ffff
+    purple = 0xA020F0
+
+def nowtime():
+    time = datetime.datetime.now()
+    return time
+
