13a2195d9af0bf04e0e4621f4066c25e367c82d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:39 +0000

    Add package rainbow-connection/0.0.16/rainbow-connection-0.0.16.tar.gz

---
+++ packages/rainbow-connection/rainbow-connection-0.0.16.tar.gz/rainbow-connection-0.0.16/rainbowconnection/__init__.py
@@ -0,0 +1,7 @@
+from .version import *
+from .sources import *
+from .atmospheres import *
+from .resampletools import bintogrid, bintoR
+from .colortools import plot_simple_rainbow
+
+import astropy.units as u
---
+++ packages/rainbow-connection/rainbow-connection-0.0.16.tar.gz/rainbow-connection-0.0.16/rainbowconnection/sources/__init__.py
@@ -0,0 +1,6 @@
+from .spectra import * 
+from .blank import *
+from .planck import *
+from .sun import *
+from .lightbulbs import *
+from .star import *
