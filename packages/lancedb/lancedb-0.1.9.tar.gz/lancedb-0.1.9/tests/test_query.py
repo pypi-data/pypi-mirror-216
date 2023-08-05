2dbd8f85232220a51e70bc28970dc6de799dfb24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:53 +0000

    Add package lancedb/0.1.9/lancedb-0.1.9.tar.gz

---
+++ packages/lancedb/lancedb-0.1.9.tar.gz/lancedb-0.1.9/tests/test_query.py
@@ -0,0 +1,77 @@
+#  Copyright 2023 LanceDB Developers
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import lance
+import numpy as np
+import pandas as pd
+import pandas.testing as tm
+import pyarrow as pa
+import pytest
+
+from lancedb.db import LanceDBConnection
+from lancedb.query import LanceQueryBuilder
+
+
+class MockTable:
+    def __init__(self, tmp_path):
+        self.uri = tmp_path
+        self._conn = LanceDBConnection("/tmp/lance/")
+
+    def to_lance(self):
+        return lance.dataset(self.uri)
+
+
+@pytest.fixture
+def table(tmp_path) -> MockTable:
+    df = pa.table(
+        {
+            "vector": pa.array(
+                [[1, 2], [3, 4]], type=pa.list_(pa.float32(), list_size=2)
+            ),
+            "id": pa.array([1, 2]),
+            "str_field": pa.array(["a", "b"]),
+            "float_field": pa.array([1.0, 2.0]),
+        }
+    )
+    lance.write_dataset(df, tmp_path)
+    return MockTable(tmp_path)
+
+
+def test_query_builder(table):
+    df = LanceQueryBuilder(table, [0, 0]).limit(1).select(["id"]).to_df()
+    assert df["id"].values[0] == 1
+    assert all(df["vector"].values[0] == [1, 2])
+
+
+def test_query_builder_with_filter(table):
+    df = LanceQueryBuilder(table, [0, 0]).where("id = 2").to_df()
+    assert df["id"].values[0] == 2
+    assert all(df["vector"].values[0] == [3, 4])
+
+
+def test_query_builder_with_metric(table):
+    query = [4, 8]
+    df_default = LanceQueryBuilder(table, query).to_df()
+    df_l2 = LanceQueryBuilder(table, query).metric("L2").to_df()
+    tm.assert_frame_equal(df_default, df_l2)
+
+    df_cosine = LanceQueryBuilder(table, query).metric("cosine").limit(1).to_df()
+    assert df_cosine.score[0] == pytest.approx(
+        cosine_distance(query, df_cosine.vector[0]),
+        abs=1e-6,
+    )
+    assert 0 <= df_cosine.score[0] <= 1
+
+
+def cosine_distance(vec1, vec2):
+    return 1 - np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
---
+++ packages/lancedb/lancedb-0.1.9.tar.gz/lancedb-0.1.9/tests/test_table.py
@@ -0,0 +1,144 @@
+#  Copyright 2023 LanceDB Developers
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import functools
+from pathlib import Path
+
+import pandas as pd
+import pyarrow as pa
+import pytest
+
+from lancedb.table import LanceTable
+
+
+class MockDB:
+    def __init__(self, uri: Path):
+        self.uri = uri
+
+    @functools.cached_property
+    def is_managed_remote(self) -> bool:
+        return False
+
+
+@pytest.fixture
+def db(tmp_path) -> MockDB:
+    return MockDB(tmp_path)
+
+
+def test_basic(db):
+    ds = LanceTable.create(
+        db,
+        "test",
+        data=[
+            {"vector": [3.1, 4.1], "item": "foo", "price": 10.0},
+            {"vector": [5.9, 26.5], "item": "bar", "price": 20.0},
+        ],
+    ).to_lance()
+
+    table = LanceTable(db, "test")
+    assert table.name == "test"
+    assert table.schema == ds.schema
+    assert table.to_lance().to_table() == ds.to_table()
+
+
+def test_create_table(db):
+    schema = pa.schema(
+        [
+            pa.field("vector", pa.list_(pa.float32(), 2)),
+            pa.field("item", pa.string()),
+            pa.field("price", pa.float32()),
+        ]
+    )
+    expected = pa.Table.from_arrays(
+        [
+            pa.FixedSizeListArray.from_arrays(pa.array([3.1, 4.1, 5.9, 26.5]), 2),
+            pa.array(["foo", "bar"]),
+            pa.array([10.0, 20.0]),
+        ],
+        schema=schema,
+    )
+    data = [
+        [
+            {"vector": [3.1, 4.1], "item": "foo", "price": 10.0},
+            {"vector": [5.9, 26.5], "item": "bar", "price": 20.0},
+        ]
+    ]
+    df = pd.DataFrame(data[0])
+    data.append(df)
+    data.append(pa.Table.from_pandas(df, schema=schema))
+
+    for i, d in enumerate(data):
+        tbl = (
+            LanceTable.create(db, f"test_{i}", data=d, schema=schema)
+            .to_lance()
+            .to_table()
+        )
+        assert expected == tbl
+
+
+def test_add(db):
+    table = LanceTable.create(
+        db,
+        "test",
+        data=[
+            {"vector": [3.1, 4.1], "item": "foo", "price": 10.0},
+            {"vector": [5.9, 26.5], "item": "bar", "price": 20.0},
+        ],
+    )
+
+    # table = LanceTable(db, "test")
+    assert len(table) == 2
+
+    count = table.add([{"vector": [6.3, 100.5], "item": "new", "price": 30.0}])
+    assert count == 3
+
+    expected = pa.Table.from_arrays(
+        [
+            pa.FixedSizeListArray.from_arrays(
+                pa.array([3.1, 4.1, 5.9, 26.5, 6.3, 100.5]), 2
+            ),
+            pa.array(["foo", "bar", "new"]),
+            pa.array([10.0, 20.0, 30.0]),
+        ],
+        schema=pa.schema(
+            [
+                pa.field("vector", pa.list_(pa.float32(), 2)),
+                pa.field("item", pa.string()),
+                pa.field("price", pa.float64()),
+            ]
+        ),
+    )
+    assert expected == table.to_arrow()
+
+
+def test_versioning(db):
+    table = LanceTable.create(
+        db,
+        "test",
+        data=[
+            {"vector": [3.1, 4.1], "item": "foo", "price": 10.0},
+            {"vector": [5.9, 26.5], "item": "bar", "price": 20.0},
+        ],
+    )
+
+    assert len(table.list_versions()) == 1
+    assert table.version == 1
+
+    table.add([{"vector": [6.3, 100.5], "item": "new", "price": 30.0}])
+    assert len(table.list_versions()) == 2
+    assert table.version == 2
+    assert len(table) == 3
+
+    table.checkout(1)
+    assert table.version == 1
+    assert len(table) == 2
