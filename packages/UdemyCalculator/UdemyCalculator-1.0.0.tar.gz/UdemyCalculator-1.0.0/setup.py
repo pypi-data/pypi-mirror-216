29a330b8ff044f32bf7fa92b8599cb1eabc99940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:15 +0000

    Add package UdemyCalculator/1.0.0/UdemyCalculator-1.0.0.tar.gz

---
+++ packages/UdemyCalculator/UdemyCalculator-1.0.0.tar.gz/UdemyCalculator-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+def read_file(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        return file.read()
+
+
+setup(
+    name="UdemyCalculator",
+    version="1.0.0",
+    packages=find_packages(include=['udemyCalculator', 'udemyCalculator.*']),
+    url="",
+    author="Idan Chen",
+    description="This is my udemy calculator package",
+    long_description=read_file(file_name="README.md"),
+    python_requires=">=3.6"
+)
