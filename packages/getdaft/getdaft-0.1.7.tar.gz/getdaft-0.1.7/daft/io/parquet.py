8c428890e6f96f1fe49927959cff1976ecf571d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:38 +0000

    Add package getdaft/0.1.7/getdaft-0.1.7.tar.gz

---
+++ packages/getdaft/getdaft-0.1.7.tar.gz/getdaft-0.1.7/daft/io/parquet.py
@@ -0,0 +1,44 @@
+# isort: dont-add-import: from __future__ import annotations
+
+from typing import Dict, Optional
+
+import fsspec
+
+from daft.api_annotations import PublicAPI
+from daft.dataframe import DataFrame
+from daft.datasources import ParquetSourceInfo
+from daft.datatype import DataType
+from daft.io.common import _get_tabular_files_scan
+
+
+@PublicAPI
+def read_parquet(
+    path: str,
+    schema_hints: Optional[Dict[str, DataType]] = None,
+    fs: Optional[fsspec.AbstractFileSystem] = None,
+) -> DataFrame:
+    """Creates a DataFrame from Parquet file(s)
+
+    Example:
+        >>> df = daft.read_parquet("/path/to/file.parquet")
+        >>> df = daft.read_parquet("/path/to/directory")
+        >>> df = daft.read_parquet("/path/to/files-*.parquet")
+        >>> df = daft.read_parquet("s3://path/to/files-*.parquet")
+
+    Args:
+        path (str): Path to Parquet file (allows for wildcards)
+        schema_hints (dict[str, DataType]): A mapping between column names and datatypes - passing this option will
+            disable all schema inference on data being read, and throw an error if data being read is incompatible.
+        fs (fsspec.AbstractFileSystem): fsspec FileSystem to use for reading data.
+            By default, Daft will automatically construct a FileSystem instance internally.
+
+    returns:
+        DataFrame: parsed DataFrame
+    """
+    plan = _get_tabular_files_scan(
+        path,
+        schema_hints,
+        ParquetSourceInfo(),
+        fs,
+    )
+    return DataFrame(plan)
