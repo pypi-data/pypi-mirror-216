c891820d00c95512770c7f0ee0f47b60c0e2567d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:40 +0000

    Add package aiogram3b8-calendar/0.0.2/aiogram3b8_calendar-0.0.2.tar.gz

---
+++ packages/aiogram3b8-calendar/aiogram3b8_calendar-0.0.2.tar.gz/aiogram3b8_calendar-0.0.2/README.md
@@ -0,0 +1,22 @@
+# Date Selection tool for Aiogram Telegram Bots
+
+## Description
+
+A simple inline calendar, date selection tool for [aiogram](https://github.com/aiogram/aiogram) telegram bots written in Python.
+Offers two types of date pickers:
+Navigation calendar - user can either select a date or move to the next or previous month/year by clicking a singe button.
+Dialog calendar - user selects year on first stage, month on next stage, day on last stage
+
+Project was forked for aiogram version 3.0.0b8 (3.0.0b7)
+
+## Usage
+Install package
+
+        pip install aiogram3b8_calendar
+
+A full working example on how to use aiogram-calendar is provided in *bot_example.py*. 
+You create a calendar and add it to a message with a *reply_markup* parameter and then you can process it in a callbackqueyhandler method using the *process_selection* method.
+
+## Gif demo:
+
+![aiogram_calendar](https://j.gifs.com/nRQlqW.gif)
---
+++ packages/aiogram3b8-calendar/aiogram3b8_calendar-0.0.2.tar.gz/aiogram3b8_calendar-0.0.2/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+  name='aiogram3b8_calendar',
+  packages=['aiogram3b8_calendar'],
+  version='0.0.2',
+  license='MIT',
+  description='Simple Inline Calendar & Date Selection tool for Aiogram Telegram bots',
+  long_description=long_description,
+  author='Andrew Nikolabay',
+  author_email='',
+  url='https://github.com/yoworu/aiogram3.0.0b8_calendar/',
+  download_url='https://github.com/yoworu/aiogram3.0.0b8_calendar.git',
+  keywords=['Aiogram', 'Telegram', 'Bots', 'Calendar'],
+  install_requires=[
+          'aiogram==3.0.0b7',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
