746defed56a44a8753968e6585ee14b75bb6bad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:33 +0000

    Add package gascompressibility/0.1.2/gascompressibility-0.1.2-py3-none-any.whl

---
+++ packages/gascompressibility/gascompressibility-0.1.2-py3-none-any.whl/gascompressibility/__init__.py
@@ -0,0 +1,5 @@
+
+from gascompressibility import pseudocritical
+from gascompressibility import z_correlation
+from gascompressibility.z_correlation.z_helper import calc_Z
+from gascompressibility.utilities.utilities import *
