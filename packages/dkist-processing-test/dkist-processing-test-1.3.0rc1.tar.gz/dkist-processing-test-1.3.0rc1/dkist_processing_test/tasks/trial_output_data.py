b6639c5cb0b437da2b0e87470fd5a65201d42805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:12 +0000

    Add package dkist-processing-test/1.3.0rc1/dkist-processing-test-1.3.0rc1.tar.gz

---
+++ packages/dkist-processing-test/dkist-processing-test-1.3.0rc1.tar.gz/dkist-processing-test-1.3.0rc1/dkist_processing_test/tasks/trial_output_data.py
@@ -0,0 +1,30 @@
+from dkist_processing_common.tasks.mixin.globus import GlobusTransferItem
+from dkist_processing_common.tasks.trial_output_data import TransferTrialDataBase
+
+
+class TransferTestTrialData(TransferTrialDataBase):
+    @property
+    def intermediate_task_names(self) -> list[str]:
+        """Return a list of intermediate tasks we want to transfer.
+
+        Just a dummy task for testing.
+        """
+        return ["DUMMY"]
+
+    def build_transfer_list(self) -> list[GlobusTransferItem]:
+        """
+        Build a list containing all files we want to transfer to the trial environment.
+
+        For the purposes of testing we try to exercise all of the provided helper methods.
+        """
+        transfer_list = []
+
+        transfer_list += self.build_debug_frame_transfer_list()
+
+        transfer_list += self.build_intermediate_frame_transfer_list()
+
+        transfer_list += self.build_output_frame_transfer_list()
+
+        transfer_list += self.build_transfer_list_from_tag_lists([["FOO", "BAR"], ["BAZ"]])
+
+        return transfer_list
