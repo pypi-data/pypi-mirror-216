3e2d7619d7845190bba633e38b4f147533e19fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:30 +0000

    Add package ignoreexceptions/0.10/ignoreexceptions-0.10.tar.gz

---
+++ packages/ignoreexceptions/ignoreexceptions-0.10.tar.gz/ignoreexceptions-0.10/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+# 
+here = os.path.abspath(os.path.dirname(__file__))
+# 
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),'README.md'), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()\
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+#long_description = (this_directory / "README.md").read_text()
+
+VERSION = '''0.10'''
+DESCRIPTION = '''Decorators that provide graceful exception handling, customization of error handling behavior, error logging'''
+
+# Setting up
+setup(
+    name="ignoreexceptions",
+    version=VERSION,
+    license='MIT',
+    url = 'https://github.com/hansalemaos/ignoreexceptions',
+    author="Johannes Fischer",
+    author_email="aulasparticularesdealemaosp@gmail.com",
+    description=DESCRIPTION,
+long_description = long_description,
+long_description_content_type="text/markdown",
+    #packages=['touchtouch'],
+    keywords=['ignore', 'exceptions', 'decorator'],
+    classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3 :: Only', 'Programming Language :: Python :: 3.10', 'Topic :: Software Development :: Libraries :: Python Modules', 'Topic :: Utilities'],
+    install_requires=['touchtouch'],
+    include_package_data=True
+)
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
