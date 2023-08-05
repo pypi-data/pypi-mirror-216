3a8818cf3d6961bbd8f5cb75b3c71088f6341d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:23 +0000

    Add package v8unpack/0.15.4/v8unpack-0.15.4.tar.gz

---
+++ packages/v8unpack/v8unpack-0.15.4.tar.gz/v8unpack-0.15.4/src/v8unpack/MetaDataObject/CommonForm.py
@@ -0,0 +1,9 @@
+from ..MetaDataObject.core.Simple import SimpleNameFolder
+from ..MetaDataObject.versions.CommonForm803 import CommonForm803
+
+
+class CommonForm(SimpleNameFolder):
+    versions = {
+        '803': CommonForm803,
+        4: CommonForm803
+    }
---
+++ packages/v8unpack/v8unpack-0.15.4.tar.gz/v8unpack-0.15.4/src/v8unpack/MetaDataObject/CommonTemplate.py
@@ -0,0 +1,9 @@
+from ..MetaDataObject import MetaDataObject
+from ..MetaDataObject.versions.CommonTemplate803 import CommonTemplate803
+
+
+class CommonTemplate(MetaDataObject):
+    versions = {
+        '803': CommonTemplate803,
+        4: CommonTemplate803
+    }
