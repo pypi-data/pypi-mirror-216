660d65f5d2e2b9cedf478459209594231dc3350e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:22 +0000

    Add package kangforecast/0.56/kangforecast-0.56.tar.gz

---
+++ packages/kangforecast/kangforecast-0.56.tar.gz/kangforecast-0.56/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kangforecast',
+    version='0.56',
+    description='A simple forecast package.',
+    author='Xiaowen Kang',
+    author_email='kangxiaowen@gmail.com',
+    url='https://www.linkedin.com/in/xiaowenkang/',  # replace with the url of your GitHub repo
+    packages=find_packages(),
+    package_data={'kangforecast': ['data/*.csv']},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  # choose a license
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires=[
+        'kanglib',
+        'pandas',
+        # other libraries...
+    ],
+    entry_points={
+        'console_scripts': [
+            'kangforecast_command=kangforecast.main:main',
+        ],
+    },
+)
