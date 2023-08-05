e239c14eb5e2a0e6fb9f85a69fe0c8adae31b8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:48 +0000

    Add package biosimulators-amici/0.1.23/biosimulators_amici-0.1.23.tar.gz

---
+++ packages/biosimulators-amici/biosimulators_amici-0.1.23.tar.gz/biosimulators_amici-0.1.23/biosimulators_amici/__init__.py
@@ -0,0 +1,24 @@
+import amici
+
+from ._version import __version__  # noqa: F401
+# :obj:`str`: version
+
+from .core import exec_sed_task, preprocess_sed_task, exec_sed_doc, exec_sedml_docs_in_combine_archive  # noqa: F401
+
+__all__ = [
+    '__version__',
+    'get_simulator_version',
+    'exec_sed_task',
+    'preprocess_sed_task',
+    'exec_sed_doc',
+    'exec_sedml_docs_in_combine_archive',
+]
+
+
+def get_simulator_version():
+    """ Get the version of AMICI
+
+    Returns:
+        :obj:`str`: version
+    """
+    return amici.__version__
