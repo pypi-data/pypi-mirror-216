86aeea444a4f7fee2d59eb55443dc1dea80f1cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:53 +0000

    Add package SaranyaNarayansetti/0.0.1/SaranyaNarayansetti-0.0.1-py3-none-any.whl

---
+++ packages/SaranyaNarayansetti/SaranyaNarayansetti-0.0.1-py3-none-any.whl/SaranyaNarayansetti/__init__.py
@@ -0,0 +1,2 @@
+from SaranyaNarayansetti.saruoperations import *
+
---
+++ packages/SaranyaNarayansetti/SaranyaNarayansetti-0.0.1-py3-none-any.whl/SaranyaNarayansetti/saruoperations.py
@@ -0,0 +1,7 @@
+def sayhello():
+    print("Hello Chinna")
+    return
+
+def message():
+    print("Balupu taggichuko waste mokam")
+    return
