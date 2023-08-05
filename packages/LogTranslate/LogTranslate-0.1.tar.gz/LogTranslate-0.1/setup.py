c8d77753eaae7140817dc16cede812e3724c8cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:25 +0000

    Add package LogTranslate/0.1/LogTranslate-0.1.tar.gz

---
+++ packages/LogTranslate/LogTranslate-0.1.tar.gz/LogTranslate-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LogTranslate',
+    version='0.1',
+    author='5hmlA',
+    author_email='jonas.jzy@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    keywords='tools log translate',
+    url='https://github.com/5hmlA/PyTools',
+    description='A Python library for translate log from log files'
+)
