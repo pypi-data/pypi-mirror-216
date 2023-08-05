b32518c2ea37ef4cfac62619a41fe5f1831529ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:04 +0000

    Add package lnauth-django/0.2.3/lnauth_django-0.2.3-py3-none-any.whl

---
+++ packages/lnauth-django/lnauth_django-0.2.3-py3-none-any.whl/lnauth_django/django_auth.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model, login
+from django.db import IntegrityError, transaction
+
+from . import exceptions
+
+User = get_user_model()
+
+
+def app_register(request):
+    try:
+        with transaction.atomic():
+
+            user = User.objects.create_user(
+                username=request.GET["key"], password=request.GET["key"]
+            )
+            user.lnauthkey.linking_key = request.GET["key"]
+            user.lnauthkey.save()
+            user.save()
+    except IntegrityError:
+        raise exceptions.DjangoAuthException("User already registered.")
+
+    login(request, user)
+
+
+def app_login(request):
+    try:
+        user = User.objects.get(lnauthkey__linking_key=request.GET["key"])
+    except User.DoesNotExist:
+        raise exceptions.DjangoAuthException("User not registered.")
+
+    login(request, user)
