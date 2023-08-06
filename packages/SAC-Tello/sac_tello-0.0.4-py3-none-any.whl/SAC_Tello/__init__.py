1aea0722ec1c2bc3b9e30e1c541aeb66686f89b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:31 +0000

    Add package SAC-Tello/0.0.4/sac_tello-0.0.4-py3-none-any.whl

---
+++ packages/SAC-Tello/sac_tello-0.0.4-py3-none-any.whl/SAC_Tello/__init__.py
@@ -0,0 +1,10 @@
+# File: __init__.py
+# Author: Michael Huelsman
+# Copyright: Dr. Michael Andrew Huelsman 2023
+# License: GNU GPLv3
+# Created On: 09 Jan 2023
+# Purpose:
+# Notes:
+
+from .tello_drone import TelloDrone
+from .tello_hud import TelloHud
