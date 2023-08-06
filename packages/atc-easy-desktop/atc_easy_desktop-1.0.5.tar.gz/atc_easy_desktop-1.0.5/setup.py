40579fe179c6c7bac1b7a94ed02e5198bfded2ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:05 +0000

    Add package atc-easy-desktop/1.0.5/atc_easy_desktop-1.0.5.tar.gz

---
+++ packages/atc-easy-desktop/atc_easy_desktop-1.0.5.tar.gz/atc_easy_desktop-1.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='atc_easy_desktop',
+    version='1.0.5',
+    description='Thư viện sử dụng để code game trên màn hình desktop',
+    long_description=long_description,
+    author='Chienbg200',
+    author_email='chienbg200400@gmail.com',
+    packages=['atc_easy_desktop'],
+    install_requires=[
+        'pywin32',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
