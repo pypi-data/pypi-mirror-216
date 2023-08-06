b05fc329ba61fb83d9e95a505b41050ed8d22078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:02 +0000

    Add package pallet-sim/0.0.2/pallet_sim-0.0.2.tar.gz

---
+++ packages/pallet-sim/pallet_sim-0.0.2.tar.gz/pallet_sim-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pallet_sim',
+    version='0.0.2',
+    author='Rodolfo Verde',
+    description='Pallet simulation library developed at THWS',
+    packages=find_packages(),
+    package_data={
+        'pallet_sim': ['textures/*.stl'],
+    },
+)
