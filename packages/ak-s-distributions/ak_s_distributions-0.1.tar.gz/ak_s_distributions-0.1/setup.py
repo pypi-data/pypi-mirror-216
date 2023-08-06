3e3f8d8a644ad0cef3c40ce7b4315ae3516443e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:57 +0000

    Add package ak-s-distributions/0.1/ak_s_distributions-0.1.tar.gz

---
+++ packages/ak-s-distributions/ak_s_distributions-0.1.tar.gz/ak_s_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ak_s_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ak_s_distributions'],
+      author="Mohd Ayan Khan",
+      author_email="ayan.k470777@gmail.com",
+      zip_safe=False)
