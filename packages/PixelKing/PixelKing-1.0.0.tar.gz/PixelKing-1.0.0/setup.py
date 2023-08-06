47a9d79789b0f9ad0f613cf646ad060414ef866f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:49 +0000

    Add package PixelKing/1.0.0/PixelKing-1.0.0.tar.gz

---
+++ packages/PixelKing/PixelKing-1.0.0.tar.gz/PixelKing-1.0.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='PixelKing',
+	version='1.0.0',
+	author='ProgramKing_PixelSun',
+	author_email='vladgamer360@gmail.com',
+	description='Моудль для новичков',
+	packages=['PixelKing'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
