733c9e6a8e12ac8bad0c72adefeb95fa8bbd92ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:35 +0000

    Add package subdivisions/1.48.0/subdivisions-1.48.0.tar.gz

---
+++ packages/subdivisions/subdivisions-1.48.0.tar.gz/subdivisions-1.48.0/subdivisions/events_v2.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+
+class OrderEvents(Enum):
+    ORDER_PROPOSAL_REQUEST = "order_proposal_request"
+    ORDER_PROPOSAL_CALCULATED = "order_proposal_calculated"
+    ORDER_PROPOSAL_ANALYZED = "order_proposal_analyzed"
+    ORDER_PROPOSAL_AVAILABLE = "order_proposal_available"
+    ORDER_PROPOSAL_ACCEPTED = "order_proposal_accepted"
+    ORDER_SCHEDULE_REQUEST = "order_schedule_request"
+    ORDER_DISBURSEMENT_MADE = "order_disbursement_made"
+
+
+class ERPEvent(Enum):
+    NEW_ERP_DATA_AVAILABLE = "new_erp_data_available"
+
+
+class CustomerEvents(Enum):
+    REGISTERED = "registered"
+    DATA_SOURCE_CONNECTED = "data_source_connected"
+    VALIDATION_FINISHED = "validation_finished"
+
+
+class BankEvent(Enum):
+    NEW_BOLETOS_ISSUED = "new_boletos_issued"
