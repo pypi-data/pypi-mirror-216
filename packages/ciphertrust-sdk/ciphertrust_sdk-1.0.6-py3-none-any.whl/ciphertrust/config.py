257206ac4782115f77e8cd17c5539accfba0581a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:55 +0000

    Add package ciphertrust-sdk/1.0.6/ciphertrust_sdk-1.0.6-py3-none-any.whl

---
+++ packages/ciphertrust-sdk/ciphertrust_sdk-1.0.6-py3-none-any.whl/ciphertrust/config.py
@@ -0,0 +1,7 @@
+"""Configurations"""
+
+class Configs:
+    AUTH: str = "https://{}/api/v1/auth/tokens"
+    API_URL: str = "https://{}/api/v1/{}"
+    ENDPOINT_KEYS: str = "https://{}/api/vault/keys2/"
+    ENDPOINT_KEYS_QUERY: str = "https://{}/api/vault/query-keys/"
