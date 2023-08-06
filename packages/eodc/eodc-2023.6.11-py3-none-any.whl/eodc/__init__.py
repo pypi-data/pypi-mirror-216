dd5c982c3803cb82734a8576b522c838a29cb4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:42 +0000

    Add package eodc/2023.6.11/eodc-2023.6.11-py3-none-any.whl

---
+++ packages/eodc/eodc-2023.6.11-py3-none-any.whl/eodc/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "2023.6.11"
+
+import logging
+
+from eodc.settings import settings  # noqa
+
+log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.INFO, format=log_format)
