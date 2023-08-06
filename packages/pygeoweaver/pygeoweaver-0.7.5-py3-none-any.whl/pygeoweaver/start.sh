dd483f7fbf2f93a596431e1743e9a62437c08f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:41 +0000

    Add package pygeoweaver/0.7.5/pygeoweaver-0.7.5-py3-none-any.whl

---
+++ packages/pygeoweaver/pygeoweaver-0.7.5-py3-none-any.whl/pygeoweaver/start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Stop running Geoweaver if any.."
+pkill -f geoweaver.jar
+
+echo "Check Java.."
+touch ~/.bashrc && source ~/.bashrc
+
+echo "Start Geoweaver.."
+nohup java -jar ~/geoweaver.jar > ~/geoweaver.log &
+
+STATUS=0
+counter=0
+until [ $STATUS == 302 ] || [ $counter == 20 ]
+do
+    sleep 2
+    STATUS=$(curl -s -o /dev/null -w "%{http_code}\n" "http://localhost:8070/Geoweaver")
+    ((counter++))
+done
+
+cat ~/geoweaver.log
+if [ $counter == 20 ] ; then
+    echo "Error: Geoweaver is not up"
+    exit 1
+else
+    echo "Success: Geoweaver is up"
+    exit 0
+fi
