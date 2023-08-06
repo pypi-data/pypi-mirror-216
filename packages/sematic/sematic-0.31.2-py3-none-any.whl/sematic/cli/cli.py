724f3e21eab546fc1b3beda618e0051fd22915c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:28 +0000

    Add package sematic/0.31.2/sematic-0.31.2-py3-none-any.whl

---
+++ packages/sematic/sematic-0.31.2-py3-none-any.whl/sematic/cli/cli.py
@@ -0,0 +1,43 @@
+# Standard Library
+import logging
+from logging.config import dictConfig
+
+# Third-party
+import click
+
+# Sematic
+from sematic.config.config import switch_env
+from sematic.db.migrate import migrate_up
+from sematic.logs import make_log_config
+
+
+@click.group("sematic")
+@click.option("-v", "--verbose", count=True)
+def cli(verbose: int):
+    """
+    Welcome to Sematic
+
+    Start Sematic:
+        $ sematic start
+
+    Run an example:
+        $ sematic run examples/mnist/pytorch
+    """
+    if verbose == 1:
+        dictConfig(make_log_config(log_to_disk=False, level=logging.INFO))
+    elif verbose > 1:
+        dictConfig(make_log_config(log_to_disk=False, level=logging.DEBUG))
+        pass
+
+    switch_env("local")
+    migrate_up()
+
+
+@cli.group("advanced")
+def advanced():
+    """
+    Advanced operations with the CLI that are primarily useful for
+    infrastructure engineers and/or those with deep knowledge of
+    Sematic.
+    """
+    pass
