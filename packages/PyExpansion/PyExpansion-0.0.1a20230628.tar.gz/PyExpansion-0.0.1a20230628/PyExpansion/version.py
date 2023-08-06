16ca76f3188c19c5fde6ec32c4a15e9cf50ccc81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:16 +0000

    Add package PyExpansion/0.0.1a20230628/PyExpansion-0.0.1a20230628.tar.gz

---
+++ packages/PyExpansion/PyExpansion-0.0.1a20230628.tar.gz/PyExpansion-0.0.1a20230628/PyExpansion/version.py
@@ -0,0 +1,46 @@
+from PyExpansion.common import version_history
+
+
+class PyICVersionHistory(version_history.VersionHistoryBase):
+
+    def version_1(self):
+        return self.default_setup(
+            application_name="PyIC",
+            version="0.0.1a20230618",
+        )
+
+    def version_2(self):
+        return self.default_setup(
+            application_name="PyIC",
+            version="0.0.1a20230622",
+            description="Singapore added"
+        )
+
+    def version_3(self):
+        return self.default_setup(
+            application_name="PyBrainFuck",
+            version="0.0.1a20230624",
+        )
+
+    def version_4(self):
+        return self.default_setup(
+            application_name="PyMorseCode",
+            version="0.0.1a20230626",
+        )
+
+    def version_5(self):
+        return self.default_setup(
+            application_name="common",
+            version="0.0.1a20230628"
+        )
+
+
+c = PyICVersionHistory()
+test = False
+if test:
+    print("How many version:", c.total_version())
+    print("Version list:", c.get_version_list())
+    print("Latest version: ", c.get_latest_version())
+    print("Last version information: ", c.get_version_info(c.get_latest_version()))
+
+version = c.get_latest_version()
