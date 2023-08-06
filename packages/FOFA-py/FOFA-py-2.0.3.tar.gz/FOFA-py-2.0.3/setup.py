446b040e502e6f0332974352c8b9300dd8482997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:23 +0000

    Add package FOFA-py/2.0.3/FOFA-py-2.0.3.tar.gz

---
+++ packages/FOFA-py/FOFA-py-2.0.3.tar.gz/FOFA-py-2.0.3/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+DEPENDENCIES = open('requirements.txt', 'r').read().split('\n')
+README = open('./docs/README_CN.md', 'r').read()
+
+
+setup(
+    name = 'FOFA-py',
+    version = '2.0.3',
+    description = 'Python library for FOFA (https://fofa.info)',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author = 'Fofa',
+    author_email = 'fofabot@baimaohui.net',
+    url = 'https://github.com/fofapro/fofa-py',
+    packages = ['fofa'],
+    entry_points={'console_scripts': ['fofa=fofa.__main__:main']},
+    install_requires=DEPENDENCIES,
+    license = "MIT",
+    keywords=['fofa', 'security', 'network'],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        "Programming Language :: Python :: 3.11",
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
