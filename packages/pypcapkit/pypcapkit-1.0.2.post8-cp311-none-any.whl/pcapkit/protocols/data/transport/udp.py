cec03d465404ae566084ac2567bb33b26015b4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:38 +0000

    Add package pypcapkit/1.0.2.post8/pypcapkit-1.0.2.post8-cp311-none-any.whl

---
+++ packages/pypcapkit/pypcapkit-1.0.2.post8-cp311-none-any.whl/pcapkit/protocols/data/transport/udp.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""data model for UDP protocol"""
+
+from typing import TYPE_CHECKING
+
+from pcapkit.corekit.infoclass import info_final
+from pcapkit.protocols.data.data import Data
+
+__all__ = ['UDP']
+
+
+@info_final
+class UDP(Data):
+    """Data model for UDP protocol."""
+
+    #: Source port.
+    srcport: 'int'
+    #: Destination port.
+    dstport: 'int'
+    #: Length (header includes).
+    len: 'int'
+    #: Checksum.
+    checksum: 'bytes'
+
+    if TYPE_CHECKING:
+        def __init__(self, srcport: 'int', dstport: 'int', len: 'int', checksum: 'bytes') -> 'None': ...  # pylint: disable=unused-argument,super-init-not-called,multiple-statements,redefined-builtin
---
+++ packages/pypcapkit/pypcapkit-1.0.2.post8-cp311-none-any.whl/pcapkit/vendor/ftp/return_code.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+"""FTP Server Return Code
+============================
+
+.. module:: pcapkit.vendor.ftp.return_code
+
+This module contains the vendor crawler for **FTP Server Return Code**,
+which is automatically generating :class:`pcapkit.const.ftp.return_code.ReturnCode`.
+
+"""
+
+import collections
+import sys
+from typing import TYPE_CHECKING
+
+import bs4
+
+from pcapkit.vendor.default import Vendor
+
+if TYPE_CHECKING:
+    from collections import Counter
+    from typing import Callable
+
+    from bs4 import BeautifulSoup
+
+__all__ = ['ReturnCode']
+
+LINE = lambda NAME, DOCS, FLAG, ENUM, MODL: f'''\
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long,consider-using-f-string
+"""{(name := DOCS.split(' [', maxsplit=1)[0])}
+{'=' * (len(name) + 6)}
+
+.. module:: {MODL.replace('vendor', 'const')}
+
+This module contains the constant enumeration for **{name}**,
+which is automatically generated from :class:`{MODL}.{NAME}`.
+
+"""
+
+from typing import TYPE_CHECKING
+
+from aenum import IntEnum, extend_enum
+
+if TYPE_CHECKING:
+    from typing import Optional, Type
+
+__all__ = ['{NAME}']
+
+#: Response kind; whether the response is good, bad or incomplete.
+KIND = {{
+    '1': 'Positive Preliminary',
+    '2': 'Positive Completion',
+    '3': 'Positive Intermediate',
+    '4': 'Transient Negative Completion',
+    '5': 'Permanent Negative Completion',
+    '6': 'Protected',
+}}  # type: dict[str, str]
+
+#: Grouping information.
+INFO = {{
+    '0': 'Syntax',
+    '1': 'Information',
+    '2': 'Connections',
+    '3': 'Authentication and accounting',
+    '4': 'Unspecified',                     # [RFC 959]
+    '5': 'File system',
+}}  # type: dict[str, str]
+
+
+class {NAME}(IntEnum):
+    """[{NAME}] {DOCS}"""
+
+    if TYPE_CHECKING:
+        #: Description of the return code.
+        description: 'Optional[str]'
+        #: Response kind.
+        kind: 'str'
+        #: Grouping information.
+        group: 'str'
+
+    def __new__(cls, value: 'int', description: 'Optional[str]' = None) -> 'Type[{NAME}]':
+        obj = int.__new__(cls, value)
+        obj._value_ = value
+
+        code = str(value)
+        obj.description = description
+        obj.kind = KIND.get(str(value)[0], 'Reserved')
+        obj.group = INFO.get(str(value)[1], 'Reserved')
+
+        return obj
+
+    def __repr__(self) -> 'str':
+        return "<%s [%s]>" % (self.__class__.__name__, self._value_)
+
+    {ENUM}
+
+    @staticmethod
+    def get(key: 'int | str', default: 'int' = -1) -> '{NAME}':
+        """Backport support for original codes.
+
+        Args:
+            key: Key to get enum item.
+            default: Default value if not found.
+
+        :meta private:
+        """
+        if isinstance(key, int):
+            return {NAME}(key)
+        if key not in {NAME}._member_map_:  # pylint: disable=no-member
+            return extend_enum({NAME}, key, default)
+        return {NAME}[key]  # type: ignore[misc]
+
+    @classmethod
+    def _missing_(cls, value: 'int') -> '{NAME}':
+        """Lookup function used when value is not found.
+
+        Args:
+            value: Value to get enum item.
+
+        """
+        if not ({FLAG}):
+            raise ValueError('%r is not a valid %s' % (value, cls.__name__))
+        return extend_enum(cls, 'CODE_%s' % value, value)
+'''  # type: Callable[[str, str, str, str, str], str]
+
+
+class ReturnCode(Vendor):
+    """FTP Server Return Code"""
+
+    #: Value limit checker.
+    FLAG = 'isinstance(value, int) and 100 <= value <= 659'
+    #: Link to registry.
+    LINK = 'https://en.wikipedia.org/wiki/List_of_FTP_server_return_codes'
+
+    def request(self, text: 'str') -> 'BeautifulSoup':  # type: ignore[override] # pylint: disable=signature-differs
+        """Fetch registry data.
+
+        Args:
+            text: Context from :attr:`~ReturnCode.LINK`.
+
+        Returns:
+            Parsed HTML source.
+
+        """
+        return bs4.BeautifulSoup(text, 'html5lib')
+
+    def context(self, soup: 'BeautifulSoup') -> 'str':  # pylint: disable=arguments-differ,arguments-renamed
+        """Generate constant context.
+
+        Args:
+            soup: Parsed HTML source.
+
+        Returns:
+            Constant context.
+
+        """
+        enum = self.process(soup)
+        ENUM = '\n\n    '.join(map(lambda s: s.rstrip(), enum))
+        return LINE(self.NAME, self.DOCS, self.FLAG, ENUM, self.__module__)
+
+    def process(self, soup: 'BeautifulSoup') -> 'list[str]':  # type: ignore[override] # pylint: disable=arguments-differ,arguments-renamed
+        """Process registry data.
+
+        Args:
+            soup: Parsed HTML source.
+
+        Returns:
+            Enumeration fields.
+
+        """
+        table = soup.find_all('table', class_='wikitable')[2]
+        content = filter(lambda item: isinstance(item, bs4.element.Tag), table.tbody)
+        next(content)  # header
+
+        enum = []  # type: list[str]
+        for item in content:
+            line = item.find_all('td')
+
+            code = ' '.join(line[0].stripped_strings)
+            if len(code) != 3:
+                continue
+
+            #desc = f"{' '.join(line[1].stripped_strings).split('.')[0].strip()}."
+            #enum.append(f'{self.NAME}[{self.rename(desc, code)!r}] = {code}')
+
+            cmmt = '. '.join(map(lambda s: s.strip(), ' '.join(
+                line[1].stripped_strings).split('.'))).replace('e. g. ,', 'e.g.,').strip()
+            sufs = self.wrap_comment(cmmt)  # pylint: disable=line-too-long
+            pref = f"CODE_{code} = {code}, {cmmt!r}"
+
+            enum.append(f'#: {sufs}\n    {pref}')
+        return enum
+
+    def count(self, soup: 'BeautifulSoup') -> 'Counter[str]':  # pylint: disable=arguments-differ,arguments-renamed,unused-argument
+        """Count field records."""
+        #table = soup.find_all('table', class_='wikitable')[2]
+        #content = filter(lambda item: isinstance(item, bs4.element.Tag), table.tbody)
+        #next(content)  # header
+
+        #temp = list()
+        #for item in content:
+        #    line = item.find_all('td')
+
+        #    code = ' '.join(line[0].stripped_strings)
+        #    if len(code) != 3:
+        #        continue
+        #    desc = f"{' '.join(line[1].stripped_strings).split('.')[0].strip()}."
+        #    temp.append(desc)
+        #return collections.Counter(temp)
+        return collections.Counter()
+
+
+if __name__ == '__main__':
+    sys.exit(ReturnCode())  # type: ignore[arg-type]
