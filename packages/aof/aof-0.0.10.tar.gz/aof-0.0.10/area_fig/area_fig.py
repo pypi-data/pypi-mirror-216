068127b29ba81c740c7d4ba3f88f3781c46cd40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:38 +0000

    Add package aof/0.0.10/aof-0.0.10.tar.gz

---
+++ packages/aof/aof-0.0.10.tar.gz/aof-0.0.10/area_fig/area_fig.py
@@ -0,0 +1,19 @@
+def square(a):
+    a = a**2
+    p = a*4
+    print(f"Area of square is: {a} & perimeter is: {p}")
+
+def rectangle(l,b):
+    a = l*b
+    p = 2*(l+b)
+    print(f"Area of rectangle is: {a} & perimeter is: {p}")
+
+def triangle(a,b,c,height):
+    ar = (1/2)*b*height
+    p = a + b + c
+    print(f"Area of triangle is: {ar} & perimeter is: {p}")
+
+def circle(r):
+    a = 3.14*r*r
+    p = 2*3.14*r
+    print(f"Area of circle is: {a} & perimeter is: {p}")
