f6ee0b25fa9d93720f2ca2a40617e2a17e34c39b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:29 +0000

    Add package roboto-sdk/0.1.2/roboto_sdk-0.1.2.tar.gz

---
+++ packages/roboto-sdk/roboto_sdk-0.1.2.tar.gz/roboto_sdk-0.1.2/src/roboto_sdk/domain/actions/invocation_http_resources.py
@@ -0,0 +1,25 @@
+import pydantic
+
+from .invocation_record import (
+    InvocationDataSourceType,
+    InvocationSource,
+    InvocationStatus,
+)
+
+
+class CreateInvocationRequest(pydantic.BaseModel):
+    input_data: list[str]
+    data_source_id: str
+    data_source_type: InvocationDataSourceType
+    invocation_source: InvocationSource
+    invocation_source_id: str | None
+
+
+class SetLogsLocationRequest(pydantic.BaseModel):
+    bucket: str
+    prefix: str
+
+
+class UpdateInvocationStatus(pydantic.BaseModel):
+    status: InvocationStatus
+    detail: str
