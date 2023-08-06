7cbf63c0c375ff19a8b355ca073045bcb5bd7cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:27 +0000

    Add package yggdrasil-framework/1.10.1.post1/yggdrasil_framework-1.10.1.post1-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/yggdrasil-framework/yggdrasil_framework-1.10.1.post1-cp37-cp37m-macosx_10_9_x86_64.whl/yggdrasil/__init__.py
@@ -0,0 +1,58 @@
+r"""This package provides a framework for integrating models across languages
+such that they can be run simultaneously, passing input back and forth."""
+import os
+import sys
+import shutil
+from ._version import get_versions
+from yggdrasil import platform, config
+from yggdrasil.runner import YggFunction
+_test_package_name = None
+_test_package = None
+config.cfg_logging()
+
+
+if platform._is_win:  # pragma: windows
+    # This is required to fix crash on Windows in case of Ctrl+C
+    # https://github.com/ContinuumIO/anaconda-issues/issues/905#issuecomment-232498034
+    os.environ['FOR_DISABLE_CONSOLE_CTRL_HANDLER'] = 'T'
+
+
+if not os.path.isfile(config.usr_config_file):  # pragma: config
+    in_install = any(x.endswith(('ygginstall.exe', 'install'))
+                     for x in sys.argv)
+    in_config = any(x.endswith(('yggconfig.exe', 'config'))
+                    for x in sys.argv)
+    if not in_install:
+        from yggdrasil.languages import install_languages
+        shutil.copy(config.def_config_file, config.usr_config_file)
+        install_languages.install_all_languages(from_setup=True)
+        if not in_config:
+            # Don't configure if that is going to happen anyway
+            config.update_language_config(verbose=True)
+
+
+def import_as_function(model_yaml, service_address=None, **kwargs):
+    r"""Import a model as a function from a yaml specification file.
+
+    Args:
+        model_yaml (str, list): Full path to one or more YAML specification
+            files containing information defining a partial integration. If
+            service_address is set, this should be the name of a service
+            registered with the service manager running at the provided
+            address.
+        service_address (str, optional): Address for service manager that is
+            capable of running the specified integration. Defaults to None
+            and is ignored.
+        **kwargs: Additional keyword arguments are passed to the
+            YggFunction constructor.
+
+    Returns:
+        YggFunction: Callable wrapper for model.
+
+    """
+    return YggFunction(model_yaml, service_address=service_address, **kwargs)
+
+
+__all__ = []
+__version__ = get_versions()['version']
+del get_versions
