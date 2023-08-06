b9435fc3ebe307c645be06209e5b2cc5c2e9b9d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:41 +0000

    Add package st-ant-statistic/0.0.4/st_ant_statistic-0.0.4-py3-none-any.whl

---
+++ packages/st-ant-statistic/st_ant_statistic-0.0.4-py3-none-any.whl/st_ant_statistic/frontend/build/precache-manifest.839d297490f0d5a02ec070a21f930d89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "174ba6846704c27170ab195f804ddf22",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b1190b877e96173995f7",
+    "url": "./static/js/2.7c06afa9.chunk.js"
+  },
+  {
+    "revision": "33d89f657bc3a602ef5b2b89d4b772a3",
+    "url": "./static/js/2.7c06afa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ea35fc20efacb5ebdd",
+    "url": "./static/js/main.4af894cf.chunk.js"
+  },
+  {
+    "revision": "389c2abfab6a66f5a473",
+    "url": "./static/js/runtime-main.502c8693.js"
+  }
+]);
