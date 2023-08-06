9eeac8b3f0eb746a95c0edd2dbfcb529e6153341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:33 +0000

    Add package pagarme-python/5.7.6/pagarme-python-5.7.6.tar.gz

---
+++ packages/pagarme-python/pagarme-python-5.7.6.tar.gz/pagarme-python-5.7.6/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info[0] < 3:
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+else:
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+
+setup(
+    name='pagarme-python',
+    version='5.7.6',
+    description='Pagarme API',
+     
+    long_description_content_type="text/markdown",
+     author='Pagarme',
+    author_email='support@pagar.me',
+    url='https://github.com/pagarme/pagarme-core-api-python',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.9.1, <3.0',
+        'jsonpickle>=0.7.1, <1.0',
+        'cachecontrol>=0.11.7, <1.0',
+        'python-dateutil>=2.5.3, <3.0'
+    ],
+    tests_require=[
+        'nose>=1.3.7'
+    ],
+    test_suite = 'nose.collector'
+)
