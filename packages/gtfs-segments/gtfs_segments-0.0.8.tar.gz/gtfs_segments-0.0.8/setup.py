c30bbe64527966782341cb272f7dde8fd26ef6ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:02 +0000

    Add package gtfs-segments/0.0.8/gtfs_segments-0.0.8.tar.gz

---
+++ packages/gtfs-segments/gtfs_segments-0.0.8.tar.gz/gtfs_segments-0.0.8/setup.py
@@ -0,0 +1,45 @@
+import os
+from setuptools import setup, find_packages
+
+__version__ = '0.0.8' 
+DESCRIPTION = 'GTFS segments'
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+    
+REQUIREMENTS = ['geopandas',
+                'scipy',
+                'partridge',
+                'shapely',
+                'numpy',
+                'pandas',
+                'matplotlib',
+                'utm',
+                'contextily',
+                'requests']
+
+os.system('pip install -e git+https://github.com/praneethd7/partridge.git@fix_geopandas_projection#egg=partridge')
+
+# Setting up
+setup(
+        name="gtfs_segments", 
+        version=__version__,
+        author="Saipraneeth Devunuri",
+        author_email="<sd37@illinois.edu>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type='text/markdown',
+        packages=find_packages(),
+        install_requires=REQUIREMENTS, # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        url = "https://github.com/UTEL-UIUC/gtfs_segments",
+        keywords=['python', 'gtfs', 'geodata'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+            "License :: OSI Approved :: MIT License",
+        ]
+)
