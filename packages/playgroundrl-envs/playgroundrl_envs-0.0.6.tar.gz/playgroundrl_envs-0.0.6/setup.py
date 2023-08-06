114c7038b42169d4a090bc5e6b42cdff5b04a660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:38 +0000

    Add package playgroundrl-envs/0.0.6/playgroundrl_envs-0.0.6.tar.gz

---
+++ packages/playgroundrl-envs/playgroundrl_envs-0.0.6.tar.gz/playgroundrl_envs-0.0.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='playgroundrl_envs',
+   version='0.0.6',
+   author='Rayan Krishnan',
+   # TODO: figure out systematic way to do this
+   packages=['playgroundrl_envs', 'playgroundrl_envs.games', 'playgroundrl_envs.games.go', 'playgroundrl_envs.games.go.engine', 'playgroundrl_envs.games.codenames', 'playgroundrl_envs.games.catan'],
+   package_dir={'':'src'},
+   scripts=[],
+   url='http://pypi.python.org/pypi/playgroundrl_envs/',
+   description='The environments hosted on Playground RL',
+   # long_description=open('README.txt').read(),
+   install_requires=[
+        'attrs==22.2.0',
+        'cattrs==22.2.0',
+   ],
+)
