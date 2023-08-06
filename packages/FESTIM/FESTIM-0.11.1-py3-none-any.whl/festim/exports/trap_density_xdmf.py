862d69deb5a2882d197dd53e9e2584d901ee6e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:34 +0000

    Add package FESTIM/0.11.1/FESTIM-0.11.1-py3-none-any.whl

---
+++ packages/FESTIM/FESTIM-0.11.1-py3-none-any.whl/festim/exports/trap_density_xdmf.py
@@ -0,0 +1,28 @@
+from festim.exports.xdmf_export import XDMFExport
+import fenics as f
+
+
+class TrapDensityXDMF(XDMFExport):
+    """
+    Args:
+        trap (festim.Trap): the trap to export density
+        kwargs (): See XDMFExport
+    """
+
+    def __init__(self, trap, **kwargs) -> None:
+        super().__init__(
+            field="1", **kwargs
+        )  # field is "1" just to make the code not crash
+
+        self.trap = trap
+
+    def write(self, t):
+        """Writes to file
+
+        Args:
+            t (float): the time
+        """
+        functionspace = self.function.function_space().collapse()
+        density_as_function = f.project(self.trap.density[0], functionspace)
+        self.function = density_as_function
+        super().write(t)
