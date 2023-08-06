678690f81cbf16261d2b851425cbb48b9ad737e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:36 +0000

    Add package arelle-release/2.11.4/arelle-release-2.11.4.tar.gz

---
+++ packages/arelle-release/arelle-release-2.11.4.tar.gz/arelle-release-2.11.4/testParser.py
@@ -0,0 +1,9 @@
+'''
+Use this module to start Arelle in windowing interactive UI mode
+
+See COPYRIGHT.md for copyright information.
+'''
+
+from arelle.formula.XPathParser import parser_unit_test
+
+parser_unit_test()
---
+++ packages/arelle-release/arelle-release-2.11.4.tar.gz/arelle-release-2.11.4/testParser2.py
@@ -0,0 +1,9 @@
+'''
+Use this module to start Arelle in windowing interactive UI mode
+
+See COPYRIGHT.md for copyright information.
+'''
+
+from arelle.formula.XPathParser import main
+
+main()
