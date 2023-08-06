fd7ae95fcba6a360a4d0738d7ec5816dfbade632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:39 +0000

    Add package gustaf/0.0.12/gustaf-0.0.12.tar.gz

---
+++ packages/gustaf/gustaf-0.0.12.tar.gz/gustaf-0.0.12/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+with open("gustaf/_version.py") as f:
+    version = eval(f.read().strip().split("=")[-1])
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name="gustaf",
+    version=version,
+    description="Process and visualize numerical-analysis-geometries.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Jaewook Lee",
+    author_email="jaewooklee042@gmail.com",
+    url="https://github.com/tataratat/gustaf",
+    packages=[
+        "gustaf",
+        "gustaf.utils",
+        "gustaf.io",
+        "gustaf.create",
+        "gustaf.helpers",
+    ],
+    install_requires=["numpy"],
+    extras_require={
+        "all": ["vedo>=2023.4.3", "scipy", "meshio", "napf>=0.0.5", "funi"]
+    },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering",
+    ],
+    license="MIT",
+)
