95c9998ec2931507c60b7f6089417650fc858b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:02 +0000

    Add package st-ant-statistic/0.0.6/st_ant_statistic-0.0.6-py3-none-any.whl

---
+++ packages/st-ant-statistic/st_ant_statistic-0.0.6-py3-none-any.whl/st_ant_statistic/frontend/build/precache-manifest.8c81e172ea72d8aee8672be482b6bbff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "291f1be21e08ee6ea47813ad0013ef4c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b1190b877e96173995f7",
+    "url": "./static/js/2.7c06afa9.chunk.js"
+  },
+  {
+    "revision": "33d89f657bc3a602ef5b2b89d4b772a3",
+    "url": "./static/js/2.7c06afa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b144b672b50b7c4abf45",
+    "url": "./static/js/main.f525c3c1.chunk.js"
+  },
+  {
+    "revision": "389c2abfab6a66f5a473",
+    "url": "./static/js/runtime-main.502c8693.js"
+  }
+]);
---
+++ packages/st-ant-statistic/st_ant_statistic-0.0.6-py3-none-any.whl/st_ant_statistic/frontend/build/precache-manifest.d4ee4a52545b54ba53a566b22654aed2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "766db2769e943e2f1341b951e52f5a55",
+    "url": "./index.html"
+  },
+  {
+    "revision": "65013f97d607fc04d5b4",
+    "url": "./static/js/2.88a1d1f2.chunk.js"
+  },
+  {
+    "revision": "33d89f657bc3a602ef5b2b89d4b772a3",
+    "url": "./static/js/2.88a1d1f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d71e9a3ef79e606eb75",
+    "url": "./static/js/main.d1199d9b.chunk.js"
+  },
+  {
+    "revision": "389c2abfab6a66f5a473",
+    "url": "./static/js/runtime-main.502c8693.js"
+  }
+]);
