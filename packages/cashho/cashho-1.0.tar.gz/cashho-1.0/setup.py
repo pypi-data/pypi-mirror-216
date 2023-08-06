ebb8e7c52207176340653d58ec59b967c71b008e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:31 +0000

    Add package cashho/1.0/cashho-1.0.tar.gz

---
+++ packages/cashho/cashho-1.0.tar.gz/cashho-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='cashho',
+    version='1.0',
+    packages=['src'],
+    install_requires=[
+        'Django>=3.2',
+        # add other dependencies here
+    ],
+)
