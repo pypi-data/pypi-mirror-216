d5e952fdf2d7d47b704d8c6c6c0f731e14823f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:35 +0000

    Add package dql-alpha/0.29.0/dql_alpha-0.29.0-py3-none-any.whl

---
+++ packages/dql-alpha/dql_alpha-0.29.0-py3-none-any.whl/dql/error.py
@@ -0,0 +1,18 @@
+class DQLError(RuntimeError):
+    pass
+
+
+class NotFoundError(Exception):
+    pass
+
+
+class DatasetNotFoundError(NotFoundError):
+    pass
+
+
+class StorageNotFoundError(NotFoundError):
+    pass
+
+
+class PendingIndexingError(Exception):
+    """An indexing operation is already in progress."""
