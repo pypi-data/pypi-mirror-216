9633ec2a0734bcd67c99769b0ce8adb8ec335aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:17 +0000

    Add package flask-rest-jsonapi-next/0.34.6/flask_rest_jsonapi_next-0.34.6-py3-none-any.whl

---
+++ packages/flask-rest-jsonapi-next/flask_rest_jsonapi_next-0.34.6-py3-none-any.whl/flask_rest_jsonapi_next/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# isort: skip_file
+# fmt: off
+
+__version__ = "0.34.6"
+
+from .api import Api
+from .resource import ResourceList, ResourceDetail, ResourceRelationship
+from .exceptions import JsonApiException
+from .data_layers.alchemy import SqlalchemyDataLayer
+from .error_responses import (
+    ErrorsAsJsonApi,
+    error_response,
+    ExceptionConverter,
+)
+
+# fmt: on
