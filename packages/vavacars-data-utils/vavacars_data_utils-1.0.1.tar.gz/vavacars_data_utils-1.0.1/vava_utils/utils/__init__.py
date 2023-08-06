00734224410bd6db3fc4750f3838ffc14503f35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:11 +0000

    Add package vavacars-data-utils/1.0.1/vavacars_data_utils-1.0.1.tar.gz

---
+++ packages/vavacars-data-utils/vavacars_data_utils-1.0.1.tar.gz/vavacars_data_utils-1.0.1/vava_utils/utils/__init__.py
@@ -0,0 +1,7 @@
+from .comparisons import get_best_match, deep_get
+from .callables import return_on_failure
+from .strings import extract_json_objects
+from .singleton import singleton
+from .naming import *
+
+__all__ = ["get_best_match", "return_on_failure", "extract_json_objects", "deep_get", "singleton"]
