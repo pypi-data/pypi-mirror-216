d2b7fc97569235b8f620c64fc434fd84f6809585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:52 +0000

    Add package orbit-component-base/1.0.18/orbit_component_base-1.0.18-py3-none-any.whl

---
+++ packages/orbit-component-base/orbit_component_base-1.0.18-py3-none-any.whl/orbit_component_base/cli/permissions.py
@@ -0,0 +1,54 @@
+from orbit_component_base.src.orbit_shared import world
+from orbit_component_base.schema.OrbitPermissions import PermissionsCollection, PermissionsTable
+from orbit_component_base.cli.cli_base import CliBase
+from rich.console import Console
+from rich.table import Table
+from orbit_database import Doc
+from orbit_database.exceptions import DuplicateKey
+
+
+class Permissions (CliBase):
+
+    COLLECTIONS = [PermissionsCollection]
+
+    def list (self):
+        table = Table(
+            title='Current Permissions',
+            title_style='bold green',
+            caption=f'authentication="{world.conf.authentication}"',
+            caption_style='bold magenta'
+        )
+        rstyle = 'cyan'
+        hstyle = 'deep_sky_blue4'
+        table.add_column('Id',         style=rstyle, header_style=hstyle, no_wrap=True)
+        table.add_column('Host Id',    style=rstyle, header_style=hstyle, no_wrap=True)
+        table.add_column('Namespace',  style=rstyle, header_style=hstyle, no_wrap=True)
+        table.add_column('Name',       style=rstyle, header_style=hstyle, no_wrap=True)
+        table.add_column('Executable', style=rstyle, header_style=hstyle, no_wrap=True)
+        for result in PermissionsCollection():
+            doc = result.doc
+            table.add_row(
+                doc.key,
+                doc._host_id,
+                doc._namespace,
+                doc._name,
+                doc._exec)
+                
+        Console().print(table)
+        print()
+        
+    def add (self, hostid, namespace, name, exec):
+        try:
+            PermissionsTable().from_doc(Doc({'host_id': hostid, 'namespace': namespace, 'name': name, 'exec': exec})).append()
+            print(f'Added permission: for host "{hostid}", namespace "{namespace}" endpoint "{name}" exec "{exec}"')
+        except DuplicateKey:
+            print(f'Error: permission already exists!')
+
+    def delete (self, hostid, namespace, name, exec):
+        doc = PermissionsTable().from_request(hostid, namespace,name, exec)
+        if doc.isValid:
+            print(f'Removed permission: for host "{hostid}", namespace "{namespace}" endpoint "{name}" exec "{exec}"')
+            doc.delete()
+        else:
+            print(f'Permission does not exist for host "{hostid}", namespace "{namespace}" endpoint "{name}" exec "{exec}"')
+
