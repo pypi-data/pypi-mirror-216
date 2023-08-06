9f43f04d24378a96b49c329b55d96ca865713304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:06 +0000

    Add package wagtail-site-inheritance/1.1.0/wagtail_site_inheritance-1.1.0-py2.py3-none-any.whl

---
+++ packages/wagtail-site-inheritance/wagtail_site_inheritance-1.1.0-py2.py3-none-any.whl/wagtail_site_inheritance/__init__.py
@@ -0,0 +1,14 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+__version__ = "1.1.0"
+
+
+class WagtailSiteInheritanceAppConfig(AppConfig):
+    name = "wagtail_site_inheritance"
+    verbose_name = _("Wagtail Site Inheritance")
+
+    def ready(self):
+        from wagtail_site_inheritance.receivers import register_handlers  # noqa
+
+        register_handlers()
