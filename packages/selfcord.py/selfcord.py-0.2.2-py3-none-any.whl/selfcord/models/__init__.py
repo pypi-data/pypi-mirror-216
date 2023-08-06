ed4aa93337214b9b52cd96a2220a06ff059b8ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:23 +0000

    Add package selfcord.py/0.2.2/selfcord.py-0.2.2-py3-none-any.whl

---
+++ packages/selfcord.py/selfcord.py-0.2.2-py3-none-any.whl/selfcord/models/__init__.py
@@ -0,0 +1,14 @@
+from .channel import (Category, DMChannel, GroupChannel, Messageable,
+                      TextChannel, Voiceable, VoiceChannel)
+from .client import Client
+from .emoji import Emoji
+from .guild import Guild
+from .interactions import InteractionUtil, Option, Search, SlashCommand
+from .member import Member
+from .message import (Action_Row, Button, Message, Select_Menu, Select_Option,
+                      Text_Input)
+from .permission import Permission
+from .role import Role
+from .sessions import Event_Session, Session
+from .user import Connected_Account, Profile, User
+from .webhook import Webhook
