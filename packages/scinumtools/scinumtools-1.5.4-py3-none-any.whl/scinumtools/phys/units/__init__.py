b80d09ab75f372d7b62cfe8c69f3337740bd8e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:08 +0000

    Add package scinumtools/1.5.4/scinumtools-1.5.4-py3-none-any.whl

---
+++ packages/scinumtools/scinumtools-1.5.4-py3-none-any.whl/scinumtools/phys/units/__init__.py
@@ -0,0 +1,5 @@
+from .QuantityClass import Quantity
+from .UnitClass import Unit, Constant
+from .DimensionsClass import Dimensions
+from .RatioClass import Ratio
+from .BaseUnitsClass import BaseUnits
