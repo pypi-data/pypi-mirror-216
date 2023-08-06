4930848811950597f98a06bd0d79367676b961b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:55 +0000

    Add package platypush/0.50.1/platypush-0.50.1.tar.gz

---
+++ packages/platypush/platypush-0.50.1.tar.gz/platypush-0.50.1/platypush/backend/http/app/routes/execute.py
@@ -0,0 +1,41 @@
+import json
+
+from flask import Blueprint, abort, request
+from flask.wrappers import Response
+
+from platypush.backend.http.app import template_folder
+from platypush.backend.http.app.utils import authenticate, logger, send_message
+
+execute = Blueprint('execute', __name__, template_folder=template_folder)
+
+# Declare routes list
+__routes__ = [
+    execute,
+]
+
+
+@execute.route('/execute', methods=['POST'])
+@authenticate(json=True)
+def execute_route():
+    """Endpoint to execute commands"""
+    try:
+        msg = json.loads(request.data.decode('utf-8'))
+    except Exception as e:
+        logger().error(
+            'Unable to parse JSON from request {}: {}'.format(request.data, str(e))
+        )
+        abort(400, str(e))
+
+    logger().info('Received message on the HTTP backend: {}'.format(msg))
+
+    try:
+        response = send_message(msg)
+        return Response(str(response or {}), mimetype='application/json')
+    except Exception as e:
+        logger().error(
+            'Error while running HTTP action: {}. Request: {}'.format(str(e), msg)
+        )
+        abort(500, str(e))
+
+
+# vim:sw=4:ts=4:et:
