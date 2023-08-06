ded9243696984ac2eb122f55f5ccd9c018a5b64c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:37 +0000

    Add package megamock/0.1.0b7/megamock-0.1.0b7.tar.gz

---
+++ packages/megamock/megamock-0.1.0b7.tar.gz/megamock-0.1.0b7/megamock/plugins/pytest.py
@@ -0,0 +1,37 @@
+from typing import Any
+import pytest
+
+from megamock.megapatches import MegaPatch
+
+
+def pytest_load_initial_conftests(*args, **kwargs) -> None:
+    import megamock
+
+    megamock.start_import_mod()
+
+
+def pytest_addoption(parser: Any) -> None:
+    parser.addoption(
+        "--do_not_autostop_megapatches",
+        help="Disable autostopping MegaPatches after every test",
+        action="store",
+        default=False,
+    )
+
+
+@pytest.fixture(autouse=True)
+def stop_all_megapatches(request) -> None:
+    if not request.config.getoption("--do_not_autostop_megapatches"):
+        MegaPatch.stop_all()
+
+
+# swap out default mocker if pytest-mock is installed
+try:
+    from pytest_mock import MockerFixture  # type: ignore  # noqa  # test for install
+
+    @pytest.fixture(autouse=True, scope="session")
+    def use_pytest_mocker(session_mocker: MockerFixture) -> None:
+        MegaPatch.default_mocker = session_mocker
+
+except ImportError:
+    pass  # not installed
