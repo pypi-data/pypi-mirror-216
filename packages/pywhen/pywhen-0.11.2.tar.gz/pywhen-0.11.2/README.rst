88ca2664219865e7af272c28b21c38720f26f0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:20 +0000

    Add package pywhen/0.11.2/pywhen-0.11.2.tar.gz

---
+++ packages/pywhen/pywhen-0.11.2.tar.gz/pywhen-0.11.2/README.rst
@@ -0,0 +1,28 @@
+when
+****
+
+A wrapper library for date/time conversion.  Takes many of the standard ISO
+date formats as strings and creates an internally consistent datetime object.
+
+Installation
+============
+
+.. code-block:: bash
+
+    $ pip install pywhen
+
+Supports
+========
+
+pywhen has been tested with Python 3.8-3.11
+
+Previously it was tested with Python 2.7, 3.6-3.7. Not much has changed since
+then and it should still work, but it isn't part of the automatic tests
+anymore.
+
+Docs & Source
+=============
+
+Docs: http://pywhen.readthedocs.io/en/latest/
+
+Source: https://github.com/cltrudeau/pywhen
---
+++ packages/pywhen/pywhen-0.11.2.tar.gz/pywhen-0.11.2/setup.py
@@ -0,0 +1,42 @@
+import os, sys
+
+from when import __version__
+
+readme = os.path.join(os.path.dirname(__file__), 'README.rst')
+long_description = open(readme).read()
+
+SETUP_ARGS = dict(
+    name='pywhen',
+    version=__version__,
+    description=('Collection of random python tools and utilities '),
+    long_description=long_description,
+    url='https://github.com/cltrudeau/wrench',
+    author='Christopher Trudeau',
+    author_email='ctrudeau+pypi@arsensa.com',
+    license='MIT',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='tools',
+    test_suite='load_tests.get_suite',
+    py_modules = ['when',],
+    install_requires=[],
+    tests_require=[
+        'waelstow==0.10.2',
+    ],
+)
+
+if __name__ == '__main__':
+    from setuptools import setup
+    setup(**SETUP_ARGS)
