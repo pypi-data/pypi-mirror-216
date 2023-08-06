f9cbb0866237942767e68c59f6a0fb6aa8e5a2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:02 +0000

    Add package how-fake-accounts/0.3.0/how_fake_accounts-0.3.0.tar.gz

---
+++ packages/how-fake-accounts/how_fake_accounts-0.3.0.tar.gz/how_fake_accounts-0.3.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='how_fake_accounts',
+    version='0.3.0',
+    author='João Nogueira',
+    packages=['how_fake_accounts'],
+    long_description=open('README.md', 'r+').read(),
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    install_requires=['faker'],
+    entry_points={
+        'console_scripts': [
+            'how_fake_accounts = how_fake_accounts.__main__:main'
+        ]
+    }
+)
