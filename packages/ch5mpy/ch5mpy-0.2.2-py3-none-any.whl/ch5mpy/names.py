bcde2903570305b2f41c748e40ee336dcf5f3b42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:11 +0000

    Add package ch5mpy/0.2.2/ch5mpy-0.2.2-py3-none-any.whl

---
+++ packages/ch5mpy/ch5mpy-0.2.2-py3-none-any.whl/ch5mpy/names.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+# Created on 16/01/2023 11:09
+# Author : matteo
+
+# ====================================================
+# imports
+from __future__ import annotations
+
+from enum import Enum
+
+
+# ====================================================
+# code
+class H5Mode(str, Enum):
+    READ = "r"  # Readonly, file must exist
+    READ_WRITE = "r+"  # Read/write, file must exist
+    WRITE_TRUNCATE = "w"  # Create file, truncate if exists
+    WRITE = "w-"  # Create file, fail if exists
+    READ_WRITE_CREATE = "a"  # Read/write if exists, create otherwise
+
+    @staticmethod
+    def has_write_intent(mode: H5Mode) -> bool:
+        return mode in (H5Mode.READ_WRITE, H5Mode.READ_WRITE_CREATE, H5Mode.WRITE, H5Mode.WRITE_TRUNCATE)
