0c988672b6023fb15c740ead9542faa6aa4c699d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:44 +0000

    Add package singularity-a-distributions/1.0/singularity_a_distributions-1.0.tar.gz

---
+++ packages/singularity-a-distributions/singularity_a_distributions-1.0.tar.gz/singularity_a_distributions-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='singularity_a_distributions',
+      version='1.0',
+
+      description='Gaussian distributions',
+
+      packages=['singularity_a_distributions'],
+
+      author= 'Abdullah Hammami',
+
+      zip_safe=False)
