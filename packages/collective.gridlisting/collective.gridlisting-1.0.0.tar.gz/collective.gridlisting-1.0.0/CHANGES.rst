b4b13bbb3ba611b5c00b268e5d1f59b57c9eab6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:17 +0000

    Add package collective.gridlisting/1.0.0/collective.gridlisting-1.0.0.tar.gz

---
+++ packages/collective.gridlisting/collective.gridlisting-1.0.0.tar.gz/collective.gridlisting-1.0.0/CHANGES.rst
@@ -0,0 +1,9 @@
+Changelog
+=========
+
+
+1.0.0 (2023-06-28)
+------------------
+
+- Initial release. See REAMDE.rst
+  [petschki]
---
+++ packages/collective.gridlisting/collective.gridlisting-1.0.0.tar.gz/collective.gridlisting-1.0.0/src/collective/gridlisting/browser/views.py
@@ -0,0 +1,10 @@
+from plone.app.contenttypes.browser.collection import CollectionView
+from plone.app.contenttypes.browser.folder import FolderView
+
+
+class FolderGridListing(FolderView):
+    pass
+
+
+class CollectionGridListing(CollectionView):
+    pass
