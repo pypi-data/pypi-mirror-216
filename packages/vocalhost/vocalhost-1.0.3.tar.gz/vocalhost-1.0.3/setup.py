a3d4b3b2df27e2b57e925f7339d82dd29efbc0ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:57 +0000

    Add package vocalhost/1.0.3/vocalhost-1.0.3.tar.gz

---
+++ packages/vocalhost/vocalhost-1.0.3.tar.gz/vocalhost-1.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='vocalhost',
+    version='1.0.3',
+    py_modules=['vocalhost'],
+    install_requires=[
+        'websockets',
+        'certifi'
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
