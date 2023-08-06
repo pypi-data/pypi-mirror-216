b029da7f8ddedab95bcd4285fe98f98c931d53ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:05 +0000

    Add package noyerTransformer/0.1.0/noyerTransformer-0.1.0.tar.gz

---
+++ packages/noyerTransformer/noyerTransformer-0.1.0.tar.gz/noyerTransformer-0.1.0/noyerTransformer/TexSoup/tex.py
@@ -0,0 +1,22 @@
+from .reader import read_expr, read_tex
+from .data import *
+from .utils import *
+from .tokens import tokenize
+from .category import categorize
+import itertools
+
+
+def read(tex, skip_envs=(), tolerance=0):
+    """Read and parse all LaTeX source.
+
+    :param Union[str,iterable] tex: LaTeX source
+    :param Union[str] skip_envs: names of environments to skip parsing
+    :param int tolerance: error tolerance level (only supports 0 or 1)
+    :return TexEnv: the global environment
+    """
+    if not isinstance(tex, str):
+        tex = ''.join(itertools.chain(*tex))
+    buf = categorize(tex)
+    buf = tokenize(buf)
+    buf = read_tex(buf, skip_envs=skip_envs, tolerance=tolerance)
+    return TexEnv('[tex]', begin='', end='', contents=buf), tex
