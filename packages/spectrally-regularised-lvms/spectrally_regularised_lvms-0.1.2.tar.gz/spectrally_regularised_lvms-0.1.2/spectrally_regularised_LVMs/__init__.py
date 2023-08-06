76f88b6384c87f20c096966edb629c1821babade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:28 +0000

    Add package spectrally-regularised-lvms/0.1.2/spectrally_regularised_lvms-0.1.2.tar.gz

---
+++ packages/spectrally-regularised-lvms/spectrally_regularised_lvms-0.1.2.tar.gz/spectrally_regularised_lvms-0.1.2/spectrally_regularised_LVMs/__init__.py
@@ -0,0 +1,40 @@
+# Copyright 2023-present Ryan Balshaw
+"""Spectrally-regularised-LVMs. Train linear LVMs with the addition
+ of a spectral regularisation term with minimal effort."""
+from .cost_functions import NegentropyCost, SympyCost, UserCost, VarianceCost
+from .helper_methods import (
+    BatchSampler,
+    DataProcessor,
+    DeflationOrthogonalisation,
+    QuasiNewton,
+    hankel_matrix,
+)
+from .negen_approx import CubeObject, ExpObject, LogcoshObject, QuadObject
+from .spectral_regulariser import SpectralObjective
+from .spectrally_regularised_model import LinearModel
+
+__author__ = "Ryan Balshaw"
+__version__ = "0.1.2"
+__email__ = "ryanbalshaw81@gmail.com"
+__description__ = (
+    "Train linear LVMs with the addition "
+    "of a spectral regularisation term with minimal effort."
+)
+__uri__ = "http://spectrally-regularised-lvms.readthedocs.io/"
+__all__ = [
+    "SpectralObjective",
+    "NegentropyCost",
+    "SympyCost",
+    "UserCost",
+    "VarianceCost",
+    "hankel_matrix",
+    "LinearModel",
+    "BatchSampler",
+    "DataProcessor",
+    "DeflationOrthogonalisation",
+    "QuasiNewton",
+    "CubeObject",
+    "ExpObject",
+    "LogcoshObject",
+    "QuadObject",
+]
