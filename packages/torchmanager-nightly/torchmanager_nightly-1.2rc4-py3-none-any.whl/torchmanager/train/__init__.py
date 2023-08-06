8e5454f6c263a61ff47c2a13a3e25f605e1973cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:35 +0000

    Add package torchmanager-nightly/1.2rc4/torchmanager_nightly-1.2rc4-py3-none-any.whl

---
+++ packages/torchmanager-nightly/torchmanager_nightly-1.2rc4-py3-none-any.whl/torchmanager/train/__init__.py
@@ -0,0 +1,2 @@
+from .checkpoint import Checkpoint, list_checkpoints, load
+from .learning_rate import update_lr
---
+++ packages/torchmanager-nightly/torchmanager_nightly-1.2rc4-py3-none-any.whl/torchmanager_core/__init__.py
@@ -0,0 +1,13 @@
+import abc, argparse, gc, math, os, shutil, sys, torch
+from torch.nn import functional
+from torch.utils import data
+
+from . import checkpoint, devices, errors, random, view
+from .errors import _raise
+from .version import Version, deprecated, API as API_VERSION, CURRENT as VERSION, DESCRIPTION
+
+try:
+    from torch.utils import tensorboard
+except ImportError:
+    view.warnings.warn("Module tensorboard is not installed.", ImportWarning)
+    tensorboard = NotImplemented
