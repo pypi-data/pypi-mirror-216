673549821076b8bb617fde01bc594c1d04745808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:39 +0000

    Add package solidwrap/1.0.1/solidwrap-1.0.1.tar.gz

---
+++ packages/solidwrap/solidwrap-1.0.1.tar.gz/solidwrap-1.0.1/solidwrap/__init__.py
@@ -0,0 +1,14 @@
+"""
+SolidWrap
+=========
+
+Copyright (c) 2023 Sean Yeatts. All rights reserved.
+
+This module wraps the SolidWorks and PDM APIs for a streamlined, Pythonic workflow.
+"""
+
+__title__       = "SolidWrap"
+__author__      = "Sean Yeatts"
+__copyright__   = "Copyright (c) 2023 Sean Yeatts. All rights reserved."
+
+from .solidwrap import *
