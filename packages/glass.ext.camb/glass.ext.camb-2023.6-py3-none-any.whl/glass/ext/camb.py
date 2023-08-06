2e7a1d4caeb4ccb8ed78ced0c63f5321d91b0f21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:46 +0000

    Add package glass.ext.camb/2023.6/glass.ext.camb-2023.6-py3-none-any.whl

---
+++ packages/glass.ext.camb/glass.ext.camb-2023.6-py3-none-any.whl/glass/ext/camb.py
@@ -0,0 +1,59 @@
+# author: Nicolas Tessore <n.tessore@ucl.ac.uk>
+# license: MIT
+'''GLASS module for CAMB interoperability'''
+
+import warnings
+import numpy as np
+import camb
+
+
+def camb_tophat_weight(z):
+    '''Weight function for tophat window functions and CAMB.
+
+    This weight function linearly ramps up the redshift at low values,
+    from :math:`w(z = 0) = 0` to :math:`w(z = 0.1) = 1`.
+
+    '''
+    return np.clip(z/0.1, None, 1.)
+
+
+def matter_cls(pars, lmax, ws, *, limber=False, limber_lmin=100):
+    '''Compute angular matter power spectra using CAMB.'''
+
+    # make a copy of input parameters so we can set the things we need
+    pars = pars.copy()
+
+    # set up parameters for angular power spectra
+    pars.WantTransfer = False
+    pars.WantCls = True
+    pars.Want_CMB = False
+    pars.min_l = 1
+    pars.set_for_lmax(lmax)
+
+    # set up parameters to only compute the intrinsic matter cls
+    pars.SourceTerms.limber_windows = limber
+    pars.SourceTerms.limber_phi_lmin = limber_lmin
+    pars.SourceTerms.counts_density = True
+    pars.SourceTerms.counts_redshift = False
+    pars.SourceTerms.counts_lensing = False
+    pars.SourceTerms.counts_velocity = False
+    pars.SourceTerms.counts_radial = False
+    pars.SourceTerms.counts_timedelay = False
+    pars.SourceTerms.counts_ISW = False
+    pars.SourceTerms.counts_potential = False
+    pars.SourceTerms.counts_evolve = False
+
+    sources = []
+    for za, wa, _ in ws:
+        s = camb.sources.SplinedSourceWindow(z=za, W=wa)
+        sources.append(s)
+    pars.SourceWindows = sources
+
+    n = len(sources)
+    cls = camb.get_results(pars).get_source_cls_dict(lmax=lmax, raw_cl=True)
+
+    for i in range(1, n+1):
+        if np.any(cls[f'W{i}xW{i}'] < 0):
+            warnings.warn('negative auto-correlation in shell {i}; improve accuracy?')
+
+    return [cls[f'W{i}xW{j}'] for i in range(1, n+1) for j in range(i, 0, -1)]
