7c45d79e0f057e8d262073218bd49e57e81a652b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:22 +0000

    Add package tkadw/0.2.3/tkadw-0.2.3.tar.gz

---
+++ packages/tkadw/tkadw-0.2.3.tar.gz/tkadw-0.2.3/tkadw/fluent/__init__.py
@@ -0,0 +1,3 @@
+from tkadw.fluent.button import FluentButton, FluentDarkButton
+from tkadw.fluent.entry import FluentEntry, FluentDarkEntry
+from tkadw.fluent.frame import FluentFrame, FluentDarkFrame
