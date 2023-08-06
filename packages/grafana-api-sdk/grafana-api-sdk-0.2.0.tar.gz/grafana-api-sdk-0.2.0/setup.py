b047a7b24189eaced8ec09d993a28e292f4ac3a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:13 +0000

    Add package grafana-api-sdk/0.2.0/grafana-api-sdk-0.2.0.tar.gz

---
+++ packages/grafana-api-sdk/grafana-api-sdk-0.2.0.tar.gz/grafana-api-sdk-0.2.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    coverage_string: str = "![Coverage report](https://github.com/ZPascal/grafana_api_sdk/blob/main/docs/coverage.svg)"
+    long_description: str = fh.read()
+
+long_description = long_description.replace(coverage_string, "")
+
+setuptools.setup(
+    name="grafana-api-sdk",
+    version="0.2.0",
+    author="Pascal Zimmermann",
+    author_email="info@theiotstudio.com",
+    description="A Grafana API SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZPascal/grafana_api_sdk",
+    project_urls={
+        "Source": "https://github.com/ZPascal/grafana_api_sdk",
+        "Bug Tracker": "https://github.com/ZPascal/grafana_api_sdk/issues",
+        "Documentation": "https://zpascal.github.io/grafana_api_sdk/",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved",
+        "Operating System :: OS Independent",
+    ],
+    packages=["grafana_api"],
+    install_requires=["urllib3"],
+    python_requires=">=3.6",
+)
