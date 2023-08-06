512692e174eaab2f01587f6d7d26ddd6572e3623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:42 +0000

    Add package synnamon/0.0.4/synnamon-0.0.4-py3-none-any.whl

---
+++ packages/synnamon/synnamon-0.0.4-py3-none-any.whl/synnamon/get_syns.py
@@ -0,0 +1,12 @@
+import json
+
+with open('../data/en_thesaurus_dict.json', 'r') as f:
+    thesaurus = json.load(f)
+
+
+def get_syns(word: str) -> dict:
+    word = word.lower()
+    if word not in thesaurus:
+        if word[-1] == 's':
+            word = word[:-1]
+    return thesaurus.get(word, {})
