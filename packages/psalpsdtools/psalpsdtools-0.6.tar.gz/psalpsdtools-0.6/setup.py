5c9c4541ddb3d8890f816c0f9c4abb779e2d8434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:12 +0000

    Add package psalpsdtools/0.6/psalpsdtools-0.6.tar.gz

---
+++ packages/psalpsdtools/psalpsdtools-0.6.tar.gz/psalpsdtools-0.6/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='psalpsdtools',
+      version='0.6',
+      description='PSA-LPSD Tools',
+      packages=['psalpsdtools'],
+      author_email='a.diaziii@psa.gov.ph',
+      zip_safe=False)
