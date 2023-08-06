589b0826f579b67f24776e252cce729ab39f7db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:09 +0000

    Add package seatsio/73.1.0/seatsio-73.1.0.tar.gz

---
+++ packages/seatsio/seatsio-73.1.0.tar.gz/seatsio-73.1.0/seatsio/events/eventProperties.py
@@ -0,0 +1,16 @@
+class EventProperties:
+    def __init__(self, event_key=None, name=None, date=None, table_booking_config=None, social_distancing_ruleset_key=None, object_categories=None, categories=None):
+        if event_key:
+            self.eventKey = event_key
+        if name:
+            self.name = name
+        if date:
+            self.date = date
+        if table_booking_config is not None:
+            self.tableBookingConfig = table_booking_config
+        if social_distancing_ruleset_key is not None:
+            self.socialDistancingRulesetKey = social_distancing_ruleset_key
+        if object_categories is not None:
+            self.objectCategories = object_categories
+        if categories is not None:
+            self.categories = categories
