63e1a6a3012cd128f32c9ee42127a2fbf9750188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:16 +0000

    Add package rockset-sqlalchemy-test/0.0.0/rockset-sqlalchemy-test-0.0.0.tar.gz

---
+++ packages/rockset-sqlalchemy-test/rockset-sqlalchemy-test-0.0.0.tar.gz/rockset-sqlalchemy-test-0.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="rockset-sqlalchemy-test",
+    version="0.0.0",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    entry_points={
+        "sqlalchemy.dialects": [
+            "rockset_sqlalchemy = rockset_sqlalchemy.client.sqlalchemy:RocksetDialect"
+        ]
+    },
+    extras_require=dict(sqlalchemy=["sqlalchemy>=1.0,<1.4", "geojson>=2.5.0"]),
+    install_requires=[
+        "rockset>=1.0.0",
+        "sqlalchemy>=1.4.0,<2.0.0"
+    ],
+)
