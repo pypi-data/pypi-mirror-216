433128df13945bd0d7539af676c83b61f616493b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:48 +0000

    Add package qiskit-han/0.0.2/qiskit_han-0.0.2.tar.gz

---
+++ packages/qiskit-han/qiskit_han-0.0.2.tar.gz/qiskit_han-0.0.2/qiskit_han/provider.py
@@ -0,0 +1,22 @@
+from qiskit.providers import ProviderV1 as Provider
+from qiskit.providers.providerutils import filter_backends
+
+from qiskit_han.backend import HanBackend
+
+class MyProvider(Provider):
+
+    def __init__(self, token=None):
+        super().__init__()
+        self.token = token
+        self.backends = [HanBackend()]
+
+    def backends(self):
+        return self.backends
+    
+    def get_backend(self, name=None, **kwargs):
+        for backend in self.backends:
+            if backend.name == name:
+                return backend
+              
+        raise Exception("Backend not found")
+       
