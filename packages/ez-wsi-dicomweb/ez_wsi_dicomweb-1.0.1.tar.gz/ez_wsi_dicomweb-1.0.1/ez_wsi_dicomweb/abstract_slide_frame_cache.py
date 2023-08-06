b04619718f2587a492a1a1ca1c90cd681b68c123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:34 +0000

    Add package ez-wsi-dicomweb/1.0.1/ez_wsi_dicomweb-1.0.1.tar.gz

---
+++ packages/ez-wsi-dicomweb/ez_wsi_dicomweb-1.0.1.tar.gz/ez_wsi_dicomweb-1.0.1/ez_wsi_dicomweb/abstract_slide_frame_cache.py
@@ -0,0 +1,37 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Abstract class for DICOM Slide frame cache."""
+import abc
+from typing import Optional
+
+
+class AbstractSlideFrameCache(metaclass=abc.ABCMeta):
+  """Abstract class for DICOM Slide frame cache."""
+
+  @abc.abstractmethod
+  def get_frame(
+      self, instance_path: str, frame_index: int, number_of_frames: int
+  ) -> Optional[bytes]:
+    """Returns DICOM instance frame bytes or None.
+
+    Args:
+      instance_path: DICOMweb path to instance.
+      frame_index: Frame index within instance.  The frames are referenced in
+        with 1-based indexing.
+      number_of_frames: Total number of frames in instance.
+
+    Returns:
+      Frame bytes or None.
+    """
