3381cc94a60bc29feb48f77c934790f9d0682452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:31 +0000

    Add package yellowdog-python-examples/6.0.5/yellowdog_python_examples-6.0.5-py3-none-any.whl

---
+++ packages/yellowdog-python-examples/yellowdog_python_examples-6.0.5-py3-none-any.whl/yd_commands/download.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+"""
+A script to download YellowDog Object Store objects.
+"""
+
+from concurrent import futures
+from os import mkdir, path
+from typing import List
+
+from yellowdog_client.model import ObjectPath, ObjectPathsRequest
+from yellowdog_client.object_store.download.abstracts.abstract_download_batch_builder import (
+    AbstractDownloadBatchBuilder,
+    AbstractTransferBatch,
+)
+from yellowdog_client.object_store.model import FileTransferStatus
+
+from yd_commands.interactive import confirmed, select
+from yd_commands.printing import print_log
+from yd_commands.wrapper import CLIENT, CONFIG_COMMON, main_wrapper
+
+
+@main_wrapper
+def main():
+    tag = CONFIG_COMMON.name_tag
+    print_log(
+        f"Downloading all Objects in namespace '{CONFIG_COMMON.namespace}' and "
+        f"names starting with '{tag}'"
+    )
+
+    object_paths: List[ObjectPath] = (
+        CLIENT.object_store_client.get_namespace_object_paths(
+            ObjectPathsRequest(CONFIG_COMMON.namespace)
+        )
+    )
+    object_paths_to_download: List[ObjectPath] = []
+    for object_path in object_paths:
+        if object_path.name.startswith(tag):
+            object_paths_to_download.append(object_path)
+
+    if len(object_paths_to_download) > 0:
+        object_paths_to_download = select(CLIENT, object_paths_to_download)
+
+    if len(object_paths_to_download) == 0:
+        print_log("No Objects to download")
+    elif confirmed(f"Download {len(object_paths_to_download)} Object Path(s)?"):
+        print_log(f"{len(object_paths_to_download)} Object Path(s) to Download")
+        download_dir: str = _create_download_directory(CONFIG_COMMON.namespace)
+        for object_path in object_paths_to_download:
+            download_batch_builder: AbstractDownloadBatchBuilder = (
+                CLIENT.object_store_client.build_download_batch()
+            )
+            download_batch_builder.destination_folder = download_dir
+            download_batch_builder.find_source_objects(
+                namespace=CONFIG_COMMON.namespace,
+                object_name_pattern=f"{object_path.name}*",
+            )
+            download_batch: AbstractTransferBatch = (
+                download_batch_builder.get_batch_if_objects_found()
+            )
+            if download_batch is None:
+                print_log(f"No Objects found in Object Path {object_path.displayName}")
+                continue
+            download_batch.start()
+            future: futures.Future = download_batch.when_status_matches(
+                lambda status: status == FileTransferStatus.Completed
+            )
+            CLIENT.object_store_client.start_transfers()
+            futures.wait((future,))
+            print_log(f"Downloaded all Objects in {object_path.displayName}")
+
+        print_log(
+            f"Downloaded all Objects in {len(object_paths_to_download)} Object Path(s)"
+        )
+
+
+def _create_download_directory(namespace: str) -> str:
+    """
+    Create a new local download directory in the current working directory,
+    using a sequence of names as follows to avoid over-writes:
+       <namespace>, <namespace>.01, ..., <namespace>.99
+    """
+    new_dir = namespace
+    if path.exists(new_dir):
+        for index in range(1, 100):
+            new_dir = namespace + "." + str(index).zfill(2)
+            if not path.exists(new_dir):
+                break
+        else:
+            raise Exception(f"Too many download directories for {namespace}")
+    mkdir(new_dir)
+    new_dir = path.abspath(new_dir)
+    print_log(f"Created local download directory: {new_dir}")
+    return new_dir
+
+
+# Entry point
+if __name__ == "__main__":
+    main()
---
+++ packages/yellowdog-python-examples/yellowdog_python_examples-6.0.5-py3-none-any.whl/yd_commands/interactive.py
@@ -0,0 +1,130 @@
+"""
+User interaction processing
+"""
+
+from os import getenv
+from typing import List, Optional, Set
+
+from yellowdog_client import PlatformClient
+
+from yd_commands.args import ARGS_PARSER
+from yd_commands.object_utilities import Item
+from yd_commands.printing import (
+    print_error,
+    print_log,
+    print_numbered_object_list,
+    print_string,
+    sorted_objects,
+)
+
+try:
+    import readline
+except ImportError:
+    pass
+
+# Environment variable to use --no-confirmation by default
+# Set to any non-empty string
+YD_YES = "YD_YES"
+
+
+def select(
+    client: PlatformClient,
+    objects: List[Item],
+    parent: Optional[Item] = None,
+    override_quiet: bool = False,
+) -> List[Item]:
+    """
+    Print a numbered list of objects.
+    Manually select objects from a list if --interactive is set.
+    Return the list of objects.
+    """
+    objects = sorted_objects(objects)
+
+    if not ARGS_PARSER.quiet or override_quiet or ARGS_PARSER.interactive:
+        print_numbered_object_list(
+            client, objects, parent, override_quiet=override_quiet
+        )
+
+    if not ARGS_PARSER.interactive:
+        return objects
+
+    def in_range(num: int) -> bool:
+        if 1 <= num <= len(objects):
+            return True
+        else:
+            print_error(f"'{num}' is out of range")
+            return False
+
+    while True:
+        selector_string = input(
+            print_string(
+                "Please select items (e.g.: 1,2,4-7) or press <Return> for none: "
+            )
+        )
+        selector_list = selector_string.split(",")
+        selector_set: Set[int] = set()
+        error_flag = False
+        for selector in selector_list:
+            try:
+                if "-" in selector:
+                    low_s, high_s = selector.split("-")
+                    low = int(low_s)
+                    high = int(high_s)
+                    if low > high:
+                        raise ValueError
+                    for i in range(int(low), int(high) + 1):
+                        if in_range(i):
+                            selector_set.add(i)
+                        else:
+                            error_flag = True
+                elif not (selector.isspace() or len(selector) == 0):
+                    i = int(selector)
+                    if in_range(i):
+                        selector_set.add(i)
+                    else:
+                        error_flag = True
+            except ValueError:
+                print(print_string(f"Error: '{selector}' is not a valid selection"))
+                error_flag = True
+        if error_flag:
+            continue
+        else:
+            break
+
+    selected_list = sorted(list(selector_set))
+    if len(selected_list) > 0:
+        print(
+            print_string(
+                f"Selected item number(s): {', '.join([str(x) for x in selected_list])}"
+            )
+        )
+    else:
+        print_log("No items selected")
+
+    return [objects[x - 1] for x in selected_list]
+
+
+def confirmed(msg: str) -> bool:
+    """
+    Confirm an action.
+    """
+    # Confirmed on the command line?
+    if ARGS_PARSER.yes:
+        print_log("Action proceeding without user confirmation")
+        return True
+
+    # Confirmed using the environment variable?
+    yd_yes = getenv(YD_YES, "")
+    if yd_yes != "":
+        print_log(f"'{YD_YES}={yd_yes}': Action proceeding without user confirmation")
+        return True
+
+    # Seek user confirmation
+    while True:
+        response = input(print_string(f"{msg} (y/N): "))
+        if response.lower() in ["y", "yes"]:
+            print_log("Action confirmed by user")
+            return True
+        elif response.lower() in ["n", "no", ""]:
+            print_log("Action cancelled by user")
+            return False
