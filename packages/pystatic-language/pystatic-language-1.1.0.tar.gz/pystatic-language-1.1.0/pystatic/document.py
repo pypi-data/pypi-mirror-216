331c8fb66055852796063019db3816389892c18a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:55 +0000

    Add package pystatic-language/1.1.0/pystatic-language-1.1.0.tar.gz

---
+++ packages/pystatic-language/pystatic-language-1.1.0.tar.gz/pystatic-language-1.1.0/pystatic/document.py
@@ -0,0 +1,20 @@
+# document.py
+
+# noinspection PyUnresolvedReferences
+from pystatic.clean import *
+# noinspection PyUnresolvedReferences
+from pystatic.overload import *
+# noinspection PyUnresolvedReferences
+from pystatic.private import *
+# noinspection PyUnresolvedReferences
+from pystatic.types import *
+# noinspection PyUnresolvedReferences
+from pystatic.casting import *
+
+from pystatic.base import document
+
+class Documentation:
+    """"""
+# end Documentation
+
+document(Documentation)
