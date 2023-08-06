9cf3543bd7decac02d3bffdecbd6672bb1572d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:23 +0000

    Add package yyperf/0.6.10.dev68/yyperf-0.6.10.dev68.tar.gz

---
+++ packages/yyperf/yyperf-0.6.10.dev68.tar.gz/yyperf-0.6.10.dev68/vue/src/components/uiDebug.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "uiDebug",
+    methods: {},
+    created() {
+        window.location = "/wda"
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
