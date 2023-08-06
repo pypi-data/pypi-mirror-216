fec78e6e91b89184b1cc56733b15c64acd036612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:46 +0000

    Add package ms2pip/4.0.0.dev0/ms2pip-4.0.0.dev0.tar.gz

---
+++ packages/ms2pip/ms2pip-4.0.0.dev0.tar.gz/ms2pip-4.0.0.dev0/setup.py
@@ -0,0 +1,49 @@
+import os
+import platform
+from glob import glob
+
+import numpy
+from Cython.Distutils import build_ext
+from setuptools import setup
+from setuptools.extension import Extension
+
+to_remove = [
+    "ms2pip/_cython_modules/ms2pip_pyx.c*",
+    "ms2pip/_cython_modules/ms2pip_pyx.so",
+]
+_ = [[os.remove(f) for f in glob(pat)] for pat in to_remove]
+
+# Large machine-written C model files require optimization to be disabled
+compile_args = {
+    "Linux": [
+        "-O0",
+        "-fno-var-tracking",
+        "-Wno-unused-result",
+        "-Wno-cpp",
+        "-Wno-unused-function",
+    ],
+    "Darwin": [
+        "-O0",
+    ],
+    "Windows": [
+        "/Od",
+        "/DEBUG",
+        "/GL-",
+        "/bigobj",
+        "/wd4244",
+    ],
+}
+
+extensions = [
+    Extension(
+        "ms2pip._cython_modules.ms2pip_pyx",
+        sources=["ms2pip/_cython_modules/ms2pip_pyx.pyx"] + glob("ms2pip/_models_c/*/*.c"),
+        extra_compile_args=compile_args[platform.system()],
+    )
+]
+
+setup(
+    ext_modules=extensions,
+    include_dirs=[numpy.get_include()],
+    cmdclass={"build_ext": build_ext},
+)
