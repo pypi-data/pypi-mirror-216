9c1de5012aa24809fcaf205376d7325c4cd640c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:34 +0000

    Add package ministats/0.1.0/ministats-0.1.0.tar.gz

---
+++ packages/ministats/ministats-0.1.0.tar.gz/ministats-0.1.0/ministats/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for Mini Statistics."""
+
+__author__ = """Ivan Savov"""
+__email__ = 'ivan@minireference.com'
+__version__ = '0.1.0'
---
+++ packages/ministats/ministats-0.1.0.tar.gz/ministats-0.1.0/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    history = history_file.read()
+
+requirements = [ ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Ivan Savov",
+    author_email='ivan@minireference.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Common statistical testing procedures used for STATS 101 topics. The code is intentionally simple to make it easy to read for beginners.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords='ministats',
+    name='ministats',
+    packages=find_packages(include=['ministats', 'ministats.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/minireference/ministats',
+    version='0.1.0',
+    zip_safe=False,
+)
