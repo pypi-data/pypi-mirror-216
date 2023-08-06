c58d7ed14d568d58a2f07bfe0ba4de9817381e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:25 +0000

    Add package pyansys-tools-versioning/0.4.0/pyansys_tools_versioning-0.4.0-py3-none-any.whl

---
+++ packages/pyansys-tools-versioning/pyansys_tools_versioning-0.4.0-py3-none-any.whl/ansys/tools/versioning/__init__.py
@@ -0,0 +1,15 @@
+"""
+PyAnsys Tools Versioning.
+
+Utilities for backwards and forwards server support.
+"""
+
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+from ansys.tools.versioning.utils import requires_version, server_meets_version
+
+__version__ = importlib_metadata.version("pyansys-tools-versioning")
+__all__ = ["requires_version", "server_meets_version"]
