d071464e7d78a1050a868db3421f990e70e14ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:23 +0000

    Add package geemap/0.23.2/geemap-0.23.2-py2.py3-none-any.whl

---
+++ packages/geemap/geemap-0.23.2-py2.py3-none-any.whl/geemap/report.py
@@ -0,0 +1,33 @@
+import scooby
+from typing import Optional
+
+
+class Report(scooby.Report):
+    def __init__(
+        self,
+        additional: Optional[dict] = None,
+        ncol: int = 3,
+        text_width: int = 80,
+        sort: bool = False,
+    ):
+        """Initiate a scooby.Report instance."""
+        core = [
+            "geemap",
+            "ee",
+            "ipyleaflet",
+            "folium",
+            "jupyterlab",
+            "notebook",
+            "ipyevents",
+        ]
+        optional = ["geopandas", "localtileserver"]
+
+        scooby.Report.__init__(
+            self,
+            additional=additional,
+            core=core,
+            optional=optional,
+            ncol=ncol,
+            text_width=text_width,
+            sort=sort,
+        )
