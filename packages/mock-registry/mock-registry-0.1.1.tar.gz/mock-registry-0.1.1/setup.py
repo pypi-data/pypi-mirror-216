bce0ed6172e3c4eb212abe0c71c18e0092abac0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:20 +0000

    Add package mock-registry/0.1.1/mock-registry-0.1.1.tar.gz

---
+++ packages/mock-registry/mock-registry-0.1.1.tar.gz/mock-registry-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='mock-registry',
+    version='0.1.1',
+    description='Foo.',
+    author='Foo',
+    author_email='foo@foo.com',
+    zip_safe=False,
+    test_suite='test'
+)
