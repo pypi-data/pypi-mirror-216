e5410927a24121e66d7fac1143487858a0885ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:58 +0000

    Add package qqwry-linux-python3/0.0.0/qqwry_linux_python3-0.0.0-py3-none-any.whl

---
+++ packages/qqwry-linux-python3/qqwry_linux_python3-0.0.0-py3-none-any.whl/qqwry/src/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/3/22 15:34
+# @Author  : unclear
+# @File    : __init__.py.py
+# @Software: PyCharm
+from qqwry.src import demo
+from qqwry.src.qqwry import QQwry
+from qqwry.src.update_qqwry import UpdateQQwry
+
+__all__ = [
+    "QQwry",
+    "UpdateQQwry",
+]
