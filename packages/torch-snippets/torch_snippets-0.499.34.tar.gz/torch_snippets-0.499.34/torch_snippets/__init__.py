2faa99ed906dac6da25f2b34a82d2e895a241c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:24 +0000

    Add package torch-snippets/0.499.34/torch_snippets-0.499.34.tar.gz

---
+++ packages/torch-snippets/torch_snippets-0.499.34.tar.gz/torch_snippets-0.499.34/torch_snippets/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.499.34"
+from .loader import *
+from .paths import *
+from .markup import *
+from .inspector import *
+from .load_defaults import *
+from .pdf_loader import PDF
+from .ipython import *
+from .decorators import *
+
+try:
+    from .torch_loader import *
+except Exception as e:
+    ...
