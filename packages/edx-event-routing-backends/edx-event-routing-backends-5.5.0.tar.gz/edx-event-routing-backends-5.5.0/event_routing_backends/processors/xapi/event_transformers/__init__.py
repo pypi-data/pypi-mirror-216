d3710fd9e70a0d48f4ba19b886d03dce8871d5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:15 +0000

    Add package edx-event-routing-backends/5.5.0/edx-event-routing-backends-5.5.0.tar.gz

---
+++ packages/edx-event-routing-backends/edx-event-routing-backends-5.5.0.tar.gz/edx-event-routing-backends-5.5.0/event_routing_backends/processors/xapi/event_transformers/__init__.py
@@ -0,0 +1,26 @@
+"""
+All xAPI transformers.
+"""
+
+from event_routing_backends.processors.xapi.event_transformers.enrollment_events import (
+    EnrollmentActivatedTransformer,
+    EnrollmentDeactivatedTransformer,
+)
+from event_routing_backends.processors.xapi.event_transformers.navigation_events import (
+    LinkClickedTransformer,
+    OutlineSelectedTransformer,
+    TabNavigationTransformer,
+)
+from event_routing_backends.processors.xapi.event_transformers.problem_interaction_events import (
+    ProblemCheckTransformer,
+    ProblemEventsTransformer,
+    ProblemSubmittedTransformer,
+)
+from event_routing_backends.processors.xapi.event_transformers.video_events import (
+    VideoCCTransformers,
+    VideoCompletedTransformer,
+    VideoInteractionTransformers,
+    VideoLoadedTransformer,
+    VideoPositionChangedTransformer,
+    VideoSpeedChangedTransformer,
+)
