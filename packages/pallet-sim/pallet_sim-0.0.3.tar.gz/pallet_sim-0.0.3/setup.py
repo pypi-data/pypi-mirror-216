94dda9be9a706abd859a990b3808e93eb735edd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:56 +0000

    Add package pallet-sim/0.0.3/pallet_sim-0.0.3.tar.gz

---
+++ packages/pallet-sim/pallet_sim-0.0.3.tar.gz/pallet_sim-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pallet_sim',
+    version='0.0.3',
+    author='Rodolfo Verde',
+    description='Pallet simulation library developed at THWS',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    package_data={
+        'pallet_sim': ['textures/*.stl'],
+    },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires=[
+        # Specify any dependencies required by your library
+        # For example:
+        # 'numpy>=1.11.1',
+    ],
+)
