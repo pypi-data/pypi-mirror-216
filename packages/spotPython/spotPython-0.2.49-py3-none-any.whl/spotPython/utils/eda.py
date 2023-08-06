241f2ac7b8f227eb3d9378d499742f6d0b9795f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:19 +0000

    Add package spotPython/0.2.49/spotPython-0.2.49-py3-none-any.whl

---
+++ packages/spotPython/spotPython-0.2.49-py3-none-any.whl/spotPython/utils/eda.py
@@ -0,0 +1,112 @@
+from tabulate import tabulate
+from spotPython.hyperparameters.values import (
+    get_default_values,
+    get_bound_values,
+    get_var_name,
+    get_var_type,
+    get_transform,
+)
+
+
+def get_stars(input_list) -> list:
+    """Converts a list of values to a list of stars, which can be used to
+        visualize the importance of a variable.
+    Args:
+        input_list (list): A list of values.
+    Returns:
+        list: A list of strings.
+    Example:
+        >>> from spotPython.utils.eda import convert_list
+        >>> get_stars([100, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+        [***, '', '', '', '', '', '', '', '']
+    """
+    output_list = []
+    for value in input_list:
+        if value > 95:
+            output_list.append("***")
+        elif value > 50:
+            output_list.append("**")
+        elif value > 1:
+            output_list.append("*")
+        elif value > 0.1:
+            output_list.append(".")
+        else:
+            output_list.append("")
+    return output_list
+
+
+def gen_design_table(fun_control: dict, spot: object = None, tablefmt="github") -> str:
+    """Generates a table with the design variables and their bounds.
+    Args:
+        fun_control (dict): A dictionary with function design variables.
+        spot (object): A spot object. Defaults to None.
+    Returns:
+        str: A table with the design variables, their default values, and their bounds.
+            If a spot object is provided,
+            the table will also include the value
+            and the importance of each hyperparameter.
+        Use the `print` function to display the table.
+    """
+    default_values = get_default_values(fun_control)
+    defaults = list(default_values.values())
+    if spot is None:
+        tab = tabulate(
+            {
+                "name": get_var_name(fun_control),
+                "type": get_var_type(fun_control),
+                "default": defaults,
+                "lower": get_bound_values(fun_control, "lower", as_list=True),
+                "upper": get_bound_values(fun_control, "upper", as_list=True),
+                "transform": get_transform(fun_control),
+            },
+            headers="keys",
+            tablefmt=tablefmt,
+        )
+    else:
+        res = spot.print_results(print_screen=False)
+        tuned = [item[1] for item in res]
+        # imp = spot.print_importance(threshold=0.0, print_screen=False)
+        # importance = [item[1] for item in imp]
+        importance = spot.get_importance()
+        stars = get_stars(importance)
+        tab = tabulate(
+            {
+                "name": get_var_name(fun_control),
+                "type": get_var_type(fun_control),
+                "default": defaults,
+                "lower": get_bound_values(fun_control, "lower", as_list=True),
+                "upper": get_bound_values(fun_control, "upper", as_list=True),
+                "tuned": tuned,
+                "transform": get_transform(fun_control),
+                "importance": importance,
+                "stars": stars,
+            },
+            headers="keys",
+            numalign="right",
+            floatfmt=("", "", "", "", "", "", "", ".2f"),
+            tablefmt=tablefmt,
+        )
+    return tab
+
+
+def compare_two_tree_models(model1, model2, headers=["Parameter", "Default", "Spot"]):
+    keys = model1.summary.keys()
+    values1 = model1.summary.values()
+    values2 = model2.summary.values()
+    tbl = []
+    for key, value1, value2 in zip(keys, values1, values2):
+        tbl.append([key, value1, value2])
+    return tabulate(tbl, headers=headers, numalign="right", tablefmt="github")
+
+
+def generate_config_id(config):
+    """Generates a unique id for a configuration.
+    Args:
+        config (dict): A dictionary with the configuration.
+    Returns:
+        str: A unique id for the configuration.
+    """
+    config_id = ""
+    for key in config:
+        config_id += str(config[key]) + "_"
+    return config_id[:-1]
