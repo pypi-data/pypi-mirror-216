e8f02a6ec320a0ef92a7be57ca7f9fee52567948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:14 +0000

    Add package rustlite/0.1.4/rustlite-0.1.4.tar.gz

---
+++ packages/rustlite/rustlite-0.1.4.tar.gz/rustlite-0.1.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='rustlite',
+    version='0.1.4',    
+    description='rust like iterators for python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/peturparkur/python-rustlite',
+    author='Peter Nagymathe',
+    author_email='peter@nagymathe.xyz',
+    license='AGPLv3+',
+    packages=['rustlite'],
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Typing :: Typed"
+    ]
+)
