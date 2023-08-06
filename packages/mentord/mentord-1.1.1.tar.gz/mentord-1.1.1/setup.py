296bb5082e637680a68982a548fe6ed4a3b3e6b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:50 +0000

    Add package mentord/1.1.1/mentord-1.1.1.tar.gz

---
+++ packages/mentord/mentord-1.1.1.tar.gz/mentord-1.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='mentord',
+      version='1.1.1',
+      description='Library for discord self bots.',
+      packages=['mentord'],
+      long_description="""
+### Example use:
+```python
+User_client: Client = Client() 
+User_client.connect(token='YOUR TOKEN')
+```
+
+### Use decorators:
+```python
+@User_client.on_message
+def client_wait_message(message: Message):
+    print('new message!')
+```
+      """,
+      long_description_content_type='text/markdown')
