fc9842d199a095e09680e0fafb0d600898ab5617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:24 +0000

    Add package pyrocko/2023.6.29/pyrocko-2023.6.29-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyrocko/pyrocko-2023.6.29-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyrocko/geonames.py
@@ -0,0 +1,12 @@
+
+import sys
+import pyrocko
+if pyrocko.grumpy == 1:
+    sys.stderr.write('using renamed pyrocko module: pyrocko.geonames\n')
+    sys.stderr.write('           -> should now use: pyrocko.dataset.geonames\n\n')
+elif pyrocko.grumpy == 2:
+    sys.stderr.write('pyrocko module has been renamed: pyrocko.geonames\n')
+    sys.stderr.write('              -> should now use: pyrocko.dataset.geonames\n\n')
+    raise ImportError('Pyrocko module "pyrocko.geonames" has been renamed to "pyrocko.dataset.geonames".')
+
+from pyrocko.dataset.geonames import *
---
+++ packages/pyrocko/pyrocko-2023.6.29-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyrocko/seisan_waveform.py
@@ -0,0 +1,12 @@
+
+import sys
+import pyrocko
+if pyrocko.grumpy == 1:
+    sys.stderr.write('using renamed pyrocko module: pyrocko.seisan_waveform\n')
+    sys.stderr.write('           -> should now use: pyrocko.io.seisan_waveform\n\n')
+elif pyrocko.grumpy == 2:
+    sys.stderr.write('pyrocko module has been renamed: pyrocko.seisan_waveform\n')
+    sys.stderr.write('              -> should now use: pyrocko.io.seisan_waveform\n\n')
+    raise ImportError('Pyrocko module "pyrocko.seisan_waveform" has been renamed to "pyrocko.io.seisan_waveform".')
+
+from pyrocko.io.seisan_waveform import *
