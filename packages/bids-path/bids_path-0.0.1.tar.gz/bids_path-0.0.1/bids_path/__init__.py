76a03dff0e7628fc812d771c1e3bef7057f6fedb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:34 +0000

    Add package bids-path/0.0.1/bids_path-0.0.1.tar.gz

---
+++ packages/bids-path/bids_path-0.0.1.tar.gz/bids_path-0.0.1/bids_path/__init__.py
@@ -0,0 +1,10 @@
+__submodules__ = ["main"]
+
+__version__ = "0.0.1"
+
+# <AUTOGEN_INIT>
+from bids_path.main import bids, print_boilerplate
+
+__all__ = ["bids", "print_boilerplate"]
+
+# </AUTOGEN_INIT>
