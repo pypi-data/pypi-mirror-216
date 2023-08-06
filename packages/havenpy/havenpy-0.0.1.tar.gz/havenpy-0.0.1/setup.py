367d3135f9d8d911c9d3b4ef3ae3f41979e6343f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:15 +0000

    Add package havenpy/0.0.1/havenpy-0.0.1.tar.gz

---
+++ packages/havenpy/havenpy-0.0.1.tar.gz/havenpy-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='havenpy',
+    version='0.0.1',
+    author='Haven Technologies Inc.',
+    author_email='hello@havenllm.com',
+    description='Haven SDK',
+    packages=find_packages(),
+    install_requires=[],
+)
