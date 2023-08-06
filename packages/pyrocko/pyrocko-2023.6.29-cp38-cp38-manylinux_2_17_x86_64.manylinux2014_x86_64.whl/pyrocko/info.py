3f2d72ba7886fc491b52d4584ab193384f7054b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:23 +0000

    Add package pyrocko/2023.6.29/pyrocko-2023.6.29-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyrocko/pyrocko-2023.6.29-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyrocko/info.py
@@ -0,0 +1,7 @@
+# This module is automatically created from setup.py
+git_sha1 = '34109fc2f3b99ae347e2af216a8657dc810839cc'
+local_modifications = False
+version = '2023.06.29'
+long_version = 'pyrocko-2023.06.29-34109fc2f3b99ae347e2af216a8657dc810839cc-2023-06-29_21:47:39'  # noqa
+installed_date = '2023-06-29_21:47:39'
+src_path = '/drone/src'
---
+++ packages/pyrocko/pyrocko-2023.6.29-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyrocko/response_plot.py
@@ -0,0 +1,12 @@
+
+import sys
+import pyrocko
+if pyrocko.grumpy == 1:
+    sys.stderr.write('using renamed pyrocko module: pyrocko.response_plot\n')
+    sys.stderr.write('           -> should now use: pyrocko.plot.response\n\n')
+elif pyrocko.grumpy == 2:
+    sys.stderr.write('pyrocko module has been renamed: pyrocko.response_plot\n')
+    sys.stderr.write('              -> should now use: pyrocko.plot.response\n\n')
+    raise ImportError('Pyrocko module "pyrocko.response_plot" has been renamed to "pyrocko.plot.response".')
+
+from pyrocko.plot.response import *
