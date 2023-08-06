bdf8b5e63a2c25d501b16492c9978db36d9bc94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:56 +0000

    Add package learn-python-check/1.0.0/learn_python_check-1.0.0.tar.gz

---
+++ packages/learn-python-check/learn_python_check-1.0.0.tar.gz/learn_python_check-1.0.0/learn_python_check/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+learn_python_check_answers
+Authors: Miguel Angel Mendoza-Lugo, Robert Lanzafame, Ahmed Farahat
+
+E-mail:  m.a.mendozalugo@tudelft.nl, R.C.Lanzafame@tudelft.nl, A.Farahat@student.tudelft.nl
+"""
+from __future__ import absolute_import
+
+from . import check_answers
+
