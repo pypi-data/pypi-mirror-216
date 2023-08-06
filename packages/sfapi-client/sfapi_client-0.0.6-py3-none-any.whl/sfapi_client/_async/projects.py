921a7194bf5e3c126883f01f8a9058083a6e90db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:09 +0000

    Add package sfapi-client/0.0.6/sfapi_client-0.0.6-py3-none-any.whl

---
+++ packages/sfapi-client/sfapi_client-0.0.6-py3-none-any.whl/sfapi_client/_async/projects.py
@@ -0,0 +1,41 @@
+from typing import Optional
+from pydantic import ValidationError, Field
+
+from .._models import (
+    ProjectStats as ProjectBase,
+    RoleStats as RoleBase
+)
+from ..exceptions import SfApiError
+
+
+class AsyncRole(RoleBase):
+    client: Optional["AsyncClient"]
+
+
+class AsyncProject(ProjectBase):
+    client: Optional["AsyncClient"]
+    name: str = Field(alias="repo_name")
+
+    async def create_group(self, name: str) -> "AsyncGroup":
+        """
+        Create a new project.
+        :param name: The project name
+        """
+        from .groups import AsyncGroup
+
+        params = {"name": name, "repo_name": self.repo_name}
+
+        r = await self.client.post("account/groups", data=params)
+        json_response = r.json()
+        try:
+            group = AsyncGroup.parse_obj(json_response)
+        except ValidationError:
+            # See if we have validation error raise it
+            if "details" in json_response:
+                raise SfApiError(r.text())
+            else:
+                raise RuntimeError(r.text())
+
+        group.client = self.client
+
+        return group
---
+++ packages/sfapi-client/sfapi_client-0.0.6-py3-none-any.whl/sfapi_client/_sync/projects.py
@@ -0,0 +1,41 @@
+from typing import Optional
+from pydantic import ValidationError, Field
+
+from .._models import (
+    ProjectStats as ProjectBase,
+    RoleStats as RoleBase
+)
+from ..exceptions import SfApiError
+
+
+class Role(RoleBase):
+    client: Optional["Client"]
+
+
+class Project(ProjectBase):
+    client: Optional["Client"]
+    name: str = Field(alias="repo_name")
+
+    def create_group(self, name: str) -> "Group":
+        """
+        Create a new project.
+        :param name: The project name
+        """
+        from .groups import Group
+
+        params = {"name": name, "repo_name": self.repo_name}
+
+        r = self.client.post("account/groups", data=params)
+        json_response = r.json()
+        try:
+            group = Group.parse_obj(json_response)
+        except ValidationError:
+            # See if we have validation error raise it
+            if "details" in json_response:
+                raise SfApiError(r.text())
+            else:
+                raise RuntimeError(r.text())
+
+        group.client = self.client
+
+        return group
