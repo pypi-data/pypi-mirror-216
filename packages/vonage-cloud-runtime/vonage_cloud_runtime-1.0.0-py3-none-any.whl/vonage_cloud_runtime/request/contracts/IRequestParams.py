ce28f8d80e8d092bb65ea3927423f123d91c68f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:43 +0000

    Add package vonage-cloud-runtime/1.0.0/vonage_cloud_runtime-1.0.0-py3-none-any.whl

---
+++ packages/vonage-cloud-runtime/vonage_cloud_runtime-1.0.0-py3-none-any.whl/vonage_cloud_runtime/request/contracts/IRequestParams.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass, field, asdict
+from typing import Dict, List, Generic, TypeVar
+from abc import ABC, abstractmethod
+
+from vonage_cloud_runtime.request.enums.responseTypes import RESPONSE_TYPE
+T = TypeVar('T')
+T = TypeVar("T")
+
+
+#interface
+class IRequestParams(ABC,Generic[T]):
+    method:str
+    url:str
+    data:T
+    headers:Dict[str,str]
+    responseType:RESPONSE_TYPE
