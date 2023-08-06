05bf75ac437e2b24fb792d3bc07cdce2e61faf0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:54 +0000

    Add package mentord/1.0.5/mentord-1.0.5.tar.gz

---
+++ packages/mentord/mentord-1.0.5.tar.gz/mentord-1.0.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='mentord',
+      version='1.0.5',
+      description='Library for discord self bots.',
+      packages=['mentord'],
+      long_description=long_description,
+      long_description_content_type='text/markdown')
