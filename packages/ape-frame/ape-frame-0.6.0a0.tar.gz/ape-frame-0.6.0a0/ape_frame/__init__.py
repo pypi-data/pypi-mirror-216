8acf6a7e199bf6a22a386c3d8db5d7b94bc19d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:01 +0000

    Add package ape-frame/0.6.0a0/ape-frame-0.6.0a0.tar.gz

---
+++ packages/ape-frame/ape-frame-0.6.0a0.tar.gz/ape-frame-0.6.0a0/ape_frame/__init__.py
@@ -0,0 +1,33 @@
+from ape import plugins
+
+from .accounts import AccountContainer, FrameAccount
+from .providers import FrameProvider
+
+NETWORKS = {
+    "ethereum": [
+        "mainnet",
+        "goerli",
+        "sepolia",
+    ],
+    "arbitrum": [
+        "mainnet",
+    ],
+    "optimism": [
+        "mainnet",
+    ],
+    "polygon": [
+        "mainnet",
+    ],
+}
+
+
+@plugins.register(plugins.AccountPlugin)
+def account_types():
+    return AccountContainer, FrameAccount
+
+
+@plugins.register(plugins.ProviderPlugin)
+def providers():
+    for ecosystem, networks in NETWORKS.items():
+        for network in networks:
+            yield ecosystem, network, FrameProvider
