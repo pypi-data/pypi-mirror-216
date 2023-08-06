1eec9221ee3b8f874e52297da6594a8ee95528a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:44 +0000

    Add package nics/0.23.0/nics-0.23.0.tar.gz

---
+++ packages/nics/nics-0.23.0.tar.gz/nics-0.23.0/nics/constants.py
@@ -0,0 +1,19 @@
+import os
+import pkg_resources
+
+
+try:
+    __version__ = pkg_resources.get_distribution('nics').version
+except pkg_resources.DistributionNotFound:
+    ## this exception occurred during development (before the software installed via pip)
+    __version__ = 'dev'
+
+
+SOFTWARE_REPO = 'https://github.com/nvfp/now-i-can-sleep'
+SOFTWARE_NAME = 'Now I Can Sleep'
+SOFTWARE_DIST_NAME = 'nics'  # distribution name
+
+ROOT_DIR_PTH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DIST_DIR_PTH = os.path.join(ROOT_DIR_PTH, SOFTWARE_DIST_NAME)
+
+ASSETS_DIR_PTH = os.path.join(DIST_DIR_PTH, 'assets')
