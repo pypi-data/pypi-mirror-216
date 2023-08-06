abb73dbd49d02f219a2d6534f0ceee4ee7e09e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:25 +0000

    Add package utils-bs/1.0.0/utils_bs-1.0.0.tar.gz

---
+++ packages/utils-bs/utils_bs-1.0.0.tar.gz/utils_bs-1.0.0/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='utils_bs',
+    version='1.0.0',
+    author='Your Name',
+    author_email='divy.jain@bombaysoftwares.com',
+    description='utils_bs',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/divyjain/utils_bs',
+    packages=['utils_bs'],
+    # install_requires=[
+    #     'dependency1',
+    #     'dependency2',
+    # ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
