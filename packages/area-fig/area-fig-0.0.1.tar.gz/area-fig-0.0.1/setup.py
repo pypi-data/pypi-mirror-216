f5e71b6b1a4acba5aab36e33863c0c0b5d4b74bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:03 +0000

    Add package area-fig/0.0.1/area-fig-0.0.1.tar.gz

---
+++ packages/area-fig/area-fig-0.0.1.tar.gz/area-fig-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='area-fig',
+    version='0.0.1',
+    author='Kailash Lohar',
+    author_email='kailash@aganitha.ai',
+    description='Area and Perimeter calculator',
+    long_description='A package to find area of square, rectangle, triangle and circle',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
