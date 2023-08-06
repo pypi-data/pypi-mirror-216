efc4a7ec6545375f03606b6e59e13a9a96e6b1c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:42 +0000

    Add package python-calamine/0.1.4/python_calamine-0.1.4-cp39-none-win32.whl

---
+++ packages/python-calamine/python_calamine-0.1.4-cp39-none-win32.whl/python_calamine/__init__.py
@@ -0,0 +1,13 @@
+from ._python_calamine import (
+    CalamineError,
+    CalamineSheet,
+    CalamineWorkbook,
+    load_workbook,
+)
+
+__all__ = (
+    "CalamineError",
+    "CalamineSheet",
+    "CalamineWorkbook",
+    "load_workbook",
+)
