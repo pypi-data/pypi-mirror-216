2d008822a2693d78a86c9c9fb6d1add3f78b3bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:02 +0000

    Add package pastastore/1.2.2/pastastore-1.2.2.tar.gz

---
+++ packages/pastastore/pastastore-1.2.2.tar.gz/pastastore-1.2.2/pastastore/__init__.py
@@ -0,0 +1,10 @@
+from . import connectors, util
+from .connectors import (
+    ArcticConnector,
+    ArcticDBConnector,
+    DictConnector,
+    PasConnector,
+    PystoreConnector,
+)
+from .store import PastaStore
+from .version import __version__
