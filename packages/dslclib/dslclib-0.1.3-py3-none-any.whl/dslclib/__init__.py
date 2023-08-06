34eabdd42ce1da05fbdf275f8c2f7a0ee2ea61a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:30 +0000

    Add package dslclib/0.1.3/dslclib-0.1.3-py3-none-any.whl

---
+++ packages/dslclib/dslclib-0.1.3-py3-none-any.whl/dslclib/__init__.py
@@ -0,0 +1,17 @@
+from .src import (
+    BaseClient,
+    BodyController,
+    ControllerType,
+    EmotionType,
+    ExpressionController,
+    ExpressionType,
+    FaceRecognitionClient,
+    GazeDirection,
+    GazeObject,
+    MotionType,
+    SpeechRecognitionClient,
+    STTRecognitionType,
+    Text2SpeechClient,
+)
+
+__version__ = "0.1.3"
