d641d4a8b98aa1bc2c4ce1e838efafbf171f3457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:23 +0000

    Add package distri-probability/1.0/distri_probability-1.0.tar.gz

---
+++ packages/distri-probability/distri_probability-1.0.tar.gz/distri_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distri_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distri_probability'],
+      author='Akandji Mujibu',
+      author_email='makandjibeni@gmail.com',
+      zip_safe=False)
