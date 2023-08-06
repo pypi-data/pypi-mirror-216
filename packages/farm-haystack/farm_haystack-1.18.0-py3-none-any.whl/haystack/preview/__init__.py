a7384029aede5082ea34632382e6bc3211096aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:47 +0000

    Add package farm-haystack/1.18.0/farm_haystack-1.18.0-py3-none-any.whl

---
+++ packages/farm-haystack/farm_haystack-1.18.0-py3-none-any.whl/haystack/preview/__init__.py
@@ -0,0 +1,3 @@
+from canals.component import component, ComponentInput, ComponentOutput
+from haystack.preview.dataclasses import Document
+from haystack.preview.pipeline import Pipeline, PipelineError, NoSuchStoreError, load_pipelines, save_pipelines
