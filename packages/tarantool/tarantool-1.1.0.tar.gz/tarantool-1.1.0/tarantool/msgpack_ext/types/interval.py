5cc3ef6376a008ad552ead1b74b3b7f24eeb78e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:19 +0000

    Add package tarantool/1.1.0/tarantool-1.1.0.tar.gz

---
+++ packages/tarantool/tarantool-1.1.0.tar.gz/tarantool-1.1.0/tarantool/msgpack_ext/types/interval.py
@@ -0,0 +1,238 @@
+"""
+Tarantool `datetime.interval`_ extension type implementation module.
+"""
+
+from enum import Enum
+
+id_map = {
+    0: 'year',
+    1: 'month',
+    2: 'week',
+    3: 'day',
+    4: 'hour',
+    5: 'minute',
+    6: 'sec',
+    7: 'nsec',
+    8: 'adjust',
+}
+
+
+# https://github.com/tarantool/c-dt/blob/cec6acebb54d9e73ea0b99c63898732abd7683a6/dt_arithmetic.h#L34
+class Adjust(Enum):
+    """
+    Interval adjustment mode for year and month arithmetic. Refer to
+    :meth:`~tarantool.Datetime.__add__`.
+    """
+
+    EXCESS = 0
+    """
+    Overflow mode.
+    """
+
+    NONE = 1
+    """
+    Only truncation toward the end of month is performed.
+    """
+
+    LAST = 2
+    """
+    Mode when day snaps to the end of month, if it happens.
+    """
+
+
+class Interval():
+    """
+    Class representing Tarantool `datetime.interval`_ info.
+
+    You can create :class:`~tarantool.Interval` objects either
+    from MessagePack data or by using the same API as in Tarantool:
+
+    .. code-block:: python
+
+        di = tarantool.Interval(year=-1, month=2, day=3,
+                                hour=4, minute=-5, sec=6,
+                                nsec=308543321,
+                                adjust=tarantool.IntervalAdjust.NONE)
+
+    Its attributes (same as in init API) are exposed, so you can
+    use them if needed.
+
+    .. _datetime.interval: https://www.tarantool.io/en/doc/latest/dev_guide/internals/msgpack_extensions/#the-interval-type
+    """
+    # pylint: disable=too-many-instance-attributes
+
+    def __init__(self, *, year=0, month=0, week=0,
+                 day=0, hour=0, minute=0, sec=0,
+                 nsec=0, adjust=Adjust.NONE):
+        """
+        :param year: Interval year value.
+        :type year: :obj:`int`, optional
+
+        :param month: Interval month value.
+        :type month: :obj:`int`, optional
+
+        :param week: Interval week value.
+        :type week: :obj:`int`, optional
+
+        :param day: Interval day value.
+        :type day: :obj:`int`, optional
+
+        :param hour: Interval hour value.
+        :type hour: :obj:`int`, optional
+
+        :param minute: Interval minute value.
+        :type minute: :obj:`int`, optional
+
+        :param sec: Interval seconds value.
+        :type sec: :obj:`int`, optional
+
+        :param nsec: Interval nanoseconds value.
+        :type nsec: :obj:`int`, optional
+
+        :param adjust: Interval adjustment rule. Refer to
+            :meth:`~tarantool.Datetime.__add__`.
+        :type adjust: :class:`~tarantool.IntervalAdjust`, optional
+        """
+
+        self.year = year
+        self.month = month
+        self.week = week
+        self.day = day
+        self.hour = hour
+        self.minute = minute
+        self.sec = sec
+        self.nsec = nsec
+        self.adjust = adjust
+
+    def __add__(self, other):
+        """
+        Valid operations:
+
+        * :class:`~tarantool.Interval` + :class:`~tarantool.Interval`
+          = :class:`~tarantool.Interval`
+
+        Adjust of the first operand is used in result.
+
+        :param other: Second operand.
+        :type other: :class:`~tarantool.Interval`
+
+        :rtype: :class:`~tarantool.Interval`
+
+        :raise: :exc:`TypeError`
+        """
+
+        if not isinstance(other, Interval):
+            raise TypeError("unsupported operand type(s) for +: "
+                            f"'{type(self)}' and '{type(other)}'")
+
+        # Tarantool saves adjust of the first argument
+        #
+        # Tarantool 2.10.1-0-g482d91c66
+        #
+        # tarantool> dt1 = datetime.interval.new{year = 2, adjust='last'}
+        # ---
+        # ...
+        #
+        # tarantool> dt2 = datetime.interval.new{year = 1, adjust='excess'}
+        # ---
+        # ...
+        #
+        # tarantool> (dt1 + dt2).adjust
+        # ---
+        # - 'cdata<enum 112>: 2'
+        # ...
+
+        return Interval(
+            year=self.year + other.year,
+            month=self.month + other.month,
+            day=self.day + other.day,
+            hour=self.hour + other.hour,
+            minute=self.minute + other.minute,
+            sec=self.sec + other.sec,
+            nsec=self.nsec + other.nsec,
+            adjust=self.adjust,
+        )
+
+    def __sub__(self, other):
+        """
+        Valid operations:
+
+        * :class:`~tarantool.Interval` - :class:`~tarantool.Interval`
+          = :class:`~tarantool.Interval`
+
+        Adjust of the first operand is used in result.
+
+        :param other: Second operand.
+        :type other: :class:`~tarantool.Interval`
+
+        :rtype: :class:`~tarantool.Interval`
+
+        :raise: :exc:`TypeError`
+        """
+
+        if not isinstance(other, Interval):
+            raise TypeError("unsupported operand type(s) for -: "
+                            f"'{type(self)}' and '{type(other)}'")
+
+        # Tarantool saves adjust of the first argument
+        #
+        # Tarantool 2.10.1-0-g482d91c66
+        #
+        # tarantool> dt1 = datetime.interval.new{year = 2, adjust='last'}
+        # ---
+        # ...
+        #
+        # tarantool> dt2 = datetime.interval.new{year = 1, adjust='excess'}
+        # ---
+        # ...
+        #
+        # tarantool> (dt1 - dt2).adjust
+        # ---
+        # - 'cdata<enum 112>: 2'
+        # ...
+
+        return Interval(
+            year=self.year - other.year,
+            month=self.month - other.month,
+            day=self.day - other.day,
+            hour=self.hour - other.hour,
+            minute=self.minute - other.minute,
+            sec=self.sec - other.sec,
+            nsec=self.nsec - other.nsec,
+            adjust=self.adjust,
+        )
+
+    def __eq__(self, other):
+        """
+        Compare equality of each field, no casts.
+
+        :param other: Second operand.
+        :type other: :class:`~tarantool.Interval`
+
+        :rtype: :obj:`bool`
+        """
+
+        if not isinstance(other, Interval):
+            return False
+
+        # Tarantool interval compare is naive too
+        #
+        # Tarantool 2.10.1-0-g482d91c66
+        #
+        # tarantool> datetime.interval.new{hour=1} == datetime.interval.new{min=60}
+        # ---
+        # - false
+        # ...
+
+        for _, field_name in id_map.items():
+            if getattr(self, field_name) != getattr(other, field_name):
+                return False
+
+        return True
+
+    def __repr__(self):
+        return f'tarantool.Interval(year={self.year}, month={self.month}, day={self.day}, ' + \
+               f'hour={self.hour}, minute={self.minute}, sec={self.sec}, ' + \
+               f'nsec={self.nsec}, adjust={self.adjust})'
+
+    __str__ = __repr__
---
+++ packages/tarantool/tarantool-1.1.0.tar.gz/tarantool-1.1.0/test/suites/test_interval.py
@@ -0,0 +1,298 @@
+"""
+This module tests work with datetime interval type.
+"""
+# pylint: disable=missing-class-docstring,missing-function-docstring,protected-access,too-many-function-args,duplicate-code
+
+import re
+import sys
+import unittest
+
+import msgpack
+
+import tarantool
+from tarantool.error import MsgpackError
+from tarantool.msgpack_ext.packer import default as packer_default
+from tarantool.msgpack_ext.unpacker import ext_hook as unpacker_ext_hook
+
+from .lib.tarantool_server import TarantoolServer
+from .lib.skip import skip_or_run_datetime_test
+
+
+class TestSuiteInterval(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print(' INTERVAL EXT TYPE '.center(70, '='), file=sys.stderr)
+        print('-' * 70, file=sys.stderr)
+        cls.srv = TarantoolServer()
+        cls.srv.script = 'test/suites/box.lua'
+        cls.srv.start()
+
+        cls.adm = cls.srv.admin
+        cls.adm(r"""
+            _, datetime = pcall(require, 'datetime')
+
+            box.schema.space.create('test')
+            box.space['test']:create_index('primary', {
+                type = 'tree',
+                parts = {1, 'string'},
+                unique = true})
+
+            box.schema.user.create('test', {password = 'test', if_not_exists = true})
+            box.schema.user.grant('test', 'read,write,execute', 'universe')
+
+            local function add(arg1, arg2)
+                return arg1 + arg2
+            end
+            rawset(_G, 'add', add)
+
+            local function sub(arg1, arg2)
+                return arg1 - arg2
+            end
+            rawset(_G, 'sub', sub)
+        """)
+
+        cls.con = tarantool.Connection(cls.srv.host, cls.srv.args['primary'],
+                                       user='test', password='test')
+
+    def setUp(self):
+        # prevent a remote tarantool from clean our session
+        if self.srv.is_started():
+            self.srv.touch_lock()
+
+        self.adm("box.space['test']:truncate()")
+
+    def test_interval_positional_init(self):
+        self.assertRaisesRegex(
+            TypeError, re.escape('__init__() takes 1 positional argument but 2 were given'),
+            lambda: tarantool.Interval(1))
+
+    cases = {
+        'year': {
+            'python': tarantool.Interval(year=1),
+            'msgpack': (b'\x02\x00\x01\x08\x01'),
+            'tarantool': r"datetime.interval.new({year=1})",
+        },
+        'big_year': {
+            'python': tarantool.Interval(year=1000),
+            'msgpack': (b'\x02\x00\xcd\x03\xe8\x08\x01'),
+            'tarantool': r"datetime.interval.new({year=1000})",
+        },
+        'date': {
+            'python': tarantool.Interval(year=1, month=2, day=3),
+            'msgpack': (b'\x04\x00\x01\x01\x02\x03\x03\x08\x01'),
+            'tarantool': r"datetime.interval.new({year=1, month=2, day=3})",
+        },
+        'big_month_date': {
+            'python': tarantool.Interval(year=1, month=100000, day=3),
+            'msgpack': (b'\x04\x00\x01\x01\xce\x00\x01\x86\xa0\x03\x03\x08\x01'),
+            'tarantool': r"datetime.interval.new({year=1, month=100000, day=3})",
+        },
+        'time': {
+            'python': tarantool.Interval(hour=1, minute=2, sec=3),
+            'msgpack': (b'\x04\x04\x01\x05\x02\x06\x03\x08\x01'),
+            'tarantool': r"datetime.interval.new({hour=1, min=2, sec=3})",
+        },
+        'big_seconds_time': {
+            'python': tarantool.Interval(hour=1, minute=2, sec=3000),
+            'msgpack': (b'\x04\x04\x01\x05\x02\x06\xcd\x0b\xb8\x08\x01'),
+            'tarantool': r"datetime.interval.new({hour=1, min=2, sec=3000})",
+        },
+        'datetime': {
+            'python': tarantool.Interval(year=1, month=2, day=3, hour=1, minute=2, sec=3000),
+            'msgpack': (b'\x07\x00\x01\x01\x02\x03\x03\x04\x01\x05\x02\x06\xcd\x0b\xb8\x08\x01'),
+            'tarantool': r"datetime.interval.new({year=1, month=2, day=3, hour=1, "
+                         r"min=2, sec=3000})",
+        },
+        'nanoseconds': {
+            'python': tarantool.Interval(nsec=10000000),
+            'msgpack': (b'\x02\x07\xce\x00\x98\x96\x80\x08\x01'),
+            'tarantool': r"datetime.interval.new({nsec=10000000})",
+        },
+        'datetime_with_nanoseconds': {
+            'python': tarantool.Interval(year=1, month=2, day=3, hour=1, minute=2,
+                                         sec=3000, nsec=10000000),
+            'msgpack': (b'\x08\x00\x01\x01\x02\x03\x03\x04\x01\x05\x02\x06\xcd\x0b\xb8\x07\xce'
+                        b'\x00\x98\x96\x80\x08\x01'),
+            'tarantool': r"datetime.interval.new({year=1, month=2, day=3, hour=1, "
+                         r"min=2, sec=3000, nsec=10000000})",
+        },
+        'datetime_none_adjust': {
+            'python': tarantool.Interval(year=1, month=2, day=3, hour=1, minute=2,
+                                         sec=3000, nsec=10000000,
+                                         adjust=tarantool.IntervalAdjust.NONE),
+            'msgpack': (b'\x08\x00\x01\x01\x02\x03\x03\x04\x01\x05\x02\x06\xcd\x0b\xb8\x07\xce'
+                        b'\x00\x98\x96\x80\x08\x01'),
+            'tarantool': r"datetime.interval.new({year=1, month=2, day=3, hour=1, "
+                         r"min=2, sec=3000, nsec=10000000, adjust='none'})",
+        },
+        'datetime_excess_adjust': {
+            'python': tarantool.Interval(year=1, month=2, day=3, hour=1, minute=2,
+                                         sec=3000, nsec=10000000,
+                                         adjust=tarantool.IntervalAdjust.EXCESS),
+            'msgpack': (b'\x07\x00\x01\x01\x02\x03\x03\x04\x01\x05\x02\x06\xcd\x0b\xb8\x07\xce'
+                        b'\x00\x98\x96\x80'),
+            'tarantool': r"datetime.interval.new({year=1, month=2, day=3, hour=1, "
+                         r"min=2, sec=3000, nsec=10000000, adjust='excess'})",
+        },
+        'datetime_last_adjust': {
+            'python': tarantool.Interval(year=1, month=2, day=3, hour=1, minute=2,
+                                         sec=3000, nsec=10000000,
+                                         adjust=tarantool.IntervalAdjust.LAST),
+            'msgpack': (b'\x08\x00\x01\x01\x02\x03\x03\x04\x01\x05\x02\x06\xcd\x0b\xb8\x07\xce'
+                        b'\x00\x98\x96\x80\x08\x02'),
+            'tarantool': r"datetime.interval.new({year=1, month=2, day=3, hour=1, "
+                         r"min=2, sec=3000, nsec=10000000, adjust='last'})",
+        },
+        'all_zeroes': {
+            'python': tarantool.Interval(adjust=tarantool.IntervalAdjust.EXCESS),
+            'msgpack': (b'\x00'),
+            'tarantool': r"datetime.interval.new({adjust='excess'})",
+        },
+    }
+
+    def test_msgpack_decode(self):
+        for name, case in self.cases.items():
+            with self.subTest(msg=name):
+                self.assertEqual(
+                    unpacker_ext_hook(
+                        6,
+                        case['msgpack'],
+                        self.con._unpacker_factory(),
+                    ),
+                    case['python'])
+
+    @skip_or_run_datetime_test
+    def test_tarantool_decode(self):
+        for name, case in self.cases.items():
+            with self.subTest(msg=name):
+                self.adm(f"box.space['test']:replace{{'{name}', {case['tarantool']}, 'field'}}")
+
+                self.assertSequenceEqual(self.con.select('test', name),
+                                         [[name, case['python'], 'field']])
+
+    def test_msgpack_encode(self):
+        for name, case in self.cases.items():
+            with self.subTest(msg=name):
+                self.assertEqual(packer_default(case['python']),
+                                 msgpack.ExtType(code=6, data=case['msgpack']))
+
+    @skip_or_run_datetime_test
+    def test_tarantool_encode(self):
+        for name, case in self.cases.items():
+            with self.subTest(msg=name):
+                self.con.insert('test', [name, case['python'], 'field'])
+
+                lua_eval = f"""
+                    local interval = {case['tarantool']}
+
+                    local tuple = box.space['test']:get('{name}')
+                    assert(tuple ~= nil)
+
+                    if tuple[2] == interval then
+                        return true
+                    else
+                        return nil, ('%s is not equal to expected %s'):format(
+                            tostring(tuple[2]), tostring(interval))
+                    end
+                """
+
+                self.assertSequenceEqual(self.adm(lua_eval), [True])
+
+    def test_unknown_field_decode(self):
+        case = b'\x01\x09\xce\x00\x98\x96\x80'
+        self.assertRaisesRegex(
+            MsgpackError, 'Unknown interval field id 9',
+            lambda: unpacker_ext_hook(6, case, self.con._unpacker_factory()))
+
+    def test_unknown_adjust_decode(self):
+        case = b'\x02\x07\xce\x00\x98\x96\x80\x08\x03'
+        self.assertRaisesRegex(
+            MsgpackError, '3 is not a valid Adjust',
+            lambda: unpacker_ext_hook(6, case, self.con._unpacker_factory()))
+
+    arithmetic_cases = {
+        'year': {
+            'arg_1': tarantool.Interval(year=2),
+            'arg_2': tarantool.Interval(year=1),
+            'res_add': tarantool.Interval(year=3),
+            'res_sub': tarantool.Interval(year=1),
+        },
+        'date': {
+            'arg_1': tarantool.Interval(year=1, month=2, day=3),
+            'arg_2': tarantool.Interval(year=3, month=2, day=1),
+            'res_add': tarantool.Interval(year=4, month=4, day=4),
+            'res_sub': tarantool.Interval(year=-2, month=0, day=2),
+        },
+        'time': {
+            'arg_1': tarantool.Interval(hour=10, minute=20, sec=30),
+            'arg_2': tarantool.Interval(hour=2, minute=15, sec=50),
+            'res_add': tarantool.Interval(hour=12, minute=35, sec=80),
+            'res_sub': tarantool.Interval(hour=8, minute=5, sec=-20),
+        },
+        'datetime': {
+            'arg_1': tarantool.Interval(year=1, month=2, day=3, hour=1, minute=2, sec=3000),
+            'arg_2': tarantool.Interval(year=2, month=1, day=31, hour=-3, minute=0, sec=-2000),
+            'res_add': tarantool.Interval(year=3, month=3, day=34, hour=-2, minute=2, sec=1000),
+            'res_sub': tarantool.Interval(year=-1, month=1, day=-28, hour=4, minute=2, sec=5000),
+        },
+        'datetime_with_nsec': {
+            'arg_1': tarantool.Interval(year=1, month=2, day=3, hour=1, minute=2,
+                                        sec=3000, nsec=10000000),
+            'arg_2': tarantool.Interval(year=2, month=1, day=31, hour=-3, minute=0,
+                                        sec=1000, nsec=9876543),
+            'res_add': tarantool.Interval(year=3, month=3, day=34, hour=-2, minute=2,
+                                          sec=4000, nsec=19876543),
+            'res_sub': tarantool.Interval(year=-1, month=1, day=-28, hour=4, minute=2,
+                                          sec=2000, nsec=123457),
+        },
+        'heterogenous': {
+            'arg_1': tarantool.Interval(year=1, month=2, day=3),
+            'arg_2': tarantool.Interval(sec=3000, nsec=9876543),
+            'res_add': tarantool.Interval(year=1, month=2, day=3,
+                                          sec=3000, nsec=9876543),
+            'res_sub': tarantool.Interval(year=1, month=2, day=3,
+                                          sec=-3000, nsec=-9876543),
+        },
+        'same_adjust': {
+            'arg_1': tarantool.Interval(year=2, adjust=tarantool.IntervalAdjust.LAST),
+            'arg_2': tarantool.Interval(year=1, adjust=tarantool.IntervalAdjust.LAST),
+            'res_add': tarantool.Interval(year=3, adjust=tarantool.IntervalAdjust.LAST),
+            'res_sub': tarantool.Interval(year=1, adjust=tarantool.IntervalAdjust.LAST),
+        },
+        'different_adjust': {
+            'arg_1': tarantool.Interval(year=2, adjust=tarantool.IntervalAdjust.LAST),
+            'arg_2': tarantool.Interval(year=1, adjust=tarantool.IntervalAdjust.EXCESS),
+            'res_add': tarantool.Interval(year=3, adjust=tarantool.IntervalAdjust.LAST),
+            'res_sub': tarantool.Interval(year=1, adjust=tarantool.IntervalAdjust.LAST),
+        },
+    }
+
+    def test_python_interval_addition(self):
+        for name, case in self.arithmetic_cases.items():
+            with self.subTest(msg=name):
+                self.assertEqual(case['arg_1'] + case['arg_2'], case['res_add'])
+
+    def test_python_interval_subtraction(self):
+        for name, case in self.arithmetic_cases.items():
+            with self.subTest(msg=name):
+                self.assertEqual(case['arg_1'] - case['arg_2'], case['res_sub'])
+
+    @skip_or_run_datetime_test
+    def test_tarantool_interval_addition(self):
+        for name, case in self.arithmetic_cases.items():
+            with self.subTest(msg=name):
+                self.assertSequenceEqual(self.con.call('add', case['arg_1'], case['arg_2']),
+                                         [case['res_add']])
+
+    @skip_or_run_datetime_test
+    def test_tarantool_interval_subtraction(self):
+        for name, case in self.arithmetic_cases.items():
+            with self.subTest(msg=name):
+                self.assertSequenceEqual(self.con.call('sub', case['arg_1'], case['arg_2']),
+                                         [case['res_sub']])
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.con.close()
+        cls.srv.stop()
+        cls.srv.clean()
