21d641d8e9fe80258dd18f5e7e8073aacb9f1bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:52 +0000

    Add package CMZ-Frameworks/1.2.0/CMZ_Frameworks-1.2.0.tar.gz

---
+++ packages/CMZ-Frameworks/CMZ_Frameworks-1.2.0.tar.gz/CMZ_Frameworks-1.2.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CMZ_Frameworks',
+    version='1.2.0',
+    author='Sandesh Vanwadi',
+    author_email='Svanwadi@slb.com',
+    description='This package will be use to clone any version of the automation frameworks available under CMZ',
+    packages=find_packages(),
+    install_requires=[
+        'requests'  # Add any dependencies your package requires
+    ],
+)
