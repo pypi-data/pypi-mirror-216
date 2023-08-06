80a55f1d77f9a54f0e2c94801d832d4a99a667dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:28 +0000

    Add package pyneng-cli/5.1.0/pyneng-cli-5.1.0.tar.gz

---
+++ packages/pyneng-cli/pyneng-cli-5.1.0.tar.gz/pyneng-cli-5.1.0/src/pyneng_cli/pyneng.py
@@ -0,0 +1,322 @@
+import sys
+import re
+import os
+import json
+from glob import glob
+
+import click
+import pytest
+from rich.console import Console
+from rich.markdown import Markdown
+from pytest_jsonreport.plugin import JSONReport
+
+from pyneng_cli import (
+    DEFAULT_BRANCH,
+    TASK_DIRS,
+    DB_TASK_DIRS,
+    TASK_NUMBER_DIR_MAP,
+)
+from pyneng_cli.exceptions import PynengError
+from pyneng_cli.pyneng_docs import DOCS
+from pyneng_cli.utils import (
+    red,
+    green,
+    save_changes_to_github,
+    current_chapter_id,
+    current_dir_name,
+    parse_json_report,
+    copy_answers,
+    update_tasks_and_tests,
+    update_chapters_tasks_and_tests,
+)
+
+
+def exception_handler(exception_type, exception, traceback):
+    """
+    sys.excepthook to disable traceback by default
+    """
+    print(f"\n{exception_type.__name__}: {exception}\n")
+
+
+def check_current_dir_name(dir_list, message):
+    current_chapter = current_dir_name()
+    if current_chapter not in dir_list:
+        task_dirs_line = "\n    ".join(
+            [d for d in dir_list if not d.startswith("task")]
+        )
+        print(red(f"\n{message}:" f"\n    {task_dirs_line}"))
+        raise click.Abort()
+
+
+def _get_tasks_tests_from_cli(self, value):
+    regex = (
+        r"(?P<all>all)|"
+        r"(?P<number_star>\d\*)|"
+        r"(?P<letters_range>\d[a-i]-[a-i])|"
+        r"(?P<numbers_range>\d-\d)|"
+        r"(?P<single_task>\d[a-i]?)"
+    )
+    tasks_list = re.split(r"[ ,]+", value)
+    current_chapter = current_chapter_id()
+    test_files = []
+    task_files = []
+    for task in tasks_list:
+        match = re.fullmatch(regex, task)
+        if match:
+            if task == "all":
+                test_files = sorted(glob(f"test_task_{current_chapter}_*.py"))
+                task_files = sorted(glob(f"task_{current_chapter}_*.py"))
+                break
+            else:
+                if match.group("letters_range"):
+                    task = f"{task[0]}[{task[1:]}]"  # convert 1a-c to 1[a-c]
+                elif match.group("numbers_range"):
+                    task = f"[{task}]"  # convert 1-3 to [1-3]
+
+                test_files += glob(f"test_task_{current_chapter}_{task}.py")
+                task_files += glob(f"task_{current_chapter}_{task}.py")
+        else:
+            self.fail(
+                red(
+                    f"This format is not supported {task}. "
+                    "Acceptable formats are listed in pyneng --help"
+                )
+            )
+    tasks_with_tests = set([test.replace("test_", "") for test in test_files])
+    tasks_without_tests = set(task_files) - tasks_with_tests
+    return sorted(test_files), sorted(tasks_without_tests), sorted(task_files)
+
+
+class CustomTasksType(click.ParamType):
+    """
+    The class creates a new type for click and converts valid task string
+    options into separate test files.
+
+    In addition, it checks whether there is such a file in the current
+    directory and leaves only those that are.
+    """
+
+    name = "CustomTasksType"
+
+    def convert(self, value, param, ctx):
+        if isinstance(value, tuple):
+            return value
+        elif value == "all" and current_dir_name() == "exercises":
+            return value
+        elif current_dir_name() not in TASK_DIRS:
+            return value
+
+        return _get_tasks_tests_from_cli(self, value)
+
+
+class CustomChapterType(click.ParamType):
+    name = "Chapters"
+
+    def convert(self, value, param, ctx):
+        if isinstance(value, tuple):
+            return value
+        regex = r"(?P<numbers_range>\d+-\d+)|" r"(?P<number>\d+)"
+        TASK_NUMBER_DIR_MAP
+        chapter_dir_list = []
+        chapter_list = re.split(r"[ ,]+", value)
+        for chapter in chapter_list:
+            match = re.fullmatch(regex, chapter)
+            if match:
+                if match.group("number"):
+                    chapter = int(match.group("number"))
+                    chapter_dir = TASK_NUMBER_DIR_MAP.get(chapter)
+                    if chapter_dir:
+                        chapter_dir_list.append(chapter_dir)
+                elif match.group("numbers_range"):
+                    start, stop = match.group("numbers_range").split("-")
+                    for chapter_id in range(int(start), int(stop) + 1):
+                        chapter_dir = TASK_NUMBER_DIR_MAP.get(chapter_id)
+                        if chapter_dir:
+                            chapter_dir_list.append(chapter_dir)
+            else:
+                self.fail(
+                    red(
+                        f"This format is not supported {chapter}. "
+                        "Valid formats in pyneng --help"
+                    )
+                )
+        return sorted(chapter_dir_list)
+
+
+def print_docs_with_pager(width=90):
+    console = Console(width=width)
+    md = Markdown(DOCS)
+    with console.pager():
+        console.print(md)
+
+
+@click.command(
+    context_settings=dict(
+        ignore_unknown_options=True, help_option_names=["-h", "--help"]
+    )
+)
+@click.argument("tasks", default="all", type=CustomTasksType())
+@click.option(
+    "--answer",
+    "-a",
+    is_flag=True,
+    help=(
+        "Copy answers for tasks that passed the tests. When this flag is added,"
+        "no traceback is output for tests."
+    ),
+)
+@click.option("--docs", is_flag=True, help="Show pyneng documentation")
+@click.option(
+    "--save-all",
+    "save_all_to_github",
+    is_flag=True,
+    help="Save to GitHub all modified files in the current directory",
+)
+@click.option(
+    "--update", "update_tasks_tests", is_flag=True, help="Update tasks and tests"
+)
+@click.option(
+    "--test-only", "update_tests_only", is_flag=True, help="Update tests only"
+)
+@click.option(
+    "--update-chapters",
+    type=CustomChapterType(),
+    help="Update all tasks and tests in the specified chapters",
+)
+@click.option("--disable-verbose", "-d", is_flag=True, help="Disable verbose output")
+@click.option("--debug", is_flag=True, help="Show exception traceback")
+@click.option("--default-branch", "-b", default="main")
+@click.option(
+    "--all",
+    "git_add_all_to_github",
+    is_flag=True,
+    help="Add git add .",
+)
+@click.option("--ignore-ssl-cert", default=False)
+@click.version_option(version="5.1.0")
+def cli(
+    tasks,
+    disable_verbose,
+    answer,
+    debug,
+    default_branch,
+    git_add_all_to_github,
+    ignore_ssl_cert,
+    update_tasks_tests,
+    update_tests_only,
+    save_all_to_github,
+    update_chapters,
+    docs,
+):
+    """
+    PYNENG-CLI: Run tests for TASKS tasks. By default, all tests will run.
+
+    \b
+    These options do not run tests
+     pyneng --docs                 Show pyneng documentation
+     pyneng --save-all             Save to GitHub all modified files in the current directory
+     pyneng --update               Update all tasks and tests in the current directory
+     pyneng --update --test-only   Update only tests in the current directory
+     pyneng 1,2 --update           Update tasks 1 and 2 and corresponding tests in current directory
+     pyneng --update-chapters 4-5  Update chapters 4 and 5 (directories will be removed and updated versions copied)
+
+    \b
+    Run tests, view answers
+    \b
+         pyneng             run all tests for current chapter
+         pyneng 1,2a,5      run tests for tasks 1, 2a and 5
+         pyneng 1,2*        run tests for tasks 1, all tasks 2 with and without letters
+         pyneng 1,3-5       run tests for tasks 1, 3, 4, 5
+         pyneng 1-5 -a      run the tests and write the answers for the tasks
+                            that passed the tests to the files answer_task_x.py
+
+    \b
+    Read more in the documentation: pyneng --docs
+    """
+    global DEFAULT_BRANCH
+    if default_branch != "main":
+        DEFAULT_BRANCH = default_branch
+
+    if docs:
+        print_docs_with_pager()
+        return
+
+    if save_all_to_github:
+        save_changes_to_github(branch=DEFAULT_BRANCH)
+        print(green("All changes in the current directory are saved to GitHub"))
+        return
+
+    if update_chapters:
+        LANG = click.prompt(
+            "Please select a language from the following",
+            type=click.Choice(["uk", "en", "ru"]),
+        )
+        check_current_dir_name(
+            ["exercises"], "Chapters must be updated from the directory"
+        )
+        update_chapters_tasks_and_tests(
+            update_chapters, branch=DEFAULT_BRANCH, lang=LANG
+        )
+        return
+
+    # it makes sense to perform further actions only if we are in the directory
+    # of a specific task chapter
+    check_current_dir_name(
+        TASK_DIRS + DB_TASK_DIRS, "Tasks can only be tested from directories"
+    )
+
+    test_files, tasks_without_tests, task_files = tasks
+
+    if update_tasks_tests:
+        LANG = click.prompt(
+            "Please select a language from the following",
+            type=click.Choice(["uk", "en", "ru"]),
+        )
+        if update_tests_only:
+            task_files = None
+            msg = green("Tests updated successfully")
+        else:
+            msg = green("Tasks and tests updated successfully")
+
+        upd = update_tasks_and_tests(
+            task_files, test_files, branch=DEFAULT_BRANCH, lang=LANG
+        )
+        if upd:
+            print(msg)
+        return
+
+    if not debug:
+        sys.excepthook = exception_handler
+
+    json_plugin = JSONReport()
+    pytest_args_common = ["--json-report-file=none", "--disable-warnings"]
+
+    if disable_verbose:
+        pytest_args = [*pytest_args_common, "--tb=short"]
+    else:
+        pytest_args = [*pytest_args_common, "-vv"]
+
+    # if the -a flag is added, it makes no sense to print traceback, since most
+    # likely the tasks have already been checked by previous runs.
+    if answer:
+        pytest_args = [*pytest_args_common, "--tb=no"]
+
+    # run pytest
+    pytest.main(test_files + pytest_args, plugins=[json_plugin])
+
+    # get pytest results in JSON format passed_tasks are tasks that have tests
+    # and passed tests
+    passed_tasks = parse_json_report(json_plugin.report)
+
+    if passed_tasks or tasks_without_tests:
+        # copy answers to answer_task_x.py files
+        if answer:
+            copy_answers(passed_tasks)
+
+    # if the --all flag is added, all changes must be saved to github
+    if git_add_all_to_github:
+        save_changes_to_github(branch=DEFAULT_BRANCH)
+
+
+if __name__ == "__main__":
+    cli()
---
+++ packages/pyneng-cli/pyneng-cli-5.1.0.tar.gz/pyneng-cli-5.1.0/src/pyneng_cli/utils.py
@@ -0,0 +1,344 @@
+import subprocess
+from platform import system as system_name
+import re
+import os
+from collections import defaultdict
+import tempfile
+import pathlib
+import stat
+import shutil
+from datetime import datetime, timedelta
+
+import click
+import github
+from rich import print as rprint
+from rich.padding import Padding
+
+from pyneng_cli.exceptions import PynengError
+from pyneng_cli import (
+    ANSWERS_URL,
+    TASK_DIRS,
+    DB_TASK_DIRS,
+    TASKS_URL,
+    TASKS_LOCAL_REPO,
+    LANG_TASKS_URL,
+    LANG_TASKS_LOCAL_REPO,
+)
+
+
+def red(msg):
+    return click.style(msg, fg="red")
+
+
+def green(msg):
+    return click.style(msg, fg="green")
+
+
+def remove_readonly(func, path, _):
+    """
+    A helper function for Windows that allows you to remove read only files
+    from a .git directory
+    """
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
+
+def call_command(command, verbose=True, return_stdout=False, return_stderr=False):
+    """
+    The function invokes the specified command via subprocess and outputs
+    stdout and stderr if the verbose flag is True.
+    """
+    result = subprocess.run(
+        command,
+        shell=True,
+        encoding="utf-8",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    std = result.stdout
+    stderr = result.stderr
+    if return_stdout:
+        return std
+    if return_stderr:
+        return result.returncode, stderr
+    if verbose:
+        print("#" * 20, command)
+        if std:
+            print(std)
+        if stderr:
+            print(stderr)
+    return result.returncode
+
+
+def working_dir_clean():
+    git_status = call_command("git status --porcelain", return_stdout=True)
+    if git_status:
+        return False
+    else:
+        return True
+
+
+def show_git_diff_short():
+    git_diff = call_command("git diff --stat")
+
+
+def git_push(branch):
+    """
+    The function calls git push on Windows
+    """
+    command = f"git push origin {branch}"
+    print("#" * 20, command)
+    result = subprocess.run(command, shell=True)
+
+
+def save_changes_to_github(
+    message="All changes saved", git_add_all=True, branch="main"
+):
+    status = call_command("git status -s", return_stdout=True)
+    if not status:
+        return
+    if git_add_all:
+        call_command("git add .")
+    call_command(f'git commit -m "{message}"')
+    windows = True if system_name().lower() == "windows" else False
+
+    if windows:
+        git_push(branch)
+    else:
+        call_command(f"git push origin {branch}")
+
+
+def current_chapter_id():
+    """
+    The function returns the number of the current task chapter where pyneng is
+    called.
+    """
+    current_chapter_name = current_dir_name()
+    if current_chapter_name in DB_TASK_DIRS:
+        current_chapter_name = TASK_DIRS[-1]
+    current_chapter = int(current_chapter_name.split("_")[0])
+    return current_chapter
+
+
+def current_dir_name():
+    pth = str(pathlib.Path().absolute())
+    current_chapter_name = os.path.split(pth)[-1]
+    return current_chapter_name
+
+
+def parse_json_report(report):
+    """
+    Selects the desired parts from the pytest run report in JSON format.
+    Returns a list of tests that passed.
+    """
+    if report and report["summary"]["total"] != 0:
+        all_tests = defaultdict(list)
+        summary = report["summary"]
+
+        test_names = [test["nodeid"] for test in report["collectors"][0]["result"]]
+        for test in report["tests"]:
+            name = test["nodeid"].split("::")[0]
+            all_tests[name].append(test["outcome"] == "passed")
+        all_passed_tasks = [name for name, outcome in all_tests.items() if all(outcome)]
+        return all_passed_tasks
+    else:
+        return []
+
+
+def git_clone_repo(repo_url, dst_dir):
+    returncode, stderr = call_command(
+        f"git clone {repo_url} {dst_dir}",
+        verbose=False,
+        return_stderr=True,
+    )
+    if returncode != 0:
+        if "could not resolve host" in stderr.lower():
+            raise PynengError(
+                red(
+                    "Failed to clone the repository. Maybe you don't have internet access?"
+                )
+            )
+        else:
+            raise PynengError(red(f"Failed to copy files. {stderr}"))
+
+
+def copy_answers(passed_tasks):
+    """
+    The function clones the answer repository and copies the answers for tasks
+    that pass the tests.
+    """
+    pth = str(pathlib.Path().absolute())
+    current_chapter_name = os.path.split(pth)[-1]
+    current_chapter_number = int(current_chapter_name.split("_")[0])
+
+    homedir = pathlib.Path.home()
+    os.chdir(homedir)
+    if os.path.exists("pyneng-answers"):
+        shutil.rmtree("pyneng-answers", onerror=remove_readonly)
+    git_clone_repo(ANSWERS_URL, "pyneng-answers")
+    os.chdir(os.path.join("pyneng-answers", "answers", current_chapter_name))
+    copy_answer_files(passed_tasks, pth)
+    print(
+        green(
+            "\nAnswers to tasks that passed the tests are copied to the files "
+            "answer_task_x.py\n"
+        )
+    )
+    os.chdir(homedir)
+    shutil.rmtree("pyneng-answers", onerror=remove_readonly)
+    os.chdir(pth)
+
+
+def copy_answer_files(passed_tasks, pth):
+    """
+    The function copies the answers for the specified tasks.
+    """
+    for test_file in passed_tasks:
+        task_name = test_file.replace("test_", "")
+        task_name = re.search(r"task_\w+\.py", task_name).group()
+        answer_name = test_file.replace("test_", "answer_")
+        answer_name = re.search(r"answer_task_\w+\.py", answer_name).group()
+        pth_answer = os.path.join(pth, answer_name)
+        if not os.path.exists(pth_answer):
+            shutil.copy2(task_name, pth_answer)
+
+
+def clone_or_pull_task_repo():
+    source_pth = str(pathlib.Path().absolute())
+    homedir = pathlib.Path.home()
+    os.chdir(homedir)
+    if os.path.exists(TASKS_LOCAL_REPO):
+        os.chdir(TASKS_LOCAL_REPO)
+        call_command("git pull")
+        os.chdir(homedir)
+    else:
+        git_clone_repo(TASKS_URL, TASKS_LOCAL_REPO)
+    os.chdir(source_pth)
+
+
+def copy_tasks_tests_from_repo(tasks, tests):
+    """
+    The function clones the repository with the latest version of tasks and
+    copies the specified tasks to the current directory.
+    """
+    source_pth = str(pathlib.Path().absolute())
+    current_chapter_name = os.path.split(source_pth)[-1]
+    current_chapter_number = int(current_chapter_name.split("_")[0])
+
+    clone_or_pull_task_repo()
+
+    homedir = pathlib.Path.home()
+    os.chdir(
+        os.path.join(homedir, TASKS_LOCAL_REPO, "exercises", current_chapter_name)
+    )
+    copy_task_test_files(source_pth, tasks, tests)
+    print(green("\nUpdated tasks and tests copied"))
+    os.chdir(source_pth)
+
+
+def copy_task_test_files(source_pth, tasks=None, tests=None):
+    """
+    The function copies task and test files.
+    """
+    file_list = []
+    if tasks:
+        file_list += tasks
+    if tests:
+        file_list += tests
+    for file in file_list:
+        shutil.copy2(file, os.path.join(source_pth, file))
+
+
+def save_working_dir(branch="main"):
+    if not working_dir_clean():
+        print(
+            red(
+                "Updating tests and tasks will overwrite the contents of unsaved files!".upper()
+            )
+        )
+        user_input = input(
+            red(
+                "There are unsaved changes in the current directory! "
+                "Do you want to save them? [y/n]: "
+            )
+        )
+        if user_input.strip().lower() not in ("n", "no"):
+            save_changes_to_github(
+                "Saving changes before updating tasks", branch=branch
+            )
+            print(
+                green(
+                    "All changes in the current directory are saved. Let's start updating..."
+                )
+            )
+
+
+def working_dir_changed_diff(branch="main"):
+    print(red("The following files have been updated:"))
+    show_git_diff_short()
+    print(
+        "\nThis is a short diff, if you want to see all the differences in detail, "
+        "press n and run git diff command.\nYou can also undo your changes with "
+        "git checkout -- file (or git restore file) if you want."
+    )
+
+    user_input = input(red("\nSave changes and add to github? [y/n]: "))
+    if user_input.strip().lower() not in ("n", "no"):
+        save_changes_to_github("Updating tasks", branch=branch)
+
+
+def change_tasks_lang(lang):
+    global TASKS_URL
+    global TASKS_LOCAL_REPO
+    TASKS_URL = LANG_TASKS_URL.get(lang)
+    TASKS_LOCAL_REPO = LANG_TASKS_LOCAL_REPO.get(lang)
+
+
+def update_tasks_and_tests(tasks_list, tests_list, lang, branch="main"):
+    change_tasks_lang(lang)
+    save_working_dir(branch=branch)
+    copy_tasks_tests_from_repo(tasks_list, tests_list)
+    if working_dir_clean():
+        print(green("Tasks and tests are already the latest version"))
+        return False
+    else:
+        working_dir_changed_diff(branch=branch)
+        return True
+
+
+def update_chapters_tasks_and_tests(update_chapters, lang, branch="main"):
+    change_tasks_lang(lang)
+    save_working_dir(branch=branch)
+    copy_chapters_from_repo(update_chapters)
+    if working_dir_clean():
+        print(green("All chapters are up to date"))
+        return False
+    else:
+        working_dir_changed_diff(branch=branch)
+        return True
+
+
+def copy_chapters_from_repo(chapters_list):
+    """
+    The function clones the repository with the latest version of tasks and
+    copies the specified tasks to the current directory.
+    """
+    source_pth = str(pathlib.Path().absolute())
+    clone_or_pull_task_repo()
+
+    homedir = pathlib.Path.home()
+    os.chdir(os.path.join(homedir, TASKS_LOCAL_REPO, "exercises"))
+    copy_chapters(source_pth, chapters_list)
+    print(green("\nUpdated sections copied"))
+    os.chdir(source_pth)
+
+
+def copy_chapters(source_pth, chapters_list):
+    """
+    Function copies chapters
+    """
+    for chapter in chapters_list:
+        to_path = os.path.join(source_pth, chapter)
+        if os.path.exists(to_path):
+            shutil.rmtree(to_path)
+        shutil.copytree(chapter, to_path)
