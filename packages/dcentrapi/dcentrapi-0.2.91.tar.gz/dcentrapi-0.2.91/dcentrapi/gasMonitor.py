cfd7ff93966b31f4115470721ce8575d1dcbb512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:44 +0000

    Add package dcentrapi/0.2.91/dcentrapi-0.2.91.tar.gz

---
+++ packages/dcentrapi/dcentrapi-0.2.91.tar.gz/dcentrapi-0.2.91/dcentrapi/gasMonitor.py
@@ -0,0 +1,16 @@
+from dcentrapi.Base import Base
+from dcentrapi.modules.requests import requests_get
+
+
+class GasMonitor(Base):
+
+    def get_optimal_gas_price(self, network_name, minutes, stats=None, values=None):
+        url = self.url + "gas_monitor/optimal_gas_price_for_network"
+        data = {
+            "network_name": network_name,
+            "minutes": minutes,
+            "stats": stats,
+            "values": values,
+        }
+        response = requests_get(url, params=data, headers=self.headers)
+        return response.json()
