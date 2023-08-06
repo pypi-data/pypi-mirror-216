78b586e2c94c82dcde0470af02491690d5571218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:10 +0000

    Add package py-ke-utils-fastapi/1.0.4/py-ke-utils-fastapi-1.0.4.tar.gz

---
+++ packages/py-ke-utils-fastapi/py-ke-utils-fastapi-1.0.4.tar.gz/py-ke-utils-fastapi-1.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0.4"
+DESCRIPTION = "Utilities with Redis and FastAPI"
+
+# Setting up
+setup(
+    name="py-ke-utils-fastapi",
+    version=VERSION,
+    author="KE",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "redis>=4.5.0",
+        "deepdiff>=6.2.1",
+        "PyJWT>=2.0.0"
+    ],
+    keywords=["python", "FastAPI", "PyJWT", "decorator", "token"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+    ],
+)
