b51f41c48c5c7d9b89fb77e850250d16a072a048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:00 +0000

    Add package pdm-autoexport/0.2.0/pdm_autoexport-0.2.0.tar.gz

---
+++ packages/pdm-autoexport/pdm_autoexport-0.2.0.tar.gz/pdm_autoexport-0.2.0/tests/test_autoexport.py
@@ -0,0 +1,38 @@
+import shutil
+from pathlib import Path
+
+import pytest
+from pdm.core import Core
+from pdm.project import Project
+
+TEMPLATE_PYPROJECT = Path(__file__).parent / "pyproject.toml"
+
+
+@pytest.fixture()
+def tmp_project(tmp_path: Path) -> Project:
+    shutil.copy2(TEMPLATE_PYPROJECT, tmp_path / "pyproject.toml")
+    core = Core()
+    return core.create_project(tmp_path)
+
+
+def test_pdm_autoexport(tmp_project: Project):
+    tmp_project.core.main(["lock", "-v"], obj=tmp_project)
+    requirements_txt = tmp_project.root.joinpath("requirements.txt").read_text()
+    requirements_test_txt = tmp_project.root.joinpath(
+        "requirements-test.txt"
+    ).read_text()
+    assert any(line.startswith("requests==") for line in requirements_txt.splitlines())
+    assert any(line.startswith("urllib3==") for line in requirements_txt.splitlines())
+    assert any(
+        line.strip().startswith("--hash=") for line in requirements_txt.splitlines()
+    )
+    assert not any(
+        line.startswith("requests==") for line in requirements_test_txt.splitlines()
+    )
+    assert any(
+        line.startswith("pytest==") for line in requirements_test_txt.splitlines()
+    )
+    assert not any(
+        line.strip().startswith("--hash=")
+        for line in requirements_test_txt.splitlines()
+    )
