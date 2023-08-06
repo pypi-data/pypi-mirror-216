3f62a22affafa7b01b25c8c6a6131dc1412b1f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:02 +0000

    Add package dcentrapi/0.2.9/dcentrapi-0.2.9.tar.gz

---
+++ packages/dcentrapi/dcentrapi-0.2.9.tar.gz/dcentrapi-0.2.9/dcentrapi/__init__.py
@@ -0,0 +1,7 @@
+from dcentrapi.Base import Base
+from dcentrapi.eventPolling import EventPolling
+from dcentrapi.rpcAggregation import RpcAggregation
+from dcentrapi.gasMonitor import GasMonitor
+from dcentrapi.web3Index import Web3Index
+from dcentrapi.hackMitigation import HackMitigation
+# from dcentrapi.merkleTree import MerkleTree, MerkleTreeNode
---
+++ packages/dcentrapi/dcentrapi-0.2.9.tar.gz/dcentrapi-0.2.9/dcentrapi/web3Index.py
@@ -0,0 +1,22 @@
+import requests
+
+from dcentrapi.Base import Base
+
+
+class Web3Index(Base):
+    def get_pairs(self, network_name: str, token_address: str):
+        url = self.web3index_url + "pairs" + f"/{network_name}/{token_address}"
+        response = requests.get(url, headers=self.headers)
+        return response.json()
+
+    def get_factories(self):
+        url = self.web3index_url + "factories"
+        response = requests.get(url, headers=self.headers)
+        return response.json()
+
+    def get_token_price_snapshot(self, info):
+        # Currently info is token symbol (str), e.g. "XCAD"
+        # In future, might also have the base token id (int)
+        url = self.web3index_url + "token_price_snapshot" + f"/{info}"
+        response = requests.get(url, headers=self.headers)
+        return response.json()
