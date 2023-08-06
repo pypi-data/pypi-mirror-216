6243731f0fb0b95432db9eac6e87cb2b98202892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:41 +0000

    Add package GB-distributions2004/1.0/GB_distributions2004-1.0.tar.gz

---
+++ packages/GB-distributions2004/GB_distributions2004-1.0.tar.gz/GB_distributions2004-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GB_distributions2004',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['GB_distributions2004'],
+      author= 'Nguyễn Đức Tâm',
+      author_email = 'tamnd04@gmail.com',
+      zip_safe=False)
