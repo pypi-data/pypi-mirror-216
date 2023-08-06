99d76d13cbae43a5840cc87c648fa53a5910ed9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:04 +0000

    Add package dgl/1.0.4/dgl-1.0.4-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/dgl/dgl-1.0.4-cp38-cp38-manylinux1_x86_64.whl/dgl/dataloading/__init__.py
@@ -0,0 +1,13 @@
+"""Package for dataloaders and samplers."""
+from .. import backend as F
+from . import negative_sampler
+from .base import *
+from .cluster_gcn import *
+from .graphsaint import *
+from .labor_sampler import *
+from .neighbor_sampler import *
+from .shadow import *
+
+if F.get_preferred_backend() == "pytorch":
+    from .dataloader import *
+    from .dist_dataloader import *
