047ef98020d869b786d4899a9136eb82d04d4b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:43 +0000

    Add package herbiv/0.1a8/herbiv-0.1a8-py3-none-any.whl

---
+++ packages/herbiv/herbiv-0.1a8-py3-none-any.whl/herbiv/get.py
@@ -0,0 +1,184 @@
+import os
+import pandas as pd
+
+
+def get_formula(by, items):
+    """
+    读取HerbiV_formula数据集，返回items中复方的信息
+    :param by: str类型，数据集中与items相匹配的列的列名
+    :param items: 任何可以使用in判断一个元素是否在其中的组合数据类型，存放要查询的复方
+    :return: pd.DataFrame类型，items中复方的信息
+    """
+
+    # 读取数据集
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    formula_all = pd.read_csv(current_directory + r'/data/HerbiV_formula.csv')
+
+    # 在HerbiV_tcm中获取items中复方的信息
+    formula = formula_all.loc[formula_all[by].isin(items)].copy()
+
+    # 重新设置索引
+    formula.index = range(formula.shape[0])
+
+    return formula
+
+
+def get_formula_tcm_links(by, items):
+    """
+    读取HerbiV_formula_tcm_links数据集，返回items中复方/中药的复方-中药连接信息
+    :param by: str类型，数据集中与items相匹配的列的列名
+    :param items: pd.DataFrame或其他任何可以使用in判断一个元素是否在其中的组合数据类型，存放要查询的复方/中药
+    :return: pd.DataFrame类型，items中复方/中药的复方-中药连接信息
+    """
+    # 读取数据集
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    formula_tcm_links_all = pd.read_csv(current_directory + r'/data/HerbiV_formula_tcm_links.csv')
+
+    # 在HerbiV_formula_tcm_links中获取items中复方/中药的复方-中药连接信息
+    formula_tcm_links = formula_tcm_links_all.loc[formula_tcm_links_all[by].isin(items)].copy()
+
+    # 重新设置索引
+    formula_tcm_links.index = range(formula_tcm_links.shape[0])
+
+    return formula_tcm_links
+
+
+def get_tcm(by, items):
+    """
+    读取HerbiV_tcm数据集，返回items中中药的信息
+    :param by: str类型，数据集中与items相匹配的列的列名
+    :param items: 任何可以使用in判断一个元素是否在其中的组合数据类型，存放要查询的中药
+    :return: pd.DataFrame类型，items中中药的信息
+    """
+
+    # 读取数据集
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    tcm_all = pd.read_csv(current_directory + r'/data/HerbiV_tcm.csv')
+
+    # 在HerbiV_tcm中获取items中中药的信息
+    tcm = tcm_all.loc[tcm_all[by].isin(items)].copy()
+
+    # 重新设置索引
+    tcm.index = range(tcm.shape[0])
+
+    return tcm
+
+
+def get_tcm_chem_links(by, items):
+    """
+    读取HerbiV_tcm_chemical_links数据集，返回items中中药/化合物的中药-成分信息
+    :param by: str类型，数据集中与items相匹配的列的列名
+    :param items: pd.DataFrame或其他任何可以使用in判断一个元素是否在其中的组合数据类型，存放要查询的中药/化合物
+    :return: pd.DataFrame类型，items中中药/化合物的中药-成分信息
+    """
+    # 读取数据集
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    tcm_chem_links_all = pd.read_csv(current_directory + r'/data/HerbiV_tcm_chemical_links.csv')
+
+    # 在HerbiV_tcm_chemical_links中获取items中中药/化合物的中药-成分连接信息
+    tcm_chem_links = tcm_chem_links_all.loc[tcm_chem_links_all[by].isin(items)].copy()
+
+    # 重新设置索引
+    tcm_chem_links.index = range(tcm_chem_links.shape[0])
+
+    return tcm_chem_links
+
+
+def get_chemicals(by, items):
+    """
+    读取HerbiV_chemicals数据集，返回items中化合物的信息
+    :param by: str类型，数据集中与items相匹配的列的列名
+    :param items: pd.DataFrame或其他任何可以使用in判断一个元素是否在其中的组合数据类型，存放要查询的化合物
+    :return: pd.DataFrame类型，items中化合物的信息
+    """
+    # 读取数据集
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    chem_all = pd.read_csv(current_directory + r'/data/HerbiV_chemicals.csv')
+
+    # 在HerbiV_chemical_protein_links中获取items中化合物的信息
+    chem = chem_all.loc[chem_all[by].isin(items)].drop_duplicates(subset=['HVCID'])
+
+    # 重新设置索引
+    chem.index = range(chem.shape[0])
+
+    return chem
+
+
+def get_chem_protein_links(by, items, score=900):
+    """
+    读取HerbiV_chemicals数据集，返回items中化合物/蛋白质的combined_score大于等于score的化合物-蛋白质（靶点）连接信息
+    :param by: str类型，数据集中与items相匹配的列的列名
+    :param items: pd.DataFrame或其他任何可以使用in判断一个元素是否在其中的组合数据类型，存放要查询的化合物/蛋白质
+    :param score: int类型，仅combined_score大于等于score的记录会被筛选出，默认为900
+    :return: pd.DataFrame类型，items中化合物/蛋白质的combined_score大于等于score的化合物-蛋白质（靶点）连接信息
+    """
+    # 读取数据集
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    chem_protein_links_all = pd.read_csv(current_directory + r'/data/HerbiV_chemical_protein_links.csv')
+
+    # 在HerbiV_chemical_protein_links中获取items中化合物/蛋白质的combined_score大于等于score的化合物-蛋白质（靶点）连接信息
+    chem_protein_links = chem_protein_links_all.loc[
+        (chem_protein_links_all[by].isin(items)) &
+        (chem_protein_links_all['Combined_score'] >= score)].copy()
+
+    # 将Combined_score变换为0-1的浮点数
+    chem_protein_links.loc[:, 'Combined_score'] = chem_protein_links.loc[:, 'Combined_score'].apply(
+        lambda x: x / 1000)
+
+    # 重新设置索引
+    chem_protein_links.index = range(chem_protein_links.shape[0])
+
+    return chem_protein_links
+
+
+def get_proteins(by, items):
+    r"""
+    从HerbiV_proteins中获得基因名与其对应的Ensemble_ID,从而可以被from_genes函数所调用，这样直接输入基因名即可
+    :param by: str类型，数据集中与items相匹配的列的列名
+    :param items: pd.DataFrame或其他任何可以使用in判断一个元素是否在其中的组合数据类型，存放要查询的蛋白质
+    :return:返回一个包含对应信息的字典
+    :return:返回一个数据框
+    """
+
+    # 数据的输入
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    proteins_all = pd.read_csv(current_directory + r'/data/HerbiV_proteins.csv')
+
+    # 在HerbiV_chemical_proteins中获取items中化合物的信息
+    proteins = proteins_all.loc[proteins_all[by].isin(items)].drop_duplicates(subset=['Ensembl_ID'])
+
+    # 重置索引
+    proteins.index = range(proteins.shape[0])
+
+    return proteins
+
+
+def get_tcm_or_formula(tcm_or_formula):
+    """
+    获取tcm_or_formula中元素对应的中药、复方及其连接信息
+    :param tcm_or_formula: 任何可以使用in判断一个元素是否在其中的组合数据类型，拟获取的中药或复方的ID
+    :return: formula: pd.DataFrame类型，复方信息
+    :return: tcm: pd.DataFrame类型，中药信息
+    :return: formula_tcm_links: pd.DataFrame类型，复方-中药连接信息
+    """
+    if tcm_or_formula[0][2] == 'P':  # 判断输入是否为复方的HVPID
+        formula = get_formula('HVPID', tcm_or_formula)
+        formula_tcm_links = get_formula_tcm_links('HVPID', formula['HVPID'])
+        tcm = get_tcm('HVMID', formula_tcm_links['HVMID'])
+    else:
+        formula = None
+        formula_tcm_links = None
+        tcm = get_tcm('HVMID', tcm_or_formula)
+    return formula, tcm, formula_tcm_links
+
+
+if __name__ == '__main__':
+    formula_info = get_formula('HVPID', ['HVP1625'])
+    formula_tcm_links_info = get_formula_tcm_links('HVPID', formula_info['HVPID'])
+    tcm_info = get_tcm('HVMID', formula_tcm_links_info['HVMID'])
+    tcm_chem_links_info = get_tcm_chem_links('HVMID', tcm_info['HVMID'])
+    chem_info = get_chemicals('HVCID', tcm_chem_links_info['HVCID'])
+    chem_protein_links_info = get_chem_protein_links('HVCID', chem_info['HVCID'])
+    protein_info = get_proteins('Ensembl_ID', chem_protein_links_info['Ensembl_ID'])
+    formula_info1, tcm_info1, formula_tcm_links_info1 = get_tcm_or_formula(['HVP1625'])
+    formula_info2, tcm_info2, formula_tcm_links_info2 = get_tcm_or_formula(['HVM0367', 'HVM1695'])
---
+++ packages/herbiv/herbiv-0.1a8-py3-none-any.whl/herbiv/output.py
@@ -0,0 +1,177 @@
+import os
+import pandas as pd
+from pyecharts import options as opts
+from pyecharts.charts import Graph
+from pyecharts.globals import ThemeType
+
+
+def out_for_cyto(tcm, tcm_chem_links, chem, chem_protein_links, protein, path='result/'):
+    r"""
+    输出Cytoscape用于作图的网络文件和属性文件
+    :param tcm: pd.DataFrame类型，中药信息
+    :param tcm_chem_links: pd.DataFrame类型，中药-化合物（中药成分）连接信息
+    :param chem: pd.DataFrame类型，化合物（中药成分）信息
+    :param chem_protein_links: pd.DataFrame类型，化合物（中药成分）-蛋白质（靶点）连接信息
+    :param path: 字符串类型，存放结果的目录
+    """
+    tcm_c = tcm.copy()
+    tcm_chem_links_c = tcm_chem_links.copy()
+    chem_c = chem.copy()
+    chem_protein_links_c = chem_protein_links.copy()
+    protein_c = protein.copy()
+
+    # 若无path目录，先创建该目录
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    out_chem_protein_links = chem_protein_links_c.iloc[:, 0:2]
+    out_chem_protein_links.columns = ['SourceNode', 'TargetNode']
+
+    out_chem_protein_links.loc[:, 'SourceNode'] = out_chem_protein_links.loc[:, 'SourceNode'].apply(
+        lambda x: chem_c.loc[chem_c['HVCID'] == x]['Name'].iloc[0] if len(
+            chem_c.loc[chem_c['HVCID'] == x]['Name']) > 0 else None)
+
+    out_chem_protein_links.dropna(subset=['SourceNode'], inplace=True)
+
+    out_chem_protein_links.loc[:, 'TargetNode'] = out_chem_protein_links.loc[:, 'TargetNode'].apply(
+        lambda x: protein_c.loc[protein_c['Ensembl_ID'] == x]['gene_name'].iloc[0] if len(
+            protein_c.loc[protein_c['Ensembl_ID'] == x]['gene_name']) > 0 else None)
+
+    out_chem_protein_links.dropna(subset=['TargetNode'], inplace=True)
+
+    out_tcm_chem = tcm_chem_links_c.iloc[:, 0:2]
+    out_tcm_chem.columns = ['SourceNode', 'TargetNode']
+
+    out_tcm_chem.loc[:, 'SourceNode'] = out_tcm_chem.loc[:, 'SourceNode'].apply(
+        lambda x: tcm_c.loc[tcm_c['HVMID'] == x]['cn_name'].iloc[0] if len(
+            tcm_c.loc[tcm_c['HVMID'] == x]['cn_name']) > 0 else None)
+
+    out_tcm_chem.dropna(subset=['SourceNode'], inplace=True)
+
+    out_tcm_chem.loc[:, 'TargetNode'] = out_tcm_chem.loc[:, 'TargetNode'].apply(
+        lambda x: chem_c.loc[chem_c['HVCID'] == x]['Name'].iloc[0] if len(
+            chem_c.loc[chem_c['HVCID'] == x]['Name']) > 0 else None)
+
+    out_tcm_chem.dropna(subset=['TargetNode'], inplace=True)
+
+    out_chem = chem_c.loc[:, ['Name']]
+    out_chem.columns = ['Key']
+    out_chem['Attribute'] = 'Chemicals'
+
+    out_tcm = tcm_c.loc[:, ['cn_name']]
+    out_tcm.columns = ['Key']
+    out_tcm['Attribute'] = 'TCM'
+
+    out_gene = protein_c.loc[:, ['gene_name']]
+    out_gene.columns = ['Key']
+    out_gene['Attribute'] = 'Proteins'
+
+    # 输出Network文件
+    pd.concat([out_chem_protein_links, out_tcm_chem]).to_csv(path + 'Network.csv', index=False)
+
+    # 输出Type文件
+    pd.concat([out_tcm, out_chem, out_gene]).to_csv(path + 'Type.csv', index=False)
+
+
+def vis(tcm, tcm_chem_links, chem, chem_protein_links, protein, path='result/'):
+    r"""
+    使用NetworkX可视化分析结果
+    :param tcm: pd.DataFrame类型，中药信息
+    :param tcm_chem_links: pd.DataFrame类型，中药-化合物（中药成分）连接信息
+    :param chem: pd.DataFrame类型，化合物（中药成分）信息
+    :param chem_protein_links: pd.DataFrame类型，化合物（中药成分）-蛋白质（靶点）连接信息
+    :param protein: pd.DataFrame类型，蛋白质（靶点）连接信息
+    :param path: 字符串类型，存放结果的目录
+    """
+    # 若无path目录，先创建该目录
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    #chem_protein_links = pd.DataFrame(chem_protein_links)
+    #tcm_chem_links = pd.DataFrame(tcm_chem_links)
+
+    tcm_chem_merged_1 = pd.merge(tcm_chem_links, tcm, on='HVMID', how='left')
+    tcm_chem_links['HVMID'] = tcm_chem_merged_1['pinyin_name']
+
+    tcm_chem_merged_2 = pd.merge(tcm_chem_links, chem, on='HVCID', how='left')
+    tcm_chem_links['HVCID'] = tcm_chem_merged_2['Name']
+
+    tcm_chem_links = tcm_chem_links[['HVMID', 'HVCID']]
+
+    chem_protein_merged_1 = pd.merge(chem_protein_links, chem, on='HVCID', how='right')
+    chem_protein_links['HVCID'] = chem_protein_merged_1['Name']
+    chem_protein_merged_2 = pd.merge(chem_protein_links, protein, on='Ensembl_ID', how='left')
+    chem_protein_links['Ensembl_ID'] = chem_protein_merged_2['gene_name']
+
+    chem_protein_links = chem_protein_links[['HVCID', 'Ensembl_ID']]
+
+    tcm_chem_links = tcm_chem_links.rename(columns = {"HCMID": "pinyin_name", "HVCID": "Name"})
+    chem_protein_links = chem_protein_links.rename(columns = {"HVCID": "Name", "Ensembl_ID": "gene_name"})
+
+    nodes = []
+    edges = []
+
+    categories = [
+        {"name": "中药", "color": "#61a0a8"},
+        {"name": "化学成分", "color": "#f47920"},
+        {"name": "靶点", "color": "#ca8622"},
+    ]
+
+    for index, row in tcm_chem_links.iloc[1:].iterrows():
+        chinese_medicine = row[0]
+        chemical_component = row[1]
+        nodes.append({'name': chinese_medicine, "symbolSize": 20, 'category': 0, "color": "#1FA9E9"})
+        nodes.append({'name': chemical_component, "symbolSize": 20, 'category': 1, "color": "#FFFF00"})
+        edges.append({'source': chinese_medicine, 'target': chemical_component})
+
+    for index, row in chem_protein_links.iloc[1:].iterrows():
+        chemical_component = row[0]
+        target = row[1]
+        nodes.append({'name': chemical_component, "symbolSize": 20, 'category': 1, "color": "#FFFF00"})
+        nodes.append({'name': target, "symbolSize": 20, 'category': 2, "color": "#000000"})
+        edges.append({'source': chemical_component, 'target': target})
+    unique_list = list(set(tuple(item.items()) for item in nodes))
+    nodes = [dict(item) for item in unique_list]
+
+    unique_list = list(set(tuple(item.items()) for item in nodes))
+    nodes = [dict(item) for item in unique_list]
+
+    Graph(init_opts=opts.InitOpts(width="2400px", height="1200px"))\
+        .add(
+        '',
+        nodes=nodes,
+        links=edges,
+        categories=categories,
+        repulsion=8000,
+        layout="circular",
+        is_rotate_label=True,
+        linestyle_opts=opts.LineStyleOpts(color="source", curve=0.3),
+        label_opts=opts.LabelOpts(position="right")
+    )\
+        .set_global_opts(
+        title_opts=opts.TitleOpts(title=''),
+        legend_opts=opts.LegendOpts(orient="vertical", pos_left="2%", pos_top="20%")
+    )\
+        .render(path=path + "Graph.html")
+
+
+
+if __name__ == '__main__':
+    import get
+
+    formula_info = get.get_formula('HVPID', ['HVP1625'])
+    formula_tcm_links_info = get.get_formula_tcm_links('HVPID', formula_info['HVPID'])
+    tcm_info = get.get_tcm('HVMID', formula_tcm_links_info['HVMID'])
+    tcm_chem_links_info = get.get_tcm_chem_links('HVMID', tcm_info['HVMID'])
+    chem_info = get.get_chemicals('HVCID', tcm_chem_links_info['HVCID'])
+    chem_protein_links_info = get.get_chem_protein_links('HVCID', chem_info['HVCID'], 990)
+    protein_info = get.get_proteins('Ensembl_ID', chem_protein_links_info['Ensembl_ID'])
+
+    chem_info = chem_info.loc[chem_info.loc[:, 'HVCID'].isin(chem_protein_links_info['HVCID'])]
+    tcm_chem_links_info = tcm_chem_links_info.loc[tcm_chem_links_info.loc[:, 'HVCID'].isin(chem_info['HVCID'])]
+    tcm_info = tcm_info.loc[tcm_info.loc[:, 'HVMID'].isin(tcm_chem_links_info['HVMID'])]
+    # 重新编号（chem和tcm在计算score时会重新编号，此处不再重新编号）
+    tcm_chem_links_info.index = range(tcm_chem_links_info.shape[0])
+
+    vis(tcm_info, tcm_chem_links_info, chem_info, chem_protein_links_info, protein_info)
+    out_for_cyto(tcm_info, tcm_chem_links_info, chem_info, chem_protein_links_info, protein_info)
