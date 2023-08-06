f89ec26d999568eae96b7e3d7333266d2a7c274f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:44 +0000

    Add package windmill-api/1.123.1/windmill_api-1.123.1-py3-none-any.whl

---
+++ packages/windmill-api/windmill_api-1.123.1-py3-none-any.whl/windmill_api/models/delete_completed_job_response_200_job_kind.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class DeleteCompletedJobResponse200JobKind(str, Enum):
+    SCRIPT = "script"
+    PREVIEW = "preview"
+    DEPENDENCIES = "dependencies"
+    FLOW = "flow"
+    FLOWPREVIEW = "flowpreview"
+    SCRIPT_HUB = "script_hub"
+    IDENTITY = "identity"
+    HTTP = "http"
+    GRAPHQL = "graphql"
+    POSTGRESQL = "postgresql"
+
+    def __str__(self) -> str:
+        return str(self.value)
