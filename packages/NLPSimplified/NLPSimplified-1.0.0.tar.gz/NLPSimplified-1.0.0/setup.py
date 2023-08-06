1bab44b8f2ac2be2713f7394f7906101ad227a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:59 +0000

    Add package NLPSimplified/1.0.0/NLPSimplified-1.0.0.tar.gz

---
+++ packages/NLPSimplified/NLPSimplified-1.0.0.tar.gz/NLPSimplified-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='NLPSimplified',
+    version='1.0.0',
+    description='A simple NLP pipeline using spaCy',
+    author='Avneh Singh Bhatia',
+    author_email='avnehb@gmail.com',
+    packages=['NLPSimplified'],
+    install_requires=[
+        'spacy>=3.0.0,<4.0.0',
+    ],
+)
