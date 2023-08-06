c015cceb741cfa7efb747e5646e099c7f74908de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:38 +0000

    Add package django-sizefield/2.1.0/django-sizefield-2.1.0.tar.gz

---
+++ packages/django-sizefield/django-sizefield-2.1.0.tar.gz/django-sizefield-2.1.0/README.rst
@@ -0,0 +1,71 @@
+
+*django-sizefield* is a file size field, stored as BigInteger and rendered
+with units in Bytes (KB, MB, ...)
+
+.. image:: https://github.com/leplatrem/django-sizefield/actions/workflows/tests.yml/badge.svg
+    :target: https://github.com/leplatrem/django-sizefield/actions/workflows/tests.yml
+
+.. image:: https://coveralls.io/repos/leplatrem/django-sizefield/badge.png
+    :target: https://coveralls.io/r/leplatrem/django-sizefield
+
+
+=======
+INSTALL
+=======
+
+::
+
+    pip install django-sizefield
+
+
+=====
+USAGE
+=====
+
+Model field
+===========
+
+::
+
+    class Data(models.Model):
+        path = models.FilePathField()
+        size = FileSizeField()
+
+
+The model form will have a TextInput, which renders the 
+value with units, and accepts values with or without units.
+
+
+Template filter
+===============
+
+It adds units to any number value:
+
+::
+
+    {% load sizefieldtags %}
+    
+    {{ value|filesize }}
+
+*will render 12.3KB (for example)*
+
+
+=======
+AUTHORS
+=======
+
+    * Mathieu Leplatre <contact@mathieu-leplatre.info>
+    * Alexander (@meteozond)
+    * Tom Yam (@perez)
+
+
+=======
+LICENSE
+=======
+
+    * Lesser GNU Public License
+
+
+
+
+
---
+++ packages/django-sizefield/django-sizefield-2.1.0.tar.gz/django-sizefield-2.1.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='django-sizefield',
+    version='2.1.0',
+    author='Mathieu Leplatre',
+    author_email='contact@mathieu-leplatre.info',
+    url='https://github.com/leplatrem/django-sizefield',
+    download_url="http://pypi.python.org/pypi/django-sizefield/",
+    description="A model field to store a file size, whose edition and display shows units.",
+    long_description=open(os.path.join(here, 'README.rst')).read() + '\n\n' +
+                     open(os.path.join(here, 'CHANGES')).read(),
+    license='LPGL, see LICENSE file.',
+    install_requires=[
+        'Django',
+    ],
+    tests_require=[
+        'tox'
+    ],
+    python_requires='>=3.5',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=['Topic :: Utilities', 
+                 'Natural Language :: English',
+                 'Operating System :: OS Independent',
+                 'Intended Audience :: Developers',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Development Status :: 5 - Production/Stable',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 ],
+)
