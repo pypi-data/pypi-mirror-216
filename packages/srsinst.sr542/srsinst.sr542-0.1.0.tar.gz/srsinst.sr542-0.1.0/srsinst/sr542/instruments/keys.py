b85d565a152938451e4cccac276b4a4a15310e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:03 +0000

    Add package srsinst.sr542/0.1.0/srsinst.sr542-0.1.0.tar.gz

---
+++ packages/srsinst.sr542/srsinst.sr542-0.1.0.tar.gz/srsinst.sr542-0.1.0/srsinst/sr542/instruments/keys.py
@@ -0,0 +1,53 @@
+
+class Keys:
+    # Config component
+    INTERNAL = 'internal'
+    VCO = 'vco'
+    LINE = 'line'
+    EXTERNAL = 'external'
+
+    RISE = 'rise'
+    FALL = 'fall'
+    SINE = 'sine'
+
+    SHAFT = 'shaft'
+    INNER = 'inner'
+    OUTER = 'outer'
+
+    # Operate component
+    OFF = 'off'
+    ON = 'on'
+
+    SOURCE = 'source'
+    SUM = 'sum'
+    DIFF = 'diff'
+    CTRL = 'ctrl'
+
+    PHASE = 'phase'
+    MULTIN = 'multin'
+    DIVM = 'divm'
+    VCOFS = 'vcofs'
+
+    # Status component
+    ESB = 'ESB'
+    MSS = 'MSS'
+    CHSB = 'CHSB'
+
+    OPC = 'Operation complete'
+    INP = 'Input Buffer Overrun'
+    QYE = 'Query Error'
+    DDE = 'Device Dependent Error'
+    EXE = 'Execution Error'
+    CME = 'Command Error'
+    URQ = 'User Request'
+    PON = 'Power On'
+
+    MON = 'MON'
+    EL = 'EL'
+    FL = 'FL'
+    PL = 'PL'
+    CMAX = 'CMAX'
+    TMAX = 'TMAX'
+
+    ChopperHeadMemoryFail = 'chopper head memory fail'
+    ChopperHeadDisconnect = 'chopper head disconnect'
