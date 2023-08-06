201df572da196d62fb4d9d392717845ff24d9532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:19 +0000

    Add package bno-probability/0.1/bno_probability-0.1.tar.gz

---
+++ packages/bno-probability/bno_probability-0.1.tar.gz/bno_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='bno_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['bno_probability'],
+      zip_safe=False)
