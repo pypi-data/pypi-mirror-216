c31f3f666d72fc8a7f06e1f6ca23ca820691b573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:38 +0000

    Add package openai-dbd/0.0.30/openai_dbd-0.0.30-py3-none-any.whl

---
+++ packages/openai-dbd/openai_dbd-0.0.30-py3-none-any.whl/openai_dbd/__init__.py
@@ -0,0 +1,10 @@
+from .openai_dbd import (
+    sql_prompt,
+    json_prompt,
+    correct_schema,
+    generate_sql,
+    generate_excel,
+    convert_json_to_sql,
+    json_to_python_models,
+    json_to_sequelize_models,
+)
