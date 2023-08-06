3bcbaa4f7b241a0956752f59a766f2ce0c655b7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:07 +0000

    Add package string-treatment/0.0.5/string_treatment-0.0.5.tar.gz

---
+++ packages/string-treatment/string_treatment-0.0.5.tar.gz/string_treatment-0.0.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5' 
+DESCRIPTION = 'String treatment package'
+LONG_DESCRIPTION = '''
+Method to treat strings with inconsistencies in your data
+
+Github: https://github.com/guilhermehuther/string_treatment
+'''
+
+setup(
+        scripts=['string_treatment.py'],
+        name="string_treatment", 
+        version=VERSION,
+        author="Guilherme Huther Baldo",
+        author_email="guilhermehuther@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['jellyfish', 'numpy', 'pandas'],
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
