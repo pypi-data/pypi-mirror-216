a44e8290e79f918e06a11c340727a3e814ac4b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:42 +0000

    Add package yz-core2/1.0.56b1/yz-core2-1.0.56b1.tar.gz

---
+++ packages/yz-core2/yz-core2-1.0.56b1.tar.gz/yz-core2-1.0.56b1/yzcore/extensions/storage/schemas.py
@@ -0,0 +1,52 @@
+from typing import Optional
+from pydantic import BaseModel, root_validator
+from .const import StorageMode, Scheme
+
+
+class BaseConfig(BaseModel):
+    mode: StorageMode
+    access_key_id: str
+    access_key_secret: str
+    bucket_name: str
+    endpoint: str
+    scheme: Optional[Scheme] = Scheme.https
+    image_domain: Optional[str] = None
+    asset_domain: Optional[str] = None
+
+    cache_path: Optional[str] = '.'
+    policy_expire_time: Optional[int]  # 上传签名有效时间
+    private_expire_time: Optional[int]  # 私有桶访问链接有效时间
+
+    @root_validator
+    def base_validator(cls, values):
+        values['mode'] = values['mode'].value
+        if not values['policy_expire_time']:
+            values['policy_expire_time'] = 30
+        if not values['private_expire_time']:
+            values['private_expire_time'] = 3600
+        return values
+
+
+class OssConfig(BaseConfig):
+    internal_endpoint: str = None
+
+
+class ObsConfig(BaseConfig):
+    pass
+
+
+class MinioConfig(BaseConfig):
+    internal_endpoint: str = None
+    disable_internal_endpoint: bool = False
+
+
+class AzureConfig(BaseConfig):
+    connection_string: str
+    account_key: str
+    account_name: str
+    access_key_id: Optional[str] = None
+    access_key_secret: Optional[str] = None
+
+
+class S3Config(BaseConfig):
+    pass
