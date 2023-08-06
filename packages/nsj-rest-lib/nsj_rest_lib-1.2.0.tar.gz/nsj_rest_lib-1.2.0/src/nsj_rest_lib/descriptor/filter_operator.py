707c928d8a0b130da6b1af9e494d78f295d070e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:46 +0000

    Add package nsj-rest-lib/1.2.0/nsj_rest_lib-1.2.0.tar.gz

---
+++ packages/nsj-rest-lib/nsj_rest_lib-1.2.0.tar.gz/nsj_rest_lib-1.2.0/src/nsj_rest_lib/descriptor/filter_operator.py
@@ -0,0 +1,12 @@
+import enum
+
+
+class FilterOperator(enum.Enum):
+    EQUALS = 'equals'
+    DIFFERENT = 'diferent'
+    GREATER_THAN = 'greater_than'
+    LESS_THAN = 'less_than'
+    GREATER_OR_EQUAL_THAN = 'greater_or_equal_than'
+    LESS_OR_EQUAL_THAN = 'less_or_equal_than'
+    LIKE = "like"
+    ILIKE = "ilike"
