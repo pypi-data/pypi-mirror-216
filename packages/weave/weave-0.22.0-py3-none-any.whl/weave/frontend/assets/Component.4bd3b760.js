3cccfd123918c423cc8fbf85233b4da60fd34a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:09 +0000

    Add package weave/0.22.0/weave-0.22.0-py3-none-any.whl

---
+++ packages/weave/weave-0.22.0-py3-none-any.whl/weave/frontend/assets/Component.4bd3b760.js
@@ -0,0 +1,2 @@
+import{ag as i,u as o,W as e}from"./index.02694492.js";const c=l=>{const a=l.input,r=i({file:a}),t=o(r);return e.createElement("div",null,t.loading?e.createElement("div",null):e.createElement("img",{style:{maxWidth:"100%"},alt:"cool-alt",src:t.result}))};export{c as default};
+//# sourceMappingURL=Component.4bd3b760.js.map
