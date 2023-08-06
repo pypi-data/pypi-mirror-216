b8ff873548a25fa1d1090fb33602d52364eafca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:30 +0000

    Add package sloth-framework/0.1.12/sloth-framework-0.1.12.tar.gz

---
+++ packages/sloth-framework/sloth-framework-0.1.12.tar.gz/sloth-framework-0.1.12/sloth/actions/fields.py
@@ -0,0 +1,34 @@
+from django import forms
+
+from . import inputs
+
+
+class QrCodeField(forms.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs.update(widget=inputs.QrCodeInput())
+        super().__init__(*args, **kwargs)
+
+class PhotoField(forms.CharField):
+    def __init__(self, *args, max_width=200, max_height=200, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.widget.attrs.update(
+            {'data-max-width': max_width, 'data-max-height': max_height, 'accept': 'image/*', 'capture': ''}
+        )
+
+
+class ModelChoiceField(forms.ModelChoiceField):
+    def __init__(self, *args, **kwargs):
+        self.username_lookup = kwargs.pop('username_lookup', None)
+        super().__init__(*args, **kwargs)
+
+
+class TextField(forms.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs.update(widget=forms.Textarea())
+        super().__init__(*args, **kwargs)
+
+
+class CurrentUserField(forms.ModelChoiceField):
+    def __init__(self, **kwargs):
+        from django.contrib.auth.models import User
+        super().__init__(User.objects, **kwargs)
