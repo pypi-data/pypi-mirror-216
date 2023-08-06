3aa47aaad64726d46c8094c589dc96331828e0ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:54 +0000

    Add package django-db-mutex/3.1.0/django-db-mutex-3.1.0.tar.gz

---
+++ packages/django-db-mutex/django-db-mutex-3.1.0.tar.gz/django-db-mutex-3.1.0/setup.py
@@ -0,0 +1,59 @@
+import re
+import multiprocessing
+from setuptools import setup, find_packages
+
+
+# import multiprocessing to avoid this bug (http://bugs.python.org/issue15881#msg170215)
+assert multiprocessing
+
+
+def get_version():
+    """
+    Extracts the version number from the version.py file.
+    """
+    VERSION_FILE = 'db_mutex/version.py'
+    mo = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', open(VERSION_FILE, 'rt').read(), re.M)
+    if mo:
+        return mo.group(1)
+    else:
+        raise RuntimeError('Unable to find version string in {0}.'.format(VERSION_FILE))
+
+
+def get_lines(file_path):
+    return open(file_path, 'r').read().split('\n')
+
+
+install_requires = get_lines('requirements/requirements.txt')
+tests_require = get_lines('requirements/requirements-testing.txt')
+
+
+setup(
+    name='django-db-mutex',
+    version=get_version(),
+    description='Acquire a mutex via the DB in Django',
+    long_description=open('README.rst').read(),
+    url='http://github.com/ambitioninc/django-db-mutex/',
+    author='Wes Kendall',
+    author_email='opensource@ambition.com',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
+        'Framework :: Django :: 4.2',
+    ],
+    license='MIT',
+    install_requires=install_requires,
+    tests_require=tests_require,
+    test_suite='run_tests.run_tests',
+    include_package_data=True,
+    zip_safe=False,
+)
