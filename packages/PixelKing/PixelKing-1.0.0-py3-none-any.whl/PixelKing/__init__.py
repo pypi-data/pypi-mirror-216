d2c2b4ab641893aebbe8d1c19a386ff3870b5e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:49 +0000

    Add package PixelKing/1.0.0/PixelKing-1.0.0-py3-none-any.whl

---
+++ packages/PixelKing/PixelKing-1.0.0-py3-none-any.whl/PixelKing/__init__.py
@@ -0,0 +1,61 @@
+import webbrowser
+import time
+from tkinter import *
+from tkinter import ttk
+import random
+
+def OpenYouTube():
+    webbrowser.open("www.youtube.com")
+
+def Developers():
+    print("Разработчики: Program King & PixelSun")
+    time.sleep(2)
+
+def NotCloseConsole():
+   while True:
+       time.sleep(100)
+
+def WindowTkinter():
+    root = Tk()
+    root.title("Tk")
+    root.geometry("500x500")
+    root.mainloop()
+
+def RandomFunction():
+    a=random.randint (0,10)
+    print(a)
+
+def HelloWorldPrint():
+    print("print")
+
+
+def Принт():
+    print("Принтик")
+
+
+def falseТеперьTrueBETA():
+    Fal = False
+
+    tr = True
+    fal = tr
+
+def Разработчик():
+    print("Создатель:PixelSun и Program King")
+    time.sleep(2)
+
+def putin():
+    input(":")
+
+def СоздатьОкноТкинтерСНадписьюЯПокакал():
+    root = Tk()
+    root.title("Я Покакал")
+    root.geometry("250x200")
+
+    label = ttk.Label(text="Я Покакал")
+    label.pack()
+
+    root.mainloop()
+
+
+
+#Beta 1.0.0
