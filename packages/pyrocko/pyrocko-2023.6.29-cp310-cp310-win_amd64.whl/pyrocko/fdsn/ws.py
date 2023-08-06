2a68f5fa9084370afc55e1bc3dae8670c951764b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:26 +0000

    Add package pyrocko/2023.6.29/pyrocko-2023.6.29-cp310-cp310-win_amd64.whl

---
+++ packages/pyrocko/pyrocko-2023.6.29-cp310-cp310-win_amd64.whl/pyrocko/fdsn/ws.py
@@ -0,0 +1,12 @@
+
+import sys
+import pyrocko
+if pyrocko.grumpy == 1:
+    sys.stderr.write('using renamed pyrocko module: pyrocko.fdsn.ws\n')
+    sys.stderr.write('           -> should now use: pyrocko.client.fdsn\n\n')
+elif pyrocko.grumpy == 2:
+    sys.stderr.write('pyrocko module has been renamed: pyrocko.fdsn.ws\n')
+    sys.stderr.write('              -> should now use: pyrocko.client.fdsn\n\n')
+    raise ImportError('Pyrocko module "pyrocko.fdsn.ws" has been renamed to "pyrocko.client.fdsn".')
+
+from pyrocko.client.fdsn import *
