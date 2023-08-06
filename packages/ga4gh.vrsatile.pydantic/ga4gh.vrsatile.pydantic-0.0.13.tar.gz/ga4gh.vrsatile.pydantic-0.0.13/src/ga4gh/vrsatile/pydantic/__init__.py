9eb44bd406f012b848b11bb622a0de19811c85bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:15 +0000

    Add package ga4gh.vrsatile.pydantic/0.0.13/ga4gh.vrsatile.pydantic-0.0.13.tar.gz

---
+++ packages/ga4gh.vrsatile.pydantic/ga4gh.vrsatile.pydantic-0.0.13.tar.gz/ga4gh.vrsatile.pydantic-0.0.13/src/ga4gh/vrsatile/pydantic/__init__.py
@@ -0,0 +1,58 @@
+"""Initialize GA4GH VRSATILE Pydantic."""
+import logging
+from abc import ABC
+
+from pydantic import BaseModel, Extra, root_validator
+
+
+logger = logging.getLogger("vrsatile-pydantic")
+
+
+class BaseModelForbidExtra(BaseModel, ABC):
+    """Base Pydantic model class with extra values forbidden."""
+
+    class Config:
+        """Class configs."""
+
+        extra = Extra.forbid
+        allow_population_by_field_name = True
+
+
+class BaseModelDeprecated(BaseModel, ABC):
+    """Base Pydantic model class to use for deprecated classes."""
+
+    @root_validator(pre=True)
+    def log_deprecated_warning(cls, values):
+        """Log warning that object class is deprecated."""
+        if hasattr(cls, "_replace_with"):
+            logger.warning(f"{cls.__name__} is deprecated. "
+                           f"Use {cls._replace_with} instead.")
+        else:
+            logger.warning(f"{cls.__name__} is deprecated.")
+        return values
+
+
+def return_value(cls, v):
+    """Return value from object.
+
+    :param ModelMetaclass cls: Pydantic Model ModelMetaclass
+    :param v: Model from vrs or vrsatile
+    :return: Value
+    """
+    if v is not None:
+        try:
+            if isinstance(v, list):
+                tmp = list()
+                for item in v:
+                    while True:
+                        try:
+                            item = item.__root__
+                        except AttributeError:
+                            break
+                    tmp.append(item)
+                v = tmp
+            else:
+                v = v.__root__
+        except AttributeError:
+            pass
+    return v
