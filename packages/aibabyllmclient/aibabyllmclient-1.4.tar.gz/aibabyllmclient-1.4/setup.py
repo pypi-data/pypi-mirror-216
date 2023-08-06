b866b1abf9414bd4a59a7f0b09aaf9ccc8fe97c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:48 +0000

    Add package aibabyllmclient/1.4/aibabyllmclient-1.4.tar.gz

---
+++ packages/aibabyllmclient/aibabyllmclient-1.4.tar.gz/aibabyllmclient-1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aibabyllmclient",
+    version="1.4",
+    author="zzy",
+    author_email="zhaichenhao1889@163.com",
+    description="llmserver_call",
+
+    # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+    packages=find_packages()
+)
