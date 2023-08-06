d49718c57dc161c2628dfb607ed74b4366ed3ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:01 +0000

    Add package salure-helpers-azure/1.0.1/salure_helpers_azure-1.0.1.tar.gz

---
+++ packages/salure-helpers-azure/salure_helpers_azure-1.0.1.tar.gz/salure_helpers_azure-1.0.1/salure_helpers/azure/blob_storage.py
@@ -0,0 +1,38 @@
+from salure_helpers.salureconnect import SalureConnect
+
+
+class BlobStorage(SalureConnect):
+    def __init__(self):
+        pass
+
+    def get_containers(self):
+        pass
+
+    def create_container(self):
+        pass
+
+    def update_container(self):
+        pass
+
+    def delete_container(self):
+        pass
+
+    def get_blobs(self):
+        pass
+
+    def create_blob(self):
+        pass
+
+    def delete_blob(self):
+        pass
+
+    def get_folders(self):
+        pass
+
+    def create_folder(self):
+        pass
+
+    def delete_folder(self):
+        pass
+
+
---
+++ packages/salure-helpers-azure/salure_helpers_azure-1.0.1.tar.gz/salure_helpers_azure-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='salure_helpers_azure',
+    version='1.0.1',
+    description='Azure wrapper from Salure',
+    long_description='Azure wrapper from Salure',
+    author='D&A Salure',
+    author_email='support@salureconnnect.com',
+    packages=["salure_helpers.azure"],
+    license='Salure License',
+    install_requires=[
+        'salure-helpers-salureconnect>=1',
+        'azure-storage-file-share>=12.6.0',
+        'msal==1.22.0'
+    ],
+    zip_safe=False,
+)
