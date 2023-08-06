a54fd9a9f11c48ccfc7c3add537baa71a19cdb87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:52 +0000

    Add package ocamlec-plt/0.0.3/ocamlec_plt-0.0.3-py3-none-any.whl

---
+++ packages/ocamlec-plt/ocamlec_plt-0.0.3-py3-none-any.whl/coevolution/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 27 10:28:54 2023
+
+@author: mariohevia
+"""
+
+from champions import plotChampions
+from champions import plotChampionsVSCommon
