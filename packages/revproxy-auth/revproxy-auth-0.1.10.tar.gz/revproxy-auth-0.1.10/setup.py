198e1be91232c3dfbf318cfe53567ab522629586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:59 +0000

    Add package revproxy-auth/0.1.10/revproxy-auth-0.1.10.tar.gz

---
+++ packages/revproxy-auth/revproxy-auth-0.1.10.tar.gz/revproxy-auth-0.1.10/setup.py
@@ -0,0 +1,33 @@
+""" Setup script """
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="revproxy-auth",
+    version="0.1.10",
+    author="Ismael Raya",
+    author_email="phornee@gmail.com",
+    description="Reverse proxy with synology authentication",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Phornee/revproxy_auth",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    data_files=[
+        ('tests/data', ['tests/data/config.yml'])
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Home Automation"
+    ],
+    install_requires=[
+        'Flask>=1.1.2',
+        'config_yml>=0.3.0',
+        'beautifulsoup4>=4.10.0'
+    ],
+    python_requires='>=3.6',
+)
