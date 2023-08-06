a9764929be1c672047f18a41f916a268dd494b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:21 +0000

    Add package cmeel/0.46.0/cmeel-0.46.0-py3-none-any.whl

---
+++ packages/cmeel/cmeel-0.46.0-py3-none-any.whl/cmeel/__init__.py
@@ -0,0 +1,12 @@
+"""Cmeel module."""
+
+try:
+    from importlib.metadata import metadata
+except ImportError:  # Python < 3.8
+    from importlib_metadata import metadata  # type: ignore
+
+__metadata__ = metadata("cmeel")
+__project_name__ = __metadata__["name"]
+__version__ = __metadata__["version"]
+__license__ = __metadata__["license"]
+__author__ = __metadata__["author"]
---
+++ packages/cmeel/cmeel-0.46.0-py3-none-any.whl/cmeel/consts.py
@@ -0,0 +1,8 @@
+"""Cmeel constants."""
+import os
+import sys
+
+CMEEL_PREFIX = "cmeel.prefix"
+SITELIB = os.sep.join(
+    ["lib", "python" + ".".join(sys.version.split(".")[:2]), "site-packages"],
+)
