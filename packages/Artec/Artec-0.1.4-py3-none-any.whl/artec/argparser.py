f83b039fe454f477ef9161ee0ee10c28abbebac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:22 +0000

    Add package Artec/0.1.4/Artec-0.1.4-py3-none-any.whl

---
+++ packages/Artec/Artec-0.1.4-py3-none-any.whl/artec/argparser.py
@@ -0,0 +1,70 @@
+"""
+	Parser class is a wrapper for easy access of argparse module
+"""
+from argparse import ArgumentParser, Namespace, RawTextHelpFormatter
+
+
+class Parser(ArgumentParser):
+    def __init__(self):
+        prog = "Artec"
+        usage = "artec [OPTIONS] -o [DEST] "
+        description = "Artec is a simple python 3 script to create a project template in a given directory."
+        epilog = """Examples:\n\tartec -o dest\n\tartec -o dest -s structure.json"""
+        super().__init__(
+            prog, usage, description, epilog, formatter_class=RawTextHelpFormatter
+        )
+
+    def setup(self):
+        self.add_argument(
+            "-o",
+            "--output-directory",
+            dest="target",
+            help="Target output path where the structure will be created",
+            type=str,
+            required=True,
+        )
+
+        self.add_argument(
+            "-s",
+            "--source-file",
+            dest="source",
+            help="Source JSON file containing structure to be created",
+            type=str,
+            required=False,
+        )
+
+        self.add_argument(
+            "-t",
+            "--template",
+            dest="template",
+            help="Uses ready-made templates.",
+            required=False,
+        )
+
+        # ! Not Implemented Yet.
+        self.add_argument(
+            "-i",
+            "--interactive",
+            dest="tui",
+            help="Runs Artec in interactive mode.",
+            action="store_true",
+            required=False,
+        )
+
+        self.add_argument(
+            "-v",
+            "--verbose",
+            dest="verbose",
+            help="Runs Artec in verbose mode.",
+            action="store_true",
+            required=False,
+        )
+
+def main_args() -> Namespace:
+    parser = Parser()
+    parser.setup()
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    pass
---
+++ packages/Artec/Artec-0.1.4-py3-none-any.whl/artec/boiler.py
@@ -0,0 +1,74 @@
+"""
+	Main module core.
+	#3 Step
+"""
+import json
+import os
+from pathlib import Path
+from .exceptions import NotJsonFile, NotValidJson, NoSource
+class boiler_builder:
+    #! TUI is not implemented yet.
+    def __init__(self, source=None, target=None, verbose=False, template=None, tui=False) -> None:
+        self.verbose = verbose
+        self.target = target
+        self.template = template    
+        if self.template is None :
+            self.structure = self._source(source)
+
+    def _source(self, source) -> list[dict[str, str]]:
+        try:
+            if os.path.isfile(source) and source.endswith('.json'):
+                with open(source, "rt", encoding="utf-8") as file_data:
+                    structure = json.load(file_data)
+            else : 
+                raise NotJsonFile(self.verbose)
+    
+        except Exception as e :
+            
+            if not hasattr(e,"errno") : 
+                NoSource()                
+
+            structure = DEFAULT_FOLDER_STRUCTURE
+        return structure
+
+    def build(self):
+        print("> Creating folder structure: {}\n".format(self.target))
+
+        for entry in self.structure:
+            for _type, name in entry.items():
+                try:
+                    joined = Path(os.path.join(self.target, name))
+                    if _type == "folder":
+                        self._make_folder(joined)
+                    elif _type == "file":
+                        self._make_file(joined)
+                    else : 
+                        raise NotValidJson(self.verbose)
+                    print("Created: %s" % joined)
+                except Exception:
+                    exit("> Fatal error - exiting...")
+                
+    def _make_file(self, path):
+        """Create an empty file in a given directory"""
+        path.parent.mkdir(parents=True, exist_ok=True)
+        open(path, "a").close()
+
+    def _make_folder(self, path):
+        """Create an empty directory"""
+        os.makedirs(path, exist_ok=True)
+
+DEFAULT_FOLDER_STRUCTURE = [
+    {"folder": "src"},
+    {"file": "src/__init__.py"},
+    {"folder": "test"},
+    {"file": "test/__init__.py"},
+    {"folder": "res"},
+    {"file": "README.md"},
+    {"file": "setup.py"},
+    {"file": "setup.cfg"},
+    {"file": "pyproject.toml"},
+]
+
+
+if __name__ == "__main__":
+    pass
