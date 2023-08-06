0dad3136150b53a2837148d1ea8c23fdb4cc6031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:54 +0000

    Add package aioarp/0.0.9/aioarp-0.0.9.tar.gz

---
+++ packages/aioarp/aioarp-0.0.9.tar.gz/aioarp-0.0.9/tests/test_sync.py
@@ -0,0 +1,40 @@
+import pytest
+
+import aioarp
+from aioarp._backends._mock import MockSocket
+
+eth_header_arp = (
+    # Eth Header
+    b"\x11\x11\x11\x11\x11\x11\x11"
+    b"\x11\x11\x11\x11\x11\x08\x06"
+)
+
+eth_header_no_arp = (
+    # Eth Header
+    b"\x11\x11\x11\x11\x11\x11\x11"
+    b"\x11\x11\x11\x11\x11\x08\x00"
+)
+
+arp_header = (
+    # Arp Header
+    b"\x00\x01\x08\x00\x06\x04"
+    b"\x00\x01\x11\x11\x11\x11"
+    b"\x11\x11\x7f\x00\x00\x01"
+    b"\x11\x11\x11\x11\x11\x11"
+    b"\x7f\x00\x00\x01"
+)
+
+
+@pytest.mark.anyio
+def test_async_send_arp():
+    msocket = MockSocket(eth_header_arp + arp_header)
+    response = aioarp.request('100.100.100.100', 'null', sock=msocket)
+    assert response
+    assert response.sender_mac == '11:11:11:11:11:11'
+
+
+@pytest.mark.anyio
+def test_async_send_arp_timeout():
+    with MockSocket(eth_header_no_arp + arp_header) as msocket:
+        with pytest.raises(aioarp.NotFoundError):
+            aioarp.request('100.100.100.100', 'null',  sock=msocket, timeout=0.5)
