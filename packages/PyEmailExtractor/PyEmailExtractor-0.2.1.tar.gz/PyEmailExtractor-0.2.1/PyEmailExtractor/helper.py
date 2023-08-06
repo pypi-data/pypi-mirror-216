6440168d0512abd44f9993877a9f226b910896fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:07 +0000

    Add package PyEmailExtractor/0.2.1/PyEmailExtractor-0.2.1.tar.gz

---
+++ packages/PyEmailExtractor/PyEmailExtractor-0.2.1.tar.gz/PyEmailExtractor-0.2.1/PyEmailExtractor/helper.py
@@ -0,0 +1,74 @@
+import re
+import os
+import PyPDF2
+import docx2txt
+from docx import Document
+import fitz
+import pytesseract
+from PIL import Image
+
+
+def fetch_files(directory, file_extensions):
+    """To fetches all the file with the given ext in the given directory"""
+    files = []
+    try:
+        for dirpath, dirnames, filenames in os.walk(directory):
+            for filename in filenames:
+                if any(filename.endswith(ext) for ext in file_extensions):
+                    filepath = os.path.join(dirpath, filename)
+                    files.append(filepath)
+    except Exception as e:
+        with open("error_logs.txt", "a") as f:
+            f.write("Error from fetch_files function: \n" + str(e) + "\n")
+    return files
+
+
+def extract_email_from_pdf(pdf_file):
+    """To extract emails from pdfs files"""
+    email_pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+    email = ""
+    try:
+        doc = fitz.open(pdf_file)
+        page1 = doc[0]
+        text = str(page1.get_text("words"))
+        email = re.findall(email_pattern, text)
+        if not email:
+            with open("no_email_logs.txt", "a") as f:
+                f.write(str(pdf_file) + "\n")
+    except Exception as e:
+        with open("error_logs.txt", "a") as f:
+            f.write("Error from extract_email_from_pdf function: \n" + str(e) + "\n")
+    return email
+
+
+def extract_email_from_docx(docx_file):
+    """To extract emails from docx files"""
+    email_pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+    email = ""
+    try:
+        text = docx2txt.process(str(docx_file))
+        email = re.findall(email_pattern, text)
+        if not email:
+            with open("no_email_logs.txt", "a") as f:
+                f.write(str(docx_file) + "\n")
+    except Exception as e:
+        with open("error_logs.txt", "a") as f:
+            f.write("Error from extract_email_from_docx function: \n" + str(e) + "\n")
+    return email
+
+
+def extract_email_from_image(image_file):
+    email_pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+    email = ""
+    try:
+        image = Image.open(str(image_file))
+
+        text = pytesseract.image_to_string(image)
+        email = re.findall(email_pattern, text)
+        if not email:
+            with open("no_email_logs.txt", "a") as f:
+                f.write(str(image_file) + "\n")
+    except Exception as e:
+        with open("error_logs.txt", "a") as f:
+            f.write("Error from extract_email_from_image function: \n" + str(e) + "\n")
+    return email
