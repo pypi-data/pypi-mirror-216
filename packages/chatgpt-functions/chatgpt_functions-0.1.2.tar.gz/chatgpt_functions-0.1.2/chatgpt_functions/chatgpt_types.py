65109325226a039415f9367136098f08d3c0106f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:53 +0000

    Add package chatgpt-functions/0.1.2/chatgpt_functions-0.1.2.tar.gz

---
+++ packages/chatgpt-functions/chatgpt_functions-0.1.2.tar.gz/chatgpt_functions-0.1.2/chatgpt_functions/chatgpt_types.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
+
+class Roles(Enum):
+    USER = "user"
+    ASSISTANT = "assistant"
+    SYSTEM = "system"
+    FUNCTION = 'function' 
+
+@dataclass
+class Message:
+    role: Roles
+    content: str
+    name: Optional[str] = None
+
+    def __dict__(self):
+        tmp = {"role": self.role.value, "content": self.content}
+        if self.name:
+            tmp["name"] = self.name
+        return tmp
