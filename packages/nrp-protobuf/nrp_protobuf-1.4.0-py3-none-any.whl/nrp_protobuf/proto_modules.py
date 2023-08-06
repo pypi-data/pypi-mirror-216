0fdfc4c4b9b6470e918f179fc5bed4a1ecf12888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:27 +0000

    Add package nrp-protobuf/1.4.0/nrp_protobuf-1.4.0-py3-none-any.whl

---
+++ packages/nrp-protobuf/nrp_protobuf-1.4.0-py3-none-any.whl/nrp_protobuf/proto_modules.py
@@ -0,0 +1,11 @@
+proto_modules = [
+    "nrp_protobuf.dump_pb2",
+"nrp_protobuf.edlutdata_pb2",
+"nrp_protobuf.gazebo_pb2",
+"nrp_protobuf.nrpgenericproto_pb2",
+"nrp_protobuf.enginetest_pb2",
+"nrp_protobuf.wrappers_pb2",
+
+]
+
+# EOF
