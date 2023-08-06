c9e53c339704becb305a8cacf22d0e309f5a758a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:51 +0000

    Add package nucleus-sdk/0.1a4/nucleus_sdk-0.1a4.tar.gz

---
+++ packages/nucleus-sdk/nucleus_sdk-0.1a4.tar.gz/nucleus_sdk-0.1a4/nucleus/sdk/processing/video/protocols.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+
+from .constants import HLS_LIST_SIZE, HLS_PLAYLIST_TYPE, HLS_TAG_VIDEO_FORMAT, HLS_TIME
+from .types import Codec
+
+
+@dataclass(slots=True)
+class HLS:
+    """HLS streaming protocol.
+
+    Usage:
+
+        # use h264 as codec
+        hls = HLS(H264())
+
+    """
+
+    codec: Codec
+
+    def __iter__(self):
+        yield 'hls_time', HLS_TIME
+        yield 'hls_list_size', HLS_LIST_SIZE
+        yield 'hls_playlist_type', HLS_PLAYLIST_TYPE,
+        yield 'tag:v', HLS_TAG_VIDEO_FORMAT,
+        yield from self.codec
+
+
+__all__ = ('HLS',)
