6802c3a59ceb59f2fce7a57d2fcb78b6a2c136c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:14 +0000

    Add package embetter/0.4.1/embetter-0.4.1-py2.py3-none-any.whl

---
+++ packages/embetter/embetter-0.4.1-py2.py3-none-any.whl/embetter/text/__init__.py
@@ -0,0 +1,24 @@
+from embetter.error import NotInstalled
+
+try:
+    from embetter.text._sbert import SentenceEncoder
+except ModuleNotFoundError:
+    SentenceEncoder = NotInstalled("SentenceEncoder", "sentence-tfm")
+
+try:
+    from embetter.text._s2v import Sense2VecEncoder
+except ModuleNotFoundError:
+    Sense2VecEncoder = NotInstalled("Sense2VecEncoder", "sense2vec")
+
+try:
+    from embetter.text._bpemb import BytePairEncoder
+except ModuleNotFoundError:
+    BytePairEncoder = NotInstalled("BytePairEncoder", "bpemb")
+
+try:
+    from embetter.text._spacy import spaCyEncoder
+except ModuleNotFoundError:
+    spaCyEncoder = NotInstalled("spaCyEncoder", "spacy")
+
+
+__all__ = ["SentenceEncoder", "Sense2VecEncoder", "BytePairEncoder", "spaCyEncoder"]
