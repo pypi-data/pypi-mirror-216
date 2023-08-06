42009a90e2bfa9c95c0f354148080d3d934b6489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:32 +0000

    Add package pymilo/0.1/pymilo-0.1.tar.gz

---
+++ packages/pymilo/pymilo-0.1.tar.gz/pymilo-0.1/pymilo/pymilo_func.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""Functions."""
+import numpy as np
+import sklearn
+from .chains.linear_model_chain import transport_linear_model
+from .transporters.transporter import Command
+
+
+def get_sklearn_version():
+    """
+    Return sklearn version.
+
+    :return: sklearn version as a str
+    """
+    return sklearn.__version__
+
+
+def get_sklearn_data(model):
+    """
+    Return sklearn data by serializing given model.
+
+    :param model: given model
+    :type model: any sklearn's model class
+    :return: sklearn data
+    """
+    return transport_linear_model(model, Command.SERIALIZE)
+
+
+def to_sklearn_model(import_obj):
+    """
+    Deserialize the imported object as a sklearn model.
+
+    :param import_obj: given object
+    :type import_obj: pymilo.Import
+    :return: sklearn model
+    """
+    return transport_linear_model(import_obj, Command.DESERIALZIE)
+
+
+def compare_model_outputs(exported_output,
+                          imported_output,
+                          epsilon_error=10**(-8)):
+    """
+    Check if the given models outputs are the same.
+
+    :param exported_output: exported model output
+    :type exported_output: dict
+    :param imported_output: imported model output
+    :type imported_output: dict
+    :param epsilon_error: error threshold for numeric comparisons
+    :type epsilon_error: float
+    :return: check result as bool
+    """
+    if len(exported_output.keys()) != len(imported_output.keys()):
+        return False  # TODO: throw exception
+    total_error = 0
+    for key in exported_output.keys():
+        if key not in imported_output.keys():
+            return False  # TODO: throw exception
+        total_error += np.abs(imported_output[key] - exported_output[key])
+    return np.abs(total_error) < epsilon_error
---
+++ packages/pymilo/pymilo-0.1.tar.gz/pymilo-0.1/pymilo/utils/util.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""utility module."""
+from numpy import ndarray
+
+
+def get_sklearn_type(model):
+    """
+    Return sklearn model type.
+
+    :param model: sklearn model
+    :type model: any sklearn's model class
+    :return: model type as str
+    """
+    raw_type = type(model)
+    return str(raw_type).split(".")[-1][:-2]
+
+
+def is_primitive(obj):
+    """
+    Check if the given object is primitive.
+
+    :param obj: given object
+    :type obj: any valid type
+    :return: True if object is primitive
+    """
+    return not hasattr(obj, '__dict__')
+
+
+def is_iterable(obj):
+    """
+    Check if the given object is iterable.
+
+    :param obj: given object
+    :type obj: any valid type
+    :return: True if object is iterable
+    """
+    try:
+        iter(obj)
+        return True
+    except TypeError:
+        return False
+
+
+def check_str_in_iterable(field, content):
+    """
+    Check if the specified string field exists in content, which is supposed to be an iterable object.
+
+    :param field: given string field
+    :type field: str
+    :param content: given supposed to be an iterable object
+    :type content: obj
+    :return: True if associated field is an iterable string in content and False otherwise.
+    """
+    if not is_iterable(content):
+        return False
+    if isinstance(content, ndarray):
+        # https://stackoverflow.com/questions/40659212/futurewarning-elementwise-comparison-failed-returning-scalar-but-in-the-futur.
+        return False
+    else:
+        return field in content
