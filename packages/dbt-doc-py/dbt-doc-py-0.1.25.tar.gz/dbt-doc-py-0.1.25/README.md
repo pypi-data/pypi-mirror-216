005358df2d402916f394a743ecc4075a1be60a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:09 +0000

    Add package dbt-doc-py/0.1.25/dbt-doc-py-0.1.25.tar.gz

---
+++ packages/dbt-doc-py/dbt-doc-py-0.1.25.tar.gz/dbt-doc-py-0.1.25/README.md
@@ -0,0 +1,43 @@
+# DBT Documentor (Python Version)
+
+Automatically generate docs for undocumented DBT models.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Documentation](#documentation)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+To install the package, run the following command:
+
+```bash
+pip install dbt-doc-py
+```
+
+## Usage
+Place inside of the DBT project folder and run this command:
+
+```
+dbt-doc-py
+```
+
+You can execute it from other directories, this way:
+
+```
+dbt-doc-py --working-directory /folder/DBTproject
+```
+
+TO-DO: Finish README file:
+
+## Documentation
+
+
+## Contributing
+
+
+## License
+This project is licensed under the MIT License. See the LICENSE file for details.
---
+++ packages/dbt-doc-py/dbt-doc-py-0.1.25.tar.gz/dbt-doc-py-0.1.25/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dbt-doc-py",
+    version="0.1.25",
+    packages=find_packages(),
+    install_requires=[
+    "PyYAML", "dataclasses", "transformers", "argparse", "httpx", "inquirer", "ruamel.yaml",
+    ],
+    entry_points={
+        "console_scripts": [
+            "dbt-doc-py=dbt_doc_py.dbt_doc_py:run_async_main",
+        ],
+    },
+    author="TextQLLabs",
+    author_email="lenin@textql.com",
+    description="Automatically generate docs for undocumented DBT models.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/TextQLLabs/dbt-doc-py.git",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",        
+        "Programming Language :: Python :: 3.11",
+    ],
+)
