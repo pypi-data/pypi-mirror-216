97504174ab5d7db79af10191b6a4686d5433fcc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:29 +0000

    Add package gaussian-bin-dist/1.0/gaussian_bin_dist-1.0.tar.gz

---
+++ packages/gaussian-bin-dist/gaussian_bin_dist-1.0.tar.gz/gaussian_bin_dist-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gaussian_bin_dist',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['gaussian_bin_dist'],
+      author='Parth Mehta',
+      zip_safe=False)
