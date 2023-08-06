ebd99ebbe77b978919f2fd7743669118a1e46cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:06 +0000

    Add package zExceptions/5.0/zExceptions-5.0.tar.gz

---
+++ packages/zExceptions/zExceptions-5.0.tar.gz/zExceptions-5.0/CHANGES.rst
@@ -0,0 +1,102 @@
+Changelog
+=========
+
+5.0 (2023-06-28)
+----------------
+
+
+- Drop support for Python 2.7, 3.5, 3.6.
+
+4.3 (2022-12-18)
+----------------
+
+- Sort imports with isort.
+
+- Add support for Python 3.11.
+
+
+4.2 (2021-10-22)
+----------------
+
+- Add support for Python 3.8, 3.9 and 3.10.
+
+- Drop support for Python 3.4.
+
+- Add an exception for the HTTP status code 418.
+
+- Don't override the `content-type` header if already set.
+  (`#12 <https://github.com/zopefoundation/zExceptions/pull/12>`_)
+
+
+4.1 (2018-10-05)
+----------------
+
+- Add support for Python 3.7.
+
+
+4.0 (2018-01-27)
+----------------
+
+- Drop support for string exceptions.
+
+3.6.1 (2017-05-17)
+------------------
+
+- Increase Python 3 compatibility
+
+3.6 (2017-02-05)
+----------------
+
+- Add realm as an argument to unauthorized exceptions, its presence
+  causing a `WWW-Authenticate` header to be emitted.
+
+- Set `location` header during `__init__` of redirect exceptions.
+
+3.5 (2017-02-05)
+----------------
+
+- Drop support for Python 3.3, add support for Python 3.6.
+
+- Use `str(self)` as detail if it is not set.
+
+- Add a `setHeader` method to add a response header to an HTTPException.
+
+- `upgradeException` now also supports finding an HTTPException class
+  with the same name as a non-HTTPException class.
+
+3.4 (2016-09-08)
+----------------
+
+- Use `HTTPException.body_template` when title and detail are set.
+
+- Add new title and detail attributes to HTTPException.
+
+3.3 (2016-08-06)
+----------------
+
+- Add exception classes for all HTTP status codes.
+
+3.2 (2016-07-22)
+----------------
+
+- Implement basic subset of Response features in HTTPException class.
+
+3.1 (2016-07-22)
+----------------
+
+- Mark exceptions with appropriate zope.publisher interfaces.
+
+- Add a new common base class `zExceptions.HTTPException` to all exceptions.
+
+3.0 (2016-04-03)
+----------------
+
+- Add compatibility with PyPy and Python 3.
+
+- Arguments to the Unauthorized exception are assumed to be utf8-encoded
+  if they are bytes.
+
+2.13.0 (2010-06-05)
+-------------------
+
+- Released as separate package.
---
+++ packages/zExceptions/zExceptions-5.0.tar.gz/zExceptions-5.0/setup.py
@@ -0,0 +1,62 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='zExceptions',
+    version='5.0',
+    url='https://github.com/zopefoundation/zExceptions',
+    license='ZPL 2.1',
+    description="zExceptions contains common exceptions used in Zope.",
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.dev',
+    project_urls={
+        'Issue Tracker': 'https://github.com/zopefoundation/'
+                         'zExceptions/issues',
+        'Sources': 'https://github.com/zopefoundation/zExceptions',
+    },
+
+    long_description=(open('README.rst').read() + '\n' +
+                      open('CHANGES.rst').read()),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    python_requires='>=3.7',
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.publisher',
+        'zope.security',
+    ],
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
