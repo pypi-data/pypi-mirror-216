f65b4c7f1a6ef7369b7546389ac698e090573de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:43 +0000

    Add package improvedgan-pytorch/0.1.0/improvedgan_pytorch-0.1.0.tar.gz

---
+++ packages/improvedgan-pytorch/improvedgan_pytorch-0.1.0.tar.gz/improvedgan_pytorch-0.1.0/improvedgan/__init__.py
@@ -0,0 +1,16 @@
+import cifar10_data
+import config
+import convert_gif
+import Nets
+import plotting
+import train
+
+__all__ = [
+    "cifar10_data",
+    "config",
+    "convert_gif",
+    "Nets",
+    "plotting",
+    "PreProcessData",
+    "train"
+]
