93a3d3158252038588eb14f643a47bca5f7b7169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:41 +0000

    Add package pyotb/2.0.0.dev4/pyotb-2.0.0.dev4-py3-none-any.whl

---
+++ packages/pyotb/pyotb-2.0.0.dev4-py3-none-any.whl/pyotb/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""This module provides convenient python wrapping of otbApplications."""
+__version__ = "2.0.0.dev4"
+
+from .helpers import logger, set_logger_level
+from .apps import *
+from .core import App, Input, Output, get_nbchannels, get_pixel_type, summarize
+from .functions import (  # pylint: disable=redefined-builtin
+    all,
+    any,
+    clip,
+    define_processing_area,
+    run_tf_function,
+    where,
+)
