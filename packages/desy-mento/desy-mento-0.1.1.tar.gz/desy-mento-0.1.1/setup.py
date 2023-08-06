f367839192ee056eb470f492d3d793cb41e762f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:33 +0000

    Add package desy-mento/0.1.1/desy-mento-0.1.1.tar.gz

---
+++ packages/desy-mento/desy-mento-0.1.1.tar.gz/desy-mento-0.1.1/setup.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2021 The MENTO Authors, DESY
+# SPDX-FileCopyrightText: 2021 S. Vijay Kartik <vijay.kartik@desy.de>, DESY
+#
+# SPDX-License-Identifier: CC0-1.0
+
+# Taken from the setuptool docs
+from setuptools import setup
+setup()
