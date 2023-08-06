70c0e4e292563266fc898b4ca2d1b3d164fc3c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:19 +0000

    Add package JustScan/1.1.30/JustScan-1.1.30.tar.gz

---
+++ packages/JustScan/JustScan-1.1.30.tar.gz/JustScan-1.1.30/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+with open("app/JustScan/README.md", "r") as f:
+    long_description = f.read()
+with open("requirements.txt", "r") as f:
+    install_requires = [line.strip() for line in f if line.strip()]
+setup(
+    name="JustScan",
+    version="1.1.30",
+    description="E-kyc for Industries application",
+    package_dir={"": "app"},
+    packages=find_packages(where="./app"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.itcgroup.io/Theodore.vo",
+    author="Theodore",
+    author_email="theodore.v@itcgroup.io",
+    keywords=["ITC", "AI", "E-kyc", "Deep Learning", "Computer Vision", "Yolov5",
+              "Interface"],
+
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'itc_service = JustScan.api.itc_cli:main',
+        ],
+    },
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.7"
+)
