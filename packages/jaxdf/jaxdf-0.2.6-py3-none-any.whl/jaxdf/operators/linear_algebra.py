87f322b95d5220109dff5a55c6ad8045f96b754d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:25 +0000

    Add package jaxdf/0.2.6/jaxdf-0.2.6-py3-none-any.whl

---
+++ packages/jaxdf/jaxdf-0.2.6-py3-none-any.whl/jaxdf/operators/linear_algebra.py
@@ -0,0 +1,13 @@
+from jax import numpy as jnp
+
+from jaxdf.core import operator
+from jaxdf.discretization import OnGrid
+
+from .functions import compose
+
+
+@operator
+def dot_product(x: OnGrid, y: OnGrid, *, params=None):
+    r"""Computes the dot product of two fields."""
+    x_conj = compose(x)(jnp.conj)
+    return jnp.sum((x_conj * y).on_grid)
