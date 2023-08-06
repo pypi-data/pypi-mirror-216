a32e5388af3433b8c00b1a025b63012c3af9d723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:21 +0000

    Add package pens/0.0.8/pens-0.0.8.tar.gz

---
+++ packages/pens/pens-0.0.8.tar.gz/pens-0.0.8/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='pens',  # required
+    version='0.0.8',
+    description='pens: utilities for comparing paleoclimate reconstruction ensembles',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    author='Feng Zhu, Julien Emile-Geay',
+    author_email='fzhu@nuist.edu.cn, julieneg@usc.edu',
+    url='https://github.com/fzhu2e/pens',
+    packages=find_packages(),
+    include_package_data=True,
+    license="GPL 3.0 license",
+    zip_safe=False,
+    keywords='paleoclimate reconstruction ensembles',
+    classifiers=[
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires=[
+        'termcolor',
+        'pandas',
+        'tqdm',
+        'xarray',
+        'dask',
+        'stochastic',
+        'properscoring',
+        'covar'
+    ],
+    extras_require={
+        'pyleo': ['pyleoclim'],
+    }
+)
