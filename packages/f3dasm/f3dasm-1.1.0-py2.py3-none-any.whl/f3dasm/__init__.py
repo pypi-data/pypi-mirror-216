e578531a3ec09299ca46feafd684f03c331b7fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:48 +0000

    Add package f3dasm/1.1.0/f3dasm-1.1.0-py2.py3-none-any.whl

---
+++ packages/f3dasm/f3dasm-1.1.0-py2.py3-none-any.whl/f3dasm/__init__.py
@@ -0,0 +1,40 @@
+"""
+f3dasm - A package for data-driven design and analysis of structures and materials
+
+This package provides tools for designing and optimizing materials, including
+functions for data analysis, design of experiments, machine learning, optimization,
+sampling, and simulation.
+
+Usage:
+  import f3dasm
+
+Author: Martin van der Schelling (M.P.vanderSchelling@tudelft.nl)
+"""
+
+#                                                                       Modules
+# =============================================================================
+
+from f3dasm import (_logging, data, design, experiment, functions,
+                    machinelearning, optimization, sampling, simulation)
+
+from ._show_versions import __version__, show_versions
+# Design classes
+from .design.design import DesignSpace, make_nd_continuous_design
+from .design.experimentdata import ExperimentData
+from .design.parameter import (CategoricalParameter, ConstantParameter,
+                               ContinuousParameter, DiscreteParameter)
+from .experiment.parallelization import run_operation_on_experiments
+# Base classes
+from .functions.function import Function
+from .machinelearning.model import Model
+from .optimization.optimizer import Optimizer
+from .run_optimization import (OptimizationResult, run_multiple_realizations,
+                               run_optimization)
+from .sampling.sampler import Sampler
+from .utils import find_class, write_json
+
+#                                                        Authorship and Credits
+# =============================================================================
+__author__ = 'Martin van der Schelling (M.P.vanderSchelling@tudelft.nl)'
+__credits__ = ['Martin van der Schelling']
+__status__ = 'Stable'
