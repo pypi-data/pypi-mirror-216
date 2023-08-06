aece851e5ea51e472a7727b4e3f93e17565cce89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:32 +0000

    Add package notebook-prototyping/0.1.0/notebook_prototyping-0.1.0.tar.gz

---
+++ packages/notebook-prototyping/notebook_prototyping-0.1.0.tar.gz/notebook_prototyping-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='notebook_prototyping',
+    version='0.1.0',
+    license='MIT',
+    author="Reitze Jansen",
+    author_email='rlh.jansen@outlook.com',
+    # packages=find_packages('src'),
+    # package_dir={'': 'src'},
+    url='https://github.com/gmyrianthous/example-publish-pypi',
+    keywords='example project',
+)
