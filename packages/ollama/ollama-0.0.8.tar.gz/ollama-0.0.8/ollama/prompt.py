a62c629a308489081a0effd6857ede9ade98da64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:17 +0000

    Add package ollama/0.0.8/ollama-0.0.8.tar.gz

---
+++ packages/ollama/ollama-0.0.8.tar.gz/ollama-0.0.8/ollama/prompt.py
@@ -0,0 +1,9 @@
+from difflib import get_close_matches
+from jinja2 import Environment, PackageLoader
+
+
+def template(name, prompt):
+    environment = Environment(loader=PackageLoader(__name__, 'templates'))
+    best_templates = get_close_matches(name, environment.list_templates(), n=1, cutoff=0)
+    template = environment.get_template(best_templates.pop())
+    return template.render(prompt=prompt)
