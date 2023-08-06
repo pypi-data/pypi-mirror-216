1d92640beabf3a7322ecece4fb18f78f53af699c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:12 +0000

    Add package vit-b16-keras/0.2/vit_b16_keras-0.2-py3-none-any.whl

---
+++ packages/vit-b16-keras/vit_b16_keras-0.2-py3-none-any.whl/vit_b16_keras/vit_b16.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import os
+
+def VisionTransformer(input_shape, patch_size, num_classes, hidden_dim, num_heads, mlp_dim, channels, dropout_rate):
+    weight_path = os.path.join(os.path.dirname(__file__), "..", "weight.h5")
+
+    in_model = tf.keras.applications.DenseNet121(
+        input_shape=input_shape,
+        include_top=False,
+        weights=weight_path,
+        classes=2
+    )
+
+    return in_model
