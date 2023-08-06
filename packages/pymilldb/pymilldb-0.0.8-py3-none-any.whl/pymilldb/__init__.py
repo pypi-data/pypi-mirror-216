3ddff8daa1fd9c2412b827c661b722e146c896e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:15 +0000

    Add package pymilldb/0.0.8/pymilldb-0.0.8-py3-none-any.whl

---
+++ packages/pymilldb/pymilldb-0.0.8-py3-none-any.whl/pymilldb/__init__.py
@@ -0,0 +1,11 @@
+from .mdb_client import MDBClient
+from .node_iterator import NodeIterator
+from .sampler import Sampler
+from .tensor_store import TensorStore
+
+__all__ = [
+    "MDBClient",
+    "Sampler",
+    "TensorStore",
+    "NodeIterator"
+]
---
+++ packages/pymilldb/pymilldb-0.0.8-py3-none-any.whl/pymilldb/sampler.py
@@ -0,0 +1,44 @@
+from typing import TYPE_CHECKING, List
+
+from .protocol import RequestType
+from .utils import packer
+from .utils.graph import Graph
+
+if TYPE_CHECKING:
+    from .mdb_client import MDBClient
+
+## Interface for generating samples from MillenniumDB.
+#
+# The MillenniumDB's server creates a new random seed after each initialization, so the
+# results may vary between different runs.
+class Sampler:
+    ## Constructor.
+    def __init__(self, client: "MDBClient"):
+        ## Client instance.
+        self.client = client
+
+    ## Returns a random subgraph
+    def subgraph(self, num_seeds: int, num_neighbors: List[int]) -> Graph:
+        # Send request
+        msg = b""
+        msg += packer.pack_uint64(num_seeds)
+        msg += packer.pack_uint64_vector(num_neighbors)
+        self.client._send(RequestType.SAMPLER_SUBGRAPH, msg)
+
+        # Handle response
+        data, _ = self.client._recv()
+        return packer.unpack_graph(data)
+
+    ## Returns a random subgraph for edge existance prediction
+    def subgraph_edge_existance(
+        self, num_preseeds: int, num_neighbors: List[int]
+    ) -> Graph:
+        # Send request
+        msg = b""
+        msg += packer.pack_uint64(num_preseeds)
+        msg += packer.pack_uint64_vector(num_neighbors)
+        self.client._send(RequestType.SAMPLER_SUBGRAPH_EDGE_EXISTANCE, msg)
+
+        # Handle response
+        data, _ = self.client._recv()
+        return packer.unpack_graph(data)
