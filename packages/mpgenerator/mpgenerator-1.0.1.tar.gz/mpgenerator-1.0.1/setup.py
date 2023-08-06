4bdad0de05289706a10097c7457274a8d31200d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:49 +0000

    Add package mpgenerator/1.0.1/mpgenerator-1.0.1.tar.gz

---
+++ packages/mpgenerator/mpgenerator-1.0.1.tar.gz/mpgenerator-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mpgenerator',
+    version='1.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Tedi Mhali',
+    author_email='tedimihali1@gmail.com',
+    description=''
+)
