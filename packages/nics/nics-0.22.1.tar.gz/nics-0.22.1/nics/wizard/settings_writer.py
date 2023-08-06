cd648de95bf37253673cb0c15a2a70b4478e0408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:24 +0000

    Add package nics/0.22.1/nics-0.22.1.tar.gz

---
+++ packages/nics/nics-0.22.1.tar.gz/nics-0.22.1/nics/wizard/settings_writer.py
@@ -0,0 +1,18 @@
+
+
+
+
+text = f"""
+#-- Welcome to nics settings!
+#----------------------------
+
+#-- Everything starts with "#--" is a comment.
+
+
+author: 'Nicholas Valentinus'
+color_hue: 28
+show_credit: True
+
+
+#-- Docs: https://nvfp.github.io/now-i-can-sleep
+"""
