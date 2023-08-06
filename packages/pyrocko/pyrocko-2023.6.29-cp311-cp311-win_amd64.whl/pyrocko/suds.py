8eed9cb5a447e5c2dc6a2b629f15d1cbcf131e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:26 +0000

    Add package pyrocko/2023.6.29/pyrocko-2023.6.29-cp311-cp311-win_amd64.whl

---
+++ packages/pyrocko/pyrocko-2023.6.29-cp311-cp311-win_amd64.whl/pyrocko/suds.py
@@ -0,0 +1,12 @@
+
+import sys
+import pyrocko
+if pyrocko.grumpy == 1:
+    sys.stderr.write('using renamed pyrocko module: pyrocko.suds\n')
+    sys.stderr.write('           -> should now use: pyrocko.io.suds\n\n')
+elif pyrocko.grumpy == 2:
+    sys.stderr.write('pyrocko module has been renamed: pyrocko.suds\n')
+    sys.stderr.write('              -> should now use: pyrocko.io.suds\n\n')
+    raise ImportError('Pyrocko module "pyrocko.suds" has been renamed to "pyrocko.io.suds".')
+
+from pyrocko.io.suds import *
---
+++ packages/pyrocko/pyrocko-2023.6.29-cp311-cp311-win_amd64.whl/pyrocko/tectonics.py
@@ -0,0 +1,12 @@
+
+import sys
+import pyrocko
+if pyrocko.grumpy == 1:
+    sys.stderr.write('using renamed pyrocko module: pyrocko.tectonics\n')
+    sys.stderr.write('           -> should now use: pyrocko.dataset.tectonics\n\n')
+elif pyrocko.grumpy == 2:
+    sys.stderr.write('pyrocko module has been renamed: pyrocko.tectonics\n')
+    sys.stderr.write('              -> should now use: pyrocko.dataset.tectonics\n\n')
+    raise ImportError('Pyrocko module "pyrocko.tectonics" has been renamed to "pyrocko.dataset.tectonics".')
+
+from pyrocko.dataset.tectonics import *
