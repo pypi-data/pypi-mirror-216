ccea8a8f7dc722149a4bf2a8d3ed2058ab373a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:28 +0000

    Add package lacam-v0/0.1.4/lacam_v0-0.1.4-py3-none-any.whl

---
+++ packages/lacam-v0/lacam_v0-0.1.4-py3-none-any.whl/lacam_v0/datasets.py
@@ -0,0 +1,40 @@
+import requests
+
+class Dados:
+    
+    def __init__(self, name: list):
+        self.arquivos_disponiveis = self._criarDict()
+        
+        for i in range(name):
+            self._baixarDados(name[i])
+
+    def _criarDict(self):
+        try:
+            dicio = {}
+            path = "data/dict_dados_disp.txt"
+            with open(path, "r") as file:
+                for line in file:
+                    key, value = line.split(";")
+                    dicio[key] = value       
+            return dicio
+        except Exception as e:
+            print(f"Error: {str(e)}")
+
+    def _baixarDados(self, name: str):
+
+        if name in self.arquivos_disponiveis.keys():
+
+            filename = self.arquivos_disponiveis.get(name)
+            url = self.arquivos_disponiveis[filename]
+            filename = filename + '.csv'
+
+            with requests.get(url) as req:
+                with open(filename, 'wb') as f:
+                    for chunk in req.iter_content(chunk_size=8192):
+                        if chunk:
+                            f.write(chunk)
+
+            print(f'O arquivo {name} foi criado')
+
+        else:
+            print(f'O arquivo {name} existe.')
