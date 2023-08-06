35f2a9095151b87f0f27f0eea75daf7daf2f6e09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:54 +0000

    Add package pedal/2.5.3/pedal-2.5.3.tar.gz

---
+++ packages/pedal/pedal-2.5.3.tar.gz/pedal-2.5.3/pedal/__init__.py
@@ -0,0 +1,17 @@
+"""
+A package for analyzing student code.
+"""
+import sys
+import os
+
+# Core Commands
+from pedal.core.report import MAIN_REPORT
+from pedal.core.submission import Submission
+from pedal.core.commands import *
+from pedal.source import *
+from pedal.sandbox.commands import *
+from pedal.cait import *
+from pedal.assertions.commands import *
+from pedal.resolvers import *
+
+student: Sandbox
