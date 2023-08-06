84c8622c5909352cad52bdc0edede7c76bca240e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:37 +0000

    Add package pallet-sim/0.0.21/pallet_sim-0.0.21.tar.gz

---
+++ packages/pallet-sim/pallet_sim-0.0.21.tar.gz/pallet_sim-0.0.21/pallet_sim/__init__.py
@@ -0,0 +1,3 @@
+from pallet_sim.pallet_sim import Pallet_Sim
+from pallet_sim.gui_tool import main as gui_main
+from pallet_sim._load_example_data import load_example_data
---
+++ packages/pallet-sim/pallet_sim-0.0.21.tar.gz/pallet_sim-0.0.21/pallet_sim/_load_example_data.py
@@ -0,0 +1,11 @@
+import os
+import pkg_resources
+
+
+# function used to load example data contain in example_data folder
+def load_example_data() -> list[str]:
+    data = []
+    for file in os.listdir(pkg_resources.resource_filename(__name__, r'./example_data')):
+        if file.endswith(".order.json"):
+            data.append(open(pkg_resources.resource_filename(__name__, r'./example_data/' + file)).read())
+    return data
