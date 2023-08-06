4ded6ebdd5499c3138989b51e3b3b849ab98c19d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:14 +0000

    Add package Heterogeneous-Highway-Env/0.0.3/Heterogeneous_Highway_Env-0.0.3.tar.gz

---
+++ packages/Heterogeneous-Highway-Env/Heterogeneous_Highway_Env-0.0.3.tar.gz/Heterogeneous_Highway_Env-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Heterogeneous_Highway_Env",
+    version="0.0.3",
+    author="Xiyang Wu",
+    author_email="wuxiyang1996@gmail.com",
+    description="Codebase for for our version of highway-way modified for iPLAN paper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wuxiyang1996/Heterogeneous_Highway_Env",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
