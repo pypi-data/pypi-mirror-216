103bf69d025e39ee09637c07bdc109665c0d6c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:22 +0000

    Add package list-printer-v1-01-01-01/1.0.0/list_printer_v1_01_01_01-1.0.0.tar.gz

---
+++ packages/list-printer-v1-01-01-01/list_printer_v1_01_01_01-1.0.0.tar.gz/list_printer_v1_01_01_01-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'list_printer_v1_01_01_01',
+        version     = '1.0.0',
+        py_modules  = ['list_printer'],
+        author      = 'Elvis_Mburu',
+        author_email= 'mburuelvis19@gmail.com',
+        url         = "https://linktr.ee/mburuelvis",
+        description = 'A simple printer of nested lists',
+)
