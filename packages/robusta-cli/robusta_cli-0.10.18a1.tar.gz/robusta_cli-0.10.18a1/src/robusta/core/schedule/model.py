16a7b48d412f6a29526e29df0c2f436012523d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:01 +0000

    Add package robusta-cli/0.10.18a1/robusta_cli-0.10.18a1.tar.gz

---
+++ packages/robusta-cli/robusta_cli-0.10.18a1.tar.gz/robusta_cli-0.10.18a1/src/robusta/core/schedule/model.py
@@ -0,0 +1,40 @@
+from enum import Enum
+from typing import List, Union
+
+from pydantic import BaseModel
+
+
+class JobStatus(Enum):
+    NEW = 1
+    RUNNING = 2
+    DONE = 3
+
+
+class FixedDelayRepeat(BaseModel):
+    repeat: int = -1  # default to run forever
+    seconds_delay: int
+
+
+class DynamicDelayRepeat(BaseModel):
+    delay_periods: List[int]
+
+
+class JobState(BaseModel):
+    exec_count: int = 0
+    job_status: JobStatus = JobStatus.NEW
+    last_exec_time_sec: int = 0
+
+
+class ScheduledJob(BaseModel):
+    job_id: str
+    runnable_name: str
+    runnable_params: dict
+    state: JobState
+    scheduling_params: Union[FixedDelayRepeat, DynamicDelayRepeat]
+    replace_existing: bool = False
+    standalone_task: bool = False  # standalone task, is a task, that once fired, will run until it ends, as opposed to non standalone tasks,
+    # that are tied to the generating playbook lifecycle
+
+
+class SchedulingInfo(BaseModel):
+    execution_count: int
