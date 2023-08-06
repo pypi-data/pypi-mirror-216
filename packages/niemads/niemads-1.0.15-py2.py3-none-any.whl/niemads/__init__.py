fba8666eca60b0731f253bdef6c4f61029022a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:12 +0000

    Add package niemads/1.0.15/niemads-1.0.15-py2.py3-none-any.whl

---
+++ packages/niemads/niemads-1.0.15-py2.py3-none-any.whl/niemads/__init__.py
@@ -0,0 +1,4 @@
+from niemads.DisjointSet import DisjointSet
+from niemads.RandomSet import RandomSet
+from niemads.MultiwayTrie import MultiwayTrie
+__all__ = ['DisjointSet', 'MultiwayTrie', 'RandomSet']
