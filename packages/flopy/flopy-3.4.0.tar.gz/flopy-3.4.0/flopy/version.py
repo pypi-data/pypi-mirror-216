625f1b3320886e0f877e005726c8adb4cfbd3ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:02 +0000

    Add package flopy/3.4.0/flopy-3.4.0.tar.gz

---
+++ packages/flopy/flopy-3.4.0.tar.gz/flopy-3.4.0/flopy/version.py
@@ -0,0 +1,11 @@
+# flopy version file automatically created using update_version.py on June 29, 2023 01:58:44
+
+# created on...June 29, 2023 01:58:44
+
+major = 3
+minor = 4
+micro = 0
+label = ""
+__version__ = "{:d}.{:d}.{:d}".format(major, minor, micro)
+if label:
+    __version__ += "{}{}".format(__version__, label)
