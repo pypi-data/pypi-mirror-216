2b8da53ecbd5d9456d34eea831c3ad2fc3dbc814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:22 +0000

    Add package pyrocko/2023.6.29/pyrocko-2023.6.29-cp311-cp311-macosx_10_9_universal2.whl

---
+++ packages/pyrocko/pyrocko-2023.6.29-cp311-cp311-macosx_10_9_universal2.whl/pyrocko/info.py
@@ -0,0 +1,7 @@
+# This module is automatically created from setup.py
+git_sha1 = '34109fc2f3b99ae347e2af216a8657dc810839cc'
+local_modifications = False
+version = '2023.06.29'
+long_version = 'pyrocko-2023.06.29-34109fc2f3b99ae347e2af216a8657dc810839cc-2023-06-29_23:46:50'  # noqa
+installed_date = '2023-06-29_23:46:50'
+src_path = '/private/var/folders/kk/j306053d5dv1dqgvxjw8x7300000gp/T/drone-Ufopkm4nYYeTw0ul/drone/src'
---
+++ packages/pyrocko/pyrocko-2023.6.29-cp311-cp311-macosx_10_9_universal2.whl/pyrocko/mseed.py
@@ -0,0 +1,12 @@
+
+import sys
+import pyrocko
+if pyrocko.grumpy == 1:
+    sys.stderr.write('using renamed pyrocko module: pyrocko.mseed\n')
+    sys.stderr.write('           -> should now use: pyrocko.io.mseed\n\n')
+elif pyrocko.grumpy == 2:
+    sys.stderr.write('pyrocko module has been renamed: pyrocko.mseed\n')
+    sys.stderr.write('              -> should now use: pyrocko.io.mseed\n\n')
+    raise ImportError('Pyrocko module "pyrocko.mseed" has been renamed to "pyrocko.io.mseed".')
+
+from pyrocko.io.mseed import *
