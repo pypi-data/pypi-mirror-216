6ef7e2a9c278e763e9cb0aa81520c0fdf60cc69a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:59 +0000

    Add package ntropy-sdk/4.19.0rc5/ntropy_sdk-4.19.0rc5-py2.py3-none-any.whl

---
+++ packages/ntropy-sdk/ntropy_sdk-4.19.0rc5-py2.py3-none-any.whl/ntropy_sdk/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "4.19.0rc5"
+
+from ntropy_sdk.ntropy_sdk import (
+    AccountHolder,
+    Transaction,
+    SDK,
+    Batch,
+    Model,
+    NtropyError,
+    NtropyBatchError,
+    EnrichedTransaction,
+    EnrichedTransactionList,
+    LabeledTransaction,
+    BankStatement,
+    BankStatementRequest,
+    Report,
+)
