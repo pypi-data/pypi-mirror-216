b61261792d93092922762966682f668a6c133a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:58 +0000

    Add package dbis-exc-manager/0.2.10/dbis-exc-manager-0.2.10.tar.gz

---
+++ packages/dbis-exc-manager/dbis-exc-manager-0.2.10.tar.gz/dbis-exc-manager-0.2.10/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup
+from excmanager.Version import Version
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent.resolve()
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+requirements = (HERE / 'requirements.txt').read_text(encoding='utf-8').split("\n")
+
+# This call to setup() does all the work
+setup(
+    name="dbis-exc-manager",
+    version=Version.version,
+    description=Version.description,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rwth-acis/dbis-exercise-manager.git",
+    author="Michal Slupczynski",
+    author_email="slupczynski@dbis.rwth-aachen.de",
+    license="Apache",
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+    ],
+    packages=["excmanager"],
+    include_package_data=True,
+    install_requires=requirements
+)
