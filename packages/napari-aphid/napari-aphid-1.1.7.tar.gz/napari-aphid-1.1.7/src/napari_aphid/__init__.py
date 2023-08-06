4ab7fbafeceeac4f51a57abcda594101e2d3bf21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:18 +0000

    Add package napari-aphid/1.1.7/napari-aphid-1.1.7.tar.gz

---
+++ packages/napari-aphid/napari-aphid-1.1.7.tar.gz/napari-aphid-1.1.7/src/napari_aphid/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "1.1.7"
+
+from ._widget import process_function_segmentation, save_modification, process_function_classification
+
+__all__ = (
+    "process_function_segmentation",
+    "save_modification",
+    "process_function_classification",
+)
