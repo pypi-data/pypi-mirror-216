829c6d685e5522af53b3d49151cace248c8017ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:13 +0000

    Add package openldap-opencensus-stats/0.0.8/openldap-opencensus-stats-0.0.8.tar.gz

---
+++ packages/openldap-opencensus-stats/openldap-opencensus-stats-0.0.8.tar.gz/openldap-opencensus-stats-0.0.8/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.8'
+DESCRIPTION = 'OpenLDAP OpenCensus Statistics'
+LONG_DESCRIPTION = 'A package to gather statistics for OpenLDAP and publish via OpenCensus.'
+
+
+setup(
+    name="openldap-opencensus-stats",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Mark Donnelly",
+    author_email="mark@painless-security.com",
+    license='AGPL 3.0',
+    packages=find_packages(),
+    install_requires=[
+        'grpcio==1.47.0',
+        'opencensus-ext-stackdriver==0.8.0',
+        'opencensus-ext-prometheus',
+        'opencensus==0.10.0',
+        'python-ldap',
+        'pyyaml',
+    ],
+    keywords='openldap opencensus metrics',
+    scripts=['openldap-opencensus-stats.py'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: No Input/Output (Daemon)",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        "Programming Language :: Python :: 3",
+        "Topic :: System :: Monitoring",
+        "Topic :: System :: Networking :: Monitoring",
+        "Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP",
+    ]
+)
