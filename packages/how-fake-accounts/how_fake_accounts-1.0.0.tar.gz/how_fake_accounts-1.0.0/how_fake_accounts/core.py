8cbe312df665511a4d3c4ff05dbee29ce27786bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:07 +0000

    Add package how-fake-accounts/1.0.0/how_fake_accounts-1.0.0.tar.gz

---
+++ packages/how-fake-accounts/how_fake_accounts-1.0.0.tar.gz/how_fake_accounts-1.0.0/how_fake_accounts/core.py
@@ -0,0 +1,42 @@
+import json
+from pathlib import Path
+
+from faker import Faker
+from faker.providers import BaseProvider
+
+fake = Faker('pt_BR')
+
+class GenerateAccountsProvider(BaseProvider):
+    def generate_accounts(self, n_accounts=10):
+        accounts = []
+        for i in range(n_accounts):
+            accounts.append({
+                'name': fake.name(),
+                'email': fake.email(),
+                'phone': fake.cellphone_number(),
+                'cpf': fake.cpf(),
+                'birth_date': fake.date_of_birth(),
+                'address': fake.address(),
+                'job': fake.job()
+            })
+        return accounts
+
+    def save_locally(self, accounts, path='.', filename='accounts.json'):
+        filename = Path(path) / 'accounts.json'
+        with open(filename, 'w') as file:
+            json.dump(accounts, file, default=str)
+
+    def save_into_s3(self, accounts):
+        #import boto3
+        #s3 = boto3.resource('s3')
+        #object = s3.Object(bucket, filename)
+        #object.put(Body=json.dumps(accounts, default=str))
+        raise NotImplementedError
+            
+
+fake.add_provider(GenerateAccountsProvider)
+
+if __name__ == '__main__':
+    accounts = fake.generate_accounts()
+    print(accounts)
+    fake.save_locally(accounts)
