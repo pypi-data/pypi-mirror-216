d318b7cfaf32c0b7e04beb2e0fafcda1c9241db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:50 +0000

    Add package fa-models/1.0.55/fa-models-1.0.55.tar.gz

---
+++ packages/fa-models/fa-models-1.0.55.tar.gz/fa-models-1.0.55/tests/test_investor.py
@@ -0,0 +1,24 @@
+import pytest
+import hashlib
+import os
+import time
+import redis
+
+@pytest.fixture(scope="function", autouse=True)
+def setup_redis():
+    if 'CI' not in os.environ:
+        os.system('docker run --name redis-unit-test -d -p 6379:6379 redis/redis-stack-server:latest')
+        # give some time for the Redis server to start
+        time.sleep(2)        
+        
+    yield
+    if 'CI' not in os.environ:
+        os.system('docker stop redis-unit-test')
+        os.system('docker rm redis-unit-test')
+
+
+def test_investor_model():
+    # Now we can create an instance of `Investor` without connecting to a real Redis server
+    from famodels.models.investor import Investor
+    investor = Investor(investor_id='123', email='test@example.com')
+
