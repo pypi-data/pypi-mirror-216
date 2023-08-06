79e56fba4e333a3d1603f2401de39faddc7acb58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:04 +0000

    Add package twilight-autocomplete/1.0.0/twilight-autocomplete-1.0.0.tar.gz

---
+++ packages/twilight-autocomplete/twilight-autocomplete-1.0.0.tar.gz/twilight-autocomplete-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='twilight-autocomplete',
+    version='1.0.0',
+    description='Autocomplete Package built using NLTK for the twilight messaging client',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Avneh Singh Bhatia',
+    author_email='avnehb@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'nltk'
+    ],
+)
