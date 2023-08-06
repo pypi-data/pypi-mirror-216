8d2ab15deddc1e7dedbd599f10b9bf87b734412c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:10 +0000

    Add package imjoy-rpc/0.5.22rc1/imjoy-rpc-0.5.22rc1.tar.gz

---
+++ packages/imjoy-rpc/imjoy-rpc-0.5.22rc1.tar.gz/imjoy-rpc-0.5.22rc1/tests/test_websocket_rpc.py
@@ -0,0 +1,91 @@
+"""Test the hypha server."""
+import pytest
+from imjoy_rpc.hypha import login, connect_to_server
+from . import WS_SERVER_URL
+import numpy as np
+import requests
+import asyncio
+
+# All test coroutines will be treated as marked.
+pytestmark = pytest.mark.asyncio
+
+
+class ImJoyPlugin:
+    """Represent a test plugin."""
+
+    def __init__(self, ws):
+        """Initialize the plugin."""
+        self._ws = ws
+
+    async def setup(self):
+        """Set up the plugin."""
+        await self._ws.log("initialized")
+
+    async def run(self, ctx):
+        """Run the plugin."""
+        await self._ws.log("hello world")
+
+    async def add(self, data):
+        """Add function."""
+        return data + 1.0
+
+
+async def test_login(socketio_server):
+    TOKEN = "sf31df234"
+
+    async def callback(context):
+        print(f"By passing login: {context['login_url']}")
+        loop = asyncio.get_running_loop()
+        await loop.run_in_executor(
+            None,
+            requests.get,
+            context["report_url"] + "?key=" + context["key"] + "&token=" + TOKEN,
+        )
+
+    # We use ai.imjoy.io to test the login for now
+    token = await login(
+        {
+            "server_url": "https://ai.imjoy.io",
+            "login_callback": callback,
+            "login_timeout": 3,
+        }
+    )
+    assert token == TOKEN
+
+
+async def test_connect_to_server(socketio_server):
+    """Test connecting to the server."""
+    # test workspace is an exception, so it can pass directly
+    ws = await connect_to_server({"name": "my plugin", "server_url": WS_SERVER_URL})
+    with pytest.raises(Exception, match=r".*Permission denied for.*"):
+        ws = await connect_to_server(
+            {"name": "my plugin", "workspace": "test", "server_url": WS_SERVER_URL}
+        )
+    ws = await connect_to_server({"name": "my plugin", "server_url": WS_SERVER_URL})
+    await ws.export(ImJoyPlugin(ws))
+
+
+async def test_numpy_array(socketio_server):
+    """Test numpy array."""
+    ws = await connect_to_server(
+        {"client_id": "test-plugin", "server_url": WS_SERVER_URL}
+    )
+    await ws.export(ImJoyPlugin(ws))
+    workspace = ws.config.workspace
+    token = await ws.generate_token()
+
+    api = await connect_to_server(
+        {
+            "client_id": "client",
+            "workspace": workspace,
+            "token": token,
+            "server_url": WS_SERVER_URL,
+        }
+    )
+    plugin = await api.get_service("test-plugin:default")
+    result = await plugin.add(2.1)
+    assert result == 2.1 + 1.0
+
+    large_array = np.zeros([2048, 2048, 4], dtype="float32")
+    result = await plugin.add(large_array)
+    np.testing.assert_array_equal(result, large_array + 1.0)
