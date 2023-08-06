d33380b4e9a5d0e9f3c8f49405bfd2cff5d902ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:05 +0000

    Add package snakemake-wrapper-utils/0.6.1/snakemake_wrapper_utils-0.6.1-py3-none-any.whl

---
+++ packages/snakemake-wrapper-utils/snakemake_wrapper_utils-0.6.1-py3-none-any.whl/snakemake_wrapper_utils/snakemake.py
@@ -0,0 +1,27 @@
+def get_mem(snakemake, out_unit="MiB"):
+    """
+    Obtain requested memory (from resources) and return in given units.
+    If no memory resources found, return 0.
+    """
+
+    # Store memory in MiB
+    mem_mb = snakemake.resources.get("mem_gb", 0.2) * 1024
+    if not mem_mb:
+        mem_mb = snakemake.resources.get("mem_mb", 205)
+
+    if out_unit == "KiB":
+        return mem_mb * 1024
+    elif out_unit == "MiB":
+        return mem_mb
+    elif out_unit == "GiB":
+        return mem_mb / 1024
+    else:
+        raise valueError("invalid output unit. Only KiB, MiB and GiB supported.")
+
+
+def is_arg(arg, cmd):
+    """Check command for the presence of argument"""
+    if arg in cmd.replace("=", " ").split(" "):
+        return True
+
+    return False
