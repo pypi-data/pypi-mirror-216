483fb6212fac2e4af656122e4915b88bf570afb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:28 +0000

    Add package EasyDeL/0.0.7/EasyDeL-0.0.7-py3-none-any.whl

---
+++ packages/EasyDeL/EasyDeL-0.0.7-py3-none-any.whl/EasyDel/modules/__init__.py
@@ -0,0 +1,19 @@
+from .llama import LlamaConfig, LlamaModel, LlamaForCausalLM, FlaxLlamaModel, FlaxLlamaForCausalLM
+from .gpt_j import FlaxGPTJModule, FlaxGPTJForCausalLMModule, FlaxGPTJModel, FlaxGPTJForCausalLM, GPTJConfig
+from .lucid_transformer import FlaxLTModel, FlaxLTConfig, FlaxLTModelModule, FlaxLTForCausalLM
+from .mosaic_mpt import MptConfig, FlaxMptModel, FlaxMptForCausalLM
+from .falcon import FalconConfig, FlaxFalconModel, FlaxFalconForCausalLM
+from .gpt_neo_x import FlaxGPTNeoXForCausalLM, GPTNeoXConfig, FlaxGPTNeoXModel
+from .palm import PalmConfig, PalmModel, FlaxPalmForCausalLM
+from .t5 import FlaxT5ForConditionalGeneration, FlaxT5Model
+
+__all__ = ['LlamaConfig', 'LlamaForCausalLM', 'LlamaModel', 'FlaxLlamaForCausalLM', 'FlaxLlamaModel',
+           'FlaxGPTJModule', 'FlaxGPTJForCausalLMModule', 'FlaxGPTJModel', 'FlaxGPTJForCausalLM', 'GPTJConfig',
+           'FlaxLTModel', 'FlaxLTConfig', 'FlaxLTModelModule', 'FlaxLTForCausalLM',
+           "MptConfig", "FlaxMptModel", "FlaxMptForCausalLM",
+           "FalconConfig", "FlaxFalconModel", "FlaxFalconForCausalLM",
+           "FlaxGPTNeoXForCausalLM", "GPTNeoXConfig", "FlaxGPTNeoXModel",
+           "FlaxT5ForConditionalGeneration", "FlaxT5Model",
+           "PalmConfig", "PalmModel", "FlaxPalmForCausalLM"
+
+           ]
