e5d54fb7f835d759a6d617e6627d42dfc9f2ec04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:19 +0000

    Add package dsp-tools/2.3.4/dsp_tools-2.3.4.tar.gz

---
+++ packages/dsp-tools/dsp_tools-2.3.4.tar.gz/dsp_tools-2.3.4/src/dsp_tools/models/model.py
@@ -0,0 +1,13 @@
+from dsp_tools.models.connection import Connection
+from dsp_tools.models.exceptions import BaseError
+
+
+class Model:
+    _con: Connection
+    _changed: set[str]
+
+    def __init__(self, con: Connection):
+        if not isinstance(con, Connection):
+            raise BaseError('"con"-parameter must be an instance of Connection')
+        self._con = con
+        self._changed = set()
