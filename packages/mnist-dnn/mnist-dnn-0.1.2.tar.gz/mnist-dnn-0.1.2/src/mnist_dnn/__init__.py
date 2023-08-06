2294cd8821954bad2ea2d3e96840548ae2f8a0f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:50 +0000

    Add package mnist-dnn/0.1.2/mnist-dnn-0.1.2.tar.gz

---
+++ packages/mnist-dnn/mnist-dnn-0.1.2.tar.gz/mnist-dnn-0.1.2/src/mnist_dnn/__init__.py
@@ -0,0 +1,6 @@
+"""
+mnist_dnn package provides essential tools to build custom dnn based on MNIST dataset
+"""
+
+# Define __version__
+__version__ = '0.1.1'
---
+++ packages/mnist-dnn/mnist-dnn-0.1.2.tar.gz/mnist-dnn-0.1.2/src/mnist_dnn/util/__init__.py
@@ -0,0 +1,6 @@
+"""
+mnist_dnn.util package provides useful tools and features
+"""
+
+# Define __version__
+__version__ = '0.1.1'
