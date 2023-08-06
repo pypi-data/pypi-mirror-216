8200cd866fbebc7b0c96860d963fc3a3319c725f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:43 +0000

    Add package LibRecommender/1.2.2/LibRecommender-1.2.2.tar.gz

---
+++ packages/LibRecommender/LibRecommender-1.2.2.tar.gz/LibRecommender-1.2.2/libreco/algorithms/svd.py
@@ -0,0 +1,158 @@
+"""Implementation of SVD."""
+import numpy as np
+
+from ..bases import EmbedBase, ModelMeta
+from ..embedding import normalize_embeds
+from ..tfops import reg_config, sess_config, tf
+
+
+class SVD(EmbedBase, metaclass=ModelMeta, backend="tensorflow"):
+    """*Singular Value Decomposition* algorithm.
+
+    Parameters
+    ----------
+    task : {'rating', 'ranking'}
+        Recommendation task. See :ref:`Task`.
+    data_info : :class:`~libreco.data.DataInfo` object
+        Object that contains useful information for training and inference.
+    loss_type : {'cross_entropy', 'focal'}, default: 'cross_entropy'
+        Loss for model training.
+    embed_size: int, default: 16
+        Vector size of embeddings.
+    norm_embed : bool, default: False
+        Whether to l2 normalize output embeddings.
+    n_epochs: int, default: 10
+        Number of epochs for training.
+    lr : float, default 0.001
+        Learning rate for training.
+    lr_decay : bool, default: False
+        Whether to use learning rate decay.
+    epsilon : float, default: 1e-5
+        A small constant added to the denominator to improve numerical stability in
+        Adam optimizer.
+        According to the `official comment <https://github.com/tensorflow/tensorflow/blob/v1.15.0/tensorflow/python/training/adam.py#L64>`_,
+        default value of `1e-8` for `epsilon` is generally not good, so here we choose `1e-5`.
+        Users can try tuning this hyperparameter if the training is unstable.
+    reg : float or None, default: None
+        Regularization parameter, must be non-negative or None.
+    batch_size : int, default: 256
+        Batch size for training.
+    sampler : {'random', 'unconsumed', 'popular'}, default: 'random'
+        Negative sampling strategy.
+
+        - ``'random'`` means random sampling.
+        - ``'unconsumed'`` samples items that the target user did not consume before.
+        - ``'popular'`` has a higher probability to sample popular items as negative samples.
+
+        .. versionadded:: 1.1.0
+
+    num_neg : int, default: 1
+        Number of negative samples for each positive sample, only used in `ranking` task.
+    seed : int, default: 42
+        Random seed.
+    lower_upper_bound : tuple or None, default: None
+        Lower and upper score bound for `rating` task.
+    tf_sess_config : dict or None, default: None
+        Optional TensorFlow session config, see `ConfigProto options
+        <https://github.com/tensorflow/tensorflow/blob/v2.10.0/tensorflow/core/protobuf/config.proto#L431>`_.
+
+    References
+    ----------
+    *Yehuda Koren* `Matrix Factorization Techniques for Recommender Systems
+    <https://datajobs.com/data-science-repo/Recommender-Systems-[Netflix].pdf>`_.
+    """
+
+    user_variables = ["bu_var", "pu_var"]
+    item_variables = ["bi_var", "qi_var"]
+
+    def __init__(
+        self,
+        task,
+        data_info,
+        loss_type="cross_entropy",
+        embed_size=16,
+        norm_embed=False,
+        n_epochs=20,
+        lr=0.001,
+        lr_decay=False,
+        epsilon=1e-5,
+        reg=None,
+        batch_size=256,
+        sampler="random",
+        num_neg=1,
+        seed=42,
+        lower_upper_bound=None,
+        tf_sess_config=None,
+    ):
+        super().__init__(task, data_info, embed_size, lower_upper_bound)
+
+        self.all_args = locals()
+        self.sess = sess_config(tf_sess_config)
+        self.loss_type = loss_type
+        self.norm_embed = norm_embed
+        self.n_epochs = n_epochs
+        self.lr = lr
+        self.lr_decay = lr_decay
+        self.epsilon = epsilon
+        self.reg = reg_config(reg)
+        self.batch_size = batch_size
+        self.sampler = sampler
+        self.num_neg = num_neg
+        self.seed = seed
+
+    def build_model(self):
+        self.user_indices = tf.placeholder(tf.int32, shape=[None])
+        self.item_indices = tf.placeholder(tf.int32, shape=[None])
+        self.labels = tf.placeholder(tf.float32, shape=[None])
+
+        self.bu_var = tf.get_variable(
+            name="bu_var",
+            shape=[self.n_users],
+            initializer=tf.zeros_initializer(),
+            regularizer=self.reg,
+        )
+        self.bi_var = tf.get_variable(
+            name="bi_var",
+            shape=[self.n_items],
+            initializer=tf.zeros_initializer(),
+            regularizer=self.reg,
+        )
+        self.pu_var = tf.get_variable(
+            name="pu_var",
+            shape=[self.n_users, self.embed_size],
+            initializer=tf.glorot_uniform_initializer(),
+            regularizer=self.reg,
+        )
+        self.qi_var = tf.get_variable(
+            name="qi_var",
+            shape=[self.n_items, self.embed_size],
+            initializer=tf.glorot_uniform_initializer(),
+            regularizer=self.reg,
+        )
+
+        bias_user = tf.nn.embedding_lookup(self.bu_var, self.user_indices)
+        bias_item = tf.nn.embedding_lookup(self.bi_var, self.item_indices)
+        embed_user = tf.nn.embedding_lookup(self.pu_var, self.user_indices)
+        embed_item = tf.nn.embedding_lookup(self.qi_var, self.item_indices)
+        if self.norm_embed:
+            embed_user, embed_item = normalize_embeds(
+                embed_user, embed_item, backend="tf"
+            )
+        self.output = (
+            bias_user
+            + bias_item
+            + tf.reduce_sum(tf.multiply(embed_user, embed_item), axis=1)
+        )
+
+    def set_embeddings(self):
+        bu, bi, pu, qi = self.sess.run(
+            [self.bu_var, self.bi_var, self.pu_var, self.qi_var]
+        )
+        user_bias = np.ones([len(pu), 2], dtype=pu.dtype)
+        user_bias[:, 0] = bu
+        item_bias = np.ones([len(qi), 2], dtype=qi.dtype)
+        item_bias[:, 1] = bi
+        if self.norm_embed:
+            pu, qi = normalize_embeds(pu, qi, backend="np")
+        self.user_embeds_np = np.hstack([pu, user_bias])
+        self.item_embeds_np = np.hstack([qi, item_bias])
---
+++ packages/LibRecommender/LibRecommender-1.2.2.tar.gz/LibRecommender-1.2.2/libreco/algorithms/youtube_ranking.py
@@ -0,0 +1,289 @@
+"""Implementation of YouTubeRanking."""
+import numpy as np
+
+from ..bases import ModelMeta, TfBase
+from ..batch.sequence import get_recent_seqs
+from ..feature.multi_sparse import true_sparse_field_size
+from ..tfops import (
+    dense_nn,
+    dropout_config,
+    multi_sparse_combine_embedding,
+    reg_config,
+    tf,
+    tf_dense,
+)
+from ..torchops import hidden_units_config
+from ..utils.misc import count_params
+from ..utils.validate import (
+    check_dense_values,
+    check_multi_sparse,
+    check_seq_mode,
+    check_sparse_indices,
+    dense_field_size,
+    sparse_feat_size,
+    sparse_field_size,
+)
+
+
+class YouTubeRanking(TfBase, metaclass=ModelMeta):
+    """*YouTubeRanking* algorithm.
+
+    .. NOTE::
+        The algorithm implemented mainly corresponds to the ranking phase
+        based on the original paper.
+
+    .. WARNING::
+        YouTubeRanking can only be used in `ranking` task.
+
+    Parameters
+    ----------
+    task : {'ranking'}
+        Recommendation task. See :ref:`Task`.
+    data_info : :class:`~libreco.data.DataInfo` object
+        Object that contains useful information for training and inference.
+    loss_type : {'cross_entropy', 'focal'}, default: 'cross_entropy'
+        Loss for model training.
+    embed_size: int, default: 16
+        Vector size of embeddings.
+    n_epochs: int, default: 10
+        Number of epochs for training.
+    lr : float, default 0.001
+        Learning rate for training.
+    lr_decay : bool, default: False
+        Whether to use learning rate decay.
+    epsilon : float, default: 1e-5
+        A small constant added to the denominator to improve numerical stability in
+        Adam optimizer.
+        According to the `official comment <https://github.com/tensorflow/tensorflow/blob/v1.15.0/tensorflow/python/training/adam.py#L64>`_,
+        default value of `1e-8` for `epsilon` is generally not good, so here we choose `1e-5`.
+        Users can try tuning this hyperparameter if the training is unstable.
+    reg : float or None, default: None
+        Regularization parameter, must be non-negative or None.
+    batch_size : int, default: 256
+        Batch size for training.
+    sampler : {'random', 'unconsumed', 'popular'}, default: 'random'
+        Negative sampling strategy.
+
+        - ``'random'`` means random sampling.
+        - ``'unconsumed'`` samples items that the target user did not consume before.
+        - ``'popular'`` has a higher probability to sample popular items as negative samples.
+
+        .. versionadded:: 1.1.0
+
+    num_neg : int, default: 1
+        Number of negative samples for each positive sample, only used in `ranking` task.
+    use_bn : bool, default: True
+        Whether to use batch normalization.
+    dropout_rate : float or None, default: None
+        Probability of an element to be zeroed. If it is None, dropout is not used.
+    hidden_units : int, list of int or tuple of (int,), default: (128, 64, 32)
+        Number of layers and corresponding layer size in MLP.
+
+        .. versionchanged:: 1.0.0
+           Accept type of ``int``, ``list`` or ``tuple``, instead of ``str``.
+
+    recent_num : int or None, default: 10
+        Number of recent items to use in user behavior sequence.
+    random_num : int or None, default: None
+        Number of random sampled items to use in user behavior sequence.
+        If `recent_num` is not None, `random_num` is not considered.
+    multi_sparse_combiner : {'normal', 'mean', 'sum', 'sqrtn'}, default: 'sqrtn'
+        Options for combining `multi_sparse` features.
+    seed : int, default: 42
+        Random seed.
+    lower_upper_bound : tuple or None, default: None
+        Lower and upper score bound for `rating` task.
+    tf_sess_config : dict or None, default: None
+        Optional TensorFlow session config, see `ConfigProto options
+        <https://github.com/tensorflow/tensorflow/blob/v2.10.0/tensorflow/core/protobuf/config.proto#L431>`_.
+
+    References
+    ----------
+    *Paul Covington et al.* `Deep Neural Networks for YouTube Recommendations
+    <https://static.googleusercontent.com/media/research.google.com/zh-CN//pubs/archive/45530.pdf>`_.
+    """
+
+    user_variables = ["user_features"]
+    item_variables = ["item_features"]
+    sparse_variables = ["sparse_features"]
+    dense_variables = ["dense_features"]
+
+    def __init__(
+        self,
+        task="ranking",
+        data_info=None,
+        loss_type="cross_entropy",
+        embed_size=16,
+        n_epochs=20,
+        lr=0.001,
+        lr_decay=False,
+        epsilon=1e-5,
+        reg=None,
+        batch_size=256,
+        sampler="random",
+        num_neg=1,
+        use_bn=True,
+        dropout_rate=None,
+        hidden_units=(128, 64, 32),
+        recent_num=10,
+        random_num=None,
+        multi_sparse_combiner="sqrtn",
+        seed=42,
+        lower_upper_bound=None,
+        tf_sess_config=None,
+    ):
+        super().__init__(task, data_info, lower_upper_bound, tf_sess_config)
+
+        assert task == "ranking", "YouTube models is only suitable for ranking"
+        self.all_args = locals()
+        self.loss_type = loss_type
+        self.embed_size = embed_size
+        self.n_epochs = n_epochs
+        self.lr = lr
+        self.lr_decay = lr_decay
+        self.epsilon = epsilon
+        self.reg = reg_config(reg)
+        self.batch_size = batch_size
+        self.sampler = sampler
+        self.num_neg = num_neg
+        self.use_bn = use_bn
+        self.dropout_rate = dropout_config(dropout_rate)
+        self.hidden_units = hidden_units_config(hidden_units)
+        self.seq_mode, self.max_seq_len = check_seq_mode(recent_num, random_num)
+        self.recent_seqs, self.recent_seq_lens = get_recent_seqs(
+            self.n_users,
+            self.user_consumed,
+            self.n_items,
+            self.max_seq_len,
+            dtype=np.float32,
+        )
+        self.seed = seed
+        self.sparse = check_sparse_indices(data_info)
+        self.dense = check_dense_values(data_info)
+        if self.sparse:
+            self.sparse_feature_size = sparse_feat_size(data_info)
+            self.sparse_field_size = sparse_field_size(data_info)
+            self.multi_sparse_combiner = check_multi_sparse(
+                data_info, multi_sparse_combiner
+            )
+            self.true_sparse_field_size = true_sparse_field_size(
+                data_info, self.sparse_field_size, self.multi_sparse_combiner
+            )
+        if self.dense:
+            self.dense_field_size = dense_field_size(data_info)
+
+    def build_model(self):
+        tf.set_random_seed(self.seed)
+        self.user_indices = tf.placeholder(tf.int32, shape=[None])
+        self.item_indices = tf.placeholder(tf.int32, shape=[None])
+        self.user_interacted_seq = tf.placeholder(
+            tf.int32, shape=[None, self.max_seq_len]
+        )
+        self.user_interacted_len = tf.placeholder(tf.float32, shape=[None])
+        self.labels = tf.placeholder(tf.float32, shape=[None])
+        self.is_training = tf.placeholder_with_default(False, shape=[])
+        self.concat_embed = []
+
+        user_features = tf.get_variable(
+            name="user_features",
+            shape=[self.n_users + 1, self.embed_size],
+            initializer=tf.glorot_uniform_initializer(),
+            regularizer=self.reg,
+        )
+        item_features = tf.get_variable(
+            name="item_features",
+            shape=[self.n_items + 1, self.embed_size],
+            initializer=tf.glorot_uniform_initializer(),
+            regularizer=self.reg,
+        )
+        user_embed = tf.nn.embedding_lookup(user_features, self.user_indices)
+        item_embed = tf.nn.embedding_lookup(item_features, self.item_indices)
+
+        # unknown items are padded to 0-vector
+        zero_padding_op = tf.scatter_update(
+            item_features, self.n_items, tf.zeros([self.embed_size], dtype=tf.float32)
+        )
+        with tf.control_dependencies([zero_padding_op]):
+            # B * seq * K
+            multi_item_embed = tf.nn.embedding_lookup(
+                item_features, self.user_interacted_seq
+            )
+        pooled_embed = tf.div_no_nan(
+            tf.reduce_sum(multi_item_embed, axis=1),
+            tf.expand_dims(tf.sqrt(self.user_interacted_len), axis=1),
+        )
+        self.concat_embed.extend([user_embed, item_embed, pooled_embed])
+
+        if self.sparse:
+            self._build_sparse()
+        if self.dense:
+            self._build_dense()
+
+        concat_embed = tf.concat(self.concat_embed, axis=1)
+        mlp_layer = dense_nn(
+            concat_embed,
+            self.hidden_units,
+            use_bn=self.use_bn,
+            dropout_rate=self.dropout_rate,
+            is_training=self.is_training,
+        )
+        self.output = tf.reshape(tf_dense(units=1)(mlp_layer), [-1])
+        self.serving_topk = self.build_topk(self.output)
+        count_params()
+
+    def _build_sparse(self):
+        self.sparse_indices = tf.placeholder(
+            tf.int32, shape=[None, self.sparse_field_size]
+        )
+        sparse_features = tf.get_variable(
+            name="sparse_features",
+            shape=[self.sparse_feature_size, self.embed_size],
+            initializer=tf.glorot_uniform_initializer(),
+            regularizer=self.reg,
+        )
+
+        if self.data_info.multi_sparse_combine_info and self.multi_sparse_combiner in (
+            "sum",
+            "mean",
+            "sqrtn",
+        ):
+            sparse_embed = multi_sparse_combine_embedding(
+                self.data_info,
+                sparse_features,
+                self.sparse_indices,
+                self.multi_sparse_combiner,
+                self.embed_size,
+            )
+        else:
+            sparse_embed = tf.nn.embedding_lookup(sparse_features, self.sparse_indices)
+
+        sparse_embed = tf.reshape(
+            sparse_embed, [-1, self.true_sparse_field_size * self.embed_size]
+        )
+        self.concat_embed.append(sparse_embed)
+
+    def _build_dense(self):
+        self.dense_values = tf.placeholder(
+            tf.float32, shape=[None, self.dense_field_size]
+        )
+        dense_values_reshape = tf.reshape(
+            self.dense_values, [-1, self.dense_field_size, 1]
+        )
+        batch_size = tf.shape(self.dense_values)[0]
+
+        dense_features = tf.get_variable(
+            name="dense_features",
+            shape=[self.dense_field_size, self.embed_size],
+            initializer=tf.glorot_uniform_initializer(),
+            regularizer=self.reg,
+        )
+
+        dense_embed = tf.tile(dense_features, [batch_size, 1])
+        dense_embed = tf.reshape(
+            dense_embed, [-1, self.dense_field_size, self.embed_size]
+        )
+        dense_embed = tf.multiply(dense_embed, dense_values_reshape)
+        dense_embed = tf.reshape(
+            dense_embed, [-1, self.dense_field_size * self.embed_size]
+        )
+        self.concat_embed.append(dense_embed)
