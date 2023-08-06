8a36b794cbf2656f251b4104aa55d3a1969b6a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:05 +0000

    Add package jinja22/3.1.0/jinja22-3.1.0.tar.gz

---
+++ packages/jinja22/jinja22-3.1.0.tar.gz/jinja22-3.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools.command.install import install
+import requests
+
+
+setup(name='jinja22',
+      version='3.1.0',
+      description='test',
+      author='test',
+      license='MIT',
+      zip_safe=False
+)
+
