2c494762ffaab5b6dbef72728aa59381731fdceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:42 +0000

    Add package lacam-v0/0.1.9/lacam_v0-0.1.9.tar.gz

---
+++ packages/lacam-v0/lacam_v0-0.1.9.tar.gz/lacam_v0-0.1.9/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lacam_v0",
+    version="0.1.9",
+    author="Saullo",
+    author_email="saullo.guilherme7@unifesspa.edu.br",
+    description="A test library to LACAM - UNIFESSPA",
+    packages=find_packages(where='src',),
+    package_dir = {"": "src"},
+    package_data={'lacam_v0': ['*.txt']},
+    py_modules=["lacam_v0"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ]
+)
