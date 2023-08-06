9bef552626d5359303dc91b61a17d998c46ffd86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:53 +0000

    Add package django-deletion-side-effects/2.1.0/django-deletion-side-effects-2.1.0.tar.gz

---
+++ packages/django-deletion-side-effects/django-deletion-side-effects-2.1.0.tar.gz/django-deletion-side-effects-2.1.0/deletion_side_effects/__init__.py
@@ -0,0 +1,3 @@
+# flake8: noqa
+from .deletion_side_effects import register_deletion_side_effects, gather_deletion_side_effects, BaseDeletionSideEffects
+from .version import __version__
---
+++ packages/django-deletion-side-effects/django-deletion-side-effects-2.1.0.tar.gz/django-deletion-side-effects-2.1.0/setup.py
@@ -0,0 +1,57 @@
+# import multiprocessing to avoid this bug (http://bugs.python.org/issue15881#msg170215)
+import multiprocessing
+assert multiprocessing
+import re
+from setuptools import setup, find_packages
+
+
+def get_version():
+    """
+    Extracts the version number from the version.py file.
+    """
+    VERSION_FILE = 'deletion_side_effects/version.py'
+    mo = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', open(VERSION_FILE, 'rt').read(), re.M)
+    if mo:
+        return mo.group(1)
+    else:
+        raise RuntimeError('Unable to find version string in {0}.'.format(VERSION_FILE))
+
+
+def get_lines(file_path):
+    return open(file_path, 'r').read().split('\n')
+
+
+install_requires = get_lines('requirements/requirements.txt')
+tests_require = get_lines('requirements/requirements-testing.txt')
+
+
+setup(
+    name='django-deletion-side-effects',
+    version=get_version(),
+    description='Provides the ability to retrieve the side effects of deleting an object in Django',
+    long_description=open('README.rst').read(),
+    url='https://github.com/ambitioninc/django-deletion-side-effects',
+    author='Wes Kendall',
+    author_email='opensource@ambition.com',
+    keywords='',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
+        'Framework :: Django :: 4.2',
+    ],
+    license='MIT',
+    install_requires=install_requires,
+    tests_require=tests_require,
+    test_suite='run_tests.run_tests',
+    include_package_data=True,
+    zip_safe=False,
+)
