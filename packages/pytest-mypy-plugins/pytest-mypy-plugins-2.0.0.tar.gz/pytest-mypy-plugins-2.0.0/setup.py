5dd90ac47fdaf52cf58afea6f2c25ca069414549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:36 +0000

    Add package pytest-mypy-plugins/2.0.0/pytest-mypy-plugins-2.0.0.tar.gz

---
+++ packages/pytest-mypy-plugins/pytest-mypy-plugins-2.0.0.tar.gz/pytest-mypy-plugins-2.0.0/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+dependencies = [
+    "pytest>=7.0.0",
+    "mypy>=1.3",
+    "decorator",
+    "pyyaml",
+    "Jinja2",
+    "regex",
+    "packaging",
+]
+
+setup(
+    name="pytest-mypy-plugins",
+    version="2.0.0",
+    description="pytest plugin for writing tests for mypy plugins",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/TypedDjango/pytest-mypy-plugins",
+    author="Maksim Kurnikov",
+    author_email="maxim.kurnikov@gmail.com",
+    maintainer="Nikita Sobolev",
+    maintainer_email="mail@sobolevn.me",
+    packages=["pytest_mypy_plugins"],
+    # the following makes a plugin available to pytest
+    entry_points={"pytest11": ["pytest-mypy-plugins = pytest_mypy_plugins.collect"]},
+    install_requires=dependencies,
+    python_requires=">=3.7",
+    package_data={
+        "pytest_mypy_plugins": ["py.typed"],
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+    ],
+)
