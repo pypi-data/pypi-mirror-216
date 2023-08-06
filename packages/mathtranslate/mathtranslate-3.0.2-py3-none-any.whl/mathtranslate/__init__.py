6fc230b40cc3d1e47f27df8ba4611a0cc922b6c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:40 +0000

    Add package mathtranslate/3.0.2/mathtranslate-3.0.2-py3-none-any.whl

---
+++ packages/mathtranslate/mathtranslate-3.0.2-py3-none-any.whl/mathtranslate/__init__.py
@@ -0,0 +1,20 @@
+__version__ = "3.0.2"
+__author__ = "MathTranslate developers"
+import os
+from appdata import AppDataPaths
+app_paths = AppDataPaths('mathtranslate')
+app_dir = app_paths.app_data_path
+os.makedirs(app_dir, exist_ok=True)
+
+from . import cache
+from . import config
+from . import translate
+from . import tencentcloud
+from . import encoding
+from . import process_latex
+from . import process_text
+from . import update
+from . import translate_tex
+from . import translate_arxiv
+from .translate_tex import main as tex_main
+from .translate_arxiv import main as arxiv_main
