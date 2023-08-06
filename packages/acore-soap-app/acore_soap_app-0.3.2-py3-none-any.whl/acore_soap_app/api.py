cafe90a61d32ecae13a54bb24315c9376b57cc55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:47 +0000

    Add package acore-soap-app/0.3.2/acore_soap_app-0.3.2-py3-none-any.whl

---
+++ packages/acore-soap-app/acore_soap_app-0.3.2-py3-none-any.whl/acore_soap_app/api.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from .agent.api import DEFAULT_USERNAME
+from .agent.api import DEFAULT_PASSWORD
+from .agent.api import DEFAULT_HOST
+from .agent.api import DEFAULT_PORT
+from .agent.api import SOAPRequest
+from .agent.api import SOAPResponse
+from .sdk.api import run_soap_command
+from .sdk.api import canned
