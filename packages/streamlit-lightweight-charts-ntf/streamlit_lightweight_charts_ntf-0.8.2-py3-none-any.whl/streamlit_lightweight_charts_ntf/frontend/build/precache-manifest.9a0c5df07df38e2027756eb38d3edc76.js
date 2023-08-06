abcfece88f0a61d50263311c73205f68134474ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:05 +0000

    Add package streamlit-lightweight-charts-ntf/0.8.2/streamlit_lightweight_charts_ntf-0.8.2-py3-none-any.whl

---
+++ packages/streamlit-lightweight-charts-ntf/streamlit_lightweight_charts_ntf-0.8.2-py3-none-any.whl/streamlit_lightweight_charts_ntf/frontend/build/precache-manifest.9a0c5df07df38e2027756eb38d3edc76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf470839559c5f4084ada9665cd00bb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "13664c96250725e1b8c1",
+    "url": "./static/js/2.e1783641.chunk.js"
+  },
+  {
+    "revision": "eb85e354bdcb02e860c0e5e9bda20a16",
+    "url": "./static/js/2.e1783641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "980da94eaac83538b894",
+    "url": "./static/js/main.e3a393b7.chunk.js"
+  },
+  {
+    "revision": "77d2551f056df0873828",
+    "url": "./static/js/runtime-main.938d375b.js"
+  }
+]);
