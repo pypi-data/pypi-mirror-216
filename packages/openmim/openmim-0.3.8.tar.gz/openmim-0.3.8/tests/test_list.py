963dcd0a0461a18d8592512ddb32533d265419d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:00 +0000

    Add package openmim/0.3.8/openmim-0.3.8.tar.gz

---
+++ packages/openmim/openmim-0.3.8.tar.gz/openmim-0.3.8/tests/test_list.py
@@ -0,0 +1,38 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from click.testing import CliRunner
+
+from mim.commands.install import cli as install
+from mim.commands.list import list_package
+from mim.commands.uninstall import cli as uninstall
+
+
+def setup_module():
+    runner = CliRunner()
+    result = runner.invoke(uninstall, ['mmcv-full', '--yes'])
+    assert result.exit_code == 0
+    result = runner.invoke(uninstall, ['mmcv', '--yes'])
+    assert result.exit_code == 0
+    result = runner.invoke(uninstall, ['mmcls', '--yes'])
+    assert result.exit_code == 0
+
+
+def test_list():
+    runner = CliRunner()
+    # mim install mmcls==0.23.0 --yes
+    result = runner.invoke(install, ['mmcls==0.23.0', '--yes'])
+    assert result.exit_code == 0
+    # mim list
+    target = ('mmcls', '0.23.0',
+              'https://github.com/open-mmlab/mmclassification')
+    result = list_package()
+    assert target in result
+
+
+def teardown_module():
+    runner = CliRunner()
+    result = runner.invoke(uninstall, ['mmcv-full', '--yes'])
+    assert result.exit_code == 0
+    result = runner.invoke(uninstall, ['mmcv', '--yes'])
+    assert result.exit_code == 0
+    result = runner.invoke(uninstall, ['mmcls', '--yes'])
+    assert result.exit_code == 0
---
+++ packages/openmim/openmim-0.3.8.tar.gz/openmim-0.3.8/tests/test_test.py
@@ -0,0 +1,60 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import pytest
+import torch
+from click.testing import CliRunner
+
+from mim.commands.install import cli as install
+from mim.commands.test import cli as test
+from mim.commands.uninstall import cli as uninstall
+
+
+def setup_module():
+    runner = CliRunner()
+    result = runner.invoke(uninstall, ['mmcv-full', '--yes'])
+    assert result.exit_code == 0
+    result = runner.invoke(uninstall, ['mmcv', '--yes'])
+    assert result.exit_code == 0
+    result = runner.invoke(uninstall, ['mmcls', '--yes'])
+    assert result.exit_code == 0
+
+
+@pytest.mark.parametrize('device', [
+    'cpu',
+    pytest.param(
+        'cuda',
+        marks=pytest.mark.skipif(
+            not torch.cuda.is_available(), reason='requires CUDA support')),
+])
+def test_test(device):
+    runner = CliRunner()
+    result = runner.invoke(install, ['mmcls>=1.0.0rc0', '--yes'])
+    assert result.exit_code == 0
+    result = runner.invoke(install, ['mmengine', '--yes'])
+    assert result.exit_code == 0
+    result = runner.invoke(install, ['mmcv>=2.0.0rc0', '--yes'])
+    assert result.exit_code == 0
+
+    result = runner.invoke(test, [
+        'mmcls', 'tests/data/lenet5_mnist_2.0.py', '--checkpoint',
+        'tests/data/epoch_1.pth'
+    ])
+    assert result.exit_code == 0
+    result = runner.invoke(test, [
+        'mmcls', 'tests/data/xxx.py', '--checkpoint', 'tests/data/epoch_1.pth'
+    ])
+    assert result.exit_code != 0
+    result = runner.invoke(test, [
+        'mmcls', 'tests/data/lenet5_mnist_2.0.py', '--checkpoint',
+        'tests/data/xxx.pth'
+    ])
+    assert result.exit_code != 0
+
+
+def teardown_module():
+    runner = CliRunner()
+    result = runner.invoke(uninstall, ['mmcv-full', '--yes'])
+    assert result.exit_code == 0
+    result = runner.invoke(uninstall, ['mmcv', '--yes'])
+    assert result.exit_code == 0
+    result = runner.invoke(uninstall, ['mmcls', '--yes'])
+    assert result.exit_code == 0
