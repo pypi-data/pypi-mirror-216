41f1eacbfdb0eed175c592a3c612fb00f41af230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:09 +0000

    Add package convtools/1.4.0/convtools-1.4.0-py3-none-any.whl

---
+++ packages/convtools/convtools-1.4.0-py3-none-any.whl/convtools/__init__.py
@@ -0,0 +1,7 @@
+"""The only object is exported - conversion.
+This object exposes the public API of conversions."""
+from ._conversion import conversion  # noqa: F401
+from ._dt import DateGrid, DateTimeGrid
+
+
+__version__ = "1.4.0"
