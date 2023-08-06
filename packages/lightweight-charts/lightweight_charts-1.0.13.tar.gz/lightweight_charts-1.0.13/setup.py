4483abef5ef2a2e81a1c8381d4a5eacd91e13cf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:43 +0000

    Add package lightweight-charts/1.0.13/lightweight_charts-1.0.13.tar.gz

---
+++ packages/lightweight-charts/lightweight_charts-1.0.13.tar.gz/lightweight_charts-1.0.13/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='lightweight_charts',
+    version='1.0.13',
+    packages=find_packages(),
+    python_requires='>=3.9',
+    install_requires=[
+        'pandas',
+        'pywebview',
+    ],
+    author='louisnw',
+    license='MIT',
+    description="Python framework for TradingView's Lightweight Charts JavaScript library.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/louisnw01/lightweight-charts-python',
+)
