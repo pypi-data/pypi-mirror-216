db90be2dead1aeac8131cd762fa3dadcbc5cc9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:44 +0000

    Add package sockets-communication/1.0.0/sockets-communication-1.0.0.tar.gz

---
+++ packages/sockets-communication/sockets-communication-1.0.0.tar.gz/sockets-communication-1.0.0/sockets_communication/document.py
@@ -0,0 +1,16 @@
+# document.py
+
+# noinspection PyUnresolvedReferences
+from sockets_communication.process import *
+# noinspection PyUnresolvedReferences
+from sockets_communication.sockets import *
+# noinspection PyUnresolvedReferences
+from sockets_communication.exceptions import *
+
+from sockets_communication.base import document
+
+class Documentation:
+    """"""
+# end Documentation
+
+document(Documentation)
