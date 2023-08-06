baecf9997ac3e9a0896364738b7ad7de03e4ad98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:34 +0000

    Add package truss/0.4.9.dev31/truss-0.4.9.dev31-py3-none-any.whl

---
+++ packages/truss/truss-0.4.9.dev31-py3-none-any.whl/truss/templates/control/control/helpers/truss_patch/model_code_patch_applier.py
@@ -0,0 +1,50 @@
+import logging
+from pathlib import Path
+
+# TODO(pankaj) In desparate need of refactoring into separate library
+try:
+    from helpers.types import Action, ModelCodePatch, Patch
+except ModuleNotFoundError:
+    from truss.templates.control.control.helpers.types import (
+        Action,
+        ModelCodePatch,
+        Patch,
+    )
+
+
+def apply_model_code_patch(
+    model_code_dir: Path,
+    patch: ModelCodePatch,
+    logger: logging.Logger,
+):
+    logger.debug(f"Applying model code patch {patch.to_dict()}")
+    filepath: Path = model_code_dir / patch.path
+    apply_file_patch(patch, filepath, logger)
+
+
+def apply_file_patch(
+    patch: Patch,
+    filepath: Path,
+    logger: logging.Logger,
+):
+    action = patch.action
+
+    if action in [Action.ADD, Action.UPDATE]:
+        filepath.parent.mkdir(parents=True, exist_ok=True)
+        logger.info(f"Updating file {filepath}")
+        with filepath.open("w") as file:
+            content = patch.content
+            if content is None:
+                raise ValueError(
+                    "Invalid patch: content of a file update patch should not be None."
+                )
+            file.write(content)
+
+    elif action == Action.REMOVE:
+        if not filepath.exists():
+            logger.warning(f"Could not delete file {filepath}: not found.")
+        else:
+            logger.info(f"Deleting file {filepath}")
+            filepath.unlink()
+    else:
+        raise ValueError(f"Unknown patch action {action}")
