f70b3bad5a5566fee83d1f4ffd425c0a84765192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:19 +0000

    Add package typegraph/0.1.10/typegraph-0.1.10.tar.gz

---
+++ packages/typegraph/typegraph-0.1.10.tar.gz/typegraph-0.1.10/typegraph/runtimes/python.py
@@ -0,0 +1,58 @@
+# Copyright Metatype OÜ, licensed under the Mozilla Public License Version 2.0.
+# SPDX-License-Identifier: MPL-2.0
+
+import ast
+import hashlib
+import inspect
+
+from astunparse import unparse
+from attr import field
+from attrs import frozen
+
+from typegraph import effects
+from typegraph.effects import Effect
+from typegraph.runtimes.base import Materializer, Runtime
+from typegraph.utils.attrs import always
+
+
+class LambdaCollector(ast.NodeTransformer):
+    @classmethod
+    def collect(cls, function):
+        source = inspect.getsource(function).lstrip()
+        tree = ast.parse(source)
+        ret = cls()
+        ret.visit(tree)
+        return ret.lambdas
+
+    def __init__(self):
+        super().__init__()
+        self.lambdas = []
+
+    def visit_Lambda(self, node):
+        self.lambdas.append(unparse(node).strip())
+
+
+@frozen
+class Python(Runtime):
+    """
+    [Documentation](https://metatype.dev/docs/reference/runtimes/python)
+    """
+
+    runtime_name: str = always("python_wasi")
+
+    def from_lambda(self, function):
+        lambdas = LambdaCollector.collect(function)
+        assert len(lambdas) == 1
+        fn = str(lambdas[0])
+        m = hashlib.sha256()
+        m.update(fn.encode("utf-8"))
+        return LambdaMat(self, m.hexdigest(), fn)
+
+
+@frozen
+class LambdaMat(Materializer):
+    runtime: Runtime
+    name: str
+    fn: str
+    materializer_name: str = always("lambda")
+    effect: Effect = field(kw_only=True, default=effects.none())
