a31945970bf0851feaaed7b732725468509c8ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:55 +0000

    Add package test-flask-zqb/0.0.1/test-flask-zqb-0.0.1.tar.gz

---
+++ packages/test-flask-zqb/test-flask-zqb-0.0.1.tar.gz/test-flask-zqb-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="test-flask-zqb",
+    version="0.0.1",
+    author="iwen",
+    author_email="iwen0501@163.com",
+    description="测试",
+    long_description="this is a test case",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    ],
+    pyhthon_requires=">=3.10",
+)
