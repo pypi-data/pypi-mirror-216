9118f96d61bf22b54549e420d3c1d066ca7f7c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:48 +0000

    Add package fipper/0.0.5/fipper-0.0.5.tar.gz

---
+++ packages/fipper/fipper-0.0.5.tar.gz/fipper-0.0.5/fipper/methods/mongo/core.py
@@ -0,0 +1,23 @@
+# Ayiin - Ubot
+# Copyright (C) 2022-2023 @AyiinXd
+#
+# This file is a part of < https://github.com/AyiinXd/AyiinUbot >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/AyiinXd/AyiinUbot/blob/main/LICENSE/>.
+#
+# FROM AyiinUbot <https://github.com/AyiinXd/AyiinUbot>
+# t.me/AyiinChats & t.me/AyiinChannel
+
+
+# ========================×========================
+#            Jangan Hapus Credit Ngentod
+# ========================×========================
+
+import os
+
+from pymongo import MongoClient
+
+MONGO_URI = os.environ.get("MONGO_URI")
+
+mongo = MongoClient(MONGO_URI)
+db = mongo['AYIIN']
