822fd6a901fa583b6248c261fe1d62e9298dc6a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:58 +0000

    Add package pypomes-logging/0.1.2/pypomes_logging-0.1.2-py3-none-any.whl

---
+++ packages/pypomes-logging/pypomes_logging-0.1.2-py3-none-any.whl/pypomes_logging/__init__.py
@@ -0,0 +1,13 @@
+from .logging_pomes import (
+    LOGGING_ID, LOGGING_LEVEL, LOGGIN_FILE, LOGGING_MODE, PYPOMES_LOGGER,
+    log_errors, logging_entries
+)
+
+__all__ = [
+    # logging_pomes
+    LOGGING_ID, LOGGING_LEVEL, LOGGIN_FILE, LOGGING_MODE, PYPOMES_LOGGER,
+    log_errors, logging_entries,
+]
+
+__version__ = "0.1.2"
+__version_info__ = (0, 1, 2)
