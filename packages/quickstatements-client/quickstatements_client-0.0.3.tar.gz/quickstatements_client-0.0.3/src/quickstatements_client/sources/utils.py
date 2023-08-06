4cc7b99d40e43c3dbf35acd4d59e964fae2ed178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:06 +0000

    Add package quickstatements-client/0.0.3/quickstatements_client-0.0.3.tar.gz

---
+++ packages/quickstatements-client/quickstatements_client-0.0.3.tar.gz/quickstatements_client-0.0.3/src/quickstatements_client/sources/utils.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+"""Utilities for Wikidata."""
+
+from typing import Any, List, Mapping
+
+import requests
+
+from ..version import get_version
+
+__all__ = [
+    "WIKIDATA_ENDPOINT",
+    "query_wikidata",
+    "removeprefix",
+]
+
+#: Wikidata SPARQL endpoint. See https://www.wikidata.org/wiki/Wikidata:SPARQL_query_service#Interfacing
+WIKIDATA_ENDPOINT = "https://query.wikidata.org/bigdata/namespace/wdq/sparql"
+
+
+def query_wikidata(sparql: str) -> List[Mapping[str, Any]]:
+    """Query Wikidata's sparql service.
+
+    :param sparql: A SPARQL query string
+    :return: A list of bindings
+    """
+    headers = {
+        "User-Agent": f"quickstatements_client v{get_version()}",
+    }
+    res = requests.get(
+        WIKIDATA_ENDPOINT,
+        params={"query": sparql, "format": "json"},
+        headers=headers,
+        timeout=300,
+    )
+    res.raise_for_status()
+    res_json = res.json()
+    return [
+        {key: _clean_value(value["value"]) for key, value in record.items()}
+        for record in res_json["results"]["bindings"]
+    ]
+
+
+def removeprefix(s: str, prefix: str) -> str:
+    """Remove a prefix."""
+    if s.startswith(prefix):
+        return s[len(prefix) :]
+    return s
+
+
+def _clean_value(value: str) -> str:
+    value = removeprefix(value, "http://www.wikidata.org/entity/")
+    return value
