badb1d5c189bd4a807a2dfa654c81d01ccd6848f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:38 +0000

    Add package streamlit-xterm/0.0.3/streamlit-xterm-0.0.3.tar.gz

---
+++ packages/streamlit-xterm/streamlit-xterm-0.0.3.tar.gz/streamlit-xterm-0.0.3/streamlit_xterm/__init__.py
@@ -0,0 +1,24 @@
+import streamlit.components.v1 as components
+import streamlit as st
+import os
+
+_RELEASE = True
+
+if not _RELEASE:
+    _component_func = components.declare_component(
+        "my_component",
+        url="http://localhost:3001",
+    )
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _component_func = components.declare_component("streamlit-xterm", path=build_dir)
+
+def terminal(text, key=None):
+    component_value = _component_func(text=text, key=key, default=None)
+    return component_value
+
+if not _RELEASE:
+    st.subheader("Terminal Test")
+    terminal(text="test")
+
