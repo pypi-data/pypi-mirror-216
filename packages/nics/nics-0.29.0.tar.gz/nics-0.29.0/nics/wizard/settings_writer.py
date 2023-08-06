726d2fb2f10243e7a8d2d50863237f5bf39561e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:18 +0000

    Add package nics/0.29.0/nics-0.29.0.tar.gz

---
+++ packages/nics/nics-0.29.0.tar.gz/nics-0.29.0/nics/wizard/settings_writer.py
@@ -0,0 +1,22 @@
+
+
+
+
+text = f"""
+#-- Welcome to NICS settings!
+#----------------------------
+
+#-- Everything starts with "#--" is a comment.
+#-- Help: https://nvfp.github.io/now-i-can-sleep
+
+
+author: 'Nicholas Valentinus'
+color_hue: 28
+show_credit: True
+
+
+#-- The variables below should not be changed and are for NICS internal use only.
+
+_gh_username: 'nvfp'
+_gh_repo: 'now-i-can-sleep'
+"""
