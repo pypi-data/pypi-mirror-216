7ccd53b6bb90e9c5bf615640f770660ab9328d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:56 +0000

    Add package psalpsdtools/1.0/psalpsdtools-1.0.tar.gz

---
+++ packages/psalpsdtools/psalpsdtools-1.0.tar.gz/psalpsdtools-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='psalpsdtools',
+      version='1.0',
+      description='PSA-LPSD Tools',
+      packages=['psalpsdtools'],
+      install_requires=[
+          'openpyxl',
+      ],
+      author_email='a.diaziii@psa.gov.ph',
+      long_description=open('README.md', 'r', encoding='utf-8').read(),
+      long_description_content_type='text/markdown',
+      license='MIT',
+      url='https://github.com/tondiaz/psalpsdtools',
+      zip_safe=False)
