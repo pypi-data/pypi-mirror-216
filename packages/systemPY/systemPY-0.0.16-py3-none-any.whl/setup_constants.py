039532c250413b887326b7f3e3e920fa354a6310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:23 +0000

    Add package systemPY/0.0.16/systemPY-0.0.16-py3-none-any.whl

---
+++ packages/systemPY/systemPY-0.0.16-py3-none-any.whl/setup_constants.py
@@ -0,0 +1,13 @@
+import os, tomli
+
+version = "0.0.16"
+name_canonical = "systemPY"
+name = name_canonical.lower()
+
+
+pyproject_config = "pyproject.toml"
+if os.path.isfile(pyproject_config):
+    with open(pyproject_config, "rb") as ppfile:
+        pyproject = tomli.load(ppfile)
+else:
+    pyproject = {}
