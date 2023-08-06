7b2b824bd190f4d41e7e50ac8a6e369e3b0f7c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:53 +0000

    Add package plone.app.mosaic/3.1.3/plone.app.mosaic-3.1.3.tar.gz

---
+++ packages/plone.app.mosaic/plone.app.mosaic-3.1.3.tar.gz/plone.app.mosaic-3.1.3/setup.py
@@ -0,0 +1,71 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+long_description = "\n\n".join(
+    [
+        open("README.rst").read(),
+        open("CHANGES.rst").read(),
+    ]
+)
+
+
+setup(
+    # metadata in setup.cfg
+    name="plone.app.mosaic",
+    version="3.1.3",
+    description="Plone Mosaic Site Builder and Site Layout",
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Addon",
+        "Framework :: Zope :: 5",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    ],
+    keywords="Python Plone javascript mosaic grideditor",
+    author="Plone Foundation",
+    author_email="foundation@plone.org",
+    url="https://github.com/plone/plone.app.mosaic",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    namespace_packages=["plone", "plone.app"],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "setuptools",
+        "Products.CMFPlone",
+        "plone.app.blocks",
+        "plone.app.tiles",
+        "plone.app.drafts",
+        "plone.app.standardtiles",
+        "plone.staticresources >= 2.1.0",
+    ],
+    extras_require={
+        "test": [
+            "plone.app.testing",
+            "plone.app.contenttypes",
+            "plone.app.robotframework",
+            "robotframework-selenium2library",
+        ],
+        "docs": [
+            "Sphinx",
+            "sphinx-rtd-theme",
+            "sphinxcontrib-robotframework",
+        ],
+    },
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
