3aa60201e471a01131d2198ddd45165f3a9b1134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:42 +0000

    Add package griptape/0.12.2/griptape-0.12.2-py3-none-any.whl

---
+++ packages/griptape/griptape-0.12.2-py3-none-any.whl/griptape/memory/tool/base_tool_memory.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Union
+from abc import ABC, abstractmethod
+from attr import define, field, Factory
+from griptape.artifacts import BaseArtifact
+from griptape.core import ActivityMixin
+
+if TYPE_CHECKING:
+    from griptape.tasks import ActionSubtask
+
+
+@define
+class BaseToolMemory(ActivityMixin, ABC):
+    id: str = field(default=Factory(lambda self: self.__class__.__name__, takes_self=True), kw_only=True)
+    namespace_metadata: dict[str, str] = field(factory=dict, kw_only=True)
+
+    def process_output(
+            self,
+            tool_activity: callable,
+            subtask: ActionSubtask,
+            artifact: Union[BaseArtifact, list[BaseArtifact]]
+    ) -> BaseArtifact:
+        return artifact
+
+    @abstractmethod
+    def load_artifacts(self, namespace: str) -> list[BaseArtifact]:
+        ...
+    
