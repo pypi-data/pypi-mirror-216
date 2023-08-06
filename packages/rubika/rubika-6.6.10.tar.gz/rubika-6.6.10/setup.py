b6c96d402fbfe26ed2196bdad16cb6b1d55132a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:33 +0000

    Add package rubika/6.6.10/rubika-6.6.10.tar.gz

---
+++ packages/rubika/rubika-6.6.10.tar.gz/rubika-6.6.10/setup.py
@@ -0,0 +1,59 @@
+import re
+from sys import argv
+# from requests import get
+from setuptools import setup, find_packages
+
+requires = ["requests", "pycryptodome==3.10.1", "urllib3",
+            "tqdm", "aiohttp", "rich", "websocket-client", "schedule"]
+version = "6.6.10"
+# get("https://raw.githubusercontent.com/Bahman-Ahmadi/rubika/main/README.md").text
+readme = "read the docs from https://github.com/Bahman-Ahmadi/rubika"
+
+setup(
+    name="rubika",
+    version=version,
+    description="this is an unofficial library for making bots in rubika. using this library you can make your own rubika bot and control that",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bahman-ahmadi/rubika",
+    download_url="https://github.com/bahman-ahmadi/rubika/releases/latest",
+    author="Bahman Ahmadi",
+    author_email="bahmanahmadi.mail@gmail.com",
+    license="LGPLv3",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Internet",
+        "Topic :: Communications",
+        "Topic :: Communications :: Chat",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries :: Application Frameworks"
+    ],
+    keywords="rubika self bot api library python",
+    project_urls={
+        "Tracker": "https://github.com/bahman-ahmadi/rubika/issues",
+        "Community": "https://t.me/rubikalib",
+        "Source": "https://github.com/bahman-ahmadi/rubika",
+        "Documentation": "https://rubikalib.github.io",
+    },
+    python_requires="~=3.5",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=requires
+)
