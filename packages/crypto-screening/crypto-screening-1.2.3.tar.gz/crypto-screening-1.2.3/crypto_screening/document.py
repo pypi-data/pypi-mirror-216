7804f16324552c77b29d481217adc6a74684e7a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:52 +0000

    Add package crypto-screening/1.2.3/crypto-screening-1.2.3.tar.gz

---
+++ packages/crypto-screening/crypto-screening-1.2.3.tar.gz/crypto-screening-1.2.3/crypto_screening/document.py
@@ -0,0 +1,28 @@
+# document.py
+
+# noinspection PyUnresolvedReferences
+from crypto_screening.interval import *
+# noinspection PyUnresolvedReferences
+from crypto_screening.symbols import *
+# noinspection PyUnresolvedReferences
+from crypto_screening.dataset import *
+# noinspection PyUnresolvedReferences
+from crypto_screening.orderbook import *
+# noinspection PyUnresolvedReferences
+from crypto_screening.ohlcv import *
+# noinspection PyUnresolvedReferences
+from crypto_screening.screener import *
+# noinspection PyUnresolvedReferences
+from crypto_screening.exchanges import *
+# noinspection PyUnresolvedReferences
+from crypto_screening.process import *
+# noinspection PyUnresolvedReferences
+from crypto_screening.collect.document import *
+
+from crypto_screening.base import document
+
+class Documentation:
+    """"""
+# end Documentation
+
+document(Documentation)
