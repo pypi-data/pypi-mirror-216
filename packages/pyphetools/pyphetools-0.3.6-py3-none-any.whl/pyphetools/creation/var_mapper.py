44794d2c5a855ad472e6bb4ec128316299195e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:32 +0000

    Add package pyphetools/0.3.6/pyphetools-0.3.6-py3-none-any.whl

---
+++ packages/pyphetools/pyphetools-0.3.6-py3-none-any.whl/pyphetools/creation/var_mapper.py
@@ -0,0 +1,12 @@
+import abc
+
+
+class VarMapper(metaclass=abc.ABCMeta):
+    """Superclass for classes that create GA4GH VariantInterpretationObjects"""
+    
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def to_ga4gh_variant_interpretation(self, acmg=None):
+        pass
