9877080a5cfcd491cc09f37cb967ae8633d3d575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:49 +0000

    Add package pylsat/0.2.2/pylsat-0.2.2.tar.gz

---
+++ packages/pylsat/pylsat-0.2.2.tar.gz/pylsat-0.2.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pylsat',
+    version='0.2.2',
+    packages=find_packages(),
+    install_requires=[
+        'fastapi', 
+        'starlette', 
+        'pymacaroons'
+    ],
+    url='https://github.com/plebos/pylsat',
+    license='MIT',
+    author='Tal Shmueli',
+    author_email='tal@shmueli.org',
+    description='A Python library for validating L402 (formerly known as LSAT) protocol for Lightning Network payments in a FastAPI application.'
+
+)
