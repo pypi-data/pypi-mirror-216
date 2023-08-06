14a10a0c5a9ab4f1a4f8a30f773220d8a42c8e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:24 +0000

    Add package ILAMB/2.7/ILAMB-2.7.tar.gz

---
+++ packages/ILAMB/ILAMB-2.7.tar.gz/ILAMB-2.7/src/ILAMB/generated_version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM ILAMB SETUP.PY
+short_version = '2.7'
+version = '2.7'
+git_revision = 'c2be0ed'
+full_version = '2.7 (%(git_revision)s)' % {
+    'git_revision': git_revision}
+release = False
+if not release:
+    version = full_version
