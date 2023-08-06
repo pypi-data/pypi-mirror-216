9673c07f8d807658a778ba53549403cb8043e198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:19 +0000

    Add package pymatillion/1.0.15/pymatillion-1.0.15-py3-none-any.whl

---
+++ packages/pymatillion/pymatillion-1.0.15-py3-none-any.whl/pymatillion/constants.py
@@ -0,0 +1,13 @@
+BASE_URL = "base_url"
+USERNAME = "username"
+PASSWORD = "password"
+PROJECT_GROUP_NAME = "project_group_name"
+PROJECT_NAME = "project_name"
+PROJECT_VERSION = "project_version"
+DEFAULT_VERSION = "default"
+
+# API HTTP Request Methods
+API_DELETE = "delete"
+API_GET = "get"
+API_POST = "post"
+API_PUT = "put"
