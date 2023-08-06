8a9782712a9d2b57d6a0521b55fa2a61193112e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:55 +0000

    Add package ckanext-googleanalyticsbasic/0.2.1/ckanext-googleanalyticsbasic-0.2.1.tar.gz

---
+++ packages/ckanext-googleanalyticsbasic/ckanext-googleanalyticsbasic-0.2.1.tar.gz/ckanext-googleanalyticsbasic-0.2.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ckanext-googleanalyticsbasic',
+    version='0.2.1',
+    description="Basic extension to add google analytics tracking code in page header",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Data.gov',
+    author_email='datagovhelp@gsa.gov',
+    url='https://github.com/GSA/ckanext-googleanalyticsbasic',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.googleanalyticsbasic'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    setup_requires=['wheel'],
+
+    entry_points="""
+        [ckan.plugins]
+        googleanalyticsbasic=ckanext.googleanalyticsbasic.plugin:GoogleAnalyticsBasicPlugin""",
+)
