f0b277dca3c52d56c0ccf2e557fc48f99fb85690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:16 +0000

    Add package django-data-schema/2.1.0/django-data-schema-2.1.0.tar.gz

---
+++ packages/django-data-schema/django-data-schema-2.1.0.tar.gz/django-data-schema-2.1.0/data_schema/migrations/0002_auto_20150705_1714.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('data_schema', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='fieldschema',
+            name='display_name',
+            field=models.CharField(default=b'', max_length=64, blank=True),
+        ),
+        migrations.AlterUniqueTogether(
+            name='fieldoption',
+            unique_together=set([('field_schema', 'value')]),
+        ),
+    ]
