fdc7c2d595a04cf2df595c2e3dbbc31f05a2c2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:18 +0000

    Add package certora-cli-alpha-cnandi-morpho-equiv/20230627.10.33.334149/certora-cli-alpha-cnandi-morpho-equiv-20230627.10.33.334149.tar.gz

---
+++ packages/certora-cli-alpha-cnandi-morpho-equiv/certora-cli-alpha-cnandi-morpho-equiv-20230627.10.33.334149.tar.gz/certora-cli-alpha-cnandi-morpho-equiv-20230627.10.33.334149/README.md
@@ -0,0 +1,1 @@
+Commit 58020d6. Build and Run scripts for executing the Certora Prover on Solidity smart contracts.
---
+++ packages/certora-cli-alpha-cnandi-morpho-equiv/certora-cli-alpha-cnandi-morpho-equiv-20230627.10.33.334149.tar.gz/certora-cli-alpha-cnandi-morpho-equiv-20230627.10.33.334149/certora_cli/certoraEqCheck.py
@@ -0,0 +1,9 @@
+from EquivalenceCheck.equivCheck import ext_equivcheck_entry_point
+
+
+def equiv_check_entry_point() -> None:
+    ext_equivcheck_entry_point()
+
+
+if __name__ == '__main__':
+    equiv_check_entry_point()
