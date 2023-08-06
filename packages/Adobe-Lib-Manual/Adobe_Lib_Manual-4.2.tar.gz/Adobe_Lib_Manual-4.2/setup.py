4ac9dd7d20fdeb4d2705338ff836cf05d02f9100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:52 +0000

    Add package Adobe-Lib-Manual/4.2/Adobe_Lib_Manual-4.2.tar.gz

---
+++ packages/Adobe-Lib-Manual/Adobe_Lib_Manual-4.2.tar.gz/Adobe_Lib_Manual-4.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='Adobe_Lib_Manual',
+  version='4.2',
+  description='abc',
+  long_description='This is no one can do',
+  url='',  
+  author='Ajay Pandey',
+  author_email='ajaypanday678@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='adobe', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
