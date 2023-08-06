cba8a08accde42eb1260404a5d9428ce2e622812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:08 +0000

    Add package diself/1.0.2/diself-1.0.2.tar.gz

---
+++ packages/diself/diself-1.0.2.tar.gz/diself-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='diself',
+    version='1.0.2',
+    license='MIT',
+    description='A python module like discord.py but for selfbot',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=['websocket-client', 'requests'],
+    url='https://github.com/lululepu/diself',
+    author='Lululepu',
+    author_email='a.no.qsdf@gmail.com'
+)
