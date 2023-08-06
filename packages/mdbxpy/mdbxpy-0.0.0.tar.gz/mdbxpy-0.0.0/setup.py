178276e03f264146e0fae59a565d97a1cbe8cdce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:56 +0000

    Add package mdbxpy/0.0.0/mdbxpy-0.0.0.tar.gz

---
+++ packages/mdbxpy/mdbxpy-0.0.0.tar.gz/mdbxpy-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mdbxpy",
+    version="0.0.0",
+    author="Simon Black",
+    url="https://github.com/simonblack/py-mdbx",
+    email="mail@simon.black",
+    description="Python bindings for libmdbx",
+    packages=find_packages(),
+)
