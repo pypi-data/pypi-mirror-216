6f11d846e19314648146b4fd4c19057fb81440ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:30 +0000

    Add package lifetimefitting/0.0.27/lifetimefitting-0.0.27.tar.gz

---
+++ packages/lifetimefitting/lifetimefitting-0.0.27.tar.gz/lifetimefitting-0.0.27/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "lifetimefitting",
+    version = "0.0.27",
+    author = "Adrea Snow",
+    author_email = "adrea.snow@gmail.com",
+    description = "PicoHarp TCSPC lifetime fitting tool",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/adreasnow/LifetimeFittingTool",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "app"},
+    packages = find_packages(where="app"),
+    extras_require = {"dev": ["twine>=4.0.2"]},
+    install_requires=['matplotlib', 'numpy', 'scipy', 'pyqt6', 'phdimporter>=0.0.3'],
+    python_requires = ">=3.8",
+)
