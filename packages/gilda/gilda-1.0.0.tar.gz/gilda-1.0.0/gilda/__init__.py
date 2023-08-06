c659f2e225aad31706e371da1524acf12a358d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:42 +0000

    Add package gilda/1.0.0/gilda-1.0.0.tar.gz

---
+++ packages/gilda/gilda-1.0.0.tar.gz/gilda-1.0.0/gilda/__init__.py
@@ -0,0 +1,32 @@
+__version__ = '1.0.0'
+
+import logging
+
+from .api import get_grounder, get_models, get_names, ground, make_grounder, annotate
+from .grounder import Grounder, ScoredMatch
+from .pandas_utils import ground_df, ground_df_map
+from .term import Term
+
+__all__ = [
+    'ground',
+    'annotate',
+    'get_models',
+    'get_names',
+    'get_grounder',
+    'make_grounder',
+    # Classes
+    'Term',
+    'Grounder',
+    'ScoredMatch',
+    # Meta
+    '__version__',
+    # Pandas utilities
+    'ground_df',
+    'ground_df_map',
+]
+
+logging.basicConfig(format=('%(levelname)s: [%(asctime)s] %(name)s'
+                            ' - %(message)s'),
+                    level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+
+logger = logging.getLogger('gilda')
