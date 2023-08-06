2a6f64cef3d1645f6ebe1d64e884b4789f224dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:41 +0000

    Add package dslclib/0.1.2/dslclib-0.1.2.tar.gz

---
+++ packages/dslclib/dslclib-0.1.2.tar.gz/dslclib-0.1.2/dslclib/__init__.py
@@ -0,0 +1,17 @@
+from .src import (
+    BaseClient,
+    BodyController,
+    ControllerType,
+    EmotionType,
+    ExpressionController,
+    ExpressionType,
+    FaceRecognitinClient,
+    GazeDirection,
+    GazeObject,
+    MotionType,
+    SpeechRecognitionClient,
+    STTRecognitionType,
+    Text2SpeechClient,
+)
+
+__version__ = "0.1.2"
