4f4299af79e2998973f70970ebda6b093c286f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:35 +0000

    Add package mentord/1.0.7/mentord-1.0.7.tar.gz

---
+++ packages/mentord/mentord-1.0.7.tar.gz/mentord-1.0.7/mentord/guild.py
@@ -0,0 +1,50 @@
+from .http_ import *
+from .member import *
+
+
+class Guild:
+
+    """
+    Класс представляющий информацию о гильдии.
+    """
+
+    def __init__(self, guild: dict, token: str):
+
+        self.guild: dict = guild
+        self.token: str = token
+
+    @property
+    def id(self) -> str:
+
+        return self.guild['id']
+
+    @property
+    def name(self) -> str:
+
+        return self.guild['name']
+
+    @property
+    def is_owner(self) -> bool:
+
+        return self.guild['owner']
+
+    def get_json_object(self) -> dict:
+
+        return self.guild
+
+    @property
+    def members(self) -> list[Member]:
+
+        guild_members: list[Member] = []
+
+        for guild in HttpClient().request("GET", 'https://discordapp.com/api/v9/guilds/{0}/members'.format(self.id),
+                                          {"Authorization": self.token, "Content-Type": "application/json"}).json():
+
+            guild_members.append(Member(guild))
+
+        return guild_members
+
+    @property
+    def members_count(self) -> int:
+
+        return self.members.count
---
+++ packages/mentord/mentord-1.0.7.tar.gz/mentord-1.0.7/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mentord',
+      version='1.0.7',
+      description='Library for discord self bots.',
+      packages=['mentord'],
+      long_description='README.md',
+      long_description_content_type='text/markdown')
