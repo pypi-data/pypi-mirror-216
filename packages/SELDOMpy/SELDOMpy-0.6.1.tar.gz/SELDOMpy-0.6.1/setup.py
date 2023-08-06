dba13ac4cd6f8c54fa02fff59c8c6942cd8a9d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:24 +0000

    Add package SELDOMpy/0.6.1/SELDOMpy-0.6.1.tar.gz

---
+++ packages/SELDOMpy/SELDOMpy-0.6.1.tar.gz/SELDOMpy-0.6.1/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import sys
+#import numpy as np
+from distutils.core import Extension
+from setuptools import setup
+from Cython.Distutils import build_ext
+import glob
+import os
+
+
+
+# Get numpy include directory (works across versions)
+#numpy_include = np.get_include()
+
+print(sys.argv)
+
+if '--disable-openmp' in sys.argv:
+    sys.argv.pop(sys.argv.index('--disable-openmp'))
+    USE_OPENMP = False
+else:
+    USE_OPENMP = True
+
+if '--icc' in sys.argv:
+    sys.argv.pop(sys.argv.index('--icc'))
+    USE_ICC = True
+else:
+    USE_ICC = False
+
+libs = ['m']
+
+extra = ['-std=gnu99']
+if USE_ICC:
+    if USE_OPENMP:
+        libs += ['gomp', 'iomp5']
+        extra += ['-openmp']
+else:
+    #    extra += ['-O2']
+    if USE_OPENMP:
+        libs += ['gomp']
+        extra += ['-fopenmp']
+
+hello = Extension(name="hello",
+                  sources=["src"],
+                  libraries=libs,
+                  include_dirs=['include/include_amigo', 'include/include_cvodes'],
+                  library_dirs=["src"],
+                  extra_compile_args=extra
+                  )
+
+setup(name="SELDOMpy",
+      description="Code for xQML",
+      author="S. Vanneste & M. Tristram",
+      version="0.6.1",
+      packages=['SELDOMpy'],
+      ext_modules=[hello],
+      install_requires=["numpy", "scikit-learn", "pandas", "mealpy", "matplotlib", "Cython"],
+      cmdclass={'build_ext': build_ext}
+      )
