4c38c4038cf57da77f08fcc4dbfd45b526b7b890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:22 +0000

    Add package lcwc/0.6/lcwc-0.6-py3-none-any.whl

---
+++ packages/lcwc/lcwc-0.6-py3-none-any.whl/lcwc/feed/incident.py
@@ -0,0 +1,29 @@
+# custom class to handle the guid attribute
+# a bit usless on its own but might be useful for statically identifying incidents
+# since apparently every detail is subject to change except maybe the timestamp
+import datetime
+from lcwc.category import IncidentCategory
+from lcwc.incident import Incident
+from lcwc.unit import Unit
+
+
+class FeedIncident(Incident):
+    """Represents an incident from the live incident feed"""
+
+    def __init__(
+        self,
+        category: IncidentCategory,
+        date: datetime,
+        description: str,
+        municipality: str,
+        intersection: str,
+        units: list[Unit] = [],
+        guid: str = None,
+    ) -> None:
+        super().__init__(category, date, description, municipality, intersection, units)
+        self._guid = guid
+
+    @property
+    def guid(self) -> str:
+        """Returns the guid of the incident"""
+        return self._guid
