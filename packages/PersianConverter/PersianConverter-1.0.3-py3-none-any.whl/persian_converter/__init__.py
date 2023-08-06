88b40cdb421a02539a856bc44f8d453bfa5c272f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:28 +0000

    Add package PersianConverter/1.0.3/PersianConverter-1.0.3-py3-none-any.whl

---
+++ packages/PersianConverter/PersianConverter-1.0.3-py3-none-any.whl/persian_converter/__init__.py
@@ -0,0 +1,8 @@
+from arabic_reshaper import arabic_reshaper
+from bidi.algorithm import get_display
+
+def fprint(text='سلام من کمیلیان هستم'):
+    persian_text = text
+    reshaped_text = arabic_reshaper.reshape(persian_text)
+    farsi_text = get_display(reshaped_text)
+    return farsi_text
