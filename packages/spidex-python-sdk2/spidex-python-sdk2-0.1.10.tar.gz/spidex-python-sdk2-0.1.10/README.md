5dd3a069a6f0ea5b880fc1b3a20b250152bc158d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:06 +0000

    Add package spidex-python-sdk2/0.1.10/spidex-python-sdk2-0.1.10.tar.gz

---
+++ packages/spidex-python-sdk2/spidex-python-sdk2-0.1.10.tar.gz/spidex-python-sdk2-0.1.10/README.md
@@ -0,0 +1,34 @@
+# Spidex
+
+Spidex is the Python client for the Spidex protocol. It allows you to trade and fetch data from Spidex using Python.
+
+## Installation
+
+```
+cd ./spidex
+```
+```
+pip install -r requirements.txt
+```
+```
+python setup.py build
+```
+```
+python setup.py install
+```
+### Successfuly Install Spidex, then you can use spidex api
+Note: requires Python >= 3.6.
+
+## Examples
+```
+from spidex.api import SpidexClient
+```
+## Test
+- about sign_request, You can refer to ```./examples/test_pubilc_private/private.py```
+- about public_request, You can refer to ```./examples/test_pubilc_private/public.py```
+- about websocket, You can refer to ```./examples/test_websocket```
+
+
+
+
+
---
+++ packages/spidex-python-sdk2/spidex-python-sdk2-0.1.10.tar.gz/spidex-python-sdk2-0.1.10/setup.py
@@ -0,0 +1,50 @@
+import os
+from setuptools import setup, find_packages
+
+with open(
+    os.path.join(os.path.dirname(__file__), "requirements.txt"), "r"
+) as fh:
+    requirements = fh.readlines()
+
+name = "spidex-python-sdk2"
+description = "This is a lightweight library for Spidex API."
+url = "https://github.com/hellosoldier/spidex-api"
+author_email = "404260423@qq.com"
+author = "spx"
+license="MIT"
+
+version = "0.1.10"
+
+about = {}
+
+# with open("REST_API.md", "r") as fh:
+#     about["long_description"] = fh.read()
+
+root = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name=name,
+    version=version,
+    license="MIT",
+    description=description,
+    long_description_content_type="text/markdown",
+    AUTHOR=author,
+    url=url,
+    keywords=["Spidex API"],
+    include_package_data=True,
+    install_requires=[req for req in requirements],
+    packages=find_packages(exclude=("tests",)),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.6",
+)
+
+
+# python setup.py bdist_wheel
