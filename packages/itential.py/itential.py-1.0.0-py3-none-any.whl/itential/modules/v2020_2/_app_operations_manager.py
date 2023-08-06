687a0bbdf187648f6c6b36cf39b9f2521310e261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:22 +0000

    Add package itential.py/1.0.0/itential.py-1.0.0-py3-none-any.whl

---
+++ packages/itential.py/itential.py-1.0.0-py3-none-any.whl/itential/modules/v2020_2/_app_operations_manager.py
@@ -0,0 +1,26 @@
+"""
+Link to Itential Docs: https://apidocs.itential.com/2020.2/api/app-operations_manager/
+
+Implemented  Doc String  Tests
+    [ ]         [ ]       [ ]   cloneAutomation
+    [ ]         [ ]       [ ]   createAutomation
+    [ ]         [ ]       [ ]   createTrigger
+    [ ]         [ ]       [ ]   deleteAutomation
+    [ ]         [ ]       [ ]   deleteTrigger
+    [ ]         [ ]       [ ]   deleteTriggersByActionId
+    [ ]         [ ]       [ ]   exportAutomation
+    [ ]         [ ]       [ ]   exportTrigger
+    [ ]         [ ]       [ ]   getAutomation
+    [ ]         [ ]       [ ]   getAutomations
+    [ ]         [ ]       [ ]   getEventDefinition
+    [ ]         [ ]       [ ]   getEvents
+    [ ]         [ ]       [ ]   getTrigger
+    [ ]         [ ]       [ ]   getTriggers
+    [ ]         [ ]       [ ]   importAutomations
+    [ ]         [ ]       [ ]   importTriggers
+    [ ]         [ ]       [ ]   runEndpointTriggerWithPost
+    [ ]         [ ]       [ ]   runManualTrigger
+    [ ]         [ ]       [ ]   updateAutomation
+    [ ]         [ ]       [ ]   updateTrigger
+
+"""
