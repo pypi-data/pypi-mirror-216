92059c1688df72fa8a3ab1c0951e828896247aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:25 +0000

    Add package jellyash/0.5.0/jellyash-0.5.0-py3-none-any.whl

---
+++ packages/jellyash/jellyash-0.5.0-py3-none-any.whl/jellyash/search.py
@@ -0,0 +1,17 @@
+from .bundle import Item
+
+
+def search_single_show(client, term: str) -> Item:
+    r = client.jellyfin.search_media_items(term=term, media="Series")
+    if len(r) == 0:
+        raise ValueError(f"{term} not found")
+    elif len(r) > 1:
+        exact = " " not in term
+        for idx, series in enumerate(r):
+            if exact and term == series.Name:
+                break
+            elif not exact and term in series.Name:
+                break
+    else:
+        idx = 0
+    return r[idx]
