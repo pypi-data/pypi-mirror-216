b9fff8baac59dc8693d57313d4a4e6a72639bb07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:35 +0000

    Add package wxee/0.4.1/wxee-0.4.1.tar.gz

---
+++ packages/wxee/wxee-0.4.1.tar.gz/wxee-0.4.1/wxee/__init__.py
@@ -0,0 +1,7 @@
+import wxee.xarray
+from wxee.collection import ImageCollection
+from wxee.image import Image
+from wxee.time_series import TimeSeries
+from wxee.utils import Initialize
+
+__version__ = "0.4.1"
