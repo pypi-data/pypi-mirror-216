f295a0dadca0c9a21494fe619ff217ed10805ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:53 +0000

    Add package flux-local/1.3.2/flux-local-1.3.2.tar.gz

---
+++ packages/flux-local/flux-local-1.3.2.tar.gz/flux-local-1.3.2/flux_local/helm.py
@@ -0,0 +1,198 @@
+"""Library for running `helm template` to produce local items in the cluster.
+
+You can instantiate a helm template with the following:
+- A HelmRepository which is a url that contains charts
+- A HelmRelease which is an instance of a HelmChart in a HelmRepository
+
+This is an example that prepares the helm repository:
+```python
+from flux_local.kustomize import Kustomize
+from flux_local.helm import Helm
+from flux_local.manifest import HelmRepository
+
+kustomize = Kustomize.build(TESTDATA_DIR)
+repos = await kustomize.grep("kind=^HelmRepository$").objects()
+helm = Helm("/tmp/path/helm", "/tmp/path/cache")
+for repo in repos:
+  helm.add_repo(HelmRepository.parse_doc(repo))
+await helm.update()
+```
+
+Then to actually instantiate a template from a HelmRelease:
+```python
+from flux_local.manifest import HelmRelease
+
+releases = await kustomize.grep("kind=^HelmRelease$").objects()
+if not len(releases) == 1:
+    raise ValueError("Expected only one HelmRelease")
+tmpl = helm.template(
+    HelmRelease.parse_doc(releases[0]),
+    releases[0]["spec"].get("values"))
+objects = await tmpl.objects()
+for object in objects:
+    print(f"Found object {object['apiVersion']} {object['kind']}")
+```
+"""
+
+import datetime
+import logging
+from pathlib import Path
+from typing import Any
+
+import aiofiles
+import yaml
+
+from . import command
+from .kustomize import Kustomize
+from .manifest import HelmRelease, HelmRepository, CRD_KIND, SECRET_KIND, REPO_TYPE_OCI
+from .exceptions import HelmException
+
+__all__ = [
+    "Helm",
+]
+
+_LOGGER = logging.getLogger(__name__)
+
+
+HELM_BIN = "helm"
+
+
+def _chart_name(repo: HelmRepository, release: HelmRelease) -> str:
+    """Return the helm chart name used for the helm template command."""
+    if repo.repo_type == REPO_TYPE_OCI:
+        return f"{repo.url}/{release.chart.name}"
+    return release.chart.chart_name
+
+
+class RepositoryConfig:
+    """Generates a helm repository configuration from flux HelmRepository objects."""
+
+    def __init__(self, repos: list[HelmRepository]) -> None:
+        """Initialize RepositoryConfig."""
+        self._repos = repos
+
+    @property
+    def config(self) -> dict[str, Any]:
+        """Return a synthetic repository config object."""
+        now = datetime.datetime.now(datetime.timezone.utc).replace(microsecond=0)
+        return {
+            "apiVersion": "",
+            "generated": now.isoformat(),
+            "repositories": [
+                {
+                    "name": f"{repo.namespace}-{repo.name}",
+                    "url": repo.url,
+                }
+                for repo in self._repos
+            ],
+        }
+
+
+class Helm:
+    """Manages local HelmRepository state."""
+
+    def __init__(self, tmp_dir: Path, cache_dir: Path) -> None:
+        """Initialize Helm."""
+        self._tmp_dir = tmp_dir
+        self._repo_config_file = self._tmp_dir / "repository-config.yaml"
+        self._flags = [
+            "--registry-config",
+            "/dev/null",
+            "--repository-cache",
+            str(cache_dir),
+            "--repository-config",
+            str(self._repo_config_file),
+        ]
+        self._repos: list[HelmRepository] = []
+
+    def add_repo(self, repo: HelmRepository) -> None:
+        """Add the specified HelmRepository to the local config."""
+        self._repos.append(repo)
+
+    def add_repos(self, repos: list[HelmRepository]) -> None:
+        """Add the specified HelmRepository to the local config."""
+        for repo in repos:
+            self._repos.append(repo)
+
+    async def update(self) -> None:
+        """Return command line arguments to update the local repo.
+
+        Typically the repository must be updated before doing any chart templating.
+        """
+        _LOGGER.debug("Updating %d repositories", len(self._repos))
+        repos = [repo for repo in self._repos if repo.repo_type != REPO_TYPE_OCI]
+        if not repos:
+            return
+        content = yaml.dump(RepositoryConfig(repos).config, sort_keys=False)
+        async with aiofiles.open(str(self._repo_config_file), mode="w") as config_file:
+            await config_file.write(content)
+        await command.run(
+            command.Command(
+                [HELM_BIN, "repo", "update"] + self._flags, exc=HelmException
+            )
+        )
+
+    async def template(
+        self,
+        release: HelmRelease,
+        values: dict[str, Any] | None = None,
+        skip_crds: bool = True,
+        skip_tests: bool = True,
+        skip_secrets: bool = False,
+        kube_version: str | None = None,
+        api_versions: str | None = None,
+    ) -> Kustomize:
+        """Return command line arguments to template the specified chart.
+
+        The default values will come from the `HelmRelease`, though you can
+        also specify values directory if not present in cluster manifest
+        e.g. it came from a truncated yaml.
+        """
+        repo = next(
+            iter([repo for repo in self._repos if repo.repo_name == release.repo_name]),
+            None,
+        )
+        if not repo:
+            raise HelmException(
+                f"Unable to find HelmRepository for {release.chart.chart_name} for "
+                f"HelmRelease {release.name}"
+            )
+        args: list[str] = [
+            HELM_BIN,
+            "template",
+            release.name,
+            _chart_name(repo, release),
+            "--namespace",
+            release.namespace,
+        ]
+        if skip_crds:
+            args.append("--skip-crds")
+        if skip_tests:
+            args.append("--skip-tests")
+        if release.chart.version:
+            args.extend(
+                [
+                    "--version",
+                    release.chart.version,
+                ]
+            )
+        if kube_version:
+            args.extend(["--kube-version", kube_version])
+        if api_versions:
+            args.extend(["--api-versions", api_versions])
+        if release.values and not values:
+            values = release.values
+        if values:
+            values_path = self._tmp_dir / f"{release.release_name}-values.yaml"
+            async with aiofiles.open(values_path, mode="w") as values_file:
+                await values_file.write(yaml.dump(values, sort_keys=False))
+            args.extend(["--values", str(values_path)])
+        cmd = Kustomize([command.Command(args + self._flags, exc=HelmException)])
+        skips = []
+        if skip_crds:
+            skips.append(CRD_KIND)
+        if skip_secrets:
+            skips.append(SECRET_KIND)
+        if skips:
+            cmd = cmd.skip_resources(skips)
+        return cmd
---
+++ packages/flux-local/flux-local-1.3.2.tar.gz/flux-local-1.3.2/flux_local/tool/flux_local.py
@@ -0,0 +1,101 @@
+"""Command line tool for building, diffing, validating flux local repositories."""
+
+import argparse
+import asyncio
+import logging
+import pathlib
+import sys
+import traceback
+from typing import Any
+
+import yaml
+
+from . import build, diff, get, test
+from flux_local.exceptions import FluxException
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def main() -> None:
+    """Flux-local command line tool main entry point."""
+
+    def str_presenter(dumper: yaml.Dumper, data: Any) -> Any:
+        """Represent multi-line yaml strings as you'd expect.
+
+        See https://github.com/yaml/pyyaml/issues/240
+        """
+        return dumper.represent_scalar(
+            "tag:yaml.org,2002:str", data, style="|" if data.count("\n") > 0 else None
+        )
+
+    yaml.add_representer(str, str_presenter)
+
+    # https://github.com/yaml/pyyaml/issues/89
+    yaml.Loader.yaml_implicit_resolvers.pop("=")
+
+    parser = argparse.ArgumentParser(
+        description="Command line utility for inspecting a local flux repository.",
+    )
+    parser.add_argument(
+        "--log-level", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+    )
+
+    subparsers = parser.add_subparsers(dest="command", help="Command", required=True)
+
+    build_args = subparsers.add_parser(
+        "build",
+        help="Build local flux Kustomization target from a local directory",
+    )
+    build_args.add_argument(
+        "path", type=pathlib.Path, help="Path to the kustomization or charts"
+    )
+    build_args.add_argument(
+        "--enable-helm",
+        type=bool,
+        action=argparse.BooleanOptionalAction,
+        help="Enable use of HelmRelease inflation",
+    )
+    # pylint: disable=duplicate-code
+    build_args.add_argument(
+        "--skip-crds",
+        type=bool,
+        default=False,
+        action=argparse.BooleanOptionalAction,
+        help="Allows disabling of outputting CRDs to reduce output size",
+    )
+    build_args.add_argument(
+        "--skip-secrets",
+        type=bool,
+        default=True,
+        action=argparse.BooleanOptionalAction,
+        help="Omit secrets from the output to reduce random output.",
+    )
+    build_args.add_argument(
+        "--kustomize-build-flags",
+        type=str,
+        default="",
+        help="If present, additional flags to pass to `kustomize build`",
+    )
+    build_args.set_defaults(cls=build.BuildAction)
+
+    get.GetAction.register(subparsers)
+    diff.DiffAction.register(subparsers)
+    test.TestAction.register(subparsers)
+
+    args = parser.parse_args()
+
+    if args.log_level:
+        logging.basicConfig(level=args.log_level)
+
+    action = args.cls()
+    try:
+        asyncio.run(action.run(**vars(args)))
+    except FluxException as err:
+        if args.log_level == "DEBUG":
+            traceback.print_exc(file=sys.stderr)
+        print("flux-local error: ", err, file=sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
