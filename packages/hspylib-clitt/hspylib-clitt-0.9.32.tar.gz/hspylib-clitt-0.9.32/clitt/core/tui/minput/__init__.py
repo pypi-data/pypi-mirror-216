332ccf8e9df7bafdf8c4cd1a8357527a5af31f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:04 +0000

    Add package hspylib-clitt/0.9.32/hspylib-clitt-0.9.32.tar.gz

---
+++ packages/hspylib-clitt/hspylib-clitt-0.9.32.tar.gz/hspylib-clitt-0.9.32/clitt/core/tui/minput/__init__.py
@@ -0,0 +1,18 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-clitt v0.9.32
+#
+# Package: main.clitt.core.tui.minput
+"""Package initialization."""
+
+__all__ = [
+  'access_type', 
+  'form_builder', 
+  'form_field', 
+  'input_type', 
+  'input_validator', 
+  'menu_input', 
+  'minput', 
+  'minput_utils'
+]
+__version__ = '0.9.32'
