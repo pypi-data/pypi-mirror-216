a18b30ead2530432b641aa790c009bdb08aa3d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:41 +0000

    Add package py-dyt/0.1.0/py_dyt-0.1.0.tar.gz

---
+++ packages/py-dyt/py_dyt-0.1.0.tar.gz/py_dyt-0.1.0/README.md
@@ -0,0 +1,13 @@
+# Py-Dyt
+## Python parser for _DyT_
+### version 0.1.0
+
+> This project needs Python >= 3.10!
+
+Simple example:
+
+```pycon
+>>> from py_dyt import translate
+>>> print(translate('Hello, $name$!', {'name': 'Nick'}))
+Hello, Nick!
+```
---
+++ packages/py-dyt/py_dyt-0.1.0.tar.gz/py_dyt-0.1.0/tests/test_variables.py
@@ -0,0 +1,22 @@
+from src.py_dyt import translate
+
+
+def test_simple():
+    expected = 'Hello, Nick!'
+    actual = translate('Hello, $name$!', {'name': 'Nick'})
+
+    assert actual == expected
+
+
+def test_number():
+    expected = "Hello, Nick! It's 5 o'clock"
+    actual = translate("Hello, $name$! It's $time$ o'clock", {'name': 'Nick', 'time': 5})
+
+    assert actual == expected
+
+
+def test_canceled():
+    expected = 'I have $5'
+    actual = translate('I have \\$$money$', {'money': 5})
+
+    assert actual == expected
