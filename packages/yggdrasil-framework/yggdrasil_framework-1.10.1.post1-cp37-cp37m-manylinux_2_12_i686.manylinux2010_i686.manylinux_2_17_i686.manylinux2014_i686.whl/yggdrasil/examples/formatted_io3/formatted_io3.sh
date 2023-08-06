cd74fd085e98db6ef765a3fdd7cadab5f55bf246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:30 +0000

    Add package yggdrasil-framework/1.10.1.post1/yggdrasil_framework-1.10.1.post1-cp37-cp37m-manylinux_2_12_i686.manylinux2010_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/yggdrasil-framework/yggdrasil_framework-1.10.1.post1-cp37-cp37m-manylinux_2_12_i686.manylinux2010_i686.manylinux_2_17_i686.manylinux2014_i686.whl/yggdrasil/examples/formatted_io3/formatted_io3.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+export YGG_DEBUG="INFO"
+export YGG_NAMESPACE="formatted_io3"
+
+yaml= 
+
+# ----------------Your Commands------------------- #
+case $1 in
+    "" | -p | --python )
+	echo "Running Python"
+	yaml='formatted_io3_python.yml'
+	;;
+    -m | --matlab )
+	echo "Running Matlab"
+	yaml='formatted_io3_matlab.yml'
+	;;
+    -c | --gcc )
+	echo "Running C"
+	yaml='formatted_io3_c.yml'
+	;;
+    --cpp | --g++)
+	echo "Running C++"
+	yaml='formatted_io3_cpp.yml'
+	;;
+    -r | -R)
+	echo "Running R"
+	yaml='formatted_io3_r.yml'
+	;;
+    -f | --fortran )
+	echo "Running Fortran"
+	yaml='formatted_io3_fortran.yml'
+	;;
+    -j | --julia)
+	echo "Running Julia"
+	yaml='formatted_io3_julia.yml'
+	;;
+esac
+
+yggrun $yaml
