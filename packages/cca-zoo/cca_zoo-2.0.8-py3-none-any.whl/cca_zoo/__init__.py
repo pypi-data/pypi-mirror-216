a6c103713267ca1d7cb9ea2b6cf3f149614396f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:11 +0000

    Add package cca-zoo/2.0.8/cca_zoo-2.0.8-py3-none-any.whl

---
+++ packages/cca-zoo/cca_zoo-2.0.8-py3-none-any.whl/cca_zoo/__init__.py
@@ -0,0 +1,20 @@
+__all__ = [
+    "data",
+    "model_selection",
+    "linear",
+    "visualisation",
+]
+
+# if can import deep add to all
+try:
+    import cca_zoo.deep
+
+    __all__.append("deep")
+except ModuleNotFoundError:
+    pass
+try:
+    import cca_zoo.probabilistic
+
+    __all__.append("probabilistic")
+except ModuleNotFoundError:
+    pass
---
+++ packages/cca-zoo/cca_zoo-2.0.8-py3-none-any.whl/cca_zoo/visualisation/__init__.py
@@ -0,0 +1,9 @@
+from .plotting import (
+    plot_pairwise_correlations,
+    plot_pairwise_scatter,
+)
+
+__all__ = [
+    "plot_pairwise_correlations",
+    "plot_pairwise_scatter",
+]
