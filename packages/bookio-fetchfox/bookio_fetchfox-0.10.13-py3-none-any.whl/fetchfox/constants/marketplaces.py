d0857c59391108b0e295ac0e4fe24985cf2a73c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:47 +0000

    Add package bookio-fetchfox/0.10.13/bookio_fetchfox-0.10.13-py3-none-any.whl

---
+++ packages/bookio-fetchfox/bookio_fetchfox-0.10.13-py3-none-any.whl/fetchfox/constants/marketplaces.py
@@ -0,0 +1,21 @@
+UNKNOWN = "unknown"
+
+# algorand
+ALGOXNFT_COM = "algoxnft.com"
+RANDGALLERY_COM = "randgallery.com"
+SHUFL_APP = "shufl.app"
+
+# cardano
+JPG_STORE = "jpg.store"
+
+# evm
+OPENSEA_IO = "opensea.io"
+
+
+MARKETPLACES = [
+    ALGOXNFT_COM,
+    RANDGALLERY_COM,
+    SHUFL_APP,
+    JPG_STORE,
+    OPENSEA_IO,
+]
