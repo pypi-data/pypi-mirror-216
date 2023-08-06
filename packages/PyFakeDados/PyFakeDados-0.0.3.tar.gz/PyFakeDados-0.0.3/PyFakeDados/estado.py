0ec88d00cb4e850a4215b562404484ee6ec48cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:34 +0000

    Add package PyFakeDados/0.0.3/PyFakeDados-0.0.3.tar.gz

---
+++ packages/PyFakeDados/PyFakeDados-0.0.3.tar.gz/PyFakeDados-0.0.3/PyFakeDados/estado.py
@@ -0,0 +1,84 @@
+import random
+
+LISTA_UF = [
+    'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 
+    'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 
+    'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'
+]
+
+LISTA_ESTADO = [
+    'Acre',
+    'Alagoas',
+    'Amapá',
+    'Amazonas',
+    'Bahia',
+    'Ceará',
+    'Distrito Federal',
+    'Espírito Santo',
+    'Goiás',
+    'Maranhão',
+    'Mato Grosso',
+    'Mato Grosso do Sul',
+    'Minas Gerais',
+    'Pará',
+    'Paraíba',
+    'Paraná',
+    'Pernambuco',
+    'Piauí',
+    'Rio de Janeiro',
+    'Rio Grande do Norte',
+    'Rio Grande do Sul',
+    'Rondônia',
+    'Roraima',
+    'Santa Catarina',
+    'São Paulo',
+    'Sergipe',
+    'Tocantins'
+]
+
+DICT_ESTADO = {
+    'AC': 'Acre',
+    'AL': 'Alagoas',
+    'AP': 'Amapá',
+    'AM': 'Amazonas',
+    'BA': 'Bahia',
+    'CE': 'Ceará',
+    'DF': 'Distrito Federal',
+    'ES': 'Espírito Santo',
+    'GO': 'Goiás',
+    'MA': 'Maranhão',
+    'MT': 'Mato Grosso',
+    'MS': 'Mato Grosso do Sul',
+    'MG': 'Minas Gerais',
+    'PA': 'Pará',
+    'PB': 'Paraíba',
+    'PR': 'Paraná',
+    'PE': 'Pernambuco',
+    'PI': 'Piauí',
+    'RJ': 'Rio de Janeiro',
+    'RN': 'Rio Grande do Norte',
+    'RS': 'Rio Grande do Sul',
+    'RO': 'Rondônia',
+    'RR': 'Roraima',
+    'SC': 'Santa Catarina',
+    'SP': 'São Paulo',
+    'SE': 'Sergipe',
+    'TO': 'Tocantins'
+}
+
+def gerar_uf():
+    return random.choice(LISTA_UF)
+
+def validar_uf(uf):
+    return uf in LISTA_UF
+
+def gerar_estado():
+    return random.choice(LISTA_ESTADO)
+
+def validar_estado(uf):
+    return uf in LISTA_ESTADO
+
+def busca_nome_uf(uf):
+    if uf.upper() not in LISTA_UF:
+        raise ValueError("UF inválida.")
+    return DICT_ESTADO[uf.upper()]
