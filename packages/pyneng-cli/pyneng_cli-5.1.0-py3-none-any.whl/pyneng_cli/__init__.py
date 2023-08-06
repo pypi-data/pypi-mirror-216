81899636f051924aa5c4c465ccafa6fd14afd4cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:28 +0000

    Add package pyneng-cli/5.1.0/pyneng_cli-5.1.0-py3-none-any.whl

---
+++ packages/pyneng-cli/pyneng_cli-5.1.0-py3-none-any.whl/pyneng_cli/__init__.py
@@ -0,0 +1,65 @@
+__version__ = "5.1.0"
+
+ANSWERS_URL = "https://github.com/natenka/pyneng-tasks-solutions"
+# needed for tasks/tests updates
+TASKS_URL = "https://github.com/natenka/pynenguk-tasks"
+TASKS_LOCAL_REPO = ".pynenguk-tasks"
+LANG_TASKS_URL = {
+    "uk": "https://github.com/natenka/pynenguk-tasks",
+    "en": "https://github.com/natenka/pyneng-examples-exercises-en",
+    "ru": "https://github.com/pyneng/pyneng-course-tasks",
+}
+LANG_TASKS_LOCAL_REPO = {
+    "uk": ".pynenguk-tasks",
+    "en": ".pyneng-examples-exercises-en",
+    "ru": ".pyneng-course-tasks",
+}
+DEFAULT_BRANCH = "main"
+TASK_DIRS = [
+    "04_data_structures",
+    "05_basic_scripts",
+    "06_control_structures",
+    "07_files",
+    "09_functions",
+    "11_modules",
+    "12_useful_modules",
+    "15_module_re",
+    "17_serialization",
+    "18_ssh_telnet",
+    "19_concurrent_connections",
+    "20_jinja2",
+    "21_textfsm",
+    "22_oop_basics",
+    "23_oop_special_methods",
+    "24_oop_inheritance",
+    "25_db",
+]
+TASK_NUMBER_DIR_MAP = {
+    4: "04_data_structures",
+    5: "05_basic_scripts",
+    6: "06_control_structures",
+    7: "07_files",
+    9: "09_functions",
+    11: "11_modules",
+    12: "12_useful_modules",
+    15: "15_module_re",
+    17: "17_serialization",
+    18: "18_ssh_telnet",
+    19: "19_concurrent_connections",
+    20: "20_jinja2",
+    21: "21_textfsm",
+    22: "22_oop_basics",
+    23: "23_oop_special_methods",
+    24: "24_oop_inheritance",
+    25: "25_db",
+}
+
+DB_TASK_DIRS = [
+    "task_25_1",
+    "task_25_2",
+    "task_25_3",
+    "task_25_4",
+    "task_25_5",
+    "task_25_5a",
+    "task_25_6",
+]
