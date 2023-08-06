2e6a90c4f1081bf18660bb2a90584e0dc1963860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:24 +0000

    Add package cluedin/2.2.0/cluedin-2.2.0-py3-none-any.whl

---
+++ packages/cluedin/cluedin-2.2.0-py3-none-any.whl/cluedin/__init__.py
@@ -0,0 +1,12 @@
+from os import environ
+
+from .context import Context
+
+from . import account
+from . import context
+from . import entity
+from . import env
+from . import gql
+from . import jwt
+from . import public
+from . import vocab
---
+++ packages/cluedin/cluedin-2.2.0-py3-none-any.whl/cluedin/gql.py
@@ -0,0 +1,120 @@
+import requests
+
+from .context import Context
+from .env import CLUEDIN_REQUEST_TIMEOUT_IN_SECONDS
+
+
+def gql(context: Context, query: str, variables: dict = None) -> dict:
+    """Send a GraphQL query to CluedIn.
+
+    Args:
+        context (Context): Context object.
+        query (str): GraphQL query.
+        variables (dict, optional): A dicrionary of variables to be used in the query.
+            Defaults to {}.
+
+    Returns:
+        dict: JSON response.
+    """
+    headers = {
+        'Authorization': f'Bearer {context.access_token}'
+    }
+
+    if variables is None:
+        variables = {}
+
+    json = {
+        'query': query,
+        'variables': variables
+    }
+
+    response = requests.post(
+        url=context.gql_api_url,
+        json=json,
+        headers=headers,
+        timeout=CLUEDIN_REQUEST_TIMEOUT_IN_SECONDS,
+        verify=context.verify_tls
+    )
+
+    if not response.ok:
+        response.raise_for_status()
+
+    return response.json()
+
+
+def org_gql(context: Context, query: str, variables: dict = None) -> dict:
+    """Send a GraphQL query to CluedIn Organization endpoint.
+
+    Args:
+        context (Context): Context object.
+        query (str): GraphQL query.
+        variables (dict, optional): A dicrionary of variables to be used in the query.
+            Defaults to {}.
+
+    Returns:
+        dict: JSON response.
+    """
+    headers = {
+        'Authorization': f'Bearer {context.access_token}'
+    }
+
+    if variables is None:
+        variables = {}
+
+    json = {
+        'query': query,
+        'variables': variables
+    }
+
+    response = requests.post(
+        url=context.gql_org_url,
+        json=json,
+        headers=headers,
+        timeout=CLUEDIN_REQUEST_TIMEOUT_IN_SECONDS,
+        verify=context.verify_tls
+    )
+
+    if not response.ok:
+        response.raise_for_status()
+
+    return response.json()
+
+
+def entries(context: Context, query: str, variables: dict = None) -> list:
+    """Get entries from a GraphQL query. This function is a generator.
+        It uses the cursor to get the next page of entries.
+
+    Args:
+        context (Context): Context object.
+        query (str): GraphQL query.
+        variables (dict, optional): A dicrionary of variables to be used in the query.
+
+    Returns:
+        list: List of entries.
+
+    Yields:
+        Iterator[list]: Iterator of entries.
+    """
+    if variables is None:
+        variables = {}
+
+    response = gql(context, query, variables)
+
+    while ('data' in response and
+           'search' in response['data'] and
+           'cursor' in response['data']['search'] and
+            'entries' in response['data']['search']):
+
+        cursor = response['data']['search']['cursor']
+
+        page_entries = response['data']['search']['entries']
+
+        if len(page_entries) == 0:
+            return
+
+        for entry in page_entries:
+            yield entry
+
+        variables['cursor'] = cursor
+
+        response = gql(context, query, variables)
