db14850dbb563787a6bae64abaf7fa308b7d456d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:02 +0000

    Add package openai-image/1.0.4/openai_image-1.0.4-py3-none-any.whl

---
+++ packages/openai-image/openai_image-1.0.4-py3-none-any.whl/openai_image/settings.py
@@ -0,0 +1,6 @@
+# https://platform.openai.com/account/api-keys
+API_KEY = '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'
+
+# PIC_SIZE = '256x256'
+PIC_SIZE = '512x512'
+# PIC_SIZE = '1024x1024'
