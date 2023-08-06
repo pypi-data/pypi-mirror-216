dc174e8bc2c805126278dcaedaa37031461b905c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:55 +0000

    Add package angola/0.14.2/angola-0.14.2.tar.gz

---
+++ packages/angola/angola-0.14.2.tar.gz/angola-0.14.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+
+def long_description():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+VERSION = "0.14.2"
+setuptools.setup(
+    name='angola',
+    version=VERSION,
+    author='Mardix',
+    author_email='mardix@blackdevhub.io',
+    description='angola ',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mardix/angola',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Database',
+    ],
+    python_requires='>=3.8.0',
+    install_requires = [
+        "Jinja2 >= 3.0",
+        "python-slugify",
+        "arrow",
+        "uuid7",
+        "python-arango"
+    ],
+    packages=['angola'],
+    package_dir={'':'src'}
+)
