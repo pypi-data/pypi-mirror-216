bee016928abdfe293824a8a6dcce2caccaadca0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:49 +0000

    Add package spyx/0.0.17/spyx-0.0.17-py3-none-any.whl

---
+++ packages/spyx/spyx-0.0.17-py3-none-any.whl/spyx/loss.py
@@ -0,0 +1,70 @@
+import jax
+import jax.numpy as jnp
+import optax
+
+# need to make these consistent...
+
+@jax.jit
+def l2_spike_reg(avg_spike_counts, target_count):
+    """Spike rate regularization based on mean squared error from target rate."""
+
+    flat = jnp.concatenate(jax.tree_util.tree_flatten(avg_spike_counts)[0])
+    return jnp.sum(optax.l2_loss(flat, jnp.array([target_count]*flat.shape[0])))
+
+@jax.jit
+def clipped_sq_err_spike_reg(avg_spike_counts, target_count, radius):
+    """
+    Spike rate regularization based on clipped squared error from target spike count.
+    
+    Attributes:
+        avg_spike_counts: array of average spikes per neuron over a batch.
+        target_count: the target number of spikes to be emitted by a neuron
+        radius: the tolerance +/- r from the target count where loss is clipped to zero
+    
+    """
+
+    flat = jnp.concatenate(jax.tree_util.tree_flatten(avg_spike_counts)[0])
+    return jnp.sum(jnp.maximum(0,optax.squared_error(flat, jnp.array([target_count]*flat.shape[0]))/radius - radius))
+
+@jax.jit
+def inverse_spike_count_reg(avg_spike_counts, time_len):
+    """
+    Spike rate regularization based on an inverse function that strongly
+    discourages neurons from silencing and gradually penalizes higher spike rates.
+    
+    """
+
+    flat = jnp.concatenate(jax.tree_util.tree_flatten(avg_spike_counts)[0])
+    k = jnp.sqrt(time_len)
+    loss = jnp.mean((flat/k) + (k/(flat+1)) )
+    return loss
+
+@jax.jit
+def integral_accuracy(traces, targets):
+    """
+    Calculate the accuracy of a network's predictions based on the voltage traces.
+    Used in combination with a Leaky-Integrate neuron model as the final layer.
+
+    """
+
+    preds = jnp.argmax(jnp.sum(traces, axis=-2), axis=-1)
+    return jnp.sum(preds == targets) / traces.shape[0], preds
+
+# should expose the smoothing rate and allow for users to partial it away or possibly schedule it...
+@jax.jit
+def integral_crossentropy(traces, targets, smoothing=0.3):
+    """
+    Calculate the crossentropy between the integral of membrane potentials.
+    Allows for label smoothing to discourage silencing 
+    the other neurons in the readout layer.
+
+    Attributes:
+        traces: the output of the final layer of the SNN
+        targets: the integer labels for each class
+        smoothing: [optional] rate at which to smooth labels.
+    """
+
+    logits = jnp.sum(traces, axis=-2) # time axis.
+    labels = optax.smooth_labels(jax.nn.one_hot(targets, logits.shape[-1]), smoothing)
+    return optax.softmax_cross_entropy(logits, labels).mean() #change to mean
+
