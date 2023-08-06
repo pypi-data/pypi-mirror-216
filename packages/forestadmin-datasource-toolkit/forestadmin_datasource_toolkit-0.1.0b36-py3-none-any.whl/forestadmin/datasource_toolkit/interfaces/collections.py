542f78da0f9083876aa54f8b9d2d27ef162e8e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:04 +0000

    Add package forestadmin-datasource-toolkit/0.1.0b36/forestadmin_datasource_toolkit-0.1.0b36-py3-none-any.whl

---
+++ packages/forestadmin-datasource-toolkit/forestadmin_datasource_toolkit-0.1.0b36-py3-none-any.whl/forestadmin/datasource_toolkit/interfaces/collections.py
@@ -0,0 +1,55 @@
+import abc
+from typing import List, Optional
+
+from forestadmin.agent_toolkit.utils.context import User
+from forestadmin.datasource_toolkit.interfaces.actions import ActionField, ActionResult
+from forestadmin.datasource_toolkit.interfaces.models.collections import Collection as CollectionModel
+from forestadmin.datasource_toolkit.interfaces.query.aggregation import AggregateResult, Aggregation
+from forestadmin.datasource_toolkit.interfaces.query.filter.paginated import PaginatedFilter
+from forestadmin.datasource_toolkit.interfaces.query.filter.unpaginated import Filter
+from forestadmin.datasource_toolkit.interfaces.query.projections import Projection
+from forestadmin.datasource_toolkit.interfaces.records import RecordsDataAlias
+
+
+class Collection(CollectionModel, abc.ABC):
+    @abc.abstractmethod
+    async def execute(
+        self,
+        caller: User,
+        name: str,
+        data: RecordsDataAlias,
+        filter: Optional[Filter],
+    ) -> ActionResult:
+        pass
+
+    @abc.abstractmethod
+    async def get_form(
+        self,
+        caller: User,
+        name: str,
+        data: Optional[RecordsDataAlias],
+        filter: Optional[Filter],
+    ) -> List[ActionField]:
+        pass
+
+    @abc.abstractmethod
+    async def create(self, caller: User, data: List[RecordsDataAlias]) -> List[RecordsDataAlias]:
+        pass
+
+    @abc.abstractmethod
+    async def list(self, caller: User, filter: PaginatedFilter, projection: Projection) -> List[RecordsDataAlias]:
+        pass
+
+    @abc.abstractmethod
+    async def update(self, caller: User, filter: Optional[Filter], patch: RecordsDataAlias) -> None:
+        pass
+
+    @abc.abstractmethod
+    async def delete(self, caller: User, filter: Optional[Filter]) -> None:
+        pass
+
+    @abc.abstractmethod
+    async def aggregate(
+        self, caller: User, filter: Optional[Filter], aggregation: Aggregation, limit: Optional[int] = None
+    ) -> List[AggregateResult]:
+        pass
