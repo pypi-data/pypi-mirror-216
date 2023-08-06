2eaed2a818417045b35d9834928405a4b9be9284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:38 +0000

    Add package pypcapkit/1.0.2.post8/pypcapkit-1.0.2.post8-cp310-none-any.whl

---
+++ packages/pypcapkit/pypcapkit-1.0.2.post8-cp310-none-any.whl/pcapkit/vendor/reg/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=unused-import
+"""Protocol Type Registry Vendor Crawlers
+============================================
+
+.. module:: pcapkit.vendor.reg
+
+This module contains all vendor crawlers of protocol type registry
+implementations. Available enumerations include:
+
+.. list-table::
+
+   * - :class:`LINKTYPE <pcapkit.vendor.reg.linktype.LinkType>`
+     - Link-Layer Header Type Values [*]_
+   * - :class:`ETHERTYPE <pcapkit.vendor.reg.ethertype.EtherType>`
+     - Ethertype IEEE 802 Numbers [*]_
+   * - :class:`TRANSTYPE <pcapkit.vendor.reg.transtype.TransType>`
+     - Transport Layer Protocol Numbers [*]_
+
+.. [*] http://www.tcpdump.org/linktypes.html
+.. [*] https://www.iana.org/assignments/ieee-802-numbers/ieee-802-numbers.xhtml#ieee-802-numbers-1
+.. [*] https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml#protocol-numbers-1
+
+"""
+
+from pcapkit.vendor.reg.ethertype import EtherType
+from pcapkit.vendor.reg.linktype import LinkType
+from pcapkit.vendor.reg.transtype import TransType
+
+__all__ = ['EtherType', 'LinkType', 'TransType']
