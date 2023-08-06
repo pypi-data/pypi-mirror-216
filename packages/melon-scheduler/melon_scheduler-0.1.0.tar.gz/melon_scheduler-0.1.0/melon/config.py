49a6e69cf0d8debe07a0c2afeb9be447c4b2066a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:49 +0000

    Add package melon-scheduler/0.1.0/melon_scheduler-0.1.0.tar.gz

---
+++ packages/melon-scheduler/melon_scheduler-0.1.0.tar.gz/melon_scheduler-0.1.0/melon/config.py
@@ -0,0 +1,9 @@
+"""This submodule only does one thing: loading configuration from the right place."""
+import pathlib
+
+import tomllib
+
+CONFIG_FOLDER = pathlib.Path("~").expanduser().resolve() / ".config" / "melon"
+
+with open(CONFIG_FOLDER / "config.toml", "rb") as f:
+    CONFIG = tomllib.load(f)
