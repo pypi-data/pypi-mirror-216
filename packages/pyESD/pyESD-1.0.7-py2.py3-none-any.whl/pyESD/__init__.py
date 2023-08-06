c2ea1bd839c827b5e66716eaf7c4b7f9a4685a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:59 +0000

    Add package pyESD/1.0.7/pyESD-1.0.7-py2.py3-none-any.whl

---
+++ packages/pyESD/pyESD-1.0.7-py2.py3-none-any.whl/pyESD/__init__.py
@@ -0,0 +1,7 @@
+"""
+PyESD: More information abou the package that would be part of the metadata
+"""
+
+__version__ = "1.0.1"
+__author__ = "Daniel Boateng"
+__credits__ = " Earth System Dynamics Research Group of University of Tuebingen"
