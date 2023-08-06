62f0e8d990c6c6f46f29f67af214fb2d43aad6a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:58 +0000

    Add package notebook-prototyping/0.1.5/notebook_prototyping-0.1.5.tar.gz

---
+++ packages/notebook-prototyping/notebook_prototyping-0.1.5.tar.gz/notebook_prototyping-0.1.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "readme.md").read_text()
+
+setup(
+    name='notebook_prototyping',
+    version='0.1.5',
+    license='MIT',
+    author="Reitze Jansen",
+    author_email='rlh.jansen@outlook.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    # packages=find_packages('src'),
+    # package_dir={'': 'src'},
+    url='https://github.com/rlhjansen/nbprototyping',
+    keywords='jupyter notebook tooling',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=['jupyter', 'ipynbname', 'ast-comments']
+)
