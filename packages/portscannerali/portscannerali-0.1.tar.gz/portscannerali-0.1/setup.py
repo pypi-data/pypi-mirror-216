9d789b8033f3e2335f6fdab1391a2f115dde169d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:12 +0000

    Add package portscannerali/0.1/portscannerali-0.1.tar.gz

---
+++ packages/portscannerali/portscannerali-0.1.tar.gz/portscannerali-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='portscannerali',
+    version='0.1',
+    description='A simple module to find open ports on a URL or IP address',
+    author='Zahid Ali',
+    author_email='zahid.arsol@gmail.com',
+    packages=['portscannerali'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.11'
+    ],
+)
