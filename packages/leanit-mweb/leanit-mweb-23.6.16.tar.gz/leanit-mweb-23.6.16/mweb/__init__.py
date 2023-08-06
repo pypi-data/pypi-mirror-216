6e23b62d387a5913b9a2b0c15f85f87e44d96cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:57 +0000

    Add package leanit-mweb/23.6.16/leanit-mweb-23.6.16.tar.gz

---
+++ packages/leanit-mweb/leanit-mweb-23.6.16.tar.gz/leanit-mweb-23.6.16/mweb/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+import logging
+
+logger = logging.getLogger(__name__)
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    pass
+
+from leanit_mweb import config
