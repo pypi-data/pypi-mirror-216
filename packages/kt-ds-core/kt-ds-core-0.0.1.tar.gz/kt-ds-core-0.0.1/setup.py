2898b9fbf44df74cb4dc0a16f4a07272e6046bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:40 +0000

    Add package kt-ds-core/0.0.1/kt-ds-core-0.0.1.tar.gz

---
+++ packages/kt-ds-core/kt-ds-core-0.0.1.tar.gz/kt-ds-core-0.0.1/setup.py
@@ -0,0 +1,69 @@
+#
+#   Copyright 2020 The SpaceONE Authors.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+from setuptools import setup, find_packages
+
+with open('VERSION', 'r') as f:
+    VERSION = f.read().strip()
+    f.close()
+
+setup(
+    name='kt-ds-core',
+    version=VERSION,
+    description='kt-ds core library',
+    long_description='',
+    url='',
+    author='',
+    author_email='',
+    license='Apache License 2.0',
+    packages=find_packages(),
+    install_requires=[
+        # 'grpcio',
+        'protobuf==3.*',
+        'grpcio-reflection',
+        'google-api-core',
+        'grpcio-health-checking',
+        'asyncio',
+        'PyYAML',
+        'unittest-xml-reporting>=3.0.0',
+        'pycryptodome',
+        # 'pycryptodome==3.8.2',
+        'schedule',
+        'cachetools',
+        'jwcrypto',
+        'python-jose',
+        # 'jwcrypto==0.7',
+        # 'python-jose==3.1.0',
+        'factory-boy',
+        'jsonschema',
+        'python-dateutil',
+        'python-consul',
+        'scheduler-cron',
+        'click',
+        'uvicorn',
+        'fastapi',
+        'fastapi-utils',
+        'python-consul',
+        'dnspython',
+        'pydantic'
+    ],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'spaceone = spaceone.core.command:cli',
+        ]
+    },
+)
