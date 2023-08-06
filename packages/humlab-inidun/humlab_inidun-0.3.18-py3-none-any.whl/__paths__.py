7cfa20c96e6dcafdae96cf756d9895c6463573fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:02 +0000

    Add package humlab-inidun/0.3.18/humlab_inidun-0.3.18-py3-none-any.whl

---
+++ packages/humlab-inidun/humlab_inidun-0.3.18-py3-none-any.whl/__paths__.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+
+def find_root_folder(x: str) -> str:
+    return os.path.join(os.getcwd().split(x)[0], x)
+
+
+project_name: str = 'text_analytics'
+
+if os.environ.get("JUPYTER_IMAGE_SPEC", "") != "":
+    root_folder: str = f"/home/jovyan/work/{project_name}"
+    corpus_folder: str = "/data/inidun"
+else:
+    root_folder: str = find_root_folder(project_name)
+    corpus_folder: str = os.path.join(root_folder, "data")
+
+if root_folder not in sys.path:
+    sys.path.insert(0, root_folder)
+
+data_folder: str = os.path.join(root_folder, "data")
+resources_folder: str = os.path.join(root_folder, "resources")
