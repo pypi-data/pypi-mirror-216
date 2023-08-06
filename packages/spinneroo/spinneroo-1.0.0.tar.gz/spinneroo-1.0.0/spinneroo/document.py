2d3c30cd4c6efbd2de4e62976e197a7641b77676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:35 +0000

    Add package spinneroo/1.0.0/spinneroo-1.0.0.tar.gz

---
+++ packages/spinneroo/spinneroo-1.0.0.tar.gz/spinneroo-1.0.0/spinneroo/document.py
@@ -0,0 +1,14 @@
+# document.py
+
+# noinspection PyUnresolvedReferences
+from spinneroo.progress import *
+# noinspection PyUnresolvedReferences
+from spinneroo.elements import *
+
+from spinneroo.base import document
+
+class Documentation:
+    """"""
+# end Documentation
+
+document(Documentation)
