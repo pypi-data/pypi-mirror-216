b992583d48946948041306731628574166ad90dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:53 +0000

    Add package simple-pygame-gui/0.0.1/simple_pygame_gui-0.0.1-py3-none-any.whl

---
+++ packages/simple-pygame-gui/simple_pygame_gui-0.0.1-py3-none-any.whl/simple_pygame_gui/Switch.py
@@ -0,0 +1,65 @@
+import pygame
+from importlib import resources
+import io
+
+
+class Switch:
+    def __init__(self, x, y, width=70, height=35, state=True, name="", font="FreeSans.ttf", fontSize=25):
+        if width == 70 and height == 35:
+            self.useSprite = True
+            with resources.open_binary('simple_pygame_gui', 'Switch_on.png') as on:
+                self.on_Sprite = on.read()
+            self.on_Sprite = pygame.image.load(io.BytesIO(self.on_Sprite)).convert_alpha()
+            
+            with resources.open_binary('simple_pygame_gui', 'Switch_off.png') as off:
+                self.off_Sprite = off.read()
+            self.off_Sprite = pygame.image.load(io.BytesIO(self.off_Sprite)).convert_alpha()
+        else:
+            self.useSprite = False
+
+
+        self.state = state
+        self.name = name
+        self.x = x
+        self.y = y
+        self.width = width
+        self.font = pygame.font.SysFont(font, fontSize)
+        self.bgRect = pygame.Rect(x, y, width, height)
+        self.baseRect = pygame.Rect(x + 2, y + 2, width - 4, height - 4)
+        self.onRect = pygame.Rect(x + 4, y + 4, (width - 4) / 2 - 2, height - 8)
+        self.offRect = pygame.Rect(x + width / 2, y + 4, (width - 4) / 2 - 2, height - 8)
+
+
+    def __bool__(self):
+        return self.state
+
+
+    def draw(self, surface):
+        if not self.useSprite:
+            pygame.draw.rect(surface, (135, 135, 135), self.bgRect)
+            pygame.draw.rect(surface, (150, 150, 150), self.baseRect)
+            pygame.draw.rect(surface, (0, 139, 0), self.onRect)
+            pygame.draw.rect(surface, (205, 55, 0), self.offRect)
+
+            if not self.state:
+                pygame.draw.rect(surface, (170, 170, 170), self.onRect)
+            else:
+                pygame.draw.rect(surface, (170, 170, 170), self.offRect)
+
+        else:
+            if not self.state:
+                surface.blit(self.off_Sprite, self.bgRect)
+            else:
+                surface.blit(self.on_Sprite, self.bgRect)
+
+
+        TXT = self.font.render(self.name, True, (0, 0, 0))
+        w, h = TXT.get_size()
+        surface.blit(TXT, (self.x + (self.width - w) / 2, self.y - h - 2))
+
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.onRect.collidepoint(event.pos) or self.offRect.collidepoint(event.pos):
+                self.state = not self.state
+
---
+++ packages/simple-pygame-gui/simple_pygame_gui-0.0.1-py3-none-any.whl/simple_pygame_gui/TextBox.py
@@ -0,0 +1,65 @@
+import pygame
+
+pygame.init()
+
+SELECTED = pygame.color.Color(220, 255, 255)
+TEXT_COLOR = pygame.color.Color(70, 70, 70)
+FONT = pygame.font.SysFont("FreeSansBold.ttf", size = 30)
+
+class TextBox:
+    def __init__(self, x, y, width = 25, height = 25, standardValue = "", name = "", nameColor = (50, 50, 50)):
+        if width < 25 or height < 25:
+            raise ValueError("minumum dimensions must be 25")
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.value = str(standardValue)
+        self.bgRect = pygame.Rect(self.x - 2, self.y - 2, self.width + 4, self.height + 4)
+        self.fgRect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.txt = FONT.render(str(standardValue), True, TEXT_COLOR)
+        self.rectColor = (255, 255, 255)
+        self.name = name
+        self.nameColor = nameColor
+        self.selected = False
+        self.savedValue = standardValue
+        self.st_val = standardValue
+
+
+    def getValue(self):
+        return self.value
+
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, (50, 50, 50), self.bgRect)
+        pygame.draw.rect(surface, self.rectColor, self.fgRect)
+
+        NAME = FONT.render(self.name, True, self.nameColor)
+        TEXT = FONT.render(str(self.value), True, TEXT_COLOR)
+        width, height = NAME.get_size()
+
+        surface.blit(NAME, (self.x - ((width - self.width) / 2), self.y - 25))
+        surface.blit(TEXT, (self.x + 2, self.y + self.height / 2 - 8))
+
+
+    def handleEvent(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.fgRect.collidepoint(event.pos):
+                self.__selected = not self.__selected
+            else:
+                self.__selected = False
+
+            self.rectColor = SELECTED if self.selected else (255, 255, 255)
+
+        if event.type == pygame.KEYDOWN:
+            if self.selected:
+                if event.key == pygame.K_RETURN:
+                    self.savedValue = self.value
+                    # self.value = stValue
+                if event.key == pygame.K_BACKSPACE:
+                    self.value = self.value[:-1]
+                elif self.txt.get_width() < self.width - 10:
+                    self.value += event.unicode
+
+                # Rendering the text
+                self.__txt = FONT.render(self.value, True, TEXT_COLOR)
