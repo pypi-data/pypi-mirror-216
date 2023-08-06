85770e5ed73fdca3cb9059e48c277a4b393b9d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:47 +0000

    Add package hspylib-clitt/0.9.31/hspylib_clitt-0.9.31-py3-none-any.whl

---
+++ packages/hspylib-clitt/hspylib_clitt-0.9.31-py3-none-any.whl/clitt/core/tui/minput/form_field.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   @project: HsPyLib
+   @package: clitt.core.tui.minput
+      @file: form_field.py
+   @created: Thu, 20 May 2021
+    @author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior"
+      @site: https://github.com/yorevs/hspylib
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright 2023, HsPyLib team
+"""
+
+from clitt.core.icons.font_awesome.form_icons import FormIcons
+from clitt.core.tui.minput.access_type import AccessType
+from clitt.core.tui.minput.input_type import InputType
+from clitt.core.tui.minput.input_validator import InputValidator
+from typing import Any
+
+
+class FormField:
+    """Represent a MenuInput form field."""
+
+    def __init__(
+        self,
+        label: str = None,
+        dest: str = None,
+        itype: InputType = InputType.TEXT,
+        min_length: int = 0,
+        max_length: int = 30,
+        access_type: AccessType = AccessType.READ_WRITE,
+        value: Any = "",
+        input_validator: InputValidator = None,
+    ):
+        self.label = label
+        self.dest = dest
+        self.itype = itype
+        self.min_length = min_length
+        self.max_length = max_length
+        self.access_type = access_type
+        self.value = value
+        self.input_validator = input_validator or InputValidator.anything(min_length, max_length)
+
+    def __str__(self) -> str:
+        return f"{self.label}: {self.itype}({self.min_length}-{self.max_length}) [{self.access_type}] = '{self.value}'"
+
+    def __repr__(self):
+        return str(self)
+
+    @property
+    def width(self) -> int:
+        return len(str(self.value)) if self.itype != InputType.SELECT else 1
+
+    @property
+    def icon(self) -> FormIcons:
+        if self.access_type == AccessType.READ_ONLY:
+            return FormIcons.LOCKED
+        match self.itype:
+            case InputType.PASSWORD:
+                icon = FormIcons.HIDDEN
+            case InputType.CHECKBOX:
+                icon = FormIcons.MARKED
+            case InputType.SELECT:
+                icon = FormIcons.SELECTABLE
+            case InputType.MASKED:
+                icon = FormIcons.MASKED
+            case InputType.TEXT:
+                if self.input_validator.pattern_type == InputValidator.PatternType.NUMBERS:
+                    icon = FormIcons.NUMBERS
+                elif self.input_validator.pattern_type == InputValidator.PatternType.LETTERS:
+                    icon = FormIcons.LETTERS
+                elif self.input_validator.pattern_type == InputValidator.PatternType.WORDS:
+                    icon = FormIcons.EDITABLE
+                else:
+                    icon = FormIcons.EDITABLE
+            case _:
+                icon = FormIcons.QUESTION_CIRCLE
+        return icon
+
+    def can_write(self) -> bool:
+        """Whether this field value can be set or not."""
+        return self.access_type == AccessType.READ_WRITE
+
+    def assign(self, value: Any, skip_validation: bool = False) -> bool:
+        """Assign a value for this field.Must match the input validator, otherwise an exception will be thrown."""
+        if skip_validation or self.validate_input(value):
+            self.value = value
+            return True
+        return False
+
+    def validate_input(self, value: Any = None) -> bool:
+        """Validate the input using the assigned validator."""
+        return self.input_validator.validate(str(value) or str(self.value)) if self.input_validator else False
