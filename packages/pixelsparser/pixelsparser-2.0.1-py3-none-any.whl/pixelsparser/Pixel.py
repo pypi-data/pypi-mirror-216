f515ebb2c9ea3c283aa2b73bde4a7766e4a4dbc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:30 +0000

    Add package pixelsparser/2.0.1/pixelsparser-2.0.1-py3-none-any.whl

---
+++ packages/pixelsparser/pixelsparser-2.0.1-py3-none-any.whl/pixelsparser/Pixel.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+
+class Pixel:
+    """ Pixel class
+
+    Keyword arguments:
+    date -- date of the pixel
+    type -- type of the pixel
+    scores -- list of scores
+    notes -- notes of the pixel
+    tags -- list of tag categories
+
+    Return: Pixel object
+    """
+
+    def __init__(self, date: str, type: str, scores: list, notes: str,
+                 tags: list):
+        self.date = datetime.strptime(date, "%Y-%m-%d")
+        self.pixelType = type
+        self.notes = notes
+        self.tags = tags
+
+        # Multiple scores not yet implemented in the app
+        self.scores = scores
+        self.score = scores[0]
+        self.mood = scores[0]
+
+    def __str__(self):
+        return f"Pixel(date={self.date}, type={self.pixelType}," \
+                f"score={self.score}, mood={self.mood}, notes={self.notes}," \
+                f"tags={self.tags})"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
