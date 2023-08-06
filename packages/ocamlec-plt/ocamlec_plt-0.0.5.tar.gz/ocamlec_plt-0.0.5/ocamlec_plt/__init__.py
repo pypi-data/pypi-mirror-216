393bbecc0fb39b3404efd842a980f99c59fb913d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:39 +0000

    Add package ocamlec-plt/0.0.5/ocamlec_plt-0.0.5.tar.gz

---
+++ packages/ocamlec-plt/ocamlec_plt-0.0.5.tar.gz/ocamlec_plt-0.0.5/ocamlec_plt/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 27 10:30:16 2023
+
+@author: mariohevia
+"""
+
+from utils import setGeccoRules
+from utils import getPaths
+from utils import MergePDFs
+from utils import ocamlFileToPandas
+from utils import filterData
