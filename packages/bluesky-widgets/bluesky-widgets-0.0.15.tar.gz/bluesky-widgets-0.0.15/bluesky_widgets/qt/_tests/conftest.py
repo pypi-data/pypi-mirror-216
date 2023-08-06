e4417da1eec533478cd2991c80c6aebdd2776a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:09 +0000

    Add package bluesky-widgets/0.0.15/bluesky-widgets-0.0.15.tar.gz

---
+++ packages/bluesky-widgets/bluesky-widgets-0.0.15.tar.gz/bluesky-widgets-0.0.15/bluesky_widgets/qt/_tests/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+
+from qtpy.QtWidgets import QApplication
+
+from bluesky_kafka.tests.conftest import (  # noqa
+    hw,
+    pytest_addoption,
+    kafka_bootstrap_servers,
+    broker_authorization_config,
+    publisher_factory,
+    temporary_topics,
+)
+
+
+@pytest.fixture
+def qtbot(qtbot):
+    """A modified qtbot fixture that makes sure no widgets have been leaked."""
+    # Adapted from napari
+    initial = QApplication.topLevelWidgets()
+    yield qtbot
+    QApplication.processEvents()
+    leaks = set(QApplication.topLevelWidgets()).difference(initial)
+    if leaks:
+        # Ignore this until we resolve
+        # AssertionError: Widgets leaked!:
+        # {matplotlib.backends.backend_qt5.MainWindow object at 0x7fdce80b7550>}
+        # raise AssertionError(f"Widgets leaked!: {leaks}")
+        pass
