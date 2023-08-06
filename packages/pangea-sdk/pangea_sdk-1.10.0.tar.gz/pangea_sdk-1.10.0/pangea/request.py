f327675797b532cecc741ca1ac4784d6d1f6a3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:19 +0000

    Add package pangea-sdk/1.10.0/pangea_sdk-1.10.0.tar.gz

---
+++ packages/pangea-sdk/pangea_sdk-1.10.0.tar.gz/pangea_sdk-1.10.0/pangea/request.py
@@ -0,0 +1,263 @@
+# Copyright 2022 Pangea Cyber Corporation
+# Author: Pangea Cyber Corporation
+
+import copy
+import json
+import logging
+import time
+from typing import Dict
+
+import pangea
+import requests
+from pangea import exceptions
+from pangea.config import PangeaConfig
+from pangea.response import PangeaResponse, ResponseStatus
+from pangea.utils import default_encoder
+from requests.adapters import HTTPAdapter, Retry
+
+
+class PangeaRequest(object):
+    """An object that makes direct calls to Pangea Service APIs.
+
+    Wraps Get/Post calls to support both API requests. If `queued_retry_enabled`
+    is enabled, the progress of long running Post requests will queried until
+    completion or until the `queued_retries` limit is reached. Both values can
+    be set in PangeaConfig.
+    """
+
+    def __init__(
+        self, config: PangeaConfig, token: str, service: str, logger: logging.Logger, check_config_id: bool = False
+    ):
+        self.config = copy.deepcopy(config)
+        self.token = token
+        self.service = service
+
+        # Queued request retry support flag
+        self._queued_retry_enabled = config.queued_retry_enabled
+
+        # Custom headers
+        self._extra_headers = {}
+        self._user_agent = ""
+        self.set_custom_user_agent(config.custom_user_agent)
+        self.session: requests.Session = self._init_session()
+        self._check_config_id = check_config_id
+
+        self.logger = logger
+
+    def __del__(self):
+        self.session.close()
+
+    def set_extra_headers(self, headers: dict):
+        """Sets any additional headers in the request.
+
+        Args:
+            headers (dict): key-value pair containing extra headers to et
+
+        Example:
+            set_extra_headers({ "My-Header" : "foobar" })
+        """
+
+        if isinstance(headers, dict):
+            self._extra_headers = headers
+
+    def set_custom_user_agent(self, user_agent: str):
+        self.config.custom_user_agent = user_agent
+        self._user_agent = f"pangea-python/{pangea.__version__}"
+        if self.config.custom_user_agent:
+            self._user_agent += f" {self.config.custom_user_agent}"
+
+    def queued_support(self, value: bool):
+        """Sets or returns the queued retry support mode.
+
+        Args:
+            value (bool): true - enable queued request retry mode, false - to disable
+        """
+        self._queued_retry_enabled = value
+
+        return self._queued_retry_enabled
+
+    def post(self, endpoint: str = "", data: Dict = {}) -> PangeaResponse:
+        """Makes the POST call to a Pangea Service endpoint.
+
+        If queued_support mode is enabled, progress checks will be made for
+        queued requests until processing is completed or until exponential
+        backoff `queued_retries` have been reached.
+
+        Args:
+            endpoint(str): The Pangea Service API endpoint.
+            data(dict): The POST body payload object
+
+        Returns:
+            PangeaResponse which contains the response in its entirety and
+               various properties to retrieve individual fields
+        """
+        url = self._url(endpoint)
+        # Set config ID if available
+        if self._check_config_id and self.config.config_id and data.pop("config_id", None) is None:
+            data["config_id"] = self.config.config_id
+
+        data_send = json.dumps(data, default=default_encoder) if isinstance(data, dict) else data
+        self.logger.debug(
+            json.dumps({"service": self.service, "action": "post", "url": url, "data": data}, default=default_encoder)
+        )
+
+        requests_response = self.session.post(url, headers=self._headers(), data=data_send)
+
+        if self._queued_retry_enabled and requests_response.status_code == 202:
+            response_json = requests_response.json()
+            self.logger.debug(
+                json.dumps(
+                    {"service": self.service, "action": "post", "url": url, "response": response_json},
+                    default=default_encoder,
+                )
+            )
+            request_id = response_json.get("request_id", None)
+
+            if not request_id:
+                raise Exception("Queue error: response did not include a 'request_id'")
+
+            pangea_response = self._handle_queued(request_id)
+        else:
+            pangea_response = PangeaResponse(requests_response)
+
+        self.logger.debug(
+            json.dumps(
+                {"service": self.service, "action": "post", "url": url, "response": pangea_response.json},
+                default=default_encoder,
+            )
+        )
+        self._check_response(pangea_response)
+        return pangea_response
+
+    def get(self, endpoint: str, path: str) -> PangeaResponse:
+        """Makes the GET call to a Pangea Service endpoint.
+
+        Args:
+            endpoint(str): The Pangea Service API endpoint.
+            path(str): Additional URL path
+
+        Returns:
+            PangeaResponse which contains the response in its entirety and
+               various properties to retrieve individual fields
+        """
+        url = self._url(f"{endpoint}/{path}")
+
+        self.logger.debug(json.dupms({"service": self.service, "action": "get", "url": url}))
+        requests_response = self.session.get(url, headers=self._headers())
+
+        pangea_response = PangeaResponse(requests_response)
+
+        self.logger.debug(
+            json.dumps(
+                {"service": self.service, "action": "post", "url": url, "response": pangea_response.json},
+                default=default_encoder,
+            )
+        )
+        self._check_response(pangea_response)
+        return pangea_response
+
+    def _handle_queued(self, request_id: str) -> PangeaResponse:
+        retry_count = 1
+
+        while True:
+            time.sleep(retry_count * retry_count)
+            pangea_response = self.get("request", request_id)
+
+            if pangea_response.code == 202 and retry_count <= self.config.queued_retries:
+                retry_count += 1
+            else:
+                return pangea_response
+
+    def _init_session(self) -> requests.Session:
+        retry_config = Retry(
+            total=self.config.request_retries,
+            backoff_factor=self.config.request_backoff,
+            status_forcelist=[500, 502, 503, 504],
+        )
+
+        adapter = HTTPAdapter(max_retries=retry_config)
+        session = requests.Session()
+
+        if self.config.insecure:
+            session.mount("http://", adapter)
+        else:
+            session.mount("https://", adapter)
+
+        return session
+
+    def _url(self, path: str) -> str:
+        if self.config.domain.startswith("http://") or self.config.domain.startswith("https://"):
+            # it's URL
+            url = f"{self.config.domain}/{path}"
+        else:
+            schema = "http://" if self.config.insecure else "https://"
+            domain = (
+                self.config.domain if self.config.environment == "local" else f"{self.service}.{self.config.domain}"
+            )
+            url = f"{schema}{domain}/{path}"
+        return url
+
+    def _headers(self) -> dict:
+        headers = {
+            "Content-Type": "application/json",
+            "User-Agent": self._user_agent,
+            "Authorization": f"Bearer {self.token}",
+        }
+
+        # We want to ignore previous headers if user tryed to set them, so we will overwrite them.
+        self._extra_headers.update(headers)
+        return self._extra_headers
+
+    def _check_response(self, response: PangeaResponse):
+        status = response.status
+        summary = response.summary
+
+        if status == ResponseStatus.SUCCESS.value:
+            return
+        else:
+            response.result = None
+
+        self.logger.error(
+            json.dumps(
+                {
+                    "service": self.service,
+                    "action": "api_error",
+                    "url": response.raw_response.url,
+                    "summary": summary,
+                    "request_id": response.request_id,
+                    "result": response.raw_result,
+                }
+            )
+        )
+
+        if status == ResponseStatus.VALIDATION_ERR.value:
+            raise exceptions.ValidationException(summary, response)
+        elif status == ResponseStatus.TOO_MANY_REQUESTS.value:
+            raise exceptions.RateLimitException(summary, response)
+        elif status == ResponseStatus.NO_CREDIT.value:
+            raise exceptions.NoCreditException(summary, response)
+        elif status == ResponseStatus.UNAUTHORIZED.value:
+            raise exceptions.UnauthorizedException(self.service, response)
+        elif status == ResponseStatus.SERVICE_NOT_ENABLED.value:
+            raise exceptions.ServiceNotEnabledException(self.service, response)
+        elif status == ResponseStatus.PROVIDER_ERR.value:
+            raise exceptions.ProviderErrorException(summary, response)
+        elif status in (ResponseStatus.MISSING_CONFIG_ID_SCOPE.value, ResponseStatus.MISSING_CONFIG_ID.value):
+            raise exceptions.MissingConfigID(self.service, response)
+        elif status == ResponseStatus.SERVICE_NOT_AVAILABLE.value:
+            raise exceptions.ServiceNotAvailableException(summary, response)
+        elif status == ResponseStatus.TREE_NOT_FOUND.value:
+            raise exceptions.TreeNotFoundException(summary, response)
+        elif status == ResponseStatus.IP_NOT_FOUND.value:
+            raise exceptions.IPNotFoundException(summary)
+        elif status == ResponseStatus.BAD_OFFSET.value:
+            raise exceptions.BadOffsetException(summary, response)
+        elif status == ResponseStatus.FORBIDDEN_VAULT_OPERATION.value:
+            raise exceptions.ForbiddenVaultOperation(summary, response)
+        elif status == ResponseStatus.VAULT_ITEM_NOT_FOUND.value:
+            raise exceptions.VaultItemNotFound(summary, response)
+        elif status == ResponseStatus.NOT_FOUND.value:
+            raise exceptions.NotFound(response.raw_response.url if response.raw_response is not None else "", response)
+        elif status == ResponseStatus.INTERNAL_SERVER_ERROR.value:
+            raise exceptions.InternalServerError(response)
+        raise exceptions.PangeaAPIException(f"{summary} ", response)
---
+++ packages/pangea-sdk/pangea_sdk-1.10.0.tar.gz/pangea_sdk-1.10.0/pangea/services/audit/models.py
@@ -0,0 +1,377 @@
+# Copyright 2022 Pangea Cyber Corporation
+# Author: Pangea Cyber Corporation
+import copy
+import datetime
+import enum
+import json
+from typing import List, Optional, Union
+
+from pangea.response import APIRequestModel, APIResponseModel, PangeaResponseResult
+
+
+class EventVerification(str, enum.Enum):
+    NONE = "none"
+    PASS = "pass"
+    FAIL = "fail"
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self.value)
+
+
+class EventSigning(enum.Enum):
+    NONE = 0
+    LOCAL = 1
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self.value)
+
+
+class Event(APIResponseModel):
+    """Event to perform an auditable activity
+
+    Arguments:
+    message -- A message describing a detailed account of what happened.
+    actor -- who performed the auditable activity.
+    action -- auditable action that occurred.
+    new -- The value of a record after it was changed.
+    old -- The value of a record before it was changed.
+    source -- Used to record the location from where an activity occurred.
+    status -- Record whether or not the activity was successful.
+    target -- Used to record the specific record that was targeted by the auditable activity.
+    timestamp -- An optional client-supplied timestamp.
+    """
+
+    message: Union[str, dict]
+    actor: Optional[str] = None
+    action: Optional[str] = None
+    new: Optional[Union[str, dict]] = None
+    old: Optional[Union[str, dict]] = None
+    source: Optional[str] = None
+    status: Optional[str] = None
+    target: Optional[str] = None
+    timestamp: Optional[datetime.datetime] = None
+    tenant_id: Optional[str] = None
+
+    _JSON_SUPPORTED_FIELDS = ["message", "new", "old"]
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.parse_json_fields()
+
+    def parse_json_fields(self):
+        """Parse JSON supported fields from string to dict"""
+        for f in self._JSON_SUPPORTED_FIELDS:
+            v = getattr(self, f)
+            if type(v) is str:
+                try:
+                    obj = json.loads(v)
+                    setattr(self, f, obj)
+                except:
+                    pass
+
+    def get_stringified_copy(self):
+        """Return object copy with JSON supported fields in string format"""
+        aux = copy.deepcopy(self)
+        for f in self._JSON_SUPPORTED_FIELDS:
+            v = getattr(aux, f, None)
+            if v is not None and type(v) is dict:
+                setattr(aux, f, self._dict_to_canonicalized_str(v))
+
+        if isinstance(aux.timestamp, (datetime.datetime, datetime.date)):
+            aux.timestamp = aux.timestamp.isoformat().replace("+00:00", "Z")
+
+        return aux
+
+    def _dict_to_canonicalized_str(self, message: dict) -> str:
+        """Convert dict to canonical str"""
+        return json.dumps(message, ensure_ascii=False, allow_nan=False, separators=(",", ":"))
+
+
+class EventEnvelope(APIResponseModel):
+    """
+    Contain extra information about an event.
+
+    Arguments:
+    event -- Event describing auditable activity.
+    signature -- An optional client-side signature for forgery protection.
+    public_key -- The base64-encoded ed25519 public key used for the signature, if one is provided
+    received_at -- A server-supplied timestamp
+    """
+
+    event: Event
+    signature: Optional[str] = None
+    public_key: Optional[str] = None
+    received_at: datetime.datetime
+
+
+class LogRequest(APIRequestModel):
+    """
+    Input class to perform a log action
+
+    Arguments:
+    event -- A structured event describing an auditable activity.
+    verbose -- If true, be verbose in the response; include membership proof, unpublished root and consistency proof, etc.
+    signature -- An optional client-side signature for forgery protection.
+    public_key -- The base64-encoded ed25519 public key used for the signature, if one is provided.
+    prev_root -- Unpublished root hash that was returned from the last log API call that was made. If the user does not provide prev_root, the consistency proof from the last known unpublished root will be provided.
+    """
+
+    event: Event
+    verbose: Optional[bool] = None
+    signature: Optional[str] = None
+    public_key: Optional[str] = None
+    prev_root: Optional[str] = None
+
+
+class LogResult(PangeaResponseResult):
+    """
+    Result class after an audit log action
+
+    envelope -- Event envelope information.
+    hash -- Event envelope hash.
+    unpublished_root -- The current unpublished root.
+    membership_proof -- A proof for verifying the unpublished root.
+    consistency_proof -- If prev_root was present in the request, this proof verifies that the new unpublished root is a continuation of the prev_root
+    """
+
+    envelope: Optional[EventEnvelope] = None
+    hash: str
+    unpublished_root: Optional[str] = None
+    membership_proof: Optional[str] = None
+    consistency_proof: Optional[List[str]] = None
+    consistency_verification: EventVerification = EventVerification.NONE
+    membership_verification: EventVerification = EventVerification.NONE
+    signature_verification: EventVerification = EventVerification.NONE
+
+
+class SearchRestriction(APIResponseModel):
+    """
+    Set of restrictions when perform an audit search action
+
+    Arguments:
+    actor -- List of actors to include in search. If empty include all.
+    action -- List of action to include in search. If empty include all.
+    source -- List of sourcers to include in search. If empty include all.
+    status -- List of status to include in search. If empty include all.
+    target -- List of targets to include in search. If empty include all.
+    """
+
+    actor: List[str] = []
+    action: List[str] = []
+    source: List[str] = []
+    status: List[str] = []
+    target: List[str] = []
+
+
+class SearchOrder(str, enum.Enum):
+    ASC = "asc"
+    DESC = "desc"
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self.value)
+
+
+class SearchOrderBy(str, enum.Enum):
+    ACTOR = "actor"
+    ACTION = "action"
+    MESSAGE = "message"
+    RECEIVED_AT = "received_at"
+    SOURCE = "source"
+    STATUS = "status"
+    TARGET = "target"
+    TIMESTAMP = "timestamp"
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self.value)
+
+
+class SearchRequest(APIRequestModel):
+    """
+    Input class to perform an audit search action
+
+    Arguments:
+        #: Query is a required field.
+        #: The following optional qualifiers are supported:
+        #:	* action:
+        #:	* actor:
+        #:	* message:
+        #:	* new:
+        #:	* old:
+        #:	* status:
+        #:	* target:
+    query -- Natural search string; list of keywords with optional `<option>:<value>` qualifiers.
+    order -- Specify the sort order of the response. "asc" or "desc".
+    order_by -- Name of column to sort the results by. "message", "actor", "status", etc.
+    start -- The start of the time range to perform the search on.
+    end -- The end of the time range to perform the search on. All records up to the latest if left out.
+    limit -- Number of audit records to include from the first page of the results.
+    max_results -- Maximum number of results to return.
+    search_restriction -- A list of keys to restrict the search results to. Useful for partitioning data available to the query string.
+    verbose -- If true, include root, membership and consistency proofs in response.
+    """
+
+    query: str
+    order: Optional[SearchOrder] = None
+    order_by: Optional[SearchOrderBy] = None
+    last: Optional[str] = None
+    start: Optional[str] = None
+    end: Optional[str] = None
+    limit: Optional[int] = None
+    max_results: Optional[int] = None
+    search_restriction: Optional[dict] = None
+    verbose: Optional[bool] = None
+
+
+class RootRequest(APIRequestModel):
+    """
+    Input class to perform a root request action
+
+    Arguments:
+
+    tree_size -- The size of the tree (the number of records).
+    """
+
+    tree_size: Optional[int] = None
+
+
+class Root(APIResponseModel):
+    """
+    Tree root information
+
+    Arguments:
+    tree_name -- The name of the Merkle Tree.
+    size -- The size of the tree (the number of records).
+    root_hash -- the root hash.
+    url -- the URL where this root has been published.
+    published_at -- The date/time when this root was published.
+    consistency_proof -- Consistency proof to verify that this root is a continuation of the previous one.
+    """
+
+    tree_name: str
+    size: int
+    root_hash: str
+    url: Optional[str] = None
+    published_at: Optional[str] = None
+    consistency_proof: Optional[List[str]] = None
+
+
+class RootSource(str, enum.Enum):
+    UNKNOWN = "unknown"
+    ARWEAVE = "arweave"
+    PANGEA = "pangea"
+
+
+class PublishedRoot(APIResponseModel):
+    """
+    Published root information
+
+    Arguments:
+    size -- The size of the tree (the number of records).
+    root_hash -- the root hash.
+    published_at -- The date/time when this root was published.
+    consistency_proof -- Consistency proof to verify that this root is a continuation of the previous one.
+    source -- This should be "pangea" or "arweave"
+    """
+
+    size: int
+    root_hash: str
+    published_at: str
+    consistency_proof: Optional[List[str]] = None
+    source: RootSource = RootSource.UNKNOWN
+
+
+class RootResult(PangeaResponseResult):
+    """
+    Result class after a root request
+    """
+
+    data: Root
+
+
+class SearchEvent(APIResponseModel):
+    """
+    Event information received after a search request
+
+    Arguments:
+    envelope -- Event related information.
+    hash -- The record's hash.
+    leaf_index -- The index of the leaf of the Merkle Tree where this record was inserted.
+    membership_proof -- A cryptographic proof that the record has been persisted in the log.
+    consistency_verification -- Consistency verification calculated if required.
+    membership_verification -- Membership verification calculated if required.
+    signature_verification -- Signature verification calculated if required.
+    """
+
+    envelope: EventEnvelope
+    hash: str
+    membership_proof: Optional[str] = None
+    published: Optional[bool] = None
+    leaf_index: Optional[int] = None
+    consistency_verification: EventVerification = EventVerification.NONE
+    membership_verification: EventVerification = EventVerification.NONE
+    signature_verification: EventVerification = EventVerification.NONE
+
+
+class SearchOutput(PangeaResponseResult):
+    """
+    Result class after an audit search action
+
+    Arguments:
+    id -- Identifier to supply to search_results API to fetch/paginate through search results. ID is always populated on a successful response.
+    expires_at -- The time when the results will no longer be available to page through via the results API.
+    count -- The total number of results that were returned by the search.
+    events -- A list of matching audit records.
+    root -- Root of a published Merkle Tree.
+    unpublished_root -- Root of a unpublished Merkle Tree
+    """
+
+    count: int
+    events: List[SearchEvent]
+    id: str
+    expires_at: datetime.datetime
+    root: Optional[Root] = None
+    unpublished_root: Optional[Root] = None
+
+
+class SearchResultRequest(APIRequestModel):
+    """
+    Class used to paginate search results
+
+    Arguments:
+    id -- A search results identifier returned by the search call.
+    limit -- Number of audit records to include from the first page of the results.
+    offset -- Offset from the start of the result set to start returning results from.
+    """
+
+    id: str
+    limit: Optional[int] = 20
+    offset: Optional[int] = 0
+
+
+class SearchResultOutput(PangeaResponseResult):
+    """
+    Return class after a pagination search
+
+    Arguments:
+    count -- The total number of results that were returned by the search.
+    events -- A list of matching audit records.
+    root -- A root of a Merkle Tree.
+    unpublished_root -- Root of a unpublished Merkle Tree
+    """
+
+    count: int
+    events: List[SearchEvent]
+    root: Optional[Root] = None
+    unpublished_root: Optional[Root] = None
