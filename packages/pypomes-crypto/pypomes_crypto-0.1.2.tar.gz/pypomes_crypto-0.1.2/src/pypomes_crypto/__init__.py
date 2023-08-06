766f0211467f62828ffc98a13f695700aec4d8f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:48 +0000

    Add package pypomes-crypto/0.1.2/pypomes_crypto-0.1.2.tar.gz

---
+++ packages/pypomes-crypto/pypomes_crypto-0.1.2.tar.gz/pypomes_crypto-0.1.2/src/pypomes_crypto/__init__.py
@@ -0,0 +1,11 @@
+from .crypto_pomes import (
+    CRYPTO_HASH_ALGORITHM, crypto_hash
+)
+
+__all__ = [
+    # crypto_pomes
+    CRYPTO_HASH_ALGORITHM, crypto_hash
+]
+
+__version__ = "0.1.2"
+__version_info__ = (0, 1, 2)
