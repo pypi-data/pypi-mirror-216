5bc6342b6756125f5d365b0393627e035db25835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:17 +0000

    Add package wlkatapython/0.0.6/wlkatapython-0.0.6.tar.gz

---
+++ packages/wlkatapython/wlkatapython-0.0.6.tar.gz/wlkatapython-0.0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='wlkatapython', # 自定义包名
+    version='0.0.6', # 包的版本号
+    description='mirobot机械臂控制', # 描述信息
+    author='dong', # 作者
+    py_modules=[
+        'wlkatapython'
+    ] # 包中包含的模块
+)
