f27af5f037da0d94f407dd29a82fec71953a322b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:23 +0000

    Add package pyrocko/2023.6.29/pyrocko-2023.6.29-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyrocko/pyrocko-2023.6.29-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyrocko/info.py
@@ -0,0 +1,7 @@
+# This module is automatically created from setup.py
+git_sha1 = '34109fc2f3b99ae347e2af216a8657dc810839cc'
+local_modifications = False
+version = '2023.06.29'
+long_version = 'pyrocko-2023.06.29-34109fc2f3b99ae347e2af216a8657dc810839cc-2023-06-29_21:45:55'  # noqa
+installed_date = '2023-06-29_21:45:55'
+src_path = '/drone/src'
---
+++ packages/pyrocko/pyrocko-2023.6.29-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyrocko/kan.py
@@ -0,0 +1,12 @@
+
+import sys
+import pyrocko
+if pyrocko.grumpy == 1:
+    sys.stderr.write('using renamed pyrocko module: pyrocko.kan\n')
+    sys.stderr.write('           -> should now use: pyrocko.io.kan\n\n')
+elif pyrocko.grumpy == 2:
+    sys.stderr.write('pyrocko module has been renamed: pyrocko.kan\n')
+    sys.stderr.write('              -> should now use: pyrocko.io.kan\n\n')
+    raise ImportError('Pyrocko module "pyrocko.kan" has been renamed to "pyrocko.io.kan".')
+
+from pyrocko.io.kan import *
